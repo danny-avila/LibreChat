74232d767178af2f3b2310d8a0530bda6e495c8a
Daniel Avila <messagedaniel@protonmail.com>
2023-02-04 17:17:17 -0500

    initial commit, project is ready to start

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import App from './src/App';
+
+reactDom.render(<App />, document.getElementById('root'));
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <h1>Welcome to React App thats build using Webpack and Babel separately</h1>;
+};
+
+export default App;
