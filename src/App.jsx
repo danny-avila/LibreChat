21a6a0587c04167621750a5e812049a1d52a9199
Daniel Avila <messagedaniel@protonmail.com>
2023-02-04 19:19:53 -0500

    adds basic layout

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+
+  return (
+    <div className="flex h-screen">
+      <div className="w-80 bg-slate-800"></div>
+      <div className="flex h-full w-full flex-col bg-gray-50 ">
+        <div className="flex-1 overflow-y-auto"></div>
+        <textarea className="m-10 h-16 p-4" />
+      </div>
+    </div>
+  );
+};
+
+export default App;
