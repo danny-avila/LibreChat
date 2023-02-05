5dc2d5efba67245a50bc0e9c0ae3fd2aa42d865f
Daniel Avila <messagedaniel@protonmail.com>
2023-02-04 20:07:08 -0500

    updates to react 18 method createRoot

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import reactDom from 'react-dom';
+// import reactDom from 'react-dom'; ---> deprecated
+import { createRoot } from 'react-dom/client';
 import App from './src/App';
 import './src/style.css';
 
-reactDom.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// reactDom.render(<App />, document.getElementById('root'));
+root.render(<App />);
