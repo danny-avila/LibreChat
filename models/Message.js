521a8eb7041797d25ac1ba9c79b9e4eafe019ca0
Daniel Avila <messagedaniel@protonmail.com>
2023-02-04 20:08:46 -0500

    templating model

---
+++ models/Message.js
@@ -0,0 +1,11 @@
+/*
+{
+  role: 'assistant',
+  id: 'cmpl-6gNfeSy5RUJulgpX1vIlppGdyXjB3',
+  parentMessageId: 'e84f127b-201b-4a1d-8b6d-899e7edab5b4',
+  conversationId: '97f657cd-be00-490c-94b9-31dba2462a7c',
+  text: 'OpenAI is an AI research lab consisting of the for-profit technological company OpenAI LP and its parent company, the non-profit OpenAI Inc. It aims to promote and develop friendly AI in a way that benefits humanity as a whole.'
+}
+*/
+
+
