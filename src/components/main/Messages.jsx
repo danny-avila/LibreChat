92b2109dc3771b62fd58f10368c8c60d1800ebf4
Danny Avila <messagedaniel@protonmail.com>
2023-02-13 16:31:18 -0500

    adds scrolltobottom button, still handling initial state when scrollbar is present

--- src/components/main/Messages.jsx
+++ src/components/main/Messages.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import useDocumentTitle from '~/hooks/useDocumentTitle';
 import useDidMountEffect from '~/hooks/useDidMountEffect';
 import Message from './Message';
+import ScrollToBottom from './ScrollToBottom';
 import Landing from './Landing';
 
 export default function Messages({ title, messages }) {
@@ -11,23 +12,33 @@ export default function Messages({ title, messages }) {
   }
 
   useDocumentTitle(title);
-
   const messagesEndRef = useRef(null);
+  const [showScrollButton, setShowScrollButton] = useState(false);
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
-  // this useEffect triggers the following warning:
-  // Warning: Internal React error: Expected static flag was missing.
-  // useEffect(() => {
-  //   scrollToBottom();
-  // }, [messages]);
+  if (document.body.clientHeight > window.innerHeight) {
+    setShowScrollButton(true);
+  }
 
-  useDidMountEffect(() => scrollToBottom(), [messages]);
+  const handleScroll = (e) => {
+    const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+    if (bottom) {
+      setShowScrollButton(false);
+    } else {
+      setShowScrollButton(true);
+    }
+  }
+
+  const scrollHandler = (e) => {
+    e.preventDefault();
+    scrollToBottom();
+  };
 
   return (
-    <div className="flex-1 overflow-y-auto ">
+    <div className="flex-1 overflow-y-auto " onScroll={handleScroll}>
       {/* <div className="flex-1 overflow-hidden"> */}
         <div className="h-full dark:bg-gray-800">
           <div className="flex h-full flex-col items-center text-sm dark:bg-gray-800">
@@ -40,6 +51,7 @@ export default function Messages({ title, messages }) {
                 error={!!message.error ? true : false}
               />
             ))}
+            {showScrollButton && <ScrollToBottom scrollHandler={scrollHandler} />}
             <div
               className="group h-32 w-full flex-shrink-0 dark:border-gray-900/50 dark:bg-gray-800 md:h-48"
               ref={messagesEndRef}
