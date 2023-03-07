9a4616d21de8f2a9ad486f31a0bd7cc52ba0ee18
Danny Avila <messagedaniel@protonmail.com>
2023-03-07 14:22:33 -0500

    fix: customGpt persistence blindspot, also remove free gpt

--- api/models/index.js
+++ api/models/index.js
@@ -1,10 +1,11 @@
 const { saveMessage, deleteMessages } = require('./Message');
 const { getCustomGpts, updateCustomGpt, updateByLabel, deleteCustomGpts } = require('./CustomGpt');
-const { saveConvo } = require('./Conversation');
+const { getConvo, saveConvo } = require('./Conversation');
 
 module.exports = {
   saveMessage,
   deleteMessages,
+  getConvo,
   saveConvo,
   getCustomGpts,
   updateCustomGpt,
