f634b81ad1eb5b3d4e75631bf8f262021cda18f4
Daniel Avila <messagedaniel@protonmail.com>
2023-03-11 16:26:41 -0500

    feat: title the chat in user's language

--- api/app/titleConvo.js
+++ api/app/titleConvo.js
@@ -13,7 +13,7 @@ const titleConvo = async ({ message, response, model }) => {
         content:
           'You are a title-generator with one job: titling the conversation provided by a user in title case.'
       },
-      { role: 'user', content: `In 5 words or less, summarize the conversation below with a title in title case. Don't refer to the participants of the conversation by name. Do not include punctuation or quotation marks. Your response should be in title case, exclusively containing the title. Conversation:\n\nUser: "${message}"\n\n${model}: "${response}"\n\nTitle: ` },
+      { role: 'user', content: `In 5 words or less, summarize the conversation below with a title in title case. Don't refer to the participants of the conversation by name. Do not include punctuation or quotation marks. Your response should be in title case, exclusively containing the title. Title the conversation in the language the user writes in. Conversation:\n\nUser: "${message}"\n\n${model}: "${response}"\n\nTitle: ` },
     ]
   });
 
