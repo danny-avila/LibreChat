e7e3f28b42bfef27371c7283939b8e57cd4fab4a
Daniel Avila <messagedaniel@protonmail.com>
2023-03-11 10:28:46 -0500

    fix: unsupported lang handler in client

--- client/src/components/Messages/Highlight.jsx
+++ client/src/components/Messages/Highlight.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import hljs from 'highlight.js';
+import languages from '~/utils/languages';
 
 export default function Highlight({language, code}) {
   const [highlightedCode, setHighlightedCode] = useState(code);
+  const lang = languages.has(language) ? language : 'shell';
 
   useEffect(() => {
-    setHighlightedCode(hljs.highlight(code, { language }).value);
-  }, [code, language]);
+    setHighlightedCode(hljs.highlight(code, { language: lang }).value);
+  }, [code, lang]);
 
   return (
     <pre>
-      <code className={`language-${language}`} dangerouslySetInnerHTML={{__html: highlightedCode}}/>
+      <code className={`language-${lang}`} dangerouslySetInnerHTML={{__html: highlightedCode}}/>
     </pre>
   );
 }
