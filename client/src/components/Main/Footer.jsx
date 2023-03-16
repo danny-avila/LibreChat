5213d2ec4214034c9f1dfbbe5dff95e3bff002b9
Chrisagon <christophe.thomas@fesc.asso.fr>
2023-03-16 16:08:18 +0100

    Update Footer.jsx

--- client/src/components/Main/Footer.jsx
+++ client/src/components/Main/Footer.jsx
@@ -4,15 +4,15 @@ export default function Footer() {
   return (
     <div className="px-3 pt-2 pb-3 text-center text-xs text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
       <a
-        href="https://github.com/danny-avila/chatgpt-clone"
+        href="https://github.com/chrisagon/valeria/"
         target="_blank"
         rel="noreferrer"
         className="underline"
       >
-        ChatGPT Lab
+        Valeria
       </a>
-      . Servir et rechercher toutes vos conversations de manière fiable. Toutes vos conversations AI sous un même toit. Payez 
-par utilisation et non par mois (quelques centimes au lieu de €€ )..
+      . Servir et rechercher toutes nos conversations de manière fiable. Toutes nos conversations AI sous un même toit. Payez 
+par utilisation et non par mois (quelques centimes au lieu de €€€ )..
     </div>
   );
 }
