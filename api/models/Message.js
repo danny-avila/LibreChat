d585219279e1908f13d0f6ceb1ecb272a432f98e
Daniel Avila <messagedaniel@protonmail.com>
2023-03-20 17:02:37 -0400

    bing hotfix, latest api, uses sydney

--- api/models/Message.js
+++ api/models/Message.js
@@ -83,6 +83,23 @@ module.exports = {
       return { message: 'Error saving message' };
     }
   },
+  saveBingMessage: async ({ messageId, oldMessageId = messageId, conversationId, parentMessageId, sender, text, isCreatedByUser=false, error }) => {
+    try {
+      await Message.findOneAndUpdate({ messageId: oldMessageId }, {
+        messageId,
+        conversationId,
+        parentMessageId,
+        sender,
+        text,
+        isCreatedByUser,
+        error
+      }, { upsert: true, new: true });
+      return { messageId, conversationId, parentMessageId, sender, text, isCreatedByUser };
+    } catch (error) {
+      console.error(error);
+      return { message: 'Error saving message' };
+    }
+  },
   deleteMessagesSince: async ({ messageId, conversationId }) => {
     try {
       const message = await Message.findOne({ messageId }).exec()
