49e8ee201108046c2f376875eca9fd22687e3ae0
Danny Avila <messagedaniel@protonmail.com>
2023-03-22 16:31:57 -0400

    markdown styling changes in progress

--- api/lib/utils/misc.js
+++ api/lib/utils/misc.js
@@ -1,8 +1,15 @@
 const cleanUpPrimaryKeyValue = (value) => {
   // For Bing convoId handling
   return value.replace(/--/g, '-');
+};
+
+function replaceSup(text) {
+  if (!text.includes('<sup>')) return text;
+  const replacedText = text.replace(/<sup>/g, '^').replace(/\s+<\/sup>/g, '^');
+  return replacedText;
 }
 
 module.exports = {
   cleanUpPrimaryKeyValue,
-}
+  replaceSup
+};
