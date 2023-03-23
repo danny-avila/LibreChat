f9ead7b430fe6e871627ddd281c6177834a1d9a4
Danny Avila <messagedaniel@protonmail.com>
2023-03-23 15:37:25 -0400

    fix: auth env var must have no value, as well as assigned username incase a falsy value is set

--- api/server/routes/authYourLogin.js
+++ api/server/routes/authYourLogin.js
@@ -5,36 +5,40 @@ const router = express.Router();
 // THIS IS NOT A READY TO USE USER SYSTEM
 // PLEASE IMPLEMENT YOUR OWN USER SYSTEM
 
-const userSystemEnabled = process.env.ENABLE_USER_SYSTEM || false
+const userSystemEnabled = process.env.ENABLE_USER_SYSTEM || false;
 
 // Logout
 router.get('/logout', (req, res) => {
   // Do anything you want
-  console.warn('logout not implemented!')
+  console.warn('logout not implemented!');
 
   // finish
-  res.redirect('/')
+  res.redirect('/');
 });
-  
+
 // Login
 router.get('/', async (req, res) => {
   // Do anything you want
-  console.warn('login not implemented! Automatic passed as sample user')
+  console.warn('login not implemented! Automatic passed as sample user');
 
   // save the user info into session
   // username will be used in db
   // display will be used in UI
-  req.session.user = {
-    username: 'sample_user',
-    display: 'Sample User',
+  if (userSystemEnabled) {
+    req.session.user = {
+      username: null, // was 'sample_user', but would break previous relationship with previous conversations before v0.1.0
+      display: 'Sample User'
+    };
   }
 
   req.session.save(function (error) {
-    if (error) {    
-        console.log(error);
-        res.send(`<h1>Login Failed. An error occurred. Please see the server logs for details.</h1>`);
-    } else res.redirect('/')
-  })
+    if (error) {
+      console.log(error);
+      res.send(`<h1>Login Failed. An error occurred. Please see the server logs for details.</h1>`);
+    } else {
+      res.redirect('/');
+    }
+  });
 });
 
 module.exports = router;
