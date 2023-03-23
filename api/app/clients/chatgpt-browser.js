74f77b26cdf89dc33661d89bd5ba061f990a70dd
Daniel Avila <messagedaniel@protonmail.com>
2023-03-22 20:09:52 -0400

    chore: update env example and add browser client config

--- api/app/clients/chatgpt-browser.js
+++ api/app/clients/chatgpt-browser.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { KeyvFile } = require('keyv-file');
+const set = new Set(["gpt-4", "text-davinci-002-render", "text-davinci-002-render-paid", "text-davinci-002-render-sha"]);
 
 const clientOptions = {
   // Warning: This will expose your access token to a third party. Consider the risks before using this.
@@ -9,6 +10,12 @@ const clientOptions = {
   // debug: true
   proxy: process.env.PROXY || null,
 };
+
+// You can check which models you have access to by opening DevTools and going to the Network tab. 
+// Refresh the page and look at the response body for https://chat.openai.com/backend-api/models.
+if (set.has(process.env.BROWSER_MODEL)) {
+  clientOptions.model = process.env.BROWSER_MODEL;
+}
 
 const browserClient = async ({ text, onProgress, convo, abortController }) => {
   const { ChatGPTBrowserClient } = await import('@waylaidwanderer/chatgpt-api');
