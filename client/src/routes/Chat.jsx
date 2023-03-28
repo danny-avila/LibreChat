e6632700722dfebe896839a3b836c5543c3ef661
Wentao Lyu <35-wentao.lyu@users.noreply.git.stereye.tech>
2023-03-29 05:48:44 +0800

    fix: show message list.

--- client/src/routes/Chat.jsx
+++ client/src/routes/Chat.jsx
@@ -68,7 +68,7 @@ export default function Chat() {
 
   return (
     <>
-      {conversationId == 'new' ? <Landing /> : <Messages />}
+      {conversationId === 'new' && !!messagesTree?.length ? <Landing /> : <Messages />}
       <TextChat />
     </>
   );
