04a8a8c85eda1ddda411450fd3b5198dc83541c1
Wentao Lyu <35-wentao.lyu@users.noreply.git.stereye.tech>
2023-04-04 13:11:59 +0800

    refactor: code clean up

--- client/src/components/Endpoints/EndpointOptionsDialog.jsx
+++ client/src/components/Endpoints/EndpointOptionsDialog.jsx
@@ -1,20 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { useSetRecoilState, useRecoilValue } from 'recoil';
-import axios from 'axios';
 import exportFromJSON from 'export-from-json';
 import DialogTemplate from '../ui/DialogTemplate.jsx';
-import { Dialog, DialogClose, DialogButton } from '../ui/Dialog.tsx';
-import { Input } from '../ui/Input.tsx';
-import { Label } from '../ui/Label.tsx';
-import Dropdown from '../ui/Dropdown.jsx';
+import { Dialog, DialogButton } from '../ui/Dialog.tsx';
 import SaveAsPresetDialog from './SaveAsPresetDialog';
-import { cn } from '~/utils/';
 import cleanupPreset from '~/utils/cleanupPreset';
 
-import OpenAISettings from './OpenAI/Settings.jsx';
-import BingAISettings from './BingAI/Settings.jsx';
-
-import store from '~/store';
+import Settings from './Settings';
 
 // A preset dialog to show readonly preset values.
 const EndpointOptionsDialog = ({ open, onOpenChange, preset: _preset, title }) => {
@@ -30,44 +21,6 @@ const EndpointOptionsDialog = ({ open, onOpenChange, preset: _preset, title }) =
       ...prevState,
       ...update
     }));
-  };
-
-  const renderSettings = () => {
-    const { endpoint } = preset || {};
-
-    if (endpoint === 'openAI')
-      return (
-        <OpenAISettings
-          readonly={true}
-          model={preset?.model}
-          setModel={setOption('model')}
-          chatGptLabel={preset?.chatGptLabel}
-          setChatGptLabel={setOption('chatGptLabel')}
-          promptPrefix={preset?.promptPrefix}
-          setPromptPrefix={setOption('promptPrefix')}
-          temperature={preset?.temperature}
-          setTemperature={setOption('temperature')}
-          topP={preset?.top_p}
-          setTopP={setOption('top_p')}
-          freqP={preset?.presence_penalty}
-          setFreqP={setOption('presence_penalty')}
-          presP={preset?.frequency_penalty}
-          setPresP={setOption('frequency_penalty')}
-        />
-      );
-    else if (endpoint === 'bingAI')
-      return (
-        <BingAISettings
-          readonly={true}
-          context={preset?.context}
-          setContext={setOption('context')}
-          systemMessage={preset?.systemMessage}
-          setSystemMessage={setOption('systemMessage')}
-          jailbreak={preset?.jailbreak}
-          setJailbreak={setOption('jailbreak')}
-        />
-      );
-    else return null;
   };
 
   const saveAsPreset = () => {
@@ -93,11 +46,17 @@ const EndpointOptionsDialog = ({ open, onOpenChange, preset: _preset, title }) =
         onOpenChange={onOpenChange}
       >
         <DialogTemplate
-          title={title || 'View Options'}
+          title={`${title || 'View Options'} - ${preset?.endpoint}`}
           className="max-w-full sm:max-w-4xl"
           main={
             <div className="flex w-full flex-col items-center gap-2">
-              <div className="w-full p-0">{renderSettings()}</div>
+              <div className="w-full p-0">
+                <Settings
+                  preset={preset}
+                  readonly={true}
+                  setOption={setOption}
+                />
+              </div>
             </div>
           }
           buttons={
