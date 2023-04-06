86b62758a1ca39e26f211b4ba0c19a88ae5dc066
Daniel Avila <messagedaniel@protonmail.com>
2023-04-06 17:33:02 -0400

    ci(playwright.yml): add Playwright tests workflow fix(Landing.jsx): add id attribute to landing page title test(landing.spec.js): update landing page title and h1 text content assertions

--- e2e/specs/landing.spec.js
+++ e2e/specs/landing.spec.js
@@ -2,6 +2,6 @@ import {expect, test} from '@playwright/test';
 
 test('landing page', async ({page}) => {
   await page.goto('/');
-  expect (await page.title()).toBe('ChatGPT Clone');
-  expect (await page.textContent('h1')).toBe('ChatGPT Clone');
+  expect (await page.title()).toBe('New Chat');
+  expect (await page.textContent('#landing-title')).toBe('ChatGPT Clone');
 });
