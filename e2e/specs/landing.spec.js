c9c42b40162143776419bf796007df81706d9a9c
Daniel Avila <messagedaniel@protonmail.com>
2023-04-06 19:33:33 -0400

    chore(playwright.yml): move 'Start API server' job to after 'Install Playwright Browsers' job fix(landing.spec.js): change page.goto URL to 'http://localhost:3080/'

--- e2e/specs/landing.spec.js
+++ e2e/specs/landing.spec.js
@@ -1,7 +1,7 @@
 import {expect, test} from '@playwright/test';
 
 test('landing page', async ({page}) => {
-  await page.goto('/');
+  await page.goto('http://localhost:3080/');
   expect (await page.title()).toBe('New Chat');
   expect (await page.textContent('#landing-title')).toBe('ChatGPT Clone');
 });
