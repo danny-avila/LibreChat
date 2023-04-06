017447b064ff7b75e61cde723c1f215f2a2bd574
Daniel Avila <messagedaniel@protonmail.com>
2023-04-05 21:34:39 -0400

    chore(api): update chatgpt-api dependency to version 1.34.0 feat(api): use gen_title response for askChatGPTBrowser.js (official title)

--- api/server/routes/ask/askChatGPTBrowser.js
+++ api/server/routes/ask/askChatGPTBrowser.js
@@ -154,7 +154,8 @@ const ask = async ({
     res.end();
 
     if (userParentMessageId == '00000000-0000-0000-0000-000000000000') {
-      const title = await titleConvo({ endpoint: endpointOption?.endpoint, text, response: responseMessage });
+      // const title = await titleConvo({ endpoint: endpointOption?.endpoint, text, response: responseMessage });
+      const title = await response.details.title;
       await updateConvo(req?.session?.user?.username, {
         conversationId: conversationId,
         title
