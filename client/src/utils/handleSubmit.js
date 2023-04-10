2eca77f321a59adf4779febd5ae08d68494358d7
Daniel Avila <messagedaniel@protonmail.com>
2023-04-10 18:27:06 -0400

    fix: only give initialResponse unfinished true value when not a cancellable endpoint

--- client/src/utils/handleSubmit.js
+++ client/src/utils/handleSubmit.js
@@ -106,7 +106,7 @@ const useMessageHandler = () => {
       parentMessageId: isRegenerate ? messageId : fakeMessageId,
       messageId: (isRegenerate ? messageId : fakeMessageId) + '_',
       conversationId,
-      unfinished: true,
+      unfinished: (endpoint === 'azureOpenAI' || endpoint === 'openAI') ? false : true,
       submitting: true
     };
 
