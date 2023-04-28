8dc8bd5cbae0edc0c5d1a8d47a3c3c6dfc3da54b
Daniel Avila <messagedaniel@protonmail.com>
2023-04-28 19:07:16 -0400

    fix(askGPTPlugins.js): fix syntax error by removing extra comma in parentMessageId field feat(askGPTPlugins.js): add default value of null to parentMessageId parameter in ask function

--- api/server/routes/ask/askGPTPlugins.js
+++ api/server/routes/ask/askGPTPlugins.js
@@ -40,9 +40,9 @@ router.post('/', async (req, res) => {
 });
 
 const ask = async ({
-  text,
-  parentMessageId,
   // endpointOption,
+  text,
+  parentMessageId = null,
   conversationId,
   req,
   res
@@ -56,8 +56,6 @@ const ask = async ({
     'Access-Control-Allow-Origin': '*',
     'X-Accel-Buffering': 'no'
   });
-
-  // if (preSendRequest) sendMessage(res, { message: userMessage, created: true });
 
   let responseMessageId = crypto.randomUUID();
 
@@ -84,7 +82,7 @@ const ask = async ({
     const abortController = new AbortController();
     let response = await browserClient({
       text,
-      parentMessageId: userParentMessageId,
+      parentMessageId,
       conversationId,
       ...endpointOption,
       onProgress: progressCallback.call(null, { res, text }),
@@ -93,60 +91,9 @@ const ask = async ({
 
     console.log('CLIENT RESPONSE', response);
 
-    const newConversationId = response.conversationId || conversationId;
-    const newUserMassageId = response.parentMessageId || userMessageId;
-    const newResponseMessageId = response.messageId;
 
     // STEP1 generate response message
     response.text = response.response || '**ChatGPT refused to answer.**';
-
-    let responseMessage = {
-      conversationId: newConversationId,
-      messageId: responseMessageId,
-      newMessageId: newResponseMessageId,
-      parentMessageId: overrideParentMessageId || newUserMassageId,
-      text: await handleText(response),
-      sender: endpointOption?.chatGptLabel || 'ChatGPT',
-      unfinished: false,
-      cancelled: false,
-      error: false
-    };
-
-    await saveMessage(responseMessage);
-    responseMessage.messageId = newResponseMessageId;
-
-    // STEP2 update the conversation
-
-    // First update conversationId if needed
-    let conversationUpdate = { conversationId: newConversationId, endpoint: 'chatGPTBrowser' };
-    if (conversationId != newConversationId)
-      if (isNewConversation) {
-        // change the conversationId to new one
-        conversationUpdate = {
-          ...conversationUpdate,
-          conversationId: conversationId,
-          newConversationId: newConversationId
-        };
-      } else {
-        // create new conversation
-        conversationUpdate = {
-          ...conversationUpdate,
-          ...endpointOption
-        };
-      }
-
-    await saveConvo(req?.session?.user?.username, conversationUpdate);
-    conversationId = newConversationId;
-
-    // STEP3 update the user message
-    userMessage.conversationId = newConversationId;
-    userMessage.messageId = newUserMassageId;
-
-    // If response has parentMessageId, the fake userMessage.messageId should be updated to the real one.
-    if (!overrideParentMessageId)
-      await saveMessage({ ...userMessage, messageId: userMessageId, newMessageId: newUserMassageId });
-    userMessageId = newUserMassageId;
-
     sendMessage(res, {
       title: await getConvoTitle(req?.session?.user?.username, conversationId),
       final: true,
@@ -156,7 +103,8 @@ const ask = async ({
     });
     res.end();
 
-    if (userParentMessageId == '00000000-0000-0000-0000-000000000000') {
+    // if (parentMessageId == '00000000-0000-0000-0000-000000000000') {
+    if (!parentMessageId) {
       // const title = await titleConvo({ endpoint: endpointOption?.endpoint, text, response: responseMessage });
       const title = await response.details.title;
       await saveConvo(req?.session?.user?.username, {
@@ -169,7 +117,7 @@ const ask = async ({
       messageId: responseMessageId,
       sender: 'ChatGPT',
       conversationId,
-      parentMessageId: overrideParentMessageId || userMessageId,
+      parentMessageId: ,
       unfinished: false,
       cancelled: false,
       error: true,
