8647ca0161a75dc938aad02161853ebf6980062e
Daniel Avila <messagedaniel@protonmail.com>
2023-04-29 11:59:02 -0400

    feat(NewConversationMenu): add alpha tag to gptPlugins endpoint and rename it to Plugins

---
+++ client/src/components/Messages/Plugin.jsx
@@ -0,0 +1,89 @@
+<div class="flex items-center rounded bg-green-100 p-3 text-sm text-gray-900">
+  <div>
+    <div class="flex items-center gap-3">
+      <div>
+        Using <b>Wolfram</b>...
+      </div>
+    </div>
+  </div>
+  <svg
+    stroke="currentColor"
+    fill="none"
+    stroke-width="2"
+    viewBox="0 0 24 24"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="ml-1 animate-spin text-center"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <line
+      x1="12"
+      y1="2"
+      x2="12"
+      y2="6"
+    ></line>
+    <line
+      x1="12"
+      y1="18"
+      x2="12"
+      y2="22"
+    ></line>
+    <line
+      x1="4.93"
+      y1="4.93"
+      x2="7.76"
+      y2="7.76"
+    ></line>
+    <line
+      x1="16.24"
+      y1="16.24"
+      x2="19.07"
+      y2="19.07"
+    ></line>
+    <line
+      x1="2"
+      y1="12"
+      x2="6"
+      y2="12"
+    ></line>
+    <line
+      x1="18"
+      y1="12"
+      x2="22"
+      y2="12"
+    ></line>
+    <line
+      x1="4.93"
+      y1="19.07"
+      x2="7.76"
+      y2="16.24"
+    ></line>
+    <line
+      x1="16.24"
+      y1="7.76"
+      x2="19.07"
+      y2="4.93"
+    ></line>
+  </svg>
+  <div
+    class="ml-12 flex items-center gap-2"
+    role="button"
+  >
+    <svg
+      stroke="currentColor"
+      fill="none"
+      stroke-width="2"
+      viewBox="0 0 24 24"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="h-4 w-4"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <polyline points="18 15 12 9 6 15"></polyline>
+    </svg>
+  </div>
+</div>;
