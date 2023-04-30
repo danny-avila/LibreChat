c235692310cac89ba1e0a96a6135bc014d281e3f
Daniel Avila <messagedaniel@protonmail.com>
2023-04-29 20:14:26 -0400

    refactor(MessageHeader.jsx): extract plugins section into a separate variable and add support for gptPlugins endpoint fix(MessageHeader.jsx): disable clicking on non-clickable endpoints

--- client/src/components/Messages/MessageHeader.jsx
+++ client/src/components/Messages/MessageHeader.jsx
@@ -21,6 +21,20 @@ const MessageHeader = ({ isSearchView = false }) => {
   const conversation = useRecoilValue(store.conversation);
   const searchQuery = useRecoilValue(store.searchQuery);
   const { endpoint } = conversation;
+  const isNotClickable = endpoint === 'chatGPTBrowser' || endpoint === 'gptPlugins';
+  const { model } = conversation;
+  const plugins = (
+    <>
+      Plugins{' '}
+      <span class="py-0.25 ml-1 rounded bg-blue-200 px-1 text-[10px] font-semibold uppercase text-[#4559A4]">
+        alpha
+      </span>
+      <span class="px-1">•</span>
+      Model: {model}
+      <span class="px-1">•</span>
+      <div>Enabled plugins:</div>
+    </>
+  );
 
   const getConversationTitle = () => {
     if (isSearchView) return `Search: ${searchQuery}`;
@@ -28,7 +42,7 @@ const MessageHeader = ({ isSearchView = false }) => {
       let _title = `${endpoint}`;
 
       if (endpoint === 'azureOpenAI' || endpoint === 'openAI') {
-        const { chatGptLabel, model } = conversation;
+        const { chatGptLabel } = conversation;
         if (model) _title += `: ${model}`;
         if (chatGptLabel) _title += ` as ${chatGptLabel}`;
       } else if (endpoint === 'bingAI') {
@@ -36,8 +50,9 @@ const MessageHeader = ({ isSearchView = false }) => {
         if (toneStyle) _title += `: ${toneStyle}`;
         if (jailbreak) _title += ` as Sydney`;
       } else if (endpoint === 'chatGPTBrowser') {
-        const { model } = conversation;
         if (model) _title += `: ${model}`;
+      } else if (endpoint === 'gptPlugins') {
+        return plugins;
       } else if (endpoint === null) {
         null;
       } else {
@@ -52,9 +67,9 @@ const MessageHeader = ({ isSearchView = false }) => {
       <div
         className={cn(
           'dark:text-gray-450 w-full gap-1 border-b border-black/10 bg-gray-50 text-sm text-gray-500 transition-all hover:bg-gray-100 hover:bg-opacity-30 dark:border-gray-900/50 dark:bg-gray-700 dark:hover:bg-gray-600 dark:hover:bg-opacity-100',
-          endpoint === 'chatGPTBrowser' ? '' : 'cursor-pointer '
+          isNotClickable ? '' : 'cursor-pointer '
         )}
-        onClick={() => (endpoint === 'chatGPTBrowser' ? null : setSaveAsDialogShow(true))}
+        onClick={() => (isNotClickable ? null : setSaveAsDialogShow(true))}
       >
         <div className="d-block flex w-full items-center justify-center p-3">{getConversationTitle()}</div>
       </div>
