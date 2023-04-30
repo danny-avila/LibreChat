f8698210feb92ac1cb8a0c41bfe971d48f69963c
Daniel D Orlando <dan@danorlando.com>
2023-04-30 08:41:09 -0700

    comment out api error watcher temporarily

--- client/src/components/Auth/ApiErrorWatcher.tsx
+++ client/src/components/Auth/ApiErrorWatcher.tsx
@@ -6,15 +6,15 @@ const ApiErrorWatcher = () => {
   const { error } = useApiErrorBoundary();
  const navigate = useNavigate();
   React.useEffect(() => {
-    if (error?.response?.status === 401) {
-       const localStorageKeys = Object.keys(localStorage);
-       localStorageKeys.forEach(key => {
-         if (key.includes('token')) {
-           localStorage.removeItem(key);
-         }
-       });
-      navigate('/login');
-    }
+    // if (error?.response?.status === 401) {
+    //    const localStorageKeys = Object.keys(localStorage);
+    //    localStorageKeys.forEach(key => {
+    //      if (key.includes('token')) {
+    //        localStorage.removeItem(key);
+    //      }
+    //    });
+    //   navigate('/login');
+    // }
   }, [error, navigate]);
 
   return null;
