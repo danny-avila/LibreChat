b4121d7ee90c4d387e859bfdfc306e1ce28ff12c
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2023-05-15 20:53:36 +0530

    Added error handling

--- client/src/components/Messages/Message.jsx
+++ client/src/components/Messages/Message.jsx
@@ -149,7 +149,16 @@ export default function Message({
               {error ? (
                 <div className="flex flex min-h-[20px] flex-grow flex-col items-start gap-2 gap-4  text-red-500">
                   <div className="rounded-md border border-red-500 bg-red-500/10 px-3 py-2 text-sm text-gray-600 dark:text-gray-100">
-                    {`An error occurred. Please try again in a few moments.\n\nError message: ${text}`}
+                    {text.error.code === 'invalid_api_key' ? (  
+                      'Invalid API key. Please check your API key and try again. You can access your API key by clicking on the model logo in the top-left corner of the textbox.'
+                    ) : text.error.code === null && text.error.type === 'requests' ? (
+                      'We\'re sorry, but the default API key has reached its limit. To continue using this service, please set up your own API key. You can do this by clicking on the model logo in the top-left corner of the textbox.'
+                    ) : (
+                      `Oops! Something went wrong. Please try again in a few moments. Here's the specific error message we encountered:  ${text}`
+
+                    )}
+
+
                   </div>
                 </div>
               ) : edit ? (
