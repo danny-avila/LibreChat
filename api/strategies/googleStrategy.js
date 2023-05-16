a9ed37bfa7d60644324dff1d18b73eb959d365dd
David Shin <42793498+dncc89@users.noreply.github.com>
2023-05-16 11:37:50 +0900

    Add local user avatar pic update from google auth

--- api/strategies/googleStrategy.js
+++ api/strategies/googleStrategy.js
@@ -18,6 +18,7 @@ const googleLogin = new GoogleStrategy(
     try {
       const oldUser = await User.findOne({ email: profile.emails[0].value });
       if (oldUser) {
+        await oldUser.updateOne({ avatar: profile.photos[0].value });
         return cb(null, oldUser);
       }
     } catch (err) {
