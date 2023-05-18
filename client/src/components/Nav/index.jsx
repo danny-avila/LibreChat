84b1a22be81f45f4ca11dbc15431f7822170b9a7
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2023-05-18 11:53:35 +0530

    Added A Toggleable sidebar

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -21,8 +21,8 @@ export default function Nav({ navVisible, setNavVisible }) {
   const [pageNumber, setPageNumber] = useState(1);
   // total pages
   const [pages, setPages] = useState(1);
-  
-  // data provider 
+
+  // data provider
   const getConversationsQuery = useGetConversationsQuery(pageNumber, { enabled: isAuthenticated });
 
   // search
@@ -41,11 +41,8 @@ export default function Nav({ navVisible, setNavVisible }) {
   const [isFetching, setIsFetching] = useState(false);
 
   const debouncedSearchTerm = useDebounce(searchQuery, 750);
-  const searchQueryFn = useSearchQuery(debouncedSearchTerm, pageNumber, { 
-    enabled: !!debouncedSearchTerm && 
-    debouncedSearchTerm.length > 0 &&
-    isSearchEnabled && 
-    isSearching,
+  const searchQueryFn = useSearchQuery(debouncedSearchTerm, pageNumber, {
+    enabled: !!debouncedSearchTerm && debouncedSearchTerm.length > 0 && isSearchEnabled && isSearching
   });
 
   const onSearchSuccess = (data, expectedPage) => {
@@ -64,11 +61,10 @@ export default function Nav({ navVisible, setNavVisible }) {
     //we use isInitialLoading here instead of isLoading because query is disabled by default
     if (searchQueryFn.isInitialLoading) {
       setIsFetching(true);
-    }
-    else if (searchQueryFn.data) {
+    } else if (searchQueryFn.data) {
       onSearchSuccess(searchQueryFn.data);
     }
-  }, [searchQueryFn.data, searchQueryFn.isInitialLoading])
+  }, [searchQueryFn.data, searchQueryFn.isInitialLoading]);
 
   const clearSearch = () => {
     setPageNumber(1);
@@ -119,7 +115,6 @@ export default function Nav({ navVisible, setNavVisible }) {
     }
   };
 
-
   const toggleNavVisible = () => {
     setNavVisible(prev => !prev);
   };
@@ -135,15 +130,10 @@ export default function Nav({ navVisible, setNavVisible }) {
 
   return (
     <>
-      <div
-        className={
-          'nav dark bg-gray-900 md:fixed md:inset-y-0 md:flex md:w-[260px] md:flex-col' +
-          (navVisible ? ' active' : '')
-        }
-      >
+      <div className={'nav dark bg-gray-900 md:inset-y-0' + (navVisible ? ' active' : '')}>
         <div className="flex h-full min-h-0 flex-col ">
-          <div className="scrollbar-trigger flex h-full w-full flex-1 items-start border-white/20 relative">
-            <nav className="flex h-full flex-1 flex-col space-y-1 p-2 relative">
+          <div className="scrollbar-trigger relative flex h-full w-full flex-1 items-start border-white/20">
+            <nav className="relative flex h-full flex-1 flex-col space-y-1 p-2">
               <NewChat />
               <div
                 className={`flex-1 flex-col overflow-y-auto ${
@@ -180,7 +170,7 @@ export default function Nav({ navVisible, setNavVisible }) {
         </div>
         <button
           type="button"
-          className="nav-close-button -ml-0.5 -mt-0.5 inline-flex h-10 w-10 items-center justify-center rounded-md text-white hover:text-gray-900 hover:text-white focus:outline-none focus:ring-white"
+          className="nav-close-button -ml-0.5 -mt-0.5 inline-flex h-10 w-10 items-center justify-center rounded-md md:light:text-gray-900 focus:outline-none focus:ring-white md:-ml-1 md:-mt-[9px] transition ease-in-out duration-150"
           onClick={toggleNavVisible}
         >
           <span className="sr-only">Open sidebar</span>
@@ -191,7 +181,7 @@ export default function Nav({ navVisible, setNavVisible }) {
             viewBox="0 0 24 24"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className="h-6 w-6"
+            className="h-7 w-7"
             height="1em"
             width="1em"
             xmlns="http://www.w3.org/2000/svg"
@@ -211,6 +201,30 @@ export default function Nav({ navVisible, setNavVisible }) {
           </svg>
         </button>
       </div>
+      {!navVisible && (
+        <button
+          type="button"
+          className="nav-open-button fixed top-1 left-2 z-10 inline-flex h-10 w-10 items-center justify-center rounded-md text-gray-900 dark:text-white hover:text-gray-800 dark:hover:text-gray-200 focus:outline-none focus:ring-white"
+          onClick={toggleNavVisible}
+        >
+          <span className="sr-only">Open sidebar</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z"
+            />
+          </svg>
+        </button>
+      )}
+
       <div
         className={'nav-mask' + (navVisible ? ' active' : '')}
         onClick={toggleNavVisible}
