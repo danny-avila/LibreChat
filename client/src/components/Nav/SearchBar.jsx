b983c3ebb8cb2aa3234d763ab4caece3a69ddb84
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2023-05-19 20:33:40 +0530

    feat: Add clear button to search bar
    This commit introduces a clear button to the SearchBar component using the X icon from Lucide-React. When the user enters a query in the input field, the clear button appears allowing them to easily remove the search term. The clear button is hidden when there is no search term entered.

--- client/src/components/Nav/SearchBar.jsx
+++ client/src/components/Nav/SearchBar.jsx
@@ -1,9 +1,11 @@
-import { Search } from 'lucide-react';
+import { Search, X } from 'lucide-react';
 import { useRecoilState } from 'recoil';
 import store from '~/store';
+import { cn } from '~/utils/index.jsx';
 
 export default function SearchBar({ clearSearch }) {
   const [searchQuery, setSearchQuery] = useRecoilState(store.searchQuery);
+  const hasValue = searchQuery.length > 0;
 
   const handleKeyUp = (e) => {
     const { value } = e.target;
@@ -20,7 +22,7 @@ export default function SearchBar({ clearSearch }) {
 
   return (
     <div className="flex cursor-pointer items-center gap-3 rounded-md px-3 py-3 text-sm text-white transition-colors duration-200 hover:bg-gray-500/10">
-      {<Search className="h-4 w-4" />}
+      <Search className={cn("h-4 w-4", hasValue && 'h-[19px] w-[19px]')} />
       <input
         type="text"
         className="m-0 mr-0 w-full border-none bg-transparent p-0 text-sm leading-tight outline-none"
@@ -32,6 +34,8 @@ export default function SearchBar({ clearSearch }) {
         placeholder="Search messages"
         onKeyUp={handleKeyUp}
       />
+      {hasValue && <X className="h-5 w-5" onClick={() => setSearchQuery('')} />}
     </div>
   );
+  
 }
