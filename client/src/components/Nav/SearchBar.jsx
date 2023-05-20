56ece70889e75be3c9019f1c21f117793a3d0030
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2023-05-20 10:34:45 +0530

    Refactor SearchBar component to show Clear Search icon dynamically
    This commit makes changes to the SearchBar React component to render the Clear Search X icon only when the input field has a value. A showClearIcon state using useState hook is added and updated every time the input value changes. The useEffect hook is used to handle the case when the user clears the input value. This allows better UX by providing clear intent to the user that the icon is clickable and will clear the search query.

--- client/src/components/Nav/SearchBar.jsx
+++ client/src/components/Nav/SearchBar.jsx
@@ -1,10 +1,12 @@
+import { forwardRef, useState, useEffect } from 'react';
 import { Search, X } from 'lucide-react';
 import { useRecoilState } from 'recoil';
 import store from '~/store';
 
-export default function SearchBar({ clearSearch }) {
+const SearchBar = forwardRef((props, ref) => {
+  const { clearSearch } = props;
   const [searchQuery, setSearchQuery] = useRecoilState(store.searchQuery);
-  const hasValue = searchQuery.length > 0;
+  const [showClearIcon, setShowClearIcon] = useState(false);
 
   const handleKeyUp = (e) => {
     const { value } = e.target;
@@ -17,14 +19,27 @@ export default function SearchBar({ clearSearch }) {
   const onChange = (e) => {
     const { value } = e.target;
     setSearchQuery(value);
+    setShowClearIcon(value.length > 0);
   };
 
+  useEffect(() => {
+    if (searchQuery.length === 0) {
+      setShowClearIcon(false);
+    } else {
+      setShowClearIcon(true);
+    }
+  }, [searchQuery])
+  
+
   return (
-    <div className="flex cursor-pointer items-center gap-3 rounded-md px-3 py-3 text-sm text-white transition-colors duration-200 hover:bg-gray-500/10 relative">
-      <Search className="h-4 w-4 absolute left-3" />
+    <div
+      ref={ref}
+      className="flex w-full cursor-pointer items-center gap-3 px-3 py-3 text-sm text-white transition-colors duration-200 hover:bg-gray-700 relative"
+    >
+      {<Search className="h-4 w-4 absolute left-3" />}
       <input
         type="text"
-        className="m-0 mr-0 w-full border-none bg-transparent p-0 text-sm leading-tight outline-none pl-8"
+        className="m-0 mr-0 w-full border-none bg-transparent p-0 text-sm leading-tight outline-none pl-7"
         value={searchQuery}
         onChange={onChange}
         onKeyDown={(e) => {
@@ -33,7 +48,15 @@ export default function SearchBar({ clearSearch }) {
         placeholder="Search messages"
         onKeyUp={handleKeyUp}
       />
-      {hasValue && <X className="h-5 w-5 absolute right-3 cursor-pointer" onClick={() => setSearchQuery('')} />}
+      <X
+        className={`h-5 w-5 absolute right-3 cursor-pointer ${showClearIcon ? 'opacity-100' : 'opacity-0'} transition-opacity duration-1000`}
+        onClick={() => {
+          setSearchQuery('');
+          clearSearch();
+        }}
+      />
     </div>
   );
-}
+});
+
+export default SearchBar;
