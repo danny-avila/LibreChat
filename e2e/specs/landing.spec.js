2488e9bbe01daefbd0618abe69181f3ef5ae6956
Daniel Avila <messagedaniel@protonmail.com>
2023-05-20 08:53:46 -0400

    test(landing.spec.js): add beforeEach hook to create a new browser context with auth.json storage state test(landing.spec.js): change test name from 'landing page' to 'Landing title' fix(package.json): change e2e:auth script to save auth.json in e2e directory

--- e2e/specs/landing.spec.js
+++ e2e/specs/landing.spec.js
@@ -1,7 +1,18 @@
 import { expect, test } from '@playwright/test';
 
-test('landing page', async ({ page }) => {
-  await page.goto('http://localhost:3080/');
-  const pageTitle = await page.textContent('#landing-title');
-  expect(pageTitle.length).toBeGreaterThan(0);
+test.describe('Landing suite', () => {
+  let myBrowser;
+
+  test.beforeEach(async ({ browser }) => {
+    myBrowser = await browser.newContext({
+      storageState: 'e2e/auth.json',
+    });
+  });
+
+  test('Landing title', async () => {
+    const page = await myBrowser.newPage();
+    await page.goto('http://localhost:3080/');
+    const pageTitle = await page.textContent('#landing-title')
+    expect(pageTitle.length).toBeGreaterThan(0);
+  });
 });
