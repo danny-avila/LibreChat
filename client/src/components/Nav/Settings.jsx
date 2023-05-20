489b48e80f3cc16ea760499fe4848481946a9788
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2023-05-20 19:05:57 +0530

    feat(Nav): Add SVG icon to settings gear
    This commit adds an SVG icon to the settings gear in the Navigation component's Settings file. The new SVG icon replaces the previous GearIcon component.

--- client/src/components/Nav/Settings.jsx
+++ client/src/components/Nav/Settings.jsx
@@ -1,4 +1,3 @@
-import GearIcon from '../svg/GearIcon';
 import { Dialog } from '../ui/Dialog.tsx';
 import * as Tabs from '@radix-ui/react-tabs';
 import { DialogContent, DialogHeader, DialogTitle } from '../ui/Dialog.tsx';
@@ -20,7 +19,6 @@ export default function ClearConvos({ open, onOpenChange }) {
     console.log('Clearing conversations...');
     clearConvosMutation.mutate();
   };
-
 
   const changeTheme = (e) => {
     setSelectedOption(e.target.value);
@@ -95,7 +93,22 @@ export default function ClearConvos({ open, onOpenChange }) {
                 )}
                 value="general"
               >
-                <GearIcon />
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 20 20"
+                  className="group-radix-state-active:fill-white h-4 h-5 w-4 w-5 fill-white dark:fill-gray-500"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                    clipRule="evenodd"
+                  />
+                </svg>
                 General
               </Tabs.Trigger>
             </Tabs.List>
