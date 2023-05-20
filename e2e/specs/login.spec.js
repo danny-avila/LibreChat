0a4e3b0d2a51e224b3aa57d7e3f4e698228b63a6
Daniel Avila <messagedaniel@protonmail.com>
2023-05-20 08:45:50 -0400

    chore(.gitignore): add auth.json to gitignore test(landing.spec.js): remove commented out code and add check for landing page title test(login.spec.js): add test for login page title feat(package.json): add e2e:auth script to generate auth.json storage file for e2e tests

--- e2e/specs/login.spec.js
+++ e2e/specs/login.spec.js
@@ -2,6 +2,8 @@ import { expect, test } from '@playwright/test';
 
 test('landing page', async ({ page }) => {
   await page.goto('http://localhost:3080/');
-  const pageTitle = await page.textContent('#landing-title');
+  const pageTitle = await page.$eval('h1', pageTitle => pageTitle.textContent);
+  console.log('pageTitle', pageTitle);
   expect(pageTitle.length).toBeGreaterThan(0);
+  expect(pageTitle).toEqual('Welcome back');
 });
