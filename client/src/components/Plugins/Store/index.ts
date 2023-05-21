48af28f050788426967d6086ebcbdddbd81a8185
Daniel D Orlando <dan@danorlando.com>
2023-05-21 12:43:25 -0700

    feat: Add tooltip with field description to plugin auth form

--- client/src/components/Plugins/Store/index.ts
+++ client/src/components/Plugins/Store/index.ts
@@ -3,3 +3,4 @@ export { default as PluginStoreItem } from './PluginStoreItem';
 export { default as PluginPagination } from './PluginPagination';
 export { default as PluginStoreLinkButton } from './PluginStoreLinkButton';
 export { default as PluginAuthForm } from './PluginAuthForm';
+export { default as PluginTooltip } from './PluginTooltip';
