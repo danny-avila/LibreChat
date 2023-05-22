753a02f01e6cc7574757f1fba4af8b50d3e1c721
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2023-05-22 19:50:13 +0530

    test: Add Navigation and Settings tests
    Add Navigation and Settings tests to verify that the navigation bar and Settings button are visible and that the Settings modal displays the expected content. The settings modal verification includes checking whether the modal is visible, if the modal title, tab list, clear conversation button and theme are present, and if the theme option can be selected to change the mode.

--- e2e/specs/landing.spec.js
+++ e2e/specs/landing.spec.js
@@ -15,30 +15,4 @@ test.describe('Landing suite', () => {
     const pageTitle = await page.textContent('#landing-title')
     expect(pageTitle.length).toBeGreaterThan(0);
   });
-
-  test('Settings modal', async () => {
-    const page = await myBrowser.newPage();
-    await page.goto('http://localhost:3080/');
-    await page.locator('[id="headlessui-menu-button-\\:r0\\:"]').click();
-    await page.getByText('Settings').click();
-
-    const modal = await page.getByRole('dialog', { name: 'Settings' }).isVisible();
-    expect(modal).toBeTruthy();
-
-    const modalTitle = await page.getByRole('heading', { name: 'Settings' }).textContent();
-    expect(modalTitle.length).toBeGreaterThan(0);
-    expect(modalTitle).toEqual('Settings');
-
-    const modalTabList = await page.getByRole('tablist', { name: 'Settings' }).isVisible();
-    expect(modalTabList).toBeTruthy();
-
-    const generalTabPanel = await page.getByRole('tabpanel', { name: 'General' }).isVisible();
-    expect(generalTabPanel).toBeTruthy();
-
-    const modalClearConvos = await page.getByRole('button', { name: 'Clear' }).isVisible();
-    expect(modalClearConvos).toBeTruthy();
-
-    const modalTheme = await page.getByRole('combobox').isVisible();
-    expect(modalTheme).toBeTruthy();
-  });
 });
---
+++ e2e/specs/nav.spec.js
@@ -0,0 +1,53 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('Navigation suite', () => {
+  let myBrowser;
+
+  test.beforeEach(async ({ browser }) => {
+    myBrowser = await browser.newContext({
+      storageState: 'e2e/auth.json',
+    });
+  });
+
+  test('Navigation bar', async () => {
+    const page = await myBrowser.newPage();
+    await page.goto('http://localhost:3080/');
+    const navBar = await page.locator('[id="headlessui-menu-button-\\:r0\\:"]').isVisible();
+    expect(navBar).toBeTruthy();
+  });
+
+  test('Settings modal', async () => {
+    const page = await myBrowser.newPage();
+    await page.goto('http://localhost:3080/');
+    await page.locator('[id="headlessui-menu-button-\\:r0\\:"]').click();
+    await page.getByText('Settings').click();
+
+    const modal = await page.getByRole('dialog', { name: 'Settings' }).isVisible();
+    expect(modal).toBeTruthy();
+
+    const modalTitle = await page.getByRole('heading', { name: 'Settings' }).textContent();
+    expect(modalTitle.length).toBeGreaterThan(0);
+    expect(modalTitle).toEqual('Settings');
+
+    const modalTabList = await page.getByRole('tablist', { name: 'Settings' }).isVisible();
+    expect(modalTabList).toBeTruthy();
+
+    const generalTabPanel = await page.getByRole('tabpanel', { name: 'General' }).isVisible();
+    expect(generalTabPanel).toBeTruthy();
+
+    const modalClearConvos = await page.getByRole('button', { name: 'Clear' }).isVisible();
+    expect(modalClearConvos).toBeTruthy();
+
+    const modalTheme = await page.getByRole('combobox');
+    expect(modalTheme.isVisible()).toBeTruthy();
+
+    // change the value to 'dark' and 'light' and see if the theme changes
+    await modalTheme.selectOption({ label: 'dark' });
+    await page.waitForTimeout(1000);
+
+    // the html will have class dark if the theme is dark, use the class in html
+    const html = await page.innerHTML('html');
+    expect(html).toContain('dark');
+
+  });
+});
