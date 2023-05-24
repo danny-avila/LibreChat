3f61f2323fd1afeae085f541c391fe063ec209cc
LaraClara <clara@youpay.ai>
2023-05-24 18:55:30 +1000

    feature(env): Env in the root + version support ie .env.prod, .env.dev, .env.test

--- config/app.js
+++ config/app.js
@@ -1,4 +1,5 @@
-const env = process.env;
+import loader from "./loader";
+const env = loader.all();
 
 const sessionExpiry = eval(env.SESSION_EXPIRY);
 const isProduction = env.NODE_ENV === 'production';
@@ -13,6 +14,34 @@ const clientDomain = env.DOMAIN_CLIENT || legacyClientDomain || 'http://localhos
 const serverDomain = env.DOMAIN_SERVER || legacyServerDomain || 'http://localhost:3080';
 const jwtSecret = env.JWT_SECRET || legacyJwtSecret;
 
+/**
+ * Validate that all required environment variables are set
+ */
+const requiredKeys = [
+  'NODE_ENV',
+  // I want to put more in here, help with install issues but I would break legacy
+];
+
+const missingKeys = requiredKeys.map(key => {
+  const variable = env.get(key);
+  if (variable === undefined || variable === null) {
+    return key;
+  }
+}).filter(value => value !== undefined);
+
+// Throw an error if any required keys are missing
+if (missingKeys.length) {
+  const message = `
+    The following required env variables are missing:
+        ${missingKeys.toString()}.
+    Please add them to your ${envFile} file
+  `;
+  throw new Error(message);
+}
+
+/**
+ * Export the config object
+ */
 module.exports = {
   publicAccess: env.PUBLIC_ACCESS || true,
   isProduction,
@@ -25,4 +54,6 @@ module.exports = {
     refreshSecret: env.JWT_REFRESH_SECRET,
     sessionExpiry: sessionExpiry,
   },
+  // Get any env variable - temp
+  get: env.get
 };
