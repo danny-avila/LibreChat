6268cf4b663b8a1997676682f7561e6440db5f5e
Daniel Avila <messagedaniel@protonmail.com>
2023-05-28 20:42:24 -0400

    refactor(handleTools.js): remove commented out code and unused imports

--- api/app/langchain/tools/handleTools.js
+++ api/app/langchain/tools/handleTools.js
@@ -3,9 +3,6 @@ const { ZapierToolKit } = require('langchain/agents');
 const {
   SerpAPI,
   ZapierNLAWrapper
-  // RequestsGetTool,
-  // RequestsPostTool,
-  // AIPluginTool
 } = require('langchain/tools');
 const { Calculator } = require('langchain/tools/calculator');
 const { WebBrowser } = require('langchain/tools/webbrowser');
@@ -52,80 +49,6 @@ const validateTools = async (user, tools = []) => {
     throw new Error(err);
   }
 };
-
-// const loadTools = async ({ user, model, tools = [] }) => {
-//   const availableTools = {
-//     calculator: () => new Calculator(),
-//     google: async () => {
-//       let cx = process.env.GOOGLE_CSE_ID;
-//       let apiKey = process.env.GOOGLE_API_KEY;
-//       if (!cx || !apiKey) {
-//         cx = await getUserPluginAuthValue(user, 'GOOGLE_CSE_ID');
-//         apiKey = await getUserPluginAuthValue(user, 'GOOGLE_API_KEY');
-//       }
-//       return new GoogleSearchAPI({ cx, apiKey });
-//     },
-//     browser: async () => {
-//       let openAIApiKey = process.env.OPENAI_API_KEY;
-//       if (!openAIApiKey) {
-//         openAIApiKey = await getUserPluginAuthValue(user, 'OPENAI_API_KEY');
-//       }
-//       return new WebBrowser({ model, embeddings: new OpenAIEmbeddings({ openAIApiKey }) });
-//     },
-//     serpapi: async () => {
-//       let apiKey = process.env.SERPAPI_API_KEY;
-//       if (!apiKey) {
-//         apiKey = await getUserPluginAuthValue(user, 'SERPAPI_API_KEY');
-//       }
-//       return new SerpAPI(apiKey, {
-//         location: 'Austin,Texas,United States',
-//         hl: 'en',
-//         gl: 'us'
-//       });
-//     },
-//     zapier: async () => {
-//       let apiKey = process.env.ZAPIER_NLA_API_KEY;
-//       if (!apiKey) {
-//         apiKey = await getUserPluginAuthValue(user, 'ZAPIER_NLA_API_KEY');
-//       }
-//       const zapier = new ZapierNLAWrapper({ apiKey });
-
-//       return ZapierToolKit.fromZapierNLAWrapper(zapier);
-//     },
-//     'dall-e': async () => {
-//       let apiKey = process.env.DALLE_API_KEY;
-//       if (!apiKey) {
-//         apiKey = await getUserPluginAuthValue(user, 'DALLE_API_KEY');
-//       }
-//       return new OpenAICreateImage({ apiKey });
-//     },
-//     // 'stable-diffusion': () => new StableDiffusionAPI(),
-//     'stable-diffusion': async () => {
-//       let url = process.env.SD_WEBUI_URL;
-//       if (!url) {
-//         url = await getUserPluginAuthValue(user, 'SD_WEBUI_URL');
-//       }
-//       return new StableDiffusionAPI({ url });
-//     },
-//     wolfram: async () => {
-//       let apiKey = process.env.WOLFRAM_APP_ID;
-//       if (!apiKey) {
-//         apiKey = await getUserPluginAuthValue(user, 'WOLFRAM_APP_ID');
-//       }
-//       return new WolframAlphaAPI({ apiKey });
-//     }
-//   };
-
-//   const requestedTools = {};
-
-//   for (const tool of tools) {
-//     if (availableTools[tool]) {
-//       requestedTools[tool] = availableTools[tool];
-//     }
-//   }
-
-//   return requestedTools;
-// };
 
 const loadToolWithAuth = async (user, authFields, ToolConstructor, options = {}) => {
   return async function () {
