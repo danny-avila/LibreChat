aa5c285d4282118da82ee3365699d1eb8306575b
Daniel D Orlando <dan@danorlando.com>
2023-05-31 09:51:22 -0700

    test: finish login tests

--- client/test/layout-test-utils.tsx
+++ client/test/layout-test-utils.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthContextProvider } from '~/hooks/AuthContext';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 const client = new QueryClient();
 
@@ -9,9 +11,11 @@ function renderWithProvidersWrapper(ui, { ...options } = {}) {
   function Wrapper({ children }) {
     return (
       <QueryClientProvider client={client}>
-        <Router>
-          <AuthContextProvider>{children}</AuthContextProvider>
-        </Router>
+        <RecoilRoot>
+          <Router>
+            <AuthContextProvider>{children}</AuthContextProvider>
+          </Router>
+        </RecoilRoot>
       </QueryClientProvider>
     );
   }
