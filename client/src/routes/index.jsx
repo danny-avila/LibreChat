9bd3858c0c7faa40cbf73ea59d46f9e6458e3f92
LaraClara <clara@youpay.ai>
2023-06-04 09:11:41 +1000

    bugfix: Revert route changes and fix registration disable calling

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -5,54 +5,53 @@ import Search from './Search';
 import { Login, Registration, RequestPasswordReset, ResetPassword } from '../components/Auth';
 import { AuthContextProvider } from '../hooks/AuthContext';
 import ApiErrorWatcher from '../components/Auth/ApiErrorWatcher';
-const publicAccess = process.env['ALLOW_REGISTRATION'];
+import { ALLOW_REGISTRATION } from '../utils/envConstants';
 
 const AuthLayout = () => (
   <AuthContextProvider>
     <Outlet />
     <ApiErrorWatcher />
   </AuthContextProvider>
-);  
+);
 
 export const router = createBrowserRouter([
-    /**
-     * Guest Routes
-     */
-    {
+  {
+    path: 'register',
+    element: ALLOW_REGISTRATION ? <Registration /> : <Navigate to="/login" replace={true} />
+  },
+  {
+    path: 'forgot-password',
+    element: <RequestPasswordReset />
+  },
+  {
+    path: 'reset-password',
+    element: <ResetPassword />
+  },
+  {
+    element: <AuthLayout />,
+    children: [
+      {
         path: 'login',
-        element: <AuthLayout><Login /></AuthLayout>
-    },
-    {
-      path: 'forgot-password',
-      element: <RequestPasswordReset />
-    },
-    {
-      path: 'reset-password',
-      element: <ResetPassword />
-    },
-    {
-      path: 'register',
-      element: publicAccess ? <Registration /> : <Navigate to="/login" replace={true} />
-    },
-    /**
-     * Authenticated Routes
-     */
-    {
-      path: '/',
-      element: <AuthLayout><Root /></AuthLayout>,
-      children: [
-        {
-          index: true,
-          element: <Navigate to="/chat/new" replace={true} />
-        },
-        {
-          path: 'chat/:conversationId?',
-          element: <Chat />
-        },
-        {
-          path: 'search/:query?',
-          element: <Search />
-        }
-      ]
-    }
-  ]);
+        element: <Login />
+      },
+      {
+        path: '/',
+        element: <Root />,
+        children: [
+          {
+            index: true,
+            element: <Navigate to="/chat/new" replace={true} />
+          },
+          {
+            path: 'chat/:conversationId?',
+            element: <Chat />
+          },
+          {
+            path: 'search/:query?',
+            element: <Search />
+          }
+        ]
+      }
+    ]
+  }
+]);
