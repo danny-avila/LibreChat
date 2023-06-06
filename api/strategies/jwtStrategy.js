3ca9decd026569b9c664bef501f2d5ad0b8b5ebb
Daniel D Orlando <dan@danorlando.com>
2023-06-05 17:41:38 -0700

    test new playwright action

--- api/strategies/jwtStrategy.js
+++ api/strategies/jwtStrategy.js
@@ -2,8 +2,7 @@ const passport = require('passport');
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const User = require('../models/User');
 
-const isProduction = process.env.NODE_ENV === 'production';
-const secretOrKey = isProduction ? process.env.JWT_SECRET_PROD : process.env.JWT_SECRET_DEV;
+const secretOrKey = process.env.JWT_SECRET_PROD || process.env.JWT_SECRET_DEV;
 
 // JWT strategy
 const jwtLogin = new JwtStrategy(
--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -53,6 +53,6 @@ export default defineConfig({
     port: 3080,
     // url: 'http://localhost:3080',
     timeout: 30_000,
-    reuseExistingServer: !process.env.CI
+    reuseExistingServer: true
   },
 });
