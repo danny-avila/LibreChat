819cb9badce494f301f5dedcd266c4f6a955e212
Hyunggyu Jang <murasakipurplez5@gmail.com>
2023-06-09 22:53:57 +0900

    feat: Generate bing's title using bing

--- api/app/index.js
+++ api/app/index.js
@@ -2,6 +2,7 @@ const { browserClient } = require('./chatgpt-browser');
 const { askBing } = require('./bingai');
 const clients = require('./clients');
 const titleConvo = require('./titleConvo');
+const titleConvoBing = require('./titleConvoBing');
 const getCitations = require('../lib/parse/getCitations');
 const citeText = require('../lib/parse/citeText');
 
@@ -9,6 +10,7 @@ module.exports = {
   browserClient,
   askBing,
   titleConvo,
+  titleConvoBing,
   getCitations,
   citeText,
   ...clients
