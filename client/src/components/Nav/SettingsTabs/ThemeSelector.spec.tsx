8c536cb93cec0cb0ecaff72707f47590e71a71ac
Daniel Avila <messagedaniel@protonmail.com>
2023-06-11 15:30:50 -0400

    test: add unit tests for ClearChatsButton and ThemeSelector components

---
+++ client/src/components/Nav/SettingsTabs/ThemeSelector.spec.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { ThemeSelector } from './General';
+
+describe('ThemeSelector', () => {
+  let mockOnChange;
+
+  beforeEach(() => {
+    mockOnChange = jest.fn();
+  });
+
+  it('renders correctly', () => {
+    const { getByText, getByDisplayValue } = render(
+      <ThemeSelector theme="system" onChange={mockOnChange} />
+    );
+
+    expect(getByText('Theme')).toBeInTheDocument();
+    expect(getByDisplayValue('System')).toBeInTheDocument();
+  });
+
+  it('calls onChange when the select value changes', () => {
+    const { getByDisplayValue } = render(
+      <ThemeSelector theme="system" onChange={mockOnChange} />
+    );
+
+    fireEvent.change(getByDisplayValue('System'), { target: { value: 'dark' } });
+
+    expect(mockOnChange).toHaveBeenCalledWith('dark');
+  });
+});
