f85819b874f1996318737b1585789c4f64950e7c
bsu3338 <bsu3338@users.noreply.github.com>
2023-06-10 23:08:06 -0500

    Create openidStrategy.js

---
+++ api/strategies/openidStrategy.js
@@ -0,0 +1,70 @@
+const passport = require('passport');
+const express = require('express');
+const { Issuer, Strategy: OpenIDStrategy } = require('openid-client');
+const config = require('../../config/loader');
+const domains = config.domains;
+
+const User = require('../models/User');
+
+Issuer.discover(process.env.OPENID_ISSUER)
+  .then(issuer => {
+    const client = new issuer.Client({
+      client_id: process.env.OPENID_CLIENT_ID,
+      client_secret: process.env.OPENID_CLIENT_SECRET,
+      redirect_uris: [domains.server + process.env.OPENID_CALLBACK_URL]
+    });
+
+    const openidLogin = new OpenIDStrategy(
+      {
+        client,
+        params: {
+          scope: process.env.OPENID_SCOPE
+        }
+      },
+      async (tokenset, userinfo, done) => {
+        try {
+          let user = await User.findOne({ email: userinfo.email });
+          if (!user) {
+            user = new User({
+              provider: 'openid',
+              openidId: userinfo.sub,
+              username: userinfo.given_name,
+              email: userinfo.email,
+              emailVerified: userinfo.email_verified,
+              name: userinfo.name,
+              avatar: userinfo.picture
+            });
+          } else {
+            user.provider = 'openid';
+            user.openidId = userinfo.sub;
+            user.username = userinfo.given_name;
+            user.name = userinfo.name;
+            user.avatar = userinfo.picture;
+          }  
+
+          await user.save();
+          done(null, user);
+        } catch (err) {
+          done(err);
+        }
+      }
+    );
+
+    passport.use('openid', openidLogin);
+
+    passport.serializeUser((user, done) => {
+      done(null, user.id);
+    });
+
+    passport.deserializeUser(async (id, done) => {
+      try {
+        const user = await User.findById(id);
+        done(null, user);
+      } catch (err) {
+        done(err);
+      }
+    });
+  })
+  .catch(err => {
+    console.error(err);
+  });
