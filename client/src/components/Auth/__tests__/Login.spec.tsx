634310291c54075e7e8c75e258e1b56fc672f771
bsu3338 <bsu3338@users.noreply.github.com>
2023-06-10 23:19:44 -0500

    Update Login.spec.tsx

--- client/src/components/Auth/__tests__/Login.spec.tsx
+++ client/src/components/Auth/__tests__/Login.spec.tsx
@@ -6,6 +6,8 @@ import * as mockDataProvider from '~/data-provider';
 jest.mock('~/utils/envConstants', () => ({
   DOMAIN_SERVER: 'mock-server',
   SHOW_GOOGLE_LOGIN_OPTION: true,
+  SHOW_OPENID_LOGIN_OPTION: true,
+  SHOW_OPENID_LOGIN_NAME: 'mock-openid-name',
   ALLOW_REGISTRATION: true
 }));
 
