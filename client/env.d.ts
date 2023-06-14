925e74cd8e4830d20b3629aeca86bfdda2665b64
bsu3338 <bsu3338@users.noreply.github.com>
2023-06-14 10:01:12 -0500

    Update env.d.ts

--- client/env.d.ts
+++ client/env.d.ts
@@ -4,7 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_SERVER_URL_PROD: string;
   readonly VITE_SHOW_GOOGLE_LOGIN_OPTION: string;
   readonly ALLOW_OPENID: string;
-  readonly OPENID_LABEL: string;
+  readonly VITE_OPENID_LABEL: string;
+  readonly VITE_OPENID_URL: string;
   readonly VITE_CLIENT_URL_DEV: string;
   readonly VITE_CLIENT_URL_PROD: string;
 }
