6c695883e7999afc1e88eabe76131a18d15f719b
bsu3338 <bsu3338@users.noreply.github.com>
2023-06-15 16:10:49 -0500

    Update config.js

--- api/server/routes/config.js
+++ api/server/routes/config.js
@@ -5,10 +5,11 @@ router.get('/', async function (req, res) {
   try {
     const appTitle = process.env.APP_TITLE || 'LibreChat';
     const googleLoginEnabled = !!process.env.GOOGLE_CLIENT_ID && !!process.env.GOOGLE_CLIENT_SECRET;
+    const openidLoginEnabled = !!process.env.OPENID_CLIENT_ID && !!process.env.OPENID_CLIENT_SECRET && !!process.env.OPENID_ISSUER && !!process.env.OPENID_SESSION_SECRET;
     const serverDomain = process.env.DOMAIN_SERVER || 'http://localhost:3080';
     const registrationEnabled = process.env.ALLOW_REGISTRATION || true;
     
-    return res.status(200).send({appTitle, googleLoginEnabled, serverDomain, registrationEnabled});
+    return res.status(200).send({appTitle, googleLoginEnabled, openidLoginEnabled, serverDomain, registrationEnabled});
   } catch (err) {
     console.error(err);
     return res.status(500).send({error: err.message});
