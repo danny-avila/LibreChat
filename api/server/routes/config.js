fcc230ad281841b77e9824c2fd9955d5fe1fda6e
HK Server <hello@aitok.ai>
2023-06-18 06:04:29 +0000

    more AITok Chat branding

--- api/server/routes/config.js
+++ api/server/routes/config.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', async function (req, res) {
   try {
-    const appTitle = process.env.APP_TITLE || 'LibreChat';
+    const appTitle = process.env.APP_TITLE || 'AITok Chat';
     const googleLoginEnabled = !!process.env.GOOGLE_CLIENT_ID && !!process.env.GOOGLE_CLIENT_SECRET;
     const serverDomain = process.env.DOMAIN_SERVER || 'http://localhost:3080';
     const registrationEnabled = process.env.ALLOW_REGISTRATION || true;
