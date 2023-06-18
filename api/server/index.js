7c26458c7ebb4571c1c68edb794e83da0780489e
bsu3338 <bsu3338@users.noreply.github.com>
2023-06-18 18:32:33 -0500

    Update index.js

--- api/server/index.js
+++ api/server/index.js
@@ -43,12 +43,12 @@ config.validate(); // Validate the config
     require('../strategies/facebookStrategy');
   }
   if (process.env.OPENID_CLIENT_ID && process.env.OPENID_CLIENT_SECRET && process.env.OPENID_ISSUER && process.env.OPENID_SCOPE && process.env.OPENID_SESSION_SECRET) {
-    app.use(session({
-      secret: process.env.OPENID_SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false
-    }));
-    app.use(passport.session());
+    // app.use(session({
+    //   secret: process.env.OPENID_SESSION_SECRET,
+    //   resave: false,
+    //   saveUninitialized: false
+    // }));
+    // app.use(passport.session());
     require('../strategies/openidStrategy');
   }
   app.use('/oauth', routes.oauth);
