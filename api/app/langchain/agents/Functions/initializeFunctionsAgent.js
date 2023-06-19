56eef970dbfae25d7b7dd37a04aba54cbff42270
Daniel Avila <messagedaniel@protonmail.com>
2023-06-19 13:52:15 -0400

    style(FunctionsAgent.js): remove unnecessary comments and update PREFIX variable refactor(initializeFunctionsAgent.js): update to use initializeAgentExecutorWithOptions deps(package.json): update langchain to v0.0.95 refactor(askGPTPlugins.js): pass endpointOption to onStart function

--- api/app/langchain/agents/Functions/initializeFunctionsAgent.js
+++ api/app/langchain/agents/Functions/initializeFunctionsAgent.js
@@ -1,32 +1,47 @@
-const FunctionsAgent = require('./FunctionsAgent');
-const { AgentExecutor } = require('langchain/agents');
+// const FunctionsAgent = require('./FunctionsAgent');
+// const { AgentExecutor, initializeAgentExecutorWithOptions } = require('langchain/agents');
+const { initializeAgentExecutorWithOptions } = require('langchain/agents');
 const { BufferMemory, ChatMessageHistory } = require('langchain/memory');
 
 const initializeFunctionsAgent = async ({
   tools,
   model,
   pastMessages,
-  currentDateString,
+  // currentDateString,
   ...rest
 }) => {
-  const agent = FunctionsAgent.fromLLMAndTools(
-    model,
-    tools, 
-    {
-      currentDateString,
-    });
+  // const agent = FunctionsAgent.fromLLMAndTools(
+  //   model,
+  //   tools, 
+  //   {
+  //     currentDateString,
+  //   });
+
 
   const memory = new BufferMemory({
     chatHistory: new ChatMessageHistory(pastMessages),
-    // returnMessages: true, // commenting this out retains memory
     memoryKey: 'chat_history',
     humanPrefix: 'User',
     aiPrefix: 'Assistant',
     inputKey: 'input',
-    outputKey: 'output'
+    outputKey: 'output',
+    returnMessages: true,
   });
 
-  return AgentExecutor.fromAgentAndTools({ agent, tools, memory, ...rest });
+  // return AgentExecutor.fromAgentAndTools({ agent, tools, memory, ...rest });
+
+  return await initializeAgentExecutorWithOptions(
+    tools,
+    model,
+    {
+      agentType: "openai-functions",
+      memory,
+      maxIterations: 4,
+      ...rest,
+    }
+  );
+
+
 };
 
 module.exports = initializeFunctionsAgent;
