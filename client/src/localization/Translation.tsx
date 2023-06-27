b2b4a2db7c86f0a1708f9c6c0f252d6355adf04a
Abner Chou <hi@abnerchou.me>
2023-06-27 12:12:08 -0400

    Change jsx to tsx

--- client/src/localization/Translation.tsx
+++ client/src/localization/Translation.tsx
@@ -4,7 +4,7 @@ import Chinese from './languages/Zh';
 
 // input: language code in string
 // returns an object of translated strings in the language
-export const getTranslations = (langCode) => {
+export const getTranslations = (langCode: string) => {
   if (langCode === 'en') return English;
   if (langCode === 'cn') return Chinese;
   // === add conditionals here for additional languages here === //
@@ -12,7 +12,7 @@ export const getTranslations = (langCode) => {
 
 // input: language code in string & phrase key in string
 // returns an corresponding phrase value in string
-export const localize = (langCode, phraseKey) => {
+export const localize = (langCode: string, phraseKey: string) => {
   const lang = getTranslations(langCode);
   return lang[phraseKey];
 };
