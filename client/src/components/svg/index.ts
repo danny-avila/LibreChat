18b0d966619fd2840c1f10243194c23f218afc4f
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2023-07-03 16:00:04 -0400

    fix(Chat.jsx): Improve Message Creation UX by Eliminating Screen Flicker (#577)
    * fix(Chat.jsx): conversation no longer navigates upon message creation, which would cause re-render/flicker
    
    * chore(.gitignore): ignore storageState.json in all directories
    chore(storageState.json): delete e2e/storageState.json file
    
    * test(e2e): fix old tests with new playwright setup & add helper script for codegen
    
    * fix(Conversation.jsx): add data-testid attribute to <a> element
    test(messages.spec.js): add test for expected navigation after receiving message
    test(messages.spec.js): add test for page navigations
    
    * chore(Plugin.jsx): import Spinner from '~/components' instead of '../svg/Spinner'
    chore(index.jsx): import Spinner from '~/components' instead of '../svg/Spinner'
    chore(Spinner.jsx): change classProp prop to className prop in Spinner component
    feat(index.ts): export Spinner component from './Spinner'

--- client/src/components/svg/index.ts
+++ client/src/components/svg/index.ts
@@ -2,4 +2,5 @@ export { default as Plugin } from './Plugin';
 export { default as GPTIcon } from './GPTIcon';
 export { default as BingIcon } from './BingIcon';
 export { default as CogIcon } from './CogIcon';
+export { default as Spinner } from './Spinner';
 export { default as MessagesSquared } from './MessagesSquared';
