088e4ddd4d65defd6655741ccfe2aeff4e7f3206
BlitzWrecker <wot.dragon040@gmail.com>
2023-07-06 11:20:51 -0600

    Set up tabs UI

--- client/src/components/ui/Landing.tsx
+++ client/src/components/ui/Landing.tsx
@@ -11,8 +11,9 @@ import {
 } from '~/data-provider';
 import SwitchPage from './SwitchPage';
 import DuplicateConvoButton from './DuplicateConvoButton';
-import { useMutation } from '@tanstack/react-query';
 import store from '~/store';
+import { Tabs, TabsList, TabsTrigger, TabsContent } from './Tabs';
+import { cn } from '~/utils';
 
 export default function Landing() {
   const [conversation, setConversation] = useState<TConversation>();
@@ -20,6 +21,7 @@ export default function Landing() {
   const [messagesTree, setMessagesTree] = useState<any>();
   const [convoIdx, setConvoIdx] = useState<number>(0);
   const [convoDataLength, setConvoDataLength] = useState<number>(1);
+  const [tabsValue, setTabsValue] = useState<string>("leaderboard");
   // @ts-ignore TODO: Fix anti-pattern - requires refactoring conversation store
   const title = '首页';
 
@@ -43,6 +45,15 @@ export default function Landing() {
     // const messageIds = msgData?.map((msg) => { return msg.messageId });
     duplicateConversationMutation.mutate({ conversation, msgData });
   }
+  const cardStyle =
+    'transition-colors shadow-md rounded-md min-w-[75px] font-normal bg-white border-black/10 hover:border-black/10 focus:border-black/10 dark:border-black/10 dark:hover:border-black/10 dark:focus:border-black/10 border dark:bg-gray-700 text-black dark:text-white';
+  const defaultClasses =
+    'p-2 rounded-md min-w-[75px] font-normal bg-white/[.60] dark:bg-gray-700 text-black text-xs';
+  const defaultSelected = cn(
+    defaultClasses,
+    'font-medium data-[state=active]:text-white text-xs text-white'
+  );
+  const selectedTab = (val: string) => val + '-tab ' + defaultSelected;
 
   // Get recent conversations
   useEffect(() => {
@@ -66,6 +77,35 @@ export default function Landing() {
   return (
     <div className="flex h-full flex-col items-center overflow-y-auto pt-0 text-sm dark:bg-gray-800">
       <div className="w-full px-6 text-gray-800 dark:text-gray-100 md:flex md:max-w-2xl md:flex-col lg:max-w-3xl">
+        <div
+          className={
+            'flex w-full flex-wrap items-center justify-center gap-2'
+          }
+        >
+          <Tabs 
+            value={tabsValue}
+            className={
+              cardStyle +
+              ' z-50 flex h-[40px] flex-none items-center justify-center px-0 hover:bg-slate-50 dark:hover:bg-gray-600'
+            }
+            onValueChange={(value) => setTabsValue(value)}
+          >
+            <TabsList className="bg-white/[.60] dark:bg-gray-700">
+              <TabsTrigger
+                value='leaderboard'
+                className={`${tabsValue === 'leaderboard' ? selectedTab('creative') : defaultClasses}`}
+              >
+                {'排行榜'}
+              </TabsTrigger>
+              <TabsTrigger
+                value='recommendations'
+                className={`${tabsValue === 'recommendations' ? selectedTab('fast') : defaultClasses}`}
+              >
+                {'对话推荐'}
+              </TabsTrigger>
+            </TabsList>
+          </Tabs>
+        </div>
         <h1
           id="landing-title"
           className="mb-10 ml-auto mr-auto mt-6 flex items-center justify-center gap-2 text-center text-4xl font-semibold sm:mb-16 md:mt-[10vh]"
