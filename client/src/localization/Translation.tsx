61c56230703abd6b12eabf0a0610700b97bc6025
Abner Chou <zhcjtht@hotmail.com>
2023-07-11 15:55:21 -0400

    Feature Localization (i18n) Support (#557)
    * init localization
    
    * Update defaul to en
    
    * Fix merge issue and import path.
    
    * Set default to en
    
    * Change jsx to tsx
    
    * Update the password max length string.
    
    * Remove languageContext as using the recoil instead.

---
+++ client/src/localization/Translation.tsx
@@ -0,0 +1,18 @@
+import English from './languages/Eng';
+import Chinese from './languages/Zh';
+// === import additional language files here === //
+
+// input: language code in string
+// returns an object of translated strings in the language
+export const getTranslations = (langCode: string) => {
+  if (langCode === 'en') return English;
+  if (langCode === 'cn') return Chinese;
+  // === add conditionals here for additional languages here === //
+};
+
+// input: language code in string & phrase key in string
+// returns an corresponding phrase value in string
+export const localize = (langCode: string, phraseKey: string) => {
+  const lang = getTranslations(langCode);
+  return lang[phraseKey];
+};
