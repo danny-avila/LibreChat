9b869d423e84a8f3a9144cb117e8daae1b444740
Daniel Avila <messagedaniel@protonmail.com>
2023-07-11 15:13:02 -0400

    fix(PluginStoreDialog.tsx): update z-index value for the dialog container
    The z-index value for the dialog container was updated to "102" to ensure it appears above other elements on the page.

--- client/src/components/Plugins/Store/PluginStoreDialog.tsx
+++ client/src/components/Plugins/Store/PluginStoreDialog.tsx
@@ -138,7 +138,7 @@ function PluginStoreDialog({ isOpen, setIsOpen }: TPluginStoreDialogProps) {
   };
 
   return (
-    <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
+    <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-[102]">
       {/* The backdrop, rendered as a fixed sibling to the panel container */}
       <div className="fixed inset-0 bg-gray-500/90 transition-opacity dark:bg-gray-800/90" />
       {/* Full-screen container to center the panel */}
