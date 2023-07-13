02b644f1a4aa37053518aa48960bb807b9aefeef
Tyler McNew <tyler@baopals.com>
2023-07-13 14:25:52 -0400

    seo

--- client/src/components/Stripe/SubscribeForm.tsx
+++ client/src/components/Stripe/SubscribeForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import { useAuthContext } from '~/hooks/AuthContext';
 import { useForm } from "react-hook-form";
@@ -6,6 +6,7 @@ import { CardElement, useElements, useStripe, Elements } from "@stripe/react-str
 import { Button } from '../ui/Button';
 import Spinner from "../svg/Spinner";
 import { loadStripe } from "@stripe/stripe-js";
+import { gtag, install } from 'ga-gtag'; // Import ga-gtag
 import './SubscribeForm.css';
 
 // Add your publishable key
@@ -17,6 +18,11 @@ const SubscribeForm = () => {
   const stripe = useStripe();
   const elements = useElements();
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    // Initialize Google AdWords gtag
+    install('G-LL9W9MWZPP'); // replace 'G-LL9W9MWZPP' with your AdWords ID
+  }, []);
 
   const onSubmit = async () => {
     setIsLoading(true); // Start loading state
@@ -57,6 +63,9 @@ const SubscribeForm = () => {
   
       console.log("Subscription Created:", createSubscriptionResult.data);
       alert("Subscription successful!");
+  
+      // Track conversion event with Google AdWords
+      gtag('event', 'conversion', { 'send_to': 'G-LL9W9MWZPP' });
   
       // Add this line to refresh the page
       window.location.reload();
