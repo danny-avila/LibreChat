5863f08ffc2e2d03964ff8013d1aaac5cedbf5ee
Danny Avila <messagedaniel@protonmail.com>
2023-07-14 14:47:46 -0400

    fix(Plugin.tsx): type issue

--- client/src/components/Messages/Plugin.tsx
+++ client/src/components/Messages/Plugin.tsx
@@ -3,7 +3,7 @@ import { Spinner } from '~/components';
 import { useRecoilValue } from 'recoil';
 import CodeBlock from './Content/CodeBlock.jsx';
 import { Disclosure } from '@headlessui/react';
-import { ChevronDownIcon } from 'lucide-react';
+import { ChevronDownIcon, LucideProps } from 'lucide-react';
 import { cn } from '~/utils/';
 import store from '~/store';
 
@@ -25,6 +25,10 @@ interface PluginProps {
 
 type PluginsMap = {
   [pluginKey: string]: string;
+};
+
+type PluginIconProps = LucideProps & {
+  className?: string;
 };
 
 function formatInputs(inputs: Input[]) {
@@ -92,43 +96,48 @@ const Plugin: React.FC<PluginProps> = ({ plugin }) => {
   return (
     <div className="flex flex-col items-start">
       <Disclosure>
-        {({ open }) => (
-          <>
-            <div
-              className={cn(
-                loading ? 'bg-green-100' : 'bg-[#ECECF1]',
-                'flex items-center rounded p-3 text-sm text-gray-900',
-              )}
-            >
-              <div>
-                <div className="flex items-center gap-3">
-                  <div>{generateStatus()}</div>
+        {({ open }) => {
+          const iconProps: PluginIconProps = {
+            className: cn(open ? 'rotate-180 transform' : '', 'h-4 w-4'),
+          };
+          return (
+            <>
+              <div
+                className={cn(
+                  loading ? 'bg-green-100' : 'bg-[#ECECF1]',
+                  'flex items-center rounded p-3 text-sm text-gray-900',
+                )}
+              >
+                <div>
+                  <div className="flex items-center gap-3">
+                    <div>{generateStatus()}</div>
+                  </div>
                 </div>
+                {loading && <Spinner className="ml-1" />}
+                <Disclosure.Button className="ml-12 flex items-center gap-2">
+                  <ChevronDownIcon {...iconProps} />
+                </Disclosure.Button>
               </div>
-              {loading && <Spinner className="ml-1" />}
-              <Disclosure.Button className="ml-12 flex items-center gap-2">
-                <ChevronDownIcon className={cn(open ? 'rotate-180 transform' : '', 'h-4 w-4')} />
-              </Disclosure.Button>
-            </div>
 
-            <Disclosure.Panel className="my-3 flex max-w-full flex-col gap-3">
-              <CodeBlock
-                lang={latestPlugin?.toUpperCase() || 'INPUTS'}
-                codeChildren={formatInputs(plugin.inputs ?? [])}
-                plugin={true}
-                classProp="max-h-[450px]"
-              />
-              {finished && (
+              <Disclosure.Panel className="my-3 flex max-w-full flex-col gap-3">
                 <CodeBlock
-                  lang="OUTPUTS"
-                  codeChildren={plugin.outputs ?? ''}
+                  lang={latestPlugin?.toUpperCase() || 'INPUTS'}
+                  codeChildren={formatInputs(plugin.inputs ?? [])}
                   plugin={true}
                   classProp="max-h-[450px]"
                 />
-              )}
-            </Disclosure.Panel>
-          </>
-        )}
+                {finished && (
+                  <CodeBlock
+                    lang="OUTPUTS"
+                    codeChildren={plugin.outputs ?? ''}
+                    plugin={true}
+                    classProp="max-h-[450px]"
+                  />
+                )}
+              </Disclosure.Panel>
+            </>
+          );
+        }}
       </Disclosure>
     </div>
   );
