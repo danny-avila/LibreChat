2f63465aa7045bf71d16d8718d565788a7d29fb3
Damien Pacaud <damien@qima.com>
2023-07-18 10:01:12 +0200

    changing names and removing undeeded files

--- client/src/components/Input/Footer.tsx
+++ client/src/components/Input/Footer.tsx
@@ -6,15 +6,12 @@ export default function Footer() {
   return (
     <div className="hidden px-3 pb-1 pt-2 text-center text-xs text-black/50 dark:text-white/50 md:block md:px-4 md:pb-4 md:pt-3">
       <a
-        href="https://github.com/danny-avila/LibreChat"
-        target="_blank"
-        rel="noreferrer"
+        href=""
         className="underline"
       >
-        {config?.appTitle || 'LibreChat'}
+        {config?.appTitle}
       </a>
-      . Serves and searches all conversations reliably. All AI convos under one house. Pay per call
-      and not per month (cents compared to dollars).
+      . Serves and searches all conversations reliably.
     </div>
   );
 }
