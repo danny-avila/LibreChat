ad29d253967f10b3fac3ddc1f865a76ac0f8aec6
Fuegovic <32828263+fuegovic@users.noreply.github.com>
2023-07-19 11:35:41 -0400

    docs: updates (#662)
    * docs: updates
    
    * docs: updates
    
    * Update Settings.jsx

--- client/src/components/Endpoints/BingAI/Settings.jsx
+++ client/src/components/Endpoints/BingAI/Settings.jsx
@@ -111,7 +111,7 @@ function Settings(props) {
                 style={{ opacity: showSystemMessage ? '1' : '0' }}
               >
                 <a
-                  href="https://github.com/danny-avila/LibreChat/blob/main/client/defaultSystemMessage.md"
+                  href="https://github.com/danny-avila/LibreChat/blob/main/docs/features/bing_jailbreak.md#default-system-message-for-jailbreak-mode-sydney"
                   target="_blank"
                   className="text-blue-500 transition-colors duration-200 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-500"
                   rel="noreferrer"
