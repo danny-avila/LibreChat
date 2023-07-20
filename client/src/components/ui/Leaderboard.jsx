0b5b0deadf9e8af4525aa4a3dbe77167ae480b7f
BlitzWrecker <wot.dragon0407@gmail.com>
2023-07-20 15:42:46 -0600

    converted leaderboard to a dialog window

--- client/src/components/ui/Leaderboard.jsx
+++ client/src/components/ui/Leaderboard.jsx
@@ -1,6 +1,8 @@
 import { useGetLeaderboardQuery } from '@librechat/data-provider';
 import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react';
 import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
+import { Dialog } from '@headlessui/react';
+import { X } from 'lucide-react';
 
 import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
 import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
@@ -35,7 +37,7 @@ function userCellRenderer(user) {
   </div>);
 }
 
-export default function Leaderboard() {
+export default function Leaderboard({ isOpen, setIsOpen }) {
   const getLeaderboardQuery = useGetLeaderboardQuery();
 
   const gridRef = useRef(); // Optional - for accessing Grid's API
@@ -90,28 +92,54 @@ export default function Leaderboard() {
   }, [getLeaderboardQuery.isSuccess, getLeaderboardQuery.data]);
 
   return (
-    <div>
+    <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
+      {/* The backdrop, rendered as a fixed sibling to the panel container */}
+      <div className="fixed inset-0 bg-gray-500/90 transition-opacity dark:bg-gray-800/90" />
+      {/* Full-screen container to center the panel */}
+      <div className="fixed inset-0 flex items-center justify-center p-4">
+        <Dialog.Panel className="relative w-full transform overflow-hidden overflow-y-auto rounded-lg bg-white text-left shadow-xl transition-all dark:bg-gray-900 max-sm:h-full sm:mx-7 sm:my-8 sm:max-w-2xl lg:max-w-5xl xl:max-w-7xl">
+          <div className="flex items-center justify-between border-b-[1px] border-black/10 px-4 pb-4 pt-5 dark:border-white/10 sm:p-6">
+            <div className="flex items-center">
+              <div className="text-center sm:text-left">
+                <Dialog.Title className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-200">
+                  邀请排行榜
+                </Dialog.Title>
+              </div>
+            </div>
+            <div>
+              <div className="sm:mt-0">
+                <button
+                  onClick={() => setIsOpen(false)}
+                  className="inline-block text-gray-500 hover:text-gray-100"
+                  tabIndex={0}
+                >
+                  <X />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="p-4 sm:p-6 sm:pt-4">
+            {/* Example using Grid's API */}
+            <button onClick={buttonListener}>Push Me</button>
+            {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
+            <div className="ag-theme-alpine" style={{ width: 800, height: 500 }}>
+              <AgGridReact
+                ref={gridRef} // Ref for accessing Grid's API
 
-      {/* Example using Grid's API */}
-      <button onClick={buttonListener}>Push Me</button>
+                rowData={rowData} // Row Data for Rows
 
-      {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
-      <div className="ag-theme-alpine" style={{ width: 800, height: 500 }}>
+                columnDefs={columnDefs} // Column Defs for Columns
+                defaultColDef={defaultColDef} // Default Column Properties
 
-        <AgGridReact
-          ref={gridRef} // Ref for accessing Grid's API
+                animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+                rowSelection='multiple' // Options - allows click selection of rows
 
-          rowData={rowData} // Row Data for Rows
-
-          columnDefs={columnDefs} // Column Defs for Columns
-          defaultColDef={defaultColDef} // Default Column Properties
-
-          animateRows={true} // Optional - set to 'true' to have rows animate when sorted
-          rowSelection='multiple' // Options - allows click selection of rows
-
-          onCellClicked={cellClickedListener} // Optional - registering for Grid Event
-        />
+                onCellClicked={cellClickedListener} // Optional - registering for Grid Event
+              />
+            </div>
+          </div>
+        </Dialog.Panel>
       </div>
-    </div>
+    </Dialog>
   );
 }
