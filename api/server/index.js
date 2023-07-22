ba210861aa790e0ceec84a6b77d530587ef4e887
Danny Avila <messagedaniel@protonmail.com>
2023-07-22 09:09:40 -0400

    refactor: improve passport strategy handling in async/await manner to prevent race conditions upon importing modules

--- api/server/index.js
+++ api/server/index.js
@@ -11,6 +11,15 @@ const passport = require('passport');
 const port = process.env.PORT || 3080;
 const host = process.env.HOST || 'localhost';
 const projectPath = path.join(__dirname, '..', '..', 'client');
+const {
+  jwtLogin,
+  passportLogin,
+  googleLogin,
+  githubLogin,
+  discordLogin,
+  facebookLogin,
+  setupOpenId,
+} = require('../strategies');
 
 // Init the config and validate it
 const config = require('../../config/loader');
@@ -40,19 +49,19 @@ config.validate(); // Validate the config
 
   // OAUTH
   app.use(passport.initialize());
-  require('../strategies/jwtStrategy');
-  require('../strategies/localStrategy');
+  passport.use(await jwtLogin());
+  passport.use(await passportLogin());
   if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
-    require('../strategies/googleStrategy');
+    passport.use(await googleLogin());
   }
   if (process.env.FACEBOOK_CLIENT_ID && process.env.FACEBOOK_CLIENT_SECRET) {
-    require('../strategies/facebookStrategy');
+    passport.use(await facebookLogin());
   }
   if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
-    require('../strategies/githubStrategy');
+    passport.use(await githubLogin());
   }
   if (process.env.DISCORD_CLIENT_ID && process.env.DISCORD_CLIENT_SECRET) {
-    require('../strategies/discordStrategy');
+    passport.use(await discordLogin());
   }
   if (
     process.env.OPENID_CLIENT_ID &&
@@ -69,7 +78,7 @@ config.validate(); // Validate the config
       }),
     );
     app.use(passport.session());
-    require('../strategies/openidStrategy');
+    await setupOpenId();
   }
   app.use('/oauth', routes.oauth);
   // api endpoint
--- api/strategies/facebookStrategy.js
+++ api/strategies/facebookStrategy.js
@@ -1,59 +1,59 @@
-const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const User = require('../models/User');
 const config = require('../../config/loader');
 const domains = config.domains;
 
 // facebook strategy
-const facebookLogin = new FacebookStrategy(
-  {
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: `${domains.server}${process.env.FACEBOOK_CALLBACK_URL}`,
-    proxy: true,
-    // profileFields: [
-    //   'id',
-    //   'email',
-    //   'gender',
-    //   'profileUrl',
-    //   'displayName',
-    //   'locale',
-    //   'name',
-    //   'timezone',
-    //   'updated_time',
-    //   'verified',
-    //   'picture.type(large)'
-    // ]
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    console.log('facebookLogin => profile', profile);
-    try {
-      const oldUser = await User.findOne({ email: profile.emails[0].value });
+const facebookLogin = async () =>
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      callbackURL: `${domains.server}${process.env.FACEBOOK_CALLBACK_URL}`,
+      proxy: true,
+      // profileFields: [
+      //   'id',
+      //   'email',
+      //   'gender',
+      //   'profileUrl',
+      //   'displayName',
+      //   'locale',
+      //   'name',
+      //   'timezone',
+      //   'updated_time',
+      //   'verified',
+      //   'picture.type(large)'
+      // ]
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log('facebookLogin => profile', profile);
+      try {
+        const oldUser = await User.findOne({ email: profile.emails[0].value });
 
-      if (oldUser) {
-        console.log('FACEBOOK LOGIN => found user', oldUser);
-        return done(null, oldUser);
+        if (oldUser) {
+          console.log('FACEBOOK LOGIN => found user', oldUser);
+          return done(null, oldUser);
+        }
+      } catch (err) {
+        console.log(err);
       }
-    } catch (err) {
-      console.log(err);
-    }
 
-    // register user
-    try {
-      const newUser = await new User({
-        provider: 'facebook',
-        facebookId: profile.id,
-        username: profile.name.givenName + profile.name.familyName,
-        email: profile.emails[0].value,
-        name: profile.displayName,
-        avatar: profile.photos[0].value,
-      }).save();
+      // register user
+      try {
+        const newUser = await new User({
+          provider: 'facebook',
+          facebookId: profile.id,
+          username: profile.name.givenName + profile.name.familyName,
+          email: profile.emails[0].value,
+          name: profile.displayName,
+          avatar: profile.photos[0].value,
+        }).save();
 
-      done(null, newUser);
-    } catch (err) {
-      console.log(err);
-    }
-  },
-);
+        done(null, newUser);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  );
 
-passport.use(facebookLogin);
+module.exports = facebookLogin;
