19c692d54c2765d42777799e11bcbdc8b8719eb6
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2023-07-22 07:29:17 -0700

    refactor: improve passport strategy handling in async/await manner to prevent race conditions upon importing modules (#682)

--- api/strategies/jwtStrategy.js
+++ api/strategies/jwtStrategy.js
@@ -1,26 +1,26 @@
-const passport = require('passport');
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const User = require('../models/User');
 
 // JWT strategy
-const jwtLogin = new JwtStrategy(
-  {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.JWT_SECRET,
-  },
-  async (payload, done) => {
-    try {
-      const user = await User.findById(payload.id);
-      if (user) {
-        done(null, user);
-      } else {
-        console.log('JwtStrategy => no user found');
-        done(null, false);
+const jwtLogin = async () =>
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    async (payload, done) => {
+      try {
+        const user = await User.findById(payload.id);
+        if (user) {
+          done(null, user);
+        } else {
+          console.log('JwtStrategy => no user found');
+          done(null, false);
+        }
+      } catch (err) {
+        done(err, false);
       }
-    } catch (err) {
-      done(err, false);
-    }
-  },
-);
+    },
+  );
 
-passport.use(jwtLogin);
+module.exports = jwtLogin;
