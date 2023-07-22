757246520fbe35a07d3d77c5acf8109426b70565
Abner Chou <hi@abnerchou.me>
2023-07-22 15:09:45 -0400

    Add localization support for endpoint pages components  (#667)
    * init localization
    
    * Update defaul to en
    
    * Fix merge issue and import path.
    
    * Set default to en
    
    * Change jsx to tsx
    
    * Update the password max length string.
    
    * Remove languageContext as using the recoil instead.
    
    * Add localization to component endpoints pages
    
    * Revert default to en after testing.
    
    * Update LoginForm.tsx
    
    * Fix translation.
    
    * Make lint happy

--- client/src/components/Endpoints/Google/OptionHover.jsx
+++ client/src/components/Endpoints/Google/OptionHover.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { HoverCardPortal, HoverCardContent } from '~/components/ui/HoverCard.tsx';
+import { useRecoilValue } from 'recoil';
+import store from '~/store';
+import { localize } from '~/localization/Translation';
 
 const types = {
-  temp: 'Higher values = more random, while lower values = more focused and deterministic. We recommend altering this or Top P but not both.',
-  topp: 'Top-p changes how the model selects tokens for output. Tokens are selected from most K (see topK parameter) probable to least until the sum of their probabilities equals the top-p value.',
-  topk: 'Top-k changes how the model selects tokens for output. A top-k of 1 means the selected token is the most probable among all tokens in the model\'s vocabulary (also called greedy decoding), while a top-k of 3 means that the next token is selected from among the 3 most probable tokens (using temperature).',
-  maxoutputtokens:
-    ' 	Maximum number of tokens that can be generated in the response. Specify a lower value for shorter responses and a higher value for longer responses.',
+  temp: 'com_endpoint_google_temp',
+  topp: 'com_endpoint_google_topp',
+  topk: 'com_endpoint_google_topk',
+  maxoutputtokens: 'com_endpoint_google_maxoutputtokens',
 };
 
 function OptionHover({ type, side }) {
@@ -14,6 +16,7 @@ function OptionHover({ type, side }) {
   // if (type === 'pres') {
   //   options.sideOffset = 45;
   // }
+  const lang = useRecoilValue(store.lang);
 
   return (
     <HoverCardPortal>
@@ -23,7 +26,7 @@ function OptionHover({ type, side }) {
         // {...options}
       >
         <div className="space-y-2">
-          <p className="text-sm text-gray-600 dark:text-gray-300">{types[type]}</p>
+          <p className="text-sm text-gray-600 dark:text-gray-300">{localize(lang, types[type])}</p>
         </div>
       </HoverCardContent>
     </HoverCardPortal>
