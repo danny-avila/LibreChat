21c45bd1e60d2cf6a5bde25612d9202f58949eac
bsu3338 <bsu3338@users.noreply.github.com>
2023-07-24 23:11:05 -0500

    Update AuthContext.tsx

--- client/src/hooks/AuthContext.tsx
+++ client/src/hooks/AuthContext.tsx
@@ -73,6 +73,36 @@ const AuthContextProvider = ({
     }
   };
 
+  function parseJwt(token) {
+    try {
+      const base64Url = token.split('.')[1];
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split('')
+        .map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join('')
+        );
+      return JSON.parse(jsonPayload);
+    } catch (error) {
+      return null;
+    }
+  };
+
+  function isTokenExpired(token) {
+      const parsedToken = parseJwt(token);
+      if (!parsedToken) {
+          return true;
+      }
+      const expirationDate = new Date(parsedToken.exp * 1000);
+      if (expirationDate < new Date()) {
+          return true;
+      }
+      return false;
+  };
+
   const setUserContext = useCallback(
     (userContext: TUserContext) => {
       const { token, isAuthenticated, user, redirect } = userContext;
