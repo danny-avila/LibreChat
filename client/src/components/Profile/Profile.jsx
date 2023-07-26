261fdc5e125f86f4bb825f5229fceed9fac6c223
ChengguiSun <cgsun0521@hotmail.com>
2023-07-25 17:07:41 -0700

    Use react-query-services/userGetUserQuery to fetch user data for building profile page

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,18 +1,20 @@
+import React from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '../ui/Dialog.tsx';
-
-// Mock user data
-const dummyUser = {
-  name: 'John Doe',
-  email: 'johndoe@example.com',
-  placeholder: 'placeholder'
-
-}
+import { useGetUserQuery } from '@librechat/data-provider';
 
 const Profile = ({ isOpen, setIsOpen }) => {
-  const { name, email, placeholder } = dummyUser;
+  const { data: user, isLoading, error } = useGetUserQuery();
 
   // If the profile is not open, don't render anything
   if (!isOpen) return null;
+
+  // While the data is loading
+  if (isLoading) return <div>Loading...</div>;
+
+  // If there's an error
+  if (error) return <div>An error occurred: {error.message}</div>;
+
+  const { fullName, username, email } = user;
 
   return (
     <Dialog
@@ -22,12 +24,12 @@ const Profile = ({ isOpen, setIsOpen }) => {
     >
       <DialogContent style={{ padding: '20px' }}>
         <DialogHeader>
-          <DialogTitle style={{ fontSize: '24px', color: 'white', marginBottom: '10px' }}>{`${name}'s Profile`}</DialogTitle>
+          <DialogTitle style={{ fontSize: '24px', color: 'white', marginBottom: '10px' }}>{`${fullName}'s Profile`}</DialogTitle>
         </DialogHeader>
         <div>
-          <h1 style={{ fontSize: '20px', color: 'white' }}>{name}</h1>
+          <h1 style={{ fontSize: '20px', color: 'white' }}>{fullName}</h1>
+          <h2 style={{ marginTop: '20px', fontSize: '20px', color: 'white' }}>{username}</h2>
           <h2 style={{ marginTop: '20px', fontSize: '20px', color: 'white' }}>{email}</h2>
-          <h2 style={{ marginTop: '20px', fontSize: '20px', color: 'white' }}>{placeholder}</h2>
           <button
             onClick={() => setIsOpen(false)}
             style={{ marginTop: '40px', padding: '10px 20px', color: '#fff', backgroundColor: '#007bff', border: 'none', borderRadius: '3px', cursor: 'pointer' }}
@@ -41,4 +43,3 @@ const Profile = ({ isOpen, setIsOpen }) => {
 };
 
 export default Profile;
-
