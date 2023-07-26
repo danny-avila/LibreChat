10ca295ac1ab99641c25be9ec1b6bdba2f04d4bd
bsu3338 <bsu3338@users.noreply.github.com>
2023-07-26 15:37:35 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,22 +16,11 @@ const App = () => {
       onError: (error) => {
          console.log('Error', error);
          const originalRequest = error.config;
+         
          if (error?.response?.status === 401 && !originalRequest._retry) {
            originalRequest._retry = true;
            window.dispatchEvent(new CustomEvent('unauthorized'));
-           // return;
          }
-         // return;
-//        if (error?.response?.status === 401 && !originalRequest._retry) {
-          //if (refreshAttempts > 3) {
-          //  refreshAttempts += 1;
-          //  console.error("Too many refresh attempts. Please log in again.");
-          // return;
-          //}
-          //setError(error);
-          //const event = new CustomEvent('unauthorized');
-          //window.dispatchEvent(event);
-        // }
       },
     }),
   });
