ad08530b31e79f007e3b2ec6c22ee8e291019f20
bsu3338 <bsu3338@users.noreply.github.com>
2023-07-26 13:41:41 -0500

    Update index.jsx

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -3,30 +3,18 @@ import Root from './Root';
 import Chat from './Chat';
 import Search from './Search';
 import { Login, Registration, RequestPasswordReset, ResetPassword } from '../components/Auth';
-import { AuthContextProvider } from '../hooks/AuthContext';
+import { AuthContext, AuthContextProvider } from '../hooks/AuthContext';
 import ApiErrorWatcher from '../components/Auth/ApiErrorWatcher';
-
-import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-export const AuthContext = React.createContext();
-
 export const AuthTokenProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
-
-  useEffect(() => {
-    // TODO: implement your logic here to retrieve and set initial access token
-  }, []);
-
   axios.interceptors.response.use(
     (response) => response,
     async (error) => {
       const originalRequest = error.config;
       if (error.response.status === 401 && !originalRequest._retry) {
         originalRequest._retry = true;
-        const token = await refreshAccessToken(); // TODO: implement this function
-        setToken(token);
-        axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+        window.dispatchEvent(new CustomEvent('unauthorized'));
         return axios(originalRequest);
       }
       return Promise.reject(error);
@@ -34,7 +22,7 @@ export const AuthTokenProvider = ({ children }) => {
   );
 
   return (
-    <AuthContextProvider value={{ token, setToken }}>
+    <AuthContextProvider>
       {children}
     </AuthContextProvider>
   );
