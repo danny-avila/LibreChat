94778d9182d04f287a47453fe0a49b4dcc6cf28b
bsu3338 <bsu3338@users.noreply.github.com>
2023-07-27 08:51:38 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,23 +6,23 @@ import { QueryClient, QueryClientProvider, QueryCache } from '@tanstack/react-qu
 import { ThemeProvider } from './hooks/ThemeContext';
 import { useApiErrorBoundary } from './hooks/ApiErrorBoundaryContext';
 import { router } from './routes';
-import { useState } from 'react';
+//import { useState } from 'react';
 
 const App = () => {
   const { setError } = useApiErrorBoundary();
-  const [refreshAttempted, setRefreshAttempted] = useState(false);
+  //const [refreshAttempted, setRefreshAttempted] = useState(false);
   
   const queryClient = new QueryClient({
     queryCache: new QueryCache({
       onError: (error) => {
         console.log('Error', error);
         if (error?.response?.status === 401){
-          if (!refreshAttempted) {
-            setRefreshAttempted(true);
+          //if (!refreshAttempted) {
+          //  setRefreshAttempted(true);
             window.dispatchEvent(new CustomEvent('unauthorized'));
-          } else {
-            window.dispatchEvent(new CustomEvent('maxRefreshAttemptsExceeded'));
-          }
+       //   } else {
+         //   window.dispatchEvent(new CustomEvent('maxRefreshAttemptsExceeded'));
+          // }
         }
       },
     }),
