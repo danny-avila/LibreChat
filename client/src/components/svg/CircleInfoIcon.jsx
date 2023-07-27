4fd678f54ce70c33ce8e8140f335c5df42ea60b9
BlitzWrecker <wot.dragon0407@gmail.com>
2023-07-27 16:59:48 -0600

    added circle info icon

---
+++ client/src/components/svg/CircleInfoIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CircleInfoIcon() {
+  return(
+    <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12 8H12.01M12 11V16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
