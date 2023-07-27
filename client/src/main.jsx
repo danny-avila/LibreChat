1190abb34542e035e9f64b31b7994a02c426ea78
BlitzWrecker <wot.dragon0407@gmail.com>
2023-07-27 17:35:31 -0600

    Merge branch 'main' into zhaoyu/ui_enhancement

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,18 @@
 import { createRoot } from 'react-dom/client';
+import React from 'react';
 import App from './App';
+import ReactGA from 'react-ga';
 import './style.css';
 import './mobile.css';
 import { ApiErrorBoundaryProvider } from './hooks/ApiErrorBoundaryContext';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+const trackingId = import.meta.env.VITE_REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
+
+if (trackingId) {
+  ReactGA.initialize(trackingId);
+}
 
 root.render(
   <ApiErrorBoundaryProvider>
