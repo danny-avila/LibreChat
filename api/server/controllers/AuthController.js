ac2af154b3bbc7441bdf641573937e0815d661b9
bsu3338 <bsu3338@users.noreply.github.com>
2023-07-27 13:36:41 -0500

    Update AuthController.js

--- api/server/controllers/AuthController.js
+++ api/server/controllers/AuthController.js
@@ -100,32 +100,6 @@ const refreshController = async (req, res, next) => {
   }
 };    
 
-//const intercept401 = async (err, req, res, next) => {
-//  console.log('Error', err);
-//  console.log('statusCode', res.statusCode);
-//  if (res.statusCode === 401 && !refreshAttempted) {
-//    // const { signedCookies = {} } = req;
-//    // const { refreshToken } = signedCookies;
-//    const refreshToken = req.headers.cookie ? cookies.parse(req.headers.cookie).refreshToken : null;
-//    if (refreshToken) {
-//      try {
-//        refreshAttempted = true;
-//        await refreshController(req, res, next);
-//      } catch (error) {
-//        res.status(401).send('Token Refresh Failed');
-//      }
-//    } else {
-//      res.status(401).send('Interceptor: Refresh token not provided');
-//    }
-//  } else if (res.statusCode === 401 && refreshAttempted) {
-//    refreshAttempted = false;
-//    res.status(401).send('Refresh Already Attempted');
-//  } else {
-//    refreshAttempted = false;
-//    next();
-//  }  
-//};
-
 module.exports = {
   getUserController,
   refreshController,
