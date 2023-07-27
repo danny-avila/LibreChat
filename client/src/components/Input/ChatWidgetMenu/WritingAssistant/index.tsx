24b5d65b714f7f1c505730484d1fc69136d417f2
BlitzWrecker <wot.dragon0407@gmail.com>
2023-07-27 17:10:04 -0600

    modified writing assistant to show on main page

--- client/src/components/Input/ChatWidgetMenu/WritingAssistant/index.tsx
+++ client/src/components/Input/ChatWidgetMenu/WritingAssistant/index.tsx
@@ -3,6 +3,11 @@ import { Label } from '~/components/ui/Label';
 import SelectDropDown from '../../../ui/SelectDropDown';
 import EssayTemplate from './EssayTemplate';
 import { cn } from '~/utils';
+import CheckMark from '~/components/svg/CheckMark';
+import CircleInfoIcon from '~/components/svg/CircleInfoIcon';
+import { useRecoilState } from 'recoil';
+
+import store from '~/store';
 
 /*
 作者职位、写作水平、字数、主题、布局、风格、引文、读者
@@ -18,8 +23,9 @@ function getTemplate(type: string) {
 }
 
 function WritingAssistant() {
-  // Essay basic bundle
   const [type, setType] = useState<string>('作文');
+  const [showExample, setShowExample] = useState<boolean>(false);
+  const [text, setText] = useRecoilState(store.text);
   const template = getTemplate(type);
 
   const defaultTextProps =
@@ -29,41 +35,59 @@ function WritingAssistant() {
   const setExample = template.setExample;
   const restoreFields = template.restoreFields;
 
-  const WidgetLayout = () => {
-    return(
-      <div className="h-[490px] overflow-y-auto md:h-[400px]">
-        <div className="grid gap-6 sm:grid-cols-2">
-          <div className="col-span-1 flex flex-col items-center justify-start gap-6">
-            <div className="grid w-full items-center gap-y-2">
-              <Label htmlFor="toneStyle-dropdown" className="text-left text-sm font-medium">
-                写作类型 <small className="opacity-40">(默认值: 作文)</small>
-              </Label>
-              <SelectDropDown
-                title={''}
-                value={type}
-                setValue={(value: string) => setType(value)}
-                availableValues={['作文']}
-                disabled={false}
-                className={cn(
-                  defaultTextProps,
-                  'flex w-full resize-none focus:outline-none focus:ring-0 focus:ring-opacity-0 focus:ring-offset-0'
-                )}
-                containerClassName="flex w-full resize-none"
-                subContainerClassName=''
-              />
-              {template.SubType()}
-            </div>
-            {template.LayoutLeft()}
-          </div>
-          <div className="col-span-1 flex flex-col items-center justify-start gap-6">
-            {template.LayoutRight()}
-          </div>
-        </div>
-      </div>
-    );
+  const setTextHandler = () => setText(getPromptText());
+  const showExampleHandler = () => {
+    showExample ? restoreFields() : setExample();
+    setShowExample(!showExample);
   }
 
-  return({ WidgetLayout, getPromptText, setExample, restoreFields });
+  return(
+    <div className="grid justify-center gap-4">
+      <div className='flex flex-row justify-center gap-10'>
+        <button
+          className='flex flex-row justify-center items-center w-16 rounded-md border-transparent border-2 bg-slate-100 hover:border-cyan-500'
+          onClick={ setTextHandler }
+        >
+          <CheckMark />
+          确认
+        </button>
+        <button
+          className='flex flex-row justify-center items-center w-16 rounded-md border-transparent border-2 bg-slate-100 hover:border-purple-500'
+          onClick={ showExampleHandler }
+        >
+          <CircleInfoIcon />
+          {showExample ? '恢复' : '示例'}
+        </button>
+      </div>
+      <div className="grid gap-6 sm:grid-cols-2">
+        <div className="col-span-1 flex flex-col items-center justify-start gap-6">
+          <div className="grid w-full items-center gap-y-2">
+            <Label htmlFor="toneStyle-dropdown" className="text-left text-sm font-medium">
+              写作类型 <small className="opacity-40">(默认值: 作文)</small>
+            </Label>
+            <SelectDropDown
+              title={''}
+              value={type}
+              setValue={(value: string) => setType(value)}
+              availableValues={['作文']}
+              disabled={false}
+              className={cn(
+                defaultTextProps,
+                'flex w-full resize-none focus:outline-none focus:ring-0 focus:ring-opacity-0 focus:ring-offset-0'
+              )}
+              containerClassName="flex w-full resize-none"
+              subContainerClassName=''
+            />
+            {template.SubType()}
+          </div>
+          {template.LayoutLeft()}
+        </div>
+        <div className="col-span-1 flex flex-col items-center justify-start gap-6">
+          {template.LayoutRight()}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WritingAssistant;
