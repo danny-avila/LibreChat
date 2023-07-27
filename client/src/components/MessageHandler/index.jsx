865399a407a1d355d08c7554fd55f712d675fd67
bsu3338 <bsu3338@users.noreply.github.com>
2023-07-26 23:02:58 -0500

    Update index.jsx

--- client/src/components/MessageHandler/index.jsx
+++ client/src/components/MessageHandler/index.jsx
@@ -237,21 +237,21 @@ export default function MessageHandler() {
       console.log('error in opening conn.');
       events.close();
 
-      // const data = JSON.parse(e.data);
+      const data = JSON.parse(e.data);
 
-      // errorHandler(data, { ...submission, message });
+      errorHandler(data, { ...submission, message });
       
-      let data;
-      try {
-        data = JSON.parse(e.data);
-        errorHandler(data, { ...submission, message });
-      } catch (err) {
-        console.log('Invalid JSON:', e.data);
-        data = {'error': e.data};
-        if (e.data === 'Unauthorized') {
-          const event = new CustomEvent('logoutEvent');
-          window.dispatchEvent(event);
-        }
+      // let data;
+      //try {
+      //  data = JSON.parse(e.data);
+      //  errorHandler(data, { ...submission, message });
+      //} catch (err) {
+      //  console.log('Invalid JSON:', e.data);
+      //  data = {'error': e.data};
+      //  if (e.data === 'Unauthorized') {
+      //    const event = new CustomEvent('logoutEvent');
+      //    window.dispatchEvent(event);
+      //  }
       }
     };
 
