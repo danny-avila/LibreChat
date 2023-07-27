b465d86fc2ede30a3670ed4d8bef2ff5fa1aacd3
BlitzWrecker <wot.dragon0407@gmail.com>
2023-07-27 13:23:18 -0600

    use store.tabValue for main page pagination

--- client/src/components/ui/Landing.tsx
+++ client/src/components/ui/Landing.tsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Tabs, TabsList, TabsTrigger } from './Tabs';
 import { cn } from '~/utils';
 import Recommendations from './Recommendations';
 import Leaderboard from './Leaderboard';
+import { useRecoilState } from 'recoil';
+
+import store from '~/store';
 
 export default function Landing() {
-  const [tabsValue, setTabsValue] = useState<string>('leaderboard');
+  const [tabValue, setTabValue] = useRecoilState<string>(store.tabValue);
 
   const cardStyle =
     'transition-colors shadow-md rounded-md min-w-[75px] font-normal bg-white border-black/10 hover:border-black/10 focus:border-black/10 dark:border-black/10 dark:hover:border-black/10 dark:focus:border-black/10 border dark:bg-gray-700 text-black dark:text-white';
@@ -25,23 +28,17 @@ export default function Landing() {
         }
       >
         <Tabs
-          value={tabsValue}
+          value={tabValue}
           className={
             cardStyle +
             ' z-50 flex h-[40px] flex-none items-center justify-center px-0 hover:bg-slate-50 dark:hover:bg-gray-600'
           }
-          onValueChange={(value: string) => setTabsValue(value)}
+          onValueChange={(value: string) => setTabValue(value)}
         >
           <TabsList className="bg-white/[.60] dark:bg-gray-700">
             <TabsTrigger
-              value='leaderboard'
-              className={`${tabsValue === 'leaderboard' ? selectedTab('creative') : defaultClasses}`}
-            >
-              {'排行榜'}
-            </TabsTrigger>
-            <TabsTrigger
-              value='recommendations'
-              className={`${tabsValue === 'recommendations' ? selectedTab('fast') : defaultClasses}`}
+              value='recent'
+              className={`${tabValue === 'recent' ? selectedTab('creative') : defaultClasses}`}
             >
               {'对话推荐'}
             </TabsTrigger>
@@ -50,7 +47,7 @@ export default function Landing() {
       </div>
       <div className="flex h-full flex-col items-center overflow-y-auto pt-0 text-sm dark:bg-gray-800">
         <div className="w-full px-6 text-gray-800 dark:text-gray-100 md:flex md:max-w-2xl md:flex-col lg:max-w-3xl">
-          { tabsValue === 'leaderboard' ? <Leaderboard /> : <Recommendations /> }
+          { tabValue === 'leaderboard' ? <Leaderboard /> : <Recommendations /> }
         </div>
       </div>
     </>
