290c8c84e63da8f6a1e3464d48449537c5a3264d
bsu3338 <bsu3338@users.noreply.github.com>
2023-07-28 11:27:02 -0500

    Update index.jsx

--- client/src/components/MessageHandler/index.jsx
+++ client/src/components/MessageHandler/index.jsx
@@ -204,9 +204,7 @@ export default function MessageHandler() {
     const currentTime = Date.now() / 1000; 
     const timeLeft = tokenPayload.exp - currentTime; 
     if (timeLeft < 15) { 
-      setTimeout(() => {
-        window.dispatchEvent(new CustomEvent('attemptRefresh'));
-      }, 2000);
+      window.dispatchEvent(new CustomEvent('attemptRefresh'));
     }
     
     const events = new SSE(server, {
@@ -250,9 +248,21 @@ export default function MessageHandler() {
       console.log('error in opening conn.');
       events.close();
 
-      const data = JSON.parse(e.data);
+      // const data = JSON.parse(e.data);
 
-      errorHandler(data, { ...submission, message });
+      // errorHandler(data, { ...submission, message });
+      let data;
+      try {
+        data = JSON.parse(e.data);
+        errorHandler(data, { ...submission, message });
+      } catch (err) {
+        console.log('Invalid JSON:', e.data);
+        // data = {'error': e.data};
+        if (e.data === 'Unauthorized') {
+          //window.dispatchEvent(new CustomEvent('intercept401'));
+          refreshConversations();
+        }
+      }
     };
 
     setIsSubmitting(true);
