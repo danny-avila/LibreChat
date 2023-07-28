f30df92034965b82b57ab035b93c8644aeb02038
BlitzWrecker <wot.dragon0407@gmail.com>
2023-07-28 14:17:09 -0600

    Added page title

--- client/src/components/ui/Leaderboard.jsx
+++ client/src/components/ui/Leaderboard.jsx
@@ -90,24 +90,34 @@ export default function Leaderboard() {
   }, [getLeaderboardQuery.isSuccess, getLeaderboardQuery.data]);
 
   return (
-    <div className='grid grid-col justify-center'>
-      {/* Example using Grid's API */}
-      <button onClick={buttonListener}>Push Me</button>
-      {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
-      <div className="ag-theme-alpine" style={{ width: 600, height: 500 }}>
-        <AgGridReact
-          ref={gridRef} // Ref for accessing Grid's API
+    <div className="flex h-full flex-col items-center overflow-y-auto pt-0 text-sm dark:bg-gray-800">
+      <h1
+        id="landing-title"
+        className="mb-5 ml-auto mr-auto mt-3 flex items-center justify-center gap-2 text-center text-4xl font-semibold sm:mb-8 md:mt-[5vh]"
+      >
+        邀请排行榜
+      </h1>
+      <div className="w-full px-6 text-gray-800 dark:text-gray-100 md:flex md:max-w-2xl md:flex-col lg:max-w-3xl">
+        <div className='grid grid-col justify-center'>
+          {/* Example using Grid's API */}
+          <button onClick={buttonListener}>Push Me</button>
+          {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
+          <div className="ag-theme-alpine" style={{ width: 600, height: 500 }}>
+            <AgGridReact
+              ref={gridRef} // Ref for accessing Grid's API
 
-          rowData={rowData} // Row Data for Rows
+              rowData={rowData} // Row Data for Rows
 
-          columnDefs={columnDefs} // Column Defs for Columns
-          defaultColDef={defaultColDef} // Default Column Properties
+              columnDefs={columnDefs} // Column Defs for Columns
+              defaultColDef={defaultColDef} // Default Column Properties
 
-          animateRows={true} // Optional - set to 'true' to have rows animate when sorted
-          rowSelection='multiple' // Options - allows click selection of rows
+              animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+              rowSelection='multiple' // Options - allows click selection of rows
 
-          onCellClicked={cellClickedListener} // Optional - registering for Grid Event
-        />
+              onCellClicked={cellClickedListener} // Optional - registering for Grid Event
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
