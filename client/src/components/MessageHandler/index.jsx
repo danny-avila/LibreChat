38e7f84d56afa51850db0f6ecdd2e7c3b86c69d4
bsu3338 <bsu3338@users.noreply.github.com>
2023-07-30 15:39:03 -0500

    Update index.jsx

--- client/src/components/MessageHandler/index.jsx
+++ client/src/components/MessageHandler/index.jsx
@@ -237,11 +237,19 @@ export default function MessageHandler() {
       console.log('error in opening conn.');
       events.close();
 
-      const data = JSON.parse(e.data);
-      console.log('Message Handler Error',data);
-      console.log('Message Handler Current Token',token);
-      if (data.status === 401) {
-        return;
+      // const data = JSON.parse(e.data);
+
+      // errorHandler(data, { ...submission, message });
+      let data;
+      try {
+        data = JSON.parse(e.data);
+        errorHandler(data, { ...submission, message });
+      } catch {
+        console.log('Message Handler Error',data);
+        console.log('Message Handler Current Token',token);
+        if (data.status === 401) {
+          return;
+        }
       }
 
       errorHandler(data, { ...submission, message });
