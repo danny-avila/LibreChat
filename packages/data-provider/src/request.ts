d09d3ddd97d148d66ab318fbbadd5b5267b97b39
bsu3338 <bsu3338@users.noreply.github.com>
2023-07-31 21:19:28 -0500

    Update request.ts

--- packages/data-provider/src/request.ts
+++ packages/data-provider/src/request.ts
@@ -1,4 +1,71 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios, { AxiosRequestConfig, AxiosError } from 'axios';
+import { refreshToken } from './data-service';
+import { setTokenHeader } from './headers-helpers';
+
+let isRefreshing = false;
+let failedQueue: {resolve: (value?: any) => void, reject: (reason?: any) => void}[] = [];
+
+const processQueue = (error: AxiosError | null, token: string | null = null) => {
+  failedQueue.forEach(prom => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+
+  failedQueue = [];
+};
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const originalRequest = error.config;
+    console.log('OriginalRequest URL',originalRequest.url);
+    if (originalRequest.url.includes('/api/auth/refresh')) {
+      window.dispatchEvent(new CustomEvent('logout'));
+      return Promise.reject(error);
+    }
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise(function(resolve, reject) {
+          failedQueue.push({resolve, reject});
+        }).then(token => {
+          originalRequest.headers['Authorization'] = 'Bearer ' + token;
+          return axios(originalRequest);
+        }).catch(err => {
+          return Promise.reject(err);
+        });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      return new Promise(function(resolve, reject) {
+        refreshToken()
+          .then(({token}) => {
+            originalRequest.headers['Authorization'] = 'Bearer ' + token;
+            setTokenHeader(token);
+            window.dispatchEvent(new CustomEvent('tokenUpdated', { detail: token }));
+            processQueue(null, token);
+            resolve(axios(originalRequest));
+          })
+          .catch((err) => {
+            if (error.response.status === 401) {
+              window.dispatchEvent(new CustomEvent('logout'));
+            }
+            processQueue(err, null);
+            reject(err);
+          })
+          .then(() => {
+            isRefreshing = false;
+          });
+      });
+    }
+    return Promise.reject(error);
+  }
+);
 
 async function _get<T>(url: string, options?: AxiosRequestConfig): Promise<T> {
   const response = await axios.get(url, { ...options });
