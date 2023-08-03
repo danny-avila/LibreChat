67ddc2647f5af8bea7651bc93ec3a0ed8ec15b18
bsu3338 <bsu3338@users.noreply.github.com>
2023-08-02 23:58:17 -0500

    Update index.js

--- api/server/index.js
+++ api/server/index.js
@@ -31,7 +31,13 @@ config.validate(); // Validate the config
   await indexSync();
 
   const app = express();
-  app.use('/', refreshTokenValidator);
+  app.use((req, res, next) => {
+    if (req.url === '/api/auth/logout' && req.method === 'POST') {
+      next();
+    } else {
+      refreshTokenValidator(req, res, next);
+    }
+  });
   app.use(errorController);
   app.use(express.json({ limit: '3mb' }));
   app.use(express.urlencoded({ extended: true, limit: '3mb' }));
