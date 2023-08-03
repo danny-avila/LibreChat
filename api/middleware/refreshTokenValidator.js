09fda893b9e173363d1d8f1996bf51cb59c86c66
bsu3338 <bsu3338@users.noreply.github.com>
2023-08-03 00:08:32 -0500

    Update refreshTokenValidator.js

--- api/middleware/refreshTokenValidator.js
+++ api/middleware/refreshTokenValidator.js
@@ -5,7 +5,6 @@ const Session = require('../models/Session');
 const User = require('../models/User');
 const crypto = require('crypto');
 const cookies = require('cookie');
-const isProduction = process.env.NODE_ENV === 'production';
 
 function log({ title, parameters }) {
   DebugControl.log.functionName(title);
@@ -15,30 +14,32 @@ function log({ title, parameters }) {
 }
 
 const refreshTokenValidator = async (req, res, next) => {
-  const refreshToken = req.headers.cookie ? cookies.parse(req.headers.cookie).refreshToken : null;
-  const token = req.headers.cookie ? cookies.parse(req.headers.cookie).token : null;
-  let user;
-  if (refreshToken && !token) {
-    try {
-      const payload = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
-      const userId = payload.id;
-      const user = await User.findOne({ _id: userId });
-      if (user) {
-        // Hash the refresh token
-        const hash = crypto.createHash('sha256');
-        const hashedToken = hash.update(refreshToken).digest('hex');
-        // Find the session with the hashed refresh token
-        const session = await Session.findOne({ user: userId, refreshTokenHash: hashedToken });
-        if (session && session.expiration > new Date()) {
-          const token = await setAuthTokens(userId, res, session._id);
+  if (req.url === '/') {
+    const refreshToken = req.headers.cookie ? cookies.parse(req.headers.cookie).refreshToken : null;
+    const token = req.headers.cookie ? cookies.parse(req.headers.cookie).token : null;
+    let user;
+    if (refreshToken && !token) {
+      try {
+        const payload = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+        const userId = payload.id;
+        const user = await User.findOne({ _id: userId });
+        if (user) {
+          // Hash the refresh token
+          const hash = crypto.createHash('sha256');
+          const hashedToken = hash.update(refreshToken).digest('hex');
+          // Find the session with the hashed refresh token
+          const session = await Session.findOne({ user: userId, refreshTokenHash: hashedToken });
+          if (session && session.expiration > new Date()) {
+            const token = await setAuthTokens(userId, res, session._id);
+          }
         }
+        res.setHeader('Authorization', `Bearer ${token}`);
+      } catch (err) {
+        log({
+          title: '(requireLocalAuth) Error refreshing token',
+          parameters: [{ name: 'error', value: err }],
+        });
       }
-      res.setHeader('Authorization', `Bearer ${token}`);
-    } catch (err) {
-      log({
-        title: '(requireLocalAuth) Error refreshing token',
-        parameters: [{ name: 'error', value: err }],
-      });
     }
   }
   next();
