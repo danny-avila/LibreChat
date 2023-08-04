81239be5fde55d58c68e89dd9b8275f776395f91
Danny Avila <messagedaniel@protonmail.com>
2023-08-04 13:17:04 -0400

    refactor(RowButton, buildTree): convert to TS

--- client/src/utils/buildTree.ts
+++ client/src/utils/buildTree.ts
@@ -1,15 +1,17 @@
+import { TMessage } from 'librechat-data-provider';
+
 const even =
   'w-full border-b border-black/10 dark:border-gray-900/50 text-gray-800 bg-white dark:text-gray-100 group dark:bg-gray-800 hover:bg-gray-100/25 hover:text-gray-700  dark:hover:bg-gray-900 dark:hover:text-gray-200';
 const odd =
   'w-full border-b border-black/10 bg-gray-50 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group bg-gray-100 dark:bg-gray-1000 hover:bg-gray-100/40 hover:text-gray-700 dark:hover:bg-[#3b3d49] dark:hover:text-gray-200';
 
-export default function buildTree(messages, groupAll = false) {
+export default function buildTree(messages: TMessage[] | null, groupAll = false) {
   if (messages === null) {
     return null;
   }
 
-  let messageMap = {};
-  let rootMessages = [];
+  const messageMap: Record<string, TMessage & { children: TMessage[] }> = {};
+  const rootMessages: TMessage[] = [];
 
   if (groupAll) {
     return messages.map((m, idx) => ({ ...m, bg: idx % 2 === 0 ? even : odd }));
