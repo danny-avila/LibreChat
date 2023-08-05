37c0f5bf0dd93d79181ee064c17e13ed41a1e9ce
bsu3338 <bsu3338@users.noreply.github.com>
2023-08-05 09:03:09 -0500

    Update ListeningIcon.tsx

--- client/src/components/svg/ListeningIcon.tsx
+++ client/src/components/svg/ListeningIcon.tsx
@@ -2,23 +2,21 @@ import React from 'react';
 
 export default function ListeningIcon() {
   return (
-    <svg
-      stroke="currentColor"
-      fill="none"
-      strokeWidth="2"
-      viewBox="0 0 24 24"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="mr-1 h-4 w-4"
-      height="1em"
-      width="1em"
-      xmlns="http://www.w3.org/2000/svg"
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      width="24" 
+      height="24" 
+      viewBox="0 0 24 24" 
+      fill="red" 
+      stroke="currentColor" 
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-mic"
     >
-      <rect x="8" y="4" width="8" height="12" stroke="currentColor" fill="currentColor" />
-      <circle cx="12" cy="4" r="4" stroke="currentColor" fill="currentColor" />
-      <rect x="10" y="16" width="4" height="6" stroke="currentColor" fill="currentColor" />
-      <line x1="4" y1="22" x2="20" y2="22" stroke="currentColor" />
-      <circle cx="18" cy="18" r="6" fill="red" />
+      <path d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3Z"/>
+      <path d="M19 10v2a7 7 0 0 1-14 0v-2"/>
+      <line x1="12" x2="12" y1="19" y2="22"/>
     </svg>
   );
 }
