5b3663691937668bcec7abe78f457a44fed0f858
Danny Avila <messagedaniel@protonmail.com>
2023-08-14 09:20:20 -0400

    refactor(validateRegistration.js): remove console.log statement

--- api/middleware/validateRegistration.js
+++ api/middleware/validateRegistration.js
@@ -1,6 +1,5 @@
 function validateRegistration(req, res, next) {
   const setting = process.env.ALLOW_REGISTRATION?.toLowerCase();
-  console.log('validateRegistration setting', setting);
   if (setting === 'true') {
     next();
   } else {
