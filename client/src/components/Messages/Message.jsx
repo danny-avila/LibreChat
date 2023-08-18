91784a0068bbf8992f0ef5caeef89542fba4edb1
Danny Avila <messagedaniel@protonmail.com>
2023-08-18 17:49:16 -0400

    wip(Message.jsx): conversion and use of new component in progress

--- client/src/components/Messages/Message.jsx
+++ client/src/components/Messages/Message.jsx
@@ -1,17 +1,14 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useState, useEffect, useRef } from 'react';
+import { useGetConversationByIdQuery } from 'librechat-data-provider';
+import { useState, useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
 import copy from 'copy-to-clipboard';
-import Plugin from './Plugin';
-import SubRow from './Content/SubRow';
-import Content from './Content/Content';
+import { Plugin, SubRow, MessageContent } from './Content';
 import MultiMessage from './MultiMessage';
 import HoverButtons from './HoverButtons';
 import SiblingSwitch from './SiblingSwitch';
 import { getIcon } from '~/components/Endpoints';
 import { useMessageHandler } from '~/hooks';
-import { useGetConversationByIdQuery } from 'librechat-data-provider';
-import { cn, getError } from '~/utils/';
 import store from '~/store';
 
 export default function Message({
@@ -27,7 +24,6 @@ export default function Message({
   const { text, searchResult, isCreatedByUser, error, submitting, unfinished } = message;
   const setLatestMessage = useSetRecoilState(store.latestMessage);
   const [abortScroll, setAbort] = useState(false);
-  const textEditor = useRef(null);
   const last = !message?.children?.length;
   const edit = message.messageId == currentEditId;
   const { isSubmitting, ask, regenerate, handleContinue } = useMessageHandler();
@@ -86,19 +82,6 @@ export default function Message({
     props.titleclass = message.bg.split(props.className)[1] + ' cursor-pointer';
   }
 
-  const resubmitMessage = () => {
-    const text = textEditor.current.innerText;
-
-    ask({
-      text,
-      parentMessageId: message?.parentMessageId,
-      conversationId: message?.conversationId,
-    });
-
-    setSiblingIdx(siblingCount - 1);
-    enterEdit(true);
-  };
-
   const regenerateMessage = () => {
     if (!isSubmitting && !message?.isCreatedByUser) {
       regenerate(message);
@@ -153,73 +136,35 @@ export default function Message({
             )}
             <div className="flex flex-grow flex-col gap-3">
               {message.plugin && <Plugin plugin={message.plugin} />}
-              {error ? (
-                <div className="flex flex min-h-[20px] flex-grow flex-col items-start gap-2 gap-4  text-red-500">
-                  <div className="rounded-md border border-red-500 bg-red-500/10 px-3 py-2 text-sm text-gray-600 dark:text-gray-100">
-                    {getError(text)}
-                  </div>
-                </div>
-              ) : edit ? (
-                <div className="flex min-h-[20px] flex-grow flex-col items-start gap-4 ">
-                  {/* <div className={`${blinker ? 'result-streaming' : ''} markdown prose dark:prose-invert light w-full break-words`}> */}
-                  <div
-                    className="markdown prose dark:prose-invert light w-full whitespace-pre-wrap break-words border-none focus:outline-none"
-                    contentEditable={true}
-                    ref={textEditor}
-                    suppressContentEditableWarning={true}
-                  >
-                    {text}
-                  </div>
-                  <div className="mt-2 flex w-full justify-center text-center">
-                    <button
-                      className="btn btn-primary relative mr-2"
-                      disabled={isSubmitting}
-                      onClick={resubmitMessage}
-                    >
-                      Save & Submit
-                    </button>
-                    <button className="btn btn-neutral relative" onClick={() => enterEdit(true)}>
-                      Cancel
-                    </button>
-                  </div>
-                </div>
-              ) : (
-                <>
-                  <div
-                    className={cn(
-                      'flex min-h-[20px] flex-grow flex-col items-start gap-4 ',
-                      isCreatedByUser ? 'whitespace-pre-wrap' : '',
-                    )}
-                  >
-                    {/* <div className={`${blinker ? 'result-streaming' : ''} markdown prose dark:prose-invert light w-full break-words`}> */}
-                    <div className="markdown prose dark:prose-invert light w-full break-words">
-                      {!isCreatedByUser ? (
-                        <>
-                          <Content content={text} message={message} />
-                        </>
-                      ) : (
-                        <>{text}</>
-                      )}
-                    </div>
-                  </div>
-                  {/* {!isSubmitting && cancelled ? (
-                    <div className="flex flex min-h-[20px] flex-grow flex-col items-start gap-2 gap-4  text-red-500">
-                      <div className="rounded-md border border-blue-400 bg-blue-500/10 px-3 py-2 text-sm text-gray-600 dark:text-gray-100">
-                        {`This is a cancelled message.`}
-                      </div>
-                    </div>
-                  ) : null} */}
-                  {!isSubmitting && unfinished ? (
-                    <div className="flex flex min-h-[20px] flex-grow flex-col items-start gap-2 gap-4  text-red-500">
-                      <div className="rounded-md border border-blue-400 bg-blue-500/10 px-3 py-2 text-sm text-gray-600 dark:text-gray-100">
-                        {
-                          'This is an unfinished message. The AI may still be generating a response, it was aborted, or a censor was triggered. Refresh or visit later to see more updates.'
-                        }
-                      </div>
-                    </div>
-                  ) : null}
-                </>
-              )}
+              {/* Message content goes here */}
+              {/*
+              type TInitialProps = {
+                text: string;
+                edit: boolean;
+                error: boolean;
+                unfinished: boolean;
+                isSubmitting: boolean;
+              };
+              type TAdditionalProps = {
+                ask: TAskFunction;
+                message: TMessage;
+                isCreatedByUser: boolean;
+                enterEdit: (cancel: boolean) => void;
+                setSiblingIdx: () => void;
+              };
+              */}
+              <MessageContent
+                ask={ask}
+                text={text}
+                edit={edit}
+                error={error}
+                message={message}
+                enterEdit={enterEdit}
+                unfinished={unfinished}
+                isSubmitting={isSubmitting}
+                isCreatedByUser={isCreatedByUser}
+                setSiblingIdx={() => setSiblingIdx(siblingIdx - 1)}
+              />
             </div>
             <HoverButtons
               isEditing={edit}
