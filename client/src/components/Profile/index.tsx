3354f71a677cc515f61ec5c99d4a59272d1fe79b
BlitzWrecker <wot.dragon0407@gmail.com>
2023-08-21 14:19:30 -0600

    added copy profile link button

--- client/src/components/Profile/index.tsx
+++ client/src/components/Profile/index.tsx
@@ -15,6 +15,7 @@ import { Spinner } from '../svg';
 function Profile() {
   const [tabValue, setTabValue] = useState<string>('');
   const [profileUser, setProfileUser] = useState<TUser | null>(null);
+  const [copied, setCopied] = useState<boolean>(false);
 
   const { userId } = useParams();
   const { user, token } = useAuthContext();
@@ -52,7 +53,7 @@ function Profile() {
 
   return (
     <div className='flex flex-col h-full justify-center md:mx-36'>
-      <div className='flex flex-row my-12'>
+      <div className='flex flex-row flex-wrap mt-6 mb-3 md:my-12'>
         <div
           title='User Icon'
           className='relative flex items-center justify-center my-1 mx-4 md:my-3 md:mx-12'
@@ -81,6 +82,55 @@ function Profile() {
               Followers
             </button>
           </div> */}
+        </div>
+        <div className='flex flex-row ml-5 py-3 text-xl gap-4 items-center md:ml-9'>
+          <button
+            className='w-32 text-gray-600 hover:text-black'
+            onClick={() => {
+              if (copied) return;
+              setCopied(true);
+              window.navigator.clipboard.writeText(window.location.href);
+              setTimeout(() => setCopied(false), 2000);
+            }}
+          >
+            <div className='flex flex-col items-center'>
+              {copied ? (
+                <>
+                  <svg
+                    stroke="currentColor"
+                    fill="none"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="h-5 w-5"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <polyline points="20 6 9 17 4 12" />
+                  </svg>
+                  {localize(lang, 'com_ui_copy_success')}
+                </>
+              ) : (
+                <>
+                  <svg className="h-5 w-5" width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g id="Communication / Share_iOS_Export">
+                      <path id="Vector" d="M9 6L12 3M12 3L15 6M12 3V13M7.00023 10C6.06835 10 5.60241 10 5.23486 10.1522C4.74481 10.3552 4.35523 10.7448 4.15224 11.2349C4 11.6024 4 12.0681 4 13V17.8C4 18.9201 4 19.4798 4.21799 19.9076C4.40973 20.2839 4.71547 20.5905 5.0918 20.7822C5.5192 21 6.07899 21 7.19691 21H16.8036C17.9215 21 18.4805 21 18.9079 20.7822C19.2842 20.5905 19.5905 20.2839 19.7822 19.9076C20 19.4802 20 18.921 20 17.8031V13C20 12.0681 19.9999 11.6024 19.8477 11.2349C19.6447 10.7448 19.2554 10.3552 18.7654 10.1522C18.3978 10 17.9319 10 17 10" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                    </g>
+                  </svg>
+                  {localize(lang, 'com_ui_share_profile')}
+                </>
+
+              )}
+            </div>
+          </button>
+          {/* <button className='w-24 text-start text-gray-600 hover:text-black'>
+            {localize(lang, 'com_ui_followers')}
+          </button>
+          <button className='w-24 text-start text-gray-600 hover:text-black'>
+            {localize(lang, 'com_ui_following')}
+          </button> */}
         </div>
       </div>
       <hr />
