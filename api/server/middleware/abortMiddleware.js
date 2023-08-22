564087b593a767c4693338d6684d73f7d6fb3c7f
Daniel Avila <messagedaniel@protonmail.com>
2023-08-21 23:19:56 -0400

    fix(abortMiddleware.js): fix condition to check partialText length chore(abortMiddleware.js): add error logging when abortMessage fails

--- api/server/middleware/abortMiddleware.js
+++ api/server/middleware/abortMiddleware.js
@@ -89,10 +89,11 @@ const handleAbortError = async (res, req, error, data) => {
     handleError(res, errorMessage);
   };
 
-  if (partialText?.length > 2) {
+  if (partialText && partialText.length > 5) {
     try {
       return await abortMessage(req, res);
     } catch (err) {
+      console.error(err);
       return respondWithError();
     }
   } else {
