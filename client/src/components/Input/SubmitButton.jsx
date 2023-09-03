6583877cb32c96f6746eaa213f8db0924faad89e
bsu3338 <bsu3338@users.noreply.github.com>
2023-09-03 17:53:18 -0500

    Update SubmitButton.jsx

--- client/src/components/Input/SubmitButton.jsx
+++ client/src/components/Input/SubmitButton.jsx
@@ -82,7 +82,7 @@ export default function SubmitButton({
     return (
       <button 
         className="group absolute bottom-0 right-0 z-[101] flex h-[100%] w-[50px] items-center justify-center bg-transparent p-1 text-gray-500"
-        disabled=true
+        disabled="true"
       >
         <div className="m-1 mr-0 rounded-md pb-[9px] pl-[9.5px] pr-[7px] pt-[11px] group-hover:bg-gray-100 group-disabled:hover:bg-transparent dark:group-hover:bg-gray-900 dark:group-hover:text-gray-400 dark:group-disabled:hover:bg-transparent">
           {countdown > 0 ? (
