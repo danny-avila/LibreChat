3afbda504bdded97e473140c9c709c5f77e00ca8
BlitzWrecker <wot.dragon0407@gmail.com>
2023-09-04 10:26:22 -0600

    checks last selected and browser language first

--- client/src/store/language.js
+++ client/src/store/language.js
@@ -1,8 +1,11 @@
 import { atom } from 'recoil';
 
+// Check for language in localStorage first
+// If no last selected language, set language to Chinese if browser language is Chinese
+// Set language to English otherwise
 const lang = atom({
   key: 'lang',
-  default: 'en',
+  default: window.localStorage.getItem('lang') || (navigator.languages[0] === 'zh-CN' ? 'cn' : 'en'),
 });
 
 export default { lang };
