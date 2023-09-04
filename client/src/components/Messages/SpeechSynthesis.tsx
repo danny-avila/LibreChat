c5ce576fb85973f20bffeb8d7eab3f3e3f626b9f
bsu3338 <bsu3338@users.noreply.github.com>
2023-09-03 22:13:20 -0500

    Update SpeechSynthesis.tsx

--- client/src/components/Messages/SpeechSynthesis.tsx
+++ client/src/components/Messages/SpeechSynthesis.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
 
 function useSpeechSynthesis() {
   //const [isSpeechEnabled, setIsSpeechEnabled] = useState(false);
