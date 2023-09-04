61335317377da07a095a6a821dcdffe31552c976
bsu3338 <bsu3338@users.noreply.github.com>
2023-09-03 21:29:54 -0500

    Update VolumeIcon.tsx

--- client/src/components/svg/VolumeIcon.tsx
+++ client/src/components/svg/VolumeIcon.tsx
@@ -4,15 +4,15 @@ export default function VolumeIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="1em"
+      height="1em"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
-      class="lucide lucide-volume-2"
+      className="h-4 w-4"
     >
       <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"/>
       <path d="M15.54 8.46a5 5 0 0 1 0 7.07"/>
