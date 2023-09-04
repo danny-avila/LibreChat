d5deb546ba59a5d701e08f37437ab480ee5e5419
BlitzWrecker <wot.dragon0407@gmail.com>
2023-09-04 10:22:34 -0600

    reduced responsibility: saves last selected language to localStorage

--- client/src/utils/setLang.jsx
+++ client/src/utils/setLang.jsx
@@ -1,9 +1,14 @@
-import { useSetRecoilState } from 'recoil';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
 import store from '~/store';
 
+// Automatically saves last selected language to localStorage
 export default function SetLanguage() {
-  const setLang = useSetRecoilState(store.lang);
-  if (navigator.languages[0] === 'zh-CN') setLang('cn');
+  const [lang] = useRecoilState(store.lang);
+
+  useEffect(() => {
+    window.localStorage.setItem('lang', lang);
+  }, [lang]);
 
   return null;
 }
