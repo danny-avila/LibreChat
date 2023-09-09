9fcf21c83737b44c0ec78d239330035de5b0b1fa
Daniel Avila <messagedaniel@protonmail.com>
2023-09-09 16:36:47 -0400

    fix: open handles in unit tests, remove KeyvMongo as not used and properly mock global fetch

--- api/cache/index.js
+++ api/cache/index.js
@@ -1,7 +1,6 @@
-const keyvMongo = require('./keyvMongo');
 const keyvFiles = require('./keyvFiles');
 const getLogStores = require('./getLogStores');
 const logViolation = require('./logViolation');
 const clearPendingReq = require('./clearPendingReq');
 
-module.exports = { keyvMongo, ...keyvFiles, getLogStores, logViolation, clearPendingReq };
+module.exports = { ...keyvFiles, getLogStores, logViolation, clearPendingReq };
