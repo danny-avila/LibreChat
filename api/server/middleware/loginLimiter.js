3a98fab0a29a9e4c4c2329885ecc72ef17504a61
Daniel Avila <messagedaniel@protonmail.com>
2023-09-09 12:51:47 -0400

    feat: add logging to login limiter

--- api/server/middleware/loginLimiter.js
+++ api/server/middleware/loginLimiter.js
@@ -1,12 +1,29 @@
 const rateLimit = require('express-rate-limit');
+const { logViolation } = require('../../cache');
+
+const type = 'logins';
+
 const windowMs = (process.env?.LOGIN_WINDOW ?? 5) * 60 * 1000; // default: 5 minutes
 const max = process.env?.LOGIN_MAX ?? 7; // default: limit each IP to 7 requests per windowMs
 const windowInMinutes = windowMs / 60000;
+
+const handler = async (req) => {
+  const userId = req.user.id;
+
+  const errorMessage = {
+    type,
+    max,
+    windowInMinutes,
+  };
+
+  await logViolation(type, userId, errorMessage);
+};
 
 const loginLimiter = rateLimit({
   windowMs,
   max,
   message: `Too many login attempts from this IP, please try again after ${windowInMinutes} minutes.`,
+  handler,
   keyGenerator: function (req) {
     // Strip out the port number from the IP address
     return req.ip.replace(/:\d+[^:]*$/, '');
