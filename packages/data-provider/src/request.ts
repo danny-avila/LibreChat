d318574860164a5ad6bf08b978bb3e85906703d4
Danny Avila <messagedaniel@protonmail.com>
2023-09-10 21:19:39 -0400

    feat(data-provider): refresh token logic

--- packages/data-provider/src/request.ts
+++ packages/data-provider/src/request.ts
@@ -1,4 +1,69 @@
-import axios, { AxiosRequestConfig } from 'axios';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios, { AxiosRequestConfig, AxiosError } from 'axios';
+// eslint-disable-next-line import/no-cycle
+import { refreshToken } from './data-service';
+import { setTokenHeader } from './headers-helpers';
+
+let isRefreshing = false;
+let failedQueue: { resolve: (value?: any) => void; reject: (reason?: any) => void }[] = [];
+
+const processQueue = (error: AxiosError | null, token: string | null = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise(function (resolve, reject) {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers['Authorization'] = 'Bearer ' + token;
+            return axios(originalRequest);
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      return new Promise(function (resolve, reject) {
+        refreshToken()
+          .then(({ token }) => {
+            if (token) {
+              originalRequest.headers['Authorization'] = 'Bearer ' + token;
+              setTokenHeader(token);
+              window.dispatchEvent(new CustomEvent('tokenUpdated', { detail: token }));
+              processQueue(null, token);
+              resolve(axios(originalRequest));
+            } else {
+              window.location.href = '/login';
+            }
+          })
+          .catch((err) => {
+            processQueue(err, null);
+            reject(err);
+          })
+          .then(() => {
+            isRefreshing = false;
+          });
+      });
+    }
+    return Promise.reject(error);
+  },
+);
 
 async function _get<T>(url: string, options?: AxiosRequestConfig): Promise<T> {
   const response = await axios.get(url, { ...options });
