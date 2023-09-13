b83d0205cffd560876be56ea23d0c1c8b4c76f7d
yinglj <yinglj@gmail.com>
2023-09-13 10:40:55 +0800

    optimize default lang

--- client/src/utils/setLang.jsx
+++ client/src/utils/setLang.jsx
@@ -2,13 +2,22 @@ import { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 import store from '~/store';
 
-// Automatically saves last selected language to localStorage
 export default function SetLanguage() {
   const [lang] = useRecoilState(store.lang);
 
+  const languageCode =
+    lang ||
+    (navigator.languages.length > 1
+      ? navigator.languages[0].startsWith('zh')
+        ? 'cn'
+        : navigator.languages[0].substring(0, 2)
+      : 'cn');
+
   useEffect(() => {
-    window.localStorage.setItem('lang', lang);
-  }, [lang]);
+    if (languageCode) {
+      window.localStorage.setItem('lang', languageCode);
+    }
+  }, [lang, languageCode]);
 
   return null;
 }
