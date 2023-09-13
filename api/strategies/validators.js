fc3de168b5fed449aadbf41d674512a6d5850c3b
Danny Avila <messagedaniel@protonmail.com>
2023-09-13 16:51:01 -0400

    chore: replace joi with zod and remove unused user validator

--- api/strategies/validators.js
+++ api/strategies/validators.js
@@ -1,24 +1,36 @@
-const Joi = require('joi');
+const { z } = require('zod');
 
-const loginSchema = Joi.object().keys({
-  email: Joi.string().trim().email().required(),
-  password: Joi.string().trim().min(8).max(128).required(),
+function errorsToString(errors) {
+  return errors
+    .map((error) => {
+      let field = error.path.join('.');
+      let message = error.message;
+
+      return `${field}: ${message}`;
+    })
+    .join(' ');
+}
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8).max(128),
 });
 
-const registerSchema = Joi.object().keys({
-  name: Joi.string().trim().min(3).max(80).required(),
-  username: Joi.string()
-    .trim()
-    .allow('')
+const registerSchema = z.object({
+  name: z.string().min(3).max(80),
+  username: z
+    .string()
     .min(2)
     .max(80)
-    .regex(/^[a-zA-Z0-9_.-@#$%&*() ]+$/),
-  email: Joi.string().trim().email().required(),
-  password: Joi.string().trim().min(8).max(128).required(),
-  confirm_password: Joi.string().trim().min(8).max(128).required(),
+    .regex(/^[a-zA-Z0-9_.-@#$%&*() ]+$/)
+    .optional(),
+  email: z.string().email(),
+  password: z.string().min(8).max(128),
+  confirm_password: z.string().min(8).max(128),
 });
 
 module.exports = {
   loginSchema,
   registerSchema,
+  errorsToString,
 };
