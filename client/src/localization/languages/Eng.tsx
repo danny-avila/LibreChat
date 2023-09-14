ccb46164c0f466df2a9d9b7ae1643c54a8789fe1
Marcus Nätteldal <yohasse@outlook.com>
2023-09-15 01:46:06 +0200

    🇸🇪: Swedish Translation (#940)
    * Language translation: swedish translation
    
    * fix: remove unwanted row in Sv translation
    
    remove com_nav_language
    
    ---------
    
    Co-authored-by: Marcus Nätteldal <marcus.natteldal@ltu.se>

--- client/src/localization/languages/Eng.tsx
+++ client/src/localization/languages/Eng.tsx
@@ -265,4 +265,5 @@ export default {
   com_nav_lang_brazilian_portuguese: 'Português Brasileiro',
   com_nav_lang_russian: 'Русский',
   com_nav_lang_japanese: '日本語',
+  com_nav_lang_swedish: 'Svenska',
 };
