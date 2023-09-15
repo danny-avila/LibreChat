cc4270eca53d13acf74a8fd18c01cb80fdff1b5a
Danny Avila <messagedaniel@protonmail.com>
2023-09-15 11:47:36 -0400

    chore: remove requireJwtAuth from /api/endpoints and /api/models as not implemented yet

--- api/server/routes/endpoints.js
+++ api/server/routes/endpoints.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const endpointController = require('../controllers/EndpointController');
-const { requireJwtAuth } = require('../middleware');
 
-router.get('/', requireJwtAuth, endpointController);
+router.get('/', endpointController);
 
 module.exports = router;
--- api/server/routes/models.js
+++ api/server/routes/models.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const modelController = require('../controllers/ModelController');
-const { requireJwtAuth } = require('../middleware');
 
-router.get('/', requireJwtAuth, modelController);
+router.get('/', modelController);
 
 module.exports = router;
