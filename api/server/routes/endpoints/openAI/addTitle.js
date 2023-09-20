b1eb48349fcdba0b89de2e8613c5929b5ef936a5
Daniel Avila <messagedaniel@protonmail.com>
2023-09-20 18:27:51 -0400

    feat: add option to disable titling as well as decide what model to use for OpenAI titling refactor: truncate conversation text so it caps around 200 tokens for titling requests, optimize some of the title prompts

--- api/server/routes/endpoints/openAI/addTitle.js
+++ api/server/routes/endpoints/openAI/addTitle.js
@@ -1,6 +1,12 @@
+const { isEnabled } = require('../../../utils');
 const { saveConvo } = require('../../../../models');
 
 const addTitle = async (req, { text, response, client }) => {
+  const { TITLE_CONVO = 'true' } = process.env ?? {};
+  if (!isEnabled(TITLE_CONVO)) {
+    return;
+  }
+
   const title = await client.titleConvo({ text, responseText: response?.text });
   await saveConvo(req.user.id, {
     conversationId: response.conversationId,
