559093baf2e00df7572644d1369ec10a3eee0ab3
Marco Beretta <81851188+Berry-13@users.noreply.github.com>
2023-09-24 01:26:23 +0200

    login logs and output

--- api/strategies/localStrategy.js
+++ api/strategies/localStrategy.js
@@ -1,7 +1,17 @@
 const { Strategy: PassportLocalStrategy } = require('passport-local');
 const User = require('../models/User');
 const { loginSchema, errorsToString } = require('./validators');
-const DebugControl = require('../utils/debug.js');
+const winston = require('winston');
+
+// Configure Winston logger
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'login-logs.log' }),
+  ],
+});
 
 async function validateLoginRequest(req) {
   const { error } = loginSchema.safeParse(req.body);
@@ -28,21 +38,25 @@ async function passportLogin(req, email, password, done) {
     const validationError = await validateLoginRequest(req);
     if (validationError) {
       logError('Passport Local Strategy - Validation Error', { reqBody: req.body });
+      logger.error(`[Login] [Login failed] [Username: ${email}] [Request-IP: ${req.ip}]`);
       return done(null, false, { message: validationError });
     }
 
     const user = await findUserByEmail(email);
     if (!user) {
       logError('Passport Local Strategy - User Not Found', { email });
+      logger.error(`[Login] [Login failed] [Username: ${email}] [Request-IP: ${req.ip}]`);
       return done(null, false, { message: 'Email does not exist.' });
     }
 
     const isMatch = await comparePassword(user, password);
     if (!isMatch) {
       logError('Passport Local Strategy - Password does not match', { isMatch });
+      logger.error(`[Login] [Login failed] [Username: ${email}] [Request-IP: ${req.ip}]`);
       return done(null, false, { message: 'Incorrect password.' });
     }
 
+    logger.info(`[Login] [Login successful] [Username: ${email}] [Request-IP: ${req.ip}]`);
     return done(null, user);
   } catch (err) {
     return done(err);
@@ -51,10 +65,7 @@ async function passportLogin(req, email, password, done) {
 
 function logError(title, parameters) {
   const entries = Object.entries(parameters).map(([name, value]) => ({ name, value }));
-  DebugControl.log.functionName(title);
-  if (entries) {
-    DebugControl.log.parameters(entries);
-  }
+  logger.error(title, { parameters: entries });
 }
 
 module.exports = () =>
