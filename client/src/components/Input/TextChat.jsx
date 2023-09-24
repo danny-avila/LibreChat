19f13ae431bf72631baa6b5db7dd7390578bec06
Danny Avila <messagedaniel@protonmail.com>
2023-09-24 19:51:48 -0400

    fix(TextChat): allow space for scrollbar in gradient block

--- client/src/components/Input/TextChat.jsx
+++ client/src/components/Input/TextChat.jsx
@@ -127,7 +127,7 @@ export default function TextChat({ isSearchView = false }) {
   return (
     <>
       <div
-        className="no-gradient-sm fixed bottom-0 left-0 w-full pt-6 sm:bg-gradient-to-b md:absolute"
+        className="no-gradient-sm fixed bottom-0 left-0 w-full pt-6 sm:bg-gradient-to-b md:absolute md:w-[calc(100%-.5rem)]"
         style={{
           background: `linear-gradient(to bottom, 
                 ${isDark ? 'rgba(52, 53, 65, 0)' : 'rgba(255, 255, 255, 0)'}, 
