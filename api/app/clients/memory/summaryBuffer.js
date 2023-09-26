7f2f8fce5b20ff59c023a8bfe40496925d7a25ea
Danny Avila <messagedaniel@protonmail.com>
2023-09-26 18:55:23 -0400

    refactor(summaryBuffer): use custom summary prompt, allow prompt to be passed, pass humanPrefix and aiPrefix to memory, along with any future variables, rename messagesToRefine to context

--- api/app/clients/memory/summaryBuffer.js
+++ api/app/clients/memory/summaryBuffer.js
@@ -1,32 +1,45 @@
 const { ConversationSummaryBufferMemory, ChatMessageHistory } = require('langchain/memory');
-const { formatLangChainMessages } = require('../prompts');
+const { formatLangChainMessages, SUMMARY_PROMPT } = require('../prompts');
 
-const createSummaryBufferMemory = ({ llm, messages }) => {
+const createSummaryBufferMemory = ({ llm, prompt, messages, ...rest }) => {
   const chatHistory = new ChatMessageHistory(messages);
   return new ConversationSummaryBufferMemory({
     llm,
+    prompt,
     chatHistory,
     returnMessages: true,
+    ...rest,
   });
 };
 
 const summaryBuffer = async ({
   llm,
   debug,
-  messagesToRefine,
+  context, // array of messages
   formatOptions = {},
   previous_summary = '',
+  prompt = SUMMARY_PROMPT,
 }) => {
   if (debug && previous_summary) {
     console.log('<-----------PREVIOUS SUMMARY----------->\n\n');
     console.log(previous_summary);
   }
 
-  const formattedMessages = formatLangChainMessages(messagesToRefine, formatOptions);
-  const chatPromptMemory = createSummaryBufferMemory({
+  const formattedMessages = formatLangChainMessages(context, formatOptions);
+  const memoryOptions = {
     llm,
+    prompt,
     messages: formattedMessages,
-  });
+  };
+
+  if (formatOptions.userName) {
+    memoryOptions.humanPrefix = formatOptions.userName;
+  }
+  if (formatOptions.userName) {
+    memoryOptions.aiPrefix = formatOptions.assistantName;
+  }
+
+  const chatPromptMemory = createSummaryBufferMemory(memoryOptions);
 
   const messages = await chatPromptMemory.chatHistory.getMessages();
 
