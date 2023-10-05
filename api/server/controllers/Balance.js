401f214cc8e76ddce513f393acf8f0567456ada9
Danny Avila <messagedaniel@protonmail.com>
2023-10-05 16:13:40 -0400

    feat(server): add balance route

---
+++ api/server/controllers/Balance.js
@@ -0,0 +1,11 @@
+const Balance = require('../../models/Balance');
+
+async function balanceController(req, res) {
+  const { tokenCredits: balance } = await Balance.findOne(
+    { user: req.user.id },
+    'tokenCredits',
+  ).lean();
+  res.status(200).send('' + balance);
+}
+
+module.exports = balanceController;
