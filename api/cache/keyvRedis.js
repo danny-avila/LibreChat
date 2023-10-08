8445876f9108e2cc0f5dd5bd270e66ba12f19a1f
Danny Avila <messagedaniel@protonmail.com>
2023-10-07 20:40:26 -0400

    feat: keyvRedis; chore: bump keyv, bun.lockb, add jsconfig for vscode file resolution

---
+++ api/cache/keyvRedis.js
@@ -0,0 +1,8 @@
+const KeyvRedis = require('@keyv/redis');
+const { REDIS_URI } = process.env ?? {};
+
+const keyvRedis = new KeyvRedis(REDIS_URI, { useRedisSets: false });
+
+keyvRedis.on('error', (err) => console.error('KeyvRedis connection error:', err));
+
+module.exports = keyvRedis;
