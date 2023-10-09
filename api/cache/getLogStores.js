9b90b742742c4f2bbece4cf680e0d627a9f47639
Danny Avila <messagedaniel@protonmail.com>
2023-10-09 16:15:47 -0400

    feat: redis support for violation logs

--- api/cache/getLogStores.js
+++ api/cache/getLogStores.js
@@ -1,26 +1,33 @@
 const Keyv = require('keyv');
 const keyvMongo = require('./keyvMongo');
-const { math } = require('../server/utils');
+const keyvRedis = require('./keyvRedis');
+const { math, isEnabled } = require('../server/utils');
 const { logFile, violationFile } = require('./keyvFiles');
 const { BAN_DURATION } = process.env ?? {};
 
 const duration = math(BAN_DURATION, 7200000);
 
+const createViolationInstance = (namespace) => {
+  const config = isEnabled(process.env.USE_REDIS)
+    ? { store: keyvRedis }
+    : { store: violationFile, namespace };
+  return new Keyv(config);
+};
+
 const namespaces = {
-  ban: new Keyv({ store: keyvMongo, ttl: duration, namespace: 'bans' }),
+  ban: new Keyv({ store: keyvMongo, namespace: 'bans', duration }),
   general: new Keyv({ store: logFile, namespace: 'violations' }),
-  concurrent: new Keyv({ store: violationFile, namespace: 'concurrent' }),
-  non_browser: new Keyv({ store: violationFile, namespace: 'non_browser' }),
-  message_limit: new Keyv({ store: violationFile, namespace: 'message_limit' }),
-  token_balance: new Keyv({ store: violationFile, namespace: 'token_balance' }),
-  registrations: new Keyv({ store: violationFile, namespace: 'registrations' }),
-  logins: new Keyv({ store: violationFile, namespace: 'logins' }),
+  concurrent: createViolationInstance('concurrent'),
+  non_browser: createViolationInstance('non_browser'),
+  message_limit: createViolationInstance('message_limit'),
+  token_balance: createViolationInstance('token_balance'),
+  registrations: createViolationInstance('registrations'),
+  logins: createViolationInstance('logins'),
 };
 
 /**
- * Returns either the logs of violations specified by type if a type is provided
- * or it returns the general log if no type is specified. If an invalid type is passed,
- * an error will be thrown.
+ * Returns the keyv cache specified by type.
+ * If an invalid type is passed, an error will be thrown.
  *
  * @module getLogStores
  * @requires keyv - a simple key-value storage that allows you to easily switch out storage adapters.
@@ -31,11 +38,10 @@ const namespaces = {
  * @throws Will throw an error if an invalid violation type is passed.
  */
 const getLogStores = (type) => {
-  if (!type) {
+  if (!type || !namespaces[type]) {
     throw new Error(`Invalid store type: ${type}`);
   }
-  const logs = namespaces[type];
-  return logs;
+  return namespaces[type];
 };
 
 module.exports = getLogStores;
