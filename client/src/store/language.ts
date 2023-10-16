b23166d6beef5ca2a16e18906985231e2c0cb945
Marco Beretta <81851188+Berry-13@users.noreply.github.com>
2023-10-16 19:36:06 +0200

    fix(language) set auto as default language (#1061)

--- client/src/store/language.ts
+++ client/src/store/language.ts
@@ -1,8 +1,10 @@
 import { atom } from 'recoil';
 
+const userLang = navigator.language || navigator.languages[0];
+
 const lang = atom({
   key: 'lang',
-  default: localStorage.getItem('lang') || 'en-US',
+  default: localStorage.getItem('lang') || userLang,
 });
 
 export default { lang };
