8306165160dfb2ee6ae682be5a35b4a83511b610
liukaixiang817 <78783855+liukaixiang817@users.noreply.github.com>
2023-10-23 23:44:24 +1000

    Revert "12345"
    This reverts commit 70483dbb483173b9d608a85ce17b4695a98ab6eb.

--- client/src/localization/Translation.tsx
+++ client/src/localization/Translation.tsx
@@ -49,7 +49,7 @@ if (!String.prototype.format) {
 // input: language code in string
 // returns an object of translated strings in the language
 export const getTranslations = (langCode: string) => {
-  const language = languageMap[langCode] || Chinese;
+  const language = languageMap[langCode] || English;
   return language;
 };
 
