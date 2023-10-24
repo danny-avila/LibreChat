b72457bd8a862ed48993f7d06670943b33ff1254
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2023-10-23 21:08:18 -0400

    feat: add CUSTOM_FOOTER env variable (#1098)

--- client/src/components/Input/Footer.tsx
+++ client/src/components/Input/Footer.tsx
@@ -8,15 +8,21 @@ export default function Footer() {
 
   return (
     <div className="hidden px-3 pb-1 pt-2 text-center text-xs text-black/50 dark:text-white/50 md:block md:px-4 md:pb-4 md:pt-3">
-      <a
-        href="https://github.com/danny-avila/LibreChat"
-        target="_blank"
-        rel="noreferrer"
-        className="underline"
-      >
-        {config?.appTitle || 'LibreChat'} v0.6.0
-      </a>
-      {' - '}. {localize('com_ui_pay_per_call')}
+      {typeof config?.customFooter === 'string' ? (
+        config.customFooter
+      ) : (
+        <>
+          <a
+            href="https://github.com/danny-avila/LibreChat"
+            target="_blank"
+            rel="noreferrer"
+            className="underline"
+          >
+            {config?.appTitle || 'LibreChat'} v0.6.0
+          </a>
+          {' - '}. {localize('com_ui_pay_per_call')}
+        </>
+      )}
     </div>
   );
 }
