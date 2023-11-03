017cf0ea8bab390a4a6d4bd1e9fe3a2e72698dbd
Marco Beretta <81851188+Berry-13@users.noreply.github.com>
2023-11-03 17:51:41 +0100

    added duration

--- config/ban-user.js
+++ config/ban-user.js
@@ -4,9 +4,27 @@ const banViolation = require('../api/cache/banViolation');
 const User = require('@librechat/backend/models/User');
 
 (async () => {
+  /**
+   * Connect to the database
+   * - If it takes a while, we'll warn the user
+   */
+  // Warn the user if this is taking a while
+  let timeout = setTimeout(() => {
+    console.orange(
+      'This is taking a while... You may need to check your connection if this fails.',
+    );
+    timeout = setTimeout(() => {
+      console.orange('Still going... Might as well assume the connection failed...');
+      timeout = setTimeout(() => {
+        console.orange('Error incoming in 3... 2... 1...');
+      }, 13000);
+    }, 10000);
+  }, 5000);
+  // Attempt to connect to the database
   try {
-    console.orange('Connecting to the database...');
+    console.orange('Warming up the engines...');
     await connectDb();
+    clearTimeout(timeout);
   } catch (e) {
     console.error(e);
     silentExit(1);
@@ -17,17 +35,30 @@ const User = require('@librechat/backend/models/User');
   console.purple('---------------------');
 
   let email = '';
+  let duration = '';
 
-  if (process.argv.length >= 3) {
+  if (process.argv.length >= 4) {
+    // Check if there are enough command-line arguments.
     email = process.argv[2];
+    duration = parseInt(process.argv[3]); // Parse the duration as an integer.
   } else {
-    console.orange('Usage: npm run ban-user <email>');
+    console.orange('Usage: npm run ban-user <email> <duration>');
     console.orange('Note: if you do not pass in the arguments, you will be prompted for them.');
     console.purple('--------------------------');
   }
 
   if (!email) {
     email = await askQuestion('Email:');
+  }
+
+  if (!duration) {
+    const durationInMinutes = await askQuestion('Duration (in minutes):');
+    duration = parseInt(durationInMinutes) * 60000;
+  }
+
+  if (isNaN(duration) || duration <= 0) {
+    console.red('Error: Invalid duration!');
+    silentExit(1);
   }
 
   if (!email.includes('@')) {
@@ -59,7 +90,7 @@ const User = require('@librechat/backend/models/User');
     violation_count: 20,
     user_id: user._id,
     prev_count: 0,
-    duration: 1000 * 60 * 60 * 24 * 7,
+    duration: duration,
   };
 
   await banViolation(req, res, errorMessage);
