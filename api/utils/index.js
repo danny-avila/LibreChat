5ab9802aa912ef2e0d5768e93ee2d943c8de9f51
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2023-11-09 14:04:36 -0500

    fix(OpenAIClient): use official SDK to identify client and avoid false Rate Limit Error (#1161)
    * chore: add eslint ignore unused var pattern
    
    * feat: add extractBaseURL helper for valid OpenAI reverse proxies, with tests
    
    * feat(OpenAIClient): add new chatCompletion using official OpenAI node SDK
    
    * fix(ci): revert change to FORCE_PROMPT condition

--- api/utils/index.js
+++ api/utils/index.js
@@ -1,9 +1,11 @@
-const azureUtils = require('./azureUtils');
 const tokenHelpers = require('./tokens');
+const azureUtils = require('./azureUtils');
+const extractBaseURL = require('./extractBaseURL');
 const findMessageContent = require('./findMessageContent');
 
 module.exports = {
   ...azureUtils,
   ...tokenHelpers,
+  extractBaseURL,
   findMessageContent,
 };
