1a11c07cf5ee07530c4b66b8b6f44e85b78f3494
ChengguiSun <cgsun0521@hotmail.com>
2023-11-10 11:40:51 -0800

    Revert "Simplify payment success confirmation"
    This reverts commit aece01ed352eea50c45bec5990ba2cb083a4fa0f.

--- client/src/components/Subscription/PaypalReturnHandler.jsx
+++ client/src/components/Subscription/PaypalReturnHandler.jsx
@@ -10,9 +10,32 @@ const PaypalReturnHandler = () => {
     const PayerID = searchParams.get('PayerID');
     const paymentReference = searchParams.get('paymentReference');
 
+    console.log(`[PaypalReturnHandler] Payment ID: ${paymentId}, Payer ID: ${PayerID}, Payment Reference: ${paymentReference}`);
+
     if (paymentId && PayerID && paymentReference) {
-      navigate(`/subscription/${paymentId}/payment-success`);
+      const queryParams = new URLSearchParams({ PayerID, paymentId, paymentReference }).toString();
+
+      fetch(`/api/payments/success?${queryParams}`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (data.status === 'success') {
+            navigate(`/subscription/${data.userId}/payment-success?paymentId=${data.paymentId}`);
+          } else {
+            navigate(`/subscription/${data.userId}/payment-failed?error=${data.error}`);
+          }
+        })
+        .catch(error => {
+          console.error('[PaypalReturnHandler] Error processing payment:', error);
+          navigate('/subscription/payment-error', { state: { error: error.message } });
+        });
     } else {
+      console.error('[PaypalReturnHandler] Missing payment information.');
       navigate('/subscription/payment-error', { state: { error: 'Missing payment information.' } });
     }
   }, [searchParams, navigate]);
