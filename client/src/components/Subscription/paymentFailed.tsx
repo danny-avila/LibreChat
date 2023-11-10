bea84ec6b17a0f42cbe76abfd7a41769484a550c
ChengguiSun <cgsun0521@hotmail.com>
2023-11-10 12:39:38 -0800

    Improve the frontend style of payment confirmation message

--- client/src/components/Subscription/paymentFailed.tsx
+++ client/src/components/Subscription/paymentFailed.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
+import './PaymentMessages.css';
 
 const PaymentFailed = () => {
   const { userId } = useParams();
@@ -12,7 +13,8 @@ const PaymentFailed = () => {
   }, [userId, error]);
 
   return (
-    <div className="payment-failed">
+    <div className="payment-message-container">
+      <div className="payment-message payment-failed"></div>
       <h1>Payment Failed</h1>
       <p>We encountered an issue processing your payment: {error}</p>
       <p>User {userId}, please try again or contact support for assistance.</p>
--- client/src/components/Subscription/paymentSuccess.tsx
+++ client/src/components/Subscription/paymentSuccess.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
+import './PaymentMessages.css'; // Import the CSS file
 
 const PaymentSuccess = () => {
   const { userId } = useParams();
@@ -8,17 +9,17 @@ const PaymentSuccess = () => {
 
   useEffect(() => {
     console.log(`Payment successful for user ID: ${userId} with Payment ID: ${paymentId}`);
-    // Additional success logic here
   }, [userId, paymentId]);
 
   return (
-    <div className="payment-success">
-      <h1>Payment Successful!</h1>
-      <p>Your payment has been processed successfully.</p>
-      <p>Thank you, user {userId}, for your subscription. Your payment ID is {paymentId}.</p>
+    <div className="payment-message-container">
+      <div className="payment-message payment-success">
+        <h1>🎉 Payment Successful!</h1>
+        <p>Your payment has been processed successfully.</p>
+        <p>Thank you, user {userId}, for your subscription. Your payment ID is {paymentId}.</p>
+      </div>
     </div>
   );
 };
 
 export default PaymentSuccess;
-
