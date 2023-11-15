7965bd3280c37511465d2198f09b15b06fd2eecd
Danny Avila <messagedaniel@protonmail.com>
2023-11-15 12:58:19 -0500

    refactor(NewEndpointsMenu): make UI components modular

--- client/src/components/Chat/Header.tsx
+++ client/src/components/Chat/Header.tsx
@@ -1,9 +1,8 @@
-import { Root, Trigger } from '@radix-ui/react-popover';
 import { useGetEndpointsQuery } from 'librechat-data-provider';
 import NewEndpointMenu from './Menus/NewEndpointMenu';
-import { mapEndpoints, alternateName } from '~/utils';
 import HeaderOptions from './Input/HeaderOptions';
 import { useChatContext } from '~/Providers';
+import { mapEndpoints } from '~/utils';
 
 export default function Header() {
   const { conversation } = useChatContext();
@@ -11,40 +10,10 @@ export default function Header() {
     select: mapEndpoints,
   });
 
-  const currentEndpoint = conversation?.endpoint ?? '';
-
   return (
     <div className="sticky top-0 z-10 flex h-14 w-full items-center justify-between bg-white/95 p-2 font-semibold dark:bg-gray-800/90 dark:text-white">
       <div className="flex items-center gap-2 overflow-x-auto">
-        <Root>
-          <Trigger asChild>
-            <div
-              className="group flex cursor-pointer items-center gap-1 rounded-xl px-3 py-2 text-lg font-medium hover:bg-gray-50 radix-state-open:bg-gray-50 dark:hover:bg-black/10 dark:radix-state-open:bg-black/20"
-              // type="button"
-            >
-              <div>
-                {alternateName[currentEndpoint]}{' '}
-                {/* <span className="text-token-text-secondary">Secondary Text</span> */}
-              </div>
-              <svg
-                width="16"
-                height="17"
-                viewBox="0 0 16 17"
-                fill="none"
-                className="text-token-text-tertiary"
-              >
-                <path
-                  d="M11.3346 7.83203L8.00131 11.1654L4.66797 7.83203"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
-          </Trigger>
-          <NewEndpointMenu endpoints={endpoints} selected={conversation?.endpoint ?? ''} />
-        </Root>
+        <NewEndpointMenu endpoints={endpoints} selected={conversation?.endpoint ?? ''} />
         <HeaderOptions />
       </div>
       {/* Empty div for spacing */}
--- client/src/components/Chat/Menus/NewEndpointMenu.tsx
+++ client/src/components/Chat/Menus/NewEndpointMenu.tsx
@@ -1,33 +1,38 @@
 import type { FC } from 'react';
-import { Content, Portal } from '@radix-ui/react-popover';
+import { Content, Portal, Root } from '@radix-ui/react-popover';
 import { EModelEndpoint } from 'librechat-data-provider';
 import MenuItems from './Endpoints/MenuItems';
+import TitleButton from './UI/TitleButton';
+import { alternateName } from '~/utils';
 
 const NewEndpointMenu: FC<{
   endpoints: EModelEndpoint[];
   selected: EModelEndpoint | '';
 }> = ({ endpoints, selected }) => {
   return (
-    <Portal>
-      <div
-        style={{
-          position: 'fixed',
-          left: '0px',
-          top: '0px',
-          transform: 'translate3d(268px, 50px, 0px)',
-          minWidth: 'max-content',
-          zIndex: 'auto',
-        }}
-      >
-        <Content
-          side="bottom"
-          align="start"
-          className="bg-token-surface-primary mt-2 min-w-[340px] overflow-hidden rounded-lg border border-gray-100 bg-gray-900 shadow-lg dark:border-gray-700 dark:text-white"
+    <Root>
+      <TitleButton primaryText={alternateName[selected] + ' '} />
+      <Portal>
+        <div
+          style={{
+            position: 'fixed',
+            left: '0px',
+            top: '0px',
+            transform: 'translate3d(268px, 50px, 0px)',
+            minWidth: 'max-content',
+            zIndex: 'auto',
+          }}
         >
-          <MenuItems endpoints={endpoints} selected={selected} />
-        </Content>
-      </div>
-    </Portal>
+          <Content
+            side="bottom"
+            align="start"
+            className="bg-token-surface-primary mt-2 min-w-[340px] overflow-hidden rounded-lg border border-gray-100 bg-gray-900 shadow-lg dark:border-gray-700 dark:text-white"
+          >
+            <MenuItems endpoints={endpoints} selected={selected} />
+          </Content>
+        </div>
+      </Portal>
+    </Root>
   );
 };
 
