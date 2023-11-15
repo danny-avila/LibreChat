d2ec96acc923b7964fb5428c4e4aef442e17704d
MACHINSOFT <110278369+machinsoft@users.noreply.github.com>
2023-11-15 23:23:22 +0300

    Change from duration-300 to duration-100

--- client/src/components/Auth/LoginForm.tsx
+++ client/src/components/Auth/LoginForm.tsx
@@ -50,7 +50,7 @@ function LoginForm({ onSubmit }: TLoginFormProps) {
           ></input>
           <label
             htmlFor="email"
-            className="absolute left-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-green-500"
+            className="absolute left-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm text-gray-500 duration-100 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-green-500"
           >
             {localize('com_auth_email_address')}
           </label>
@@ -86,7 +86,7 @@ function LoginForm({ onSubmit }: TLoginFormProps) {
           ></input>
           <label
             htmlFor="password"
-            className="absolute left-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-green-500"
+            className="absolute left-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm text-gray-500 duration-100 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-green-500"
           >
             {localize('com_auth_password')}
           </label>
