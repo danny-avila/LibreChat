b082abaa8a36e6a547320c8f704c9253d0952238
Danny Avila <messagedaniel@protonmail.com>
2023-11-17 05:26:30 -0500

    fix: make advanced settings popover appear over nav

--- client/src/components/Chat/Input/OptionsPopover.tsx
+++ client/src/components/Chat/Input/OptionsPopover.tsx
@@ -36,7 +36,7 @@ export default function OptionsPopover({
 
   const localize = useLocalize();
   const cardStyle =
-    'shadow-xl rounded-md min-w-[75px] font-normal bg-white border-black/10 border dark:bg-gray-700 text-black dark:text-white';
+    'shadow-xl rounded-md min-w-[75px] font-normal bg-white border-black/10 border dark:bg-gray-700 text-black dark:text-white ';
 
   if (!visible) {
     return null;
@@ -45,7 +45,7 @@ export default function OptionsPopover({
   return (
     <Portal>
       <Content sideOffset={8} align="start" ref={popoverRef} asChild>
-        <div className="z-0 flex w-full flex-col items-center md:px-4">
+        <div className="z-[70] flex w-full flex-col items-center md:px-4">
           <div
             className={cn(
               cardStyle,
