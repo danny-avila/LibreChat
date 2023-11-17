9afc9999ee9c04d673f212e041d135b4c96f89b0
Danny Avila <messagedaniel@protonmail.com>
2023-11-17 05:28:24 -0500

    refactor(textarea): minor padding restyling, add max height

--- client/src/components/Chat/Input/Textarea.tsx
+++ client/src/components/Chat/Input/Textarea.tsx
@@ -1,6 +1,7 @@
 import TextareaAutosize from 'react-textarea-autosize';
 import { supportsFiles } from '~/common';
 import { useTextarea } from '~/hooks';
+import { cn, removeFocusOutlines } from '~/utils';
 
 export default function Textarea({ value, onChange, setText, submitMessage, endpoint }) {
   const {
@@ -14,8 +15,10 @@ export default function Textarea({ value, onChange, setText, submitMessage, endp
   } = useTextarea({ setText, submitMessage });
 
   const className = supportsFiles[endpoint]
-    ? 'm-0 w-full resize-none border-0 bg-transparent py-3.5 pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent placeholder-black/50 dark:placeholder-white/50 pl-10 md:py-3.5 md:pr-12 md:pl-[55px]'
-    : 'm-0 w-full resize-none border-0 bg-transparent py-[10px] pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:py-4 md:pr-12 gizmo:md:py-3.5 gizmo:placeholder-black/50 gizmo:dark:placeholder-white/50 pl-3 md:pl-4';
+    ? // ? 'm-0 w-full resize-none border-0 bg-transparent py-3.5 pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent placeholder-black/50 dark:placeholder-white/50 pl-10 md:py-3.5 md:pr-12 md:pl-[55px]'
+  // : 'm-0 w-full resize-none border-0 bg-transparent py-[10px] pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:py-4 md:pr-12 gizmo:md:py-3.5 gizmo:placeholder-black/50 gizmo:dark:placeholder-white/50 pl-3 md:pl-4';
+    'm-0 w-full resize-none border-0 bg-transparent py-3.5 pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent placeholder-black/50 dark:placeholder-white/50 pl-10 md:py-3.5 md:pr-12 md:pl-[55px]'
+    : 'm-0 w-full resize-none border-0 bg-transparent py-[10px] pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:py-3.5 md:pr-12 placeholder-black/50 dark:placeholder-white/50 pl-3 md:pl-4';
 
   return (
     <TextareaAutosize
@@ -36,7 +39,7 @@ export default function Textarea({ value, onChange, setText, submitMessage, endp
       placeholder={placeholder}
       // className="m-0 w-full resize-none border-0 bg-transparent py-[10px] pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:py-4 md:pr-12 gizmo:md:py-3.5 gizmo:placeholder-black/50 gizmo:dark:placeholder-white/50 pl-12 gizmo:pl-10 md:pl-[46px] gizmo:md:pl-[55px]"
       // className="gizmo:md:py-3.5 gizmo:placeholder-black/50 gizmo:dark:placeholder-white/50 gizmo:pl-10 gizmo:md:pl-[55px] m-0 h-auto max-h-52 w-full resize-none overflow-y-hidden border-0 bg-transparent py-[10px] pl-12 pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:py-4 md:pl-[46px] md:pr-12"
-      className={className}
+      className={cn(className, removeFocusOutlines, 'max-h-52')}
     />
   );
 }
