a803e4a3ad423febefb81764f9d2c4962e45e933
Danny Avila <messagedaniel@protonmail.com>
2023-11-19 17:52:45 -0500

    fix(useDragHelpers): correct context issue with `files` dependency

--- client/src/hooks/useDragHelpers.ts
+++ client/src/hooks/useDragHelpers.ts
@@ -4,32 +4,35 @@ import type { DropTargetMonitor } from 'react-dnd';
 import useFileHandling from './useFileHandling';
 
 export default function useDragHelpers() {
-  const { handleFiles } = useFileHandling();
-  const [{ canDrop, isOver }, drop] = useDrop(() => ({
-    accept: [NativeTypes.FILE],
-    drop(item: { files: File[] }) {
-      console.log('drop', item.files);
-      handleFiles(item.files);
-    },
-    canDrop() {
-      // console.log('canDrop', item.files, item.items);
-      return true;
-    },
-    // hover() {
-    //   // console.log('hover', item.files, item.items);
-    // },
-    collect: (monitor: DropTargetMonitor) => {
-      // const item = monitor.getItem() as File[];
-      // if (item) {
-      //   console.log('collect', item.files, item.items);
-      // }
+  const { files, handleFiles } = useFileHandling();
+  const [{ canDrop, isOver }, drop] = useDrop(
+    () => ({
+      accept: [NativeTypes.FILE],
+      drop(item: { files: File[] }) {
+        console.log('drop', item.files);
+        handleFiles(item.files);
+      },
+      canDrop() {
+        // console.log('canDrop', item.files, item.items);
+        return true;
+      },
+      // hover() {
+      //   // console.log('hover', item.files, item.items);
+      // },
+      collect: (monitor: DropTargetMonitor) => {
+        // const item = monitor.getItem() as File[];
+        // if (item) {
+        //   console.log('collect', item.files, item.items);
+        // }
 
-      return {
-        isOver: monitor.isOver(),
-        canDrop: monitor.canDrop(),
-      };
-    },
-  }));
+        return {
+          isOver: monitor.isOver(),
+          canDrop: monitor.canDrop(),
+        };
+      },
+    }),
+    [files],
+  );
 
   return {
     canDrop,
