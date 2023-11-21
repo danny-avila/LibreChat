8d6f8a56754f5c372d88758347874393ddb29da7
Danny Avila <messagedaniel@protonmail.com>
2023-11-20 20:14:06 -0500

    style: change maxheight of all settings textareas to 138px from 300px

--- client/src/components/Endpoints/Settings/Google.tsx
+++ client/src/components/Endpoints/Settings/Google.tsx
@@ -79,7 +79,7 @@ export default function Settings({ conversation, setOption, models, readonly }: 
                 placeholder={localize('com_endpoint_prompt_prefix_placeholder')}
                 className={cn(
                   defaultTextProps,
-                  'flex max-h-[300px] min-h-[100px] w-full resize-none px-3 py-2 ',
+                  'flex max-h-[138px] min-h-[100px] w-full resize-none px-3 py-2 ',
                 )}
               />
             </div>
