cc60f7d4530582c2feee919f5efb35c1924d210a
Danny Avila <messagedaniel@protonmail.com>
2023-11-23 18:34:41 -0500

    fix: handle webp images correctly

--- api/server/services/Files/images/convert.js
+++ api/server/services/Files/images/convert.js
@@ -3,9 +3,19 @@ const sharp = require('sharp');
 const fs = require('fs').promises;
 const { resizeImage } = require('./resize');
 
-async function convertToWebP(inputFilePath, resolution = 'high') {
+async function convertToWebP(file, resolution = 'high') {
+  const inputFilePath = file.path;
   const { buffer: resizedBuffer, width, height } = await resizeImage(inputFilePath, resolution);
-  const outputFilePath = inputFilePath.replace(/\.[^/.]+$/, '') + '.webp';
+  const extension = path.extname(inputFilePath);
+
+  if (extension.toLowerCase() === '.webp') {
+    const bytes = Buffer.byteLength(resizedBuffer);
+    await fs.writeFile(inputFilePath, resizedBuffer);
+    const filepath = path.posix.join('/', 'images', 'temp', path.basename(inputFilePath));
+    return { filepath, bytes, width, height };
+  }
+
+  const outputFilePath = inputFilePath.replace(extension, '.webp');
   const data = await sharp(resizedBuffer).toFormat('webp').toBuffer();
   await fs.writeFile(outputFilePath, data);
   const bytes = Buffer.byteLength(data);
--- api/server/services/Files/localStrategy.js
+++ api/server/services/Files/localStrategy.js
@@ -14,7 +14,7 @@ const { convertToWebP } = require('./images/convert');
  */
 const localStrategy = async ({ res, file, metadata }) => {
   const { file_id, temp_file_id } = metadata;
-  const { filepath, bytes, width, height } = await convertToWebP(file.path);
+  const { filepath, bytes, width, height } = await convertToWebP(file);
   const result = await createFile(
     {
       file_id,
