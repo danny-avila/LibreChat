e9e980d4f575cb70977c3f13a30e8d330ff89306
Raí Santos <140329135+itzraiss@users.noreply.github.com>
2023-11-28 06:00:46 -0300

    Update: Added Compatibility for Previous Environment Variable Names in AzureAISearch Plugin

--- api/app/clients/tools/AzureAiSearch.js
+++ api/app/clients/tools/AzureAiSearch.js
@@ -1,19 +1,20 @@
-const { Tool } = require('langchain/tools');
+const { StructuredTool } = require('langchain/tools');
+const { z } = require('zod');
 const { SearchClient, AzureKeyCredential } = require('@azure/search-documents');
 
-class AzureAISearch extends Tool {
+class AzureAISearch extends StructuredTool {
   constructor(fields = {}) {
     super();
     this.serviceEndpoint =
-      fields.AZURE_AI_SEARCH_SERVICE_ENDPOINT || this.getServiceEndpoint();
-    this.indexName = fields.AZURE_AI_SEARCH_INDEX_NAME || this.getIndexName();
-    this.apiKey = fields.AZURE_AI_SEARCH_API_KEY || this.getApiKey();
+      fields.AZURE_AI_SEARCH_SERVICE_ENDPOINT || process.env.AZURE_AI_SEARCH_SERVICE_ENDPOINT || fields.AZURE_COGNITIVE_SEARCH_SERVICE_ENDPOINT || this.getServiceEndpoint();
+    this.indexName = fields.AZURE_AI_SEARCH_INDEX_NAME || process.env.AZURE_AI_SEARCH_INDEX_NAME || fields.AZURE_COGNITIVE_SEARCH_INDEX_NAME || this.getIndexName();
+    this.apiKey = fields.AZURE_AI_SEARCH_API_KEY || process.env.AZURE_AI_SEARCH_API_KEY || fields.AZURE_COGNITIVE_SEARCH_API_KEY || this.getApiKey();
 
-    this.apiVersion = fields.AZURE_AI_SEARCH_API_VERSION || this.getApiVersion();
+    this.apiVersion = fields.AZURE_AI_SEARCH_API_VERSION || process.env.AZURE_AI_SEARCH_API_VERSION || fields.AZURE_COGNITIVE_SEARCH_API_VERSION || this.getApiVersion();
 
-    this.queryType = fields.AZURE_AI_SEARCH_SEARCH_OPTION_QUERY_TYPE || this.getQueryType();
-    this.top = fields.AZURE_AI_SEARCH_SEARCH_OPTION_TOP || this.getTop();
-    this.select = fields.AZURE_AI_SEARCH_SEARCH_OPTION_SELECT || this.getSelect();
+    this.queryType = fields.AZURE_AI_SEARCH_SEARCH_OPTION_QUERY_TYPE || process.env.AZURE_AI_SEARCH_SEARCH_OPTION_QUERY_TYPE || fields.AZURE_COGNITIVE_SEARCH_SEARCH_OPTION_QUERY_TYPE || this.getQueryType();
+    this.top = fields.AZURE_AI_SEARCH_SEARCH_OPTION_TOP || process.env.AZURE_AI_SEARCH_SEARCH_OPTION_TOP || fields.AZURE_COGNITIVE_SEARCH_SEARCH_OPTION_TOP || this.getTop();
+    this.select = fields.AZURE_AI_SEARCH_SEARCH_OPTION_SELECT || process.env.AZURE_AI_SEARCH_SEARCH_OPTION_SELECT || fields.AZURE_COGNITIVE_SEARCH_SEARCH_OPTION_SELECT || this.getSelect();
 
     this.client = new SearchClient(
       this.serviceEndpoint,
@@ -23,6 +24,9 @@ class AzureAISearch extends Tool {
         apiVersion: this.apiVersion,
       },
     );
+    this.schema = z.object({
+      query: z.string().describe('Search word or phrase to Azure AI Search'),
+    });
   }
 
   /**
@@ -39,54 +43,55 @@ class AzureAISearch extends Tool {
     'Use the \'azure-ai-search\' tool to retrieve search results relevant to your input';
 
   getServiceEndpoint() {
-    const serviceEndpoint = process.env.AZURE_AI_SEARCH_SERVICE_ENDPOINT || '';
+    const serviceEndpoint = process.env.AZURE_AI_SEARCH_SERVICE_ENDPOINT || process.env.AZURE_COGNITIVE_SEARCH_SERVICE_ENDPOINT || '';
     if (!serviceEndpoint) {
-      throw new Error('Missing AZURE_AI_SEARCH_SERVICE_ENDPOINT environment variable.');
+      throw new Error('Missing AZURE_AI_SEARCH_SERVICE_ENDPOINT or AZURE_COGNITIVE_SEARCH_SERVICE_ENDPOINT environment variable.');
     }
     return serviceEndpoint;
   }
 
   getIndexName() {
-    const indexName = process.env.AZURE_AI_SEARCH_INDEX_NAME || '';
+    const indexName = process.env.AZURE_AI_SEARCH_INDEX_NAME || process.env.AZURE_COGNITIVE_SEARCH_INDEX_NAME || '';
     if (!indexName) {
-      throw new Error('Missing AZURE_AI_SEARCH_INDEX_NAME environment variable.');
+      throw new Error('Missing AZURE_AI_SEARCH_INDEX_NAME or AZURE_COGNITIVE_SEARCH_INDEX_NAME environment variable.');
     }
     return indexName;
   }
 
   getApiKey() {
-    const apiKey = process.env.AZURE_AI_SEARCH_API_KEY || '';
+    const apiKey = process.env.AZURE_AI_SEARCH_API_KEY || process.env.AZURE_COGNITIVE_SEARCH_API_KEY || '';
     if (!apiKey) {
-      throw new Error('Missing AZURE_AI_SEARCH_API_KEY environment variable.');
+      throw new Error('Missing AZURE_AI_SEARCH_API_KEY or AZURE_COGNITIVE_SEARCH_API_KEY environment variable.');
     }
     return apiKey;
   }
 
   getApiVersion() {
-    return process.env.AZURE_AI_SEARCH_API_VERSION || '2023-11-01';
+    return process.env.AZURE_AI_SEARCH_API_VERSION || process.env.AZURE_COGNITIVE_SEARCH_API_VERSION || '2023-11-01';
   }
 
   getQueryType() {
-    return process.env.AZURE_AI_SEARCH_SEARCH_OPTION_QUERY_TYPE || 'simple';
+    return process.env.AZURE_AI_SEARCH_SEARCH_OPTION_QUERY_TYPE || process.env.AZURE_COGNITIVE_SEARCH_SEARCH_OPTION_QUERY_TYPE || 'simple';
   }
 
   getTop() {
-    if (process.env.AZURE_AI_SEARCH_SEARCH_OPTION_TOP) {
-      return Number(process.env.AZURE_AI_SEARCH_SEARCH_OPTION_TOP);
+    if (process.env.AZURE_AI_SEARCH_SEARCH_OPTION_TOP || process.env.AZURE_COGNITIVE_SEARCH_SEARCH_OPTION_TOP) {
+      return Number(process.env.AZURE_AI_SEARCH_SEARCH_OPTION_TOP || process.env.AZURE_COGNITIVE_SEARCH_SEARCH_OPTION_TOP);
     } else {
       return 5;
     }
   }
 
   getSelect() {
-    if (process.env.AZURE_AI_SEARCH_SEARCH_OPTION_SELECT) {
-      return process.env.AZURE_AI_SEARCH_SEARCH_OPTION_SELECT.split(',');
+    if (process.env.AZURE_AI_SEARCH_SEARCH_OPTION_SELECT || process.env.AZURE_COGNITIVE_SEARCH_SEARCH_OPTION_SELECT) {
+      return (process.env.AZURE_AI_SEARCH_SEARCH_OPTION_SELECT || process.env.AZURE_COGNITIVE_SEARCH_SEARCH_OPTION_SELECT).split(',');
     } else {
       return null;
     }
   }
 
-  async _call(query) {
+  async _call(data) {
+    const { query } = data;
     try {
       const searchOption = {
         queryType: this.queryType,
