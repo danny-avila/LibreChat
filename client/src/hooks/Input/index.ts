00b6af8c744a02bc00bdc9c92b42327c8f3071a9
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2023-12-05 09:38:04 -0500

    fix: Ensure Message Send Requires Key 🔑 (#1281)
    * fix: only allow message send when key is provided when required
    - create useRequiresKey hook
    - pass same disabled prop to Textarea, AttachFile, and SendButton
    - EndpointItem: add localization, stopPropagation, and remove commented code
    - separate some hooks to new Input dir
    - completely remove textareaHeight recoil state as is not needed
    - update imports for moved hooks
    - pass disabled prop to useTextarea
    
    * feat: add localization to textarea placeholders

---
+++ client/src/hooks/Input/index.ts
@@ -0,0 +1,4 @@
+export { default as useUserKey } from './useUserKey';
+export { default as useDebounce } from './useDebounce';
+export { default as useTextarea } from './useTextarea';
+export { default as useRequiresKey } from './useRequiresKey';
--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,4 +1,5 @@
 export * from './Messages';
+export * from './Input';
 
 export * from './AuthContext';
 export * from './ThemeContext';
@@ -7,10 +8,7 @@ export * from './ApiErrorBoundaryContext';
 export { default as useSSE } from './useSSE';
 export { default as useToast } from './useToast';
 export { default as useTimeout } from './useTimeout';
-export { default as useUserKey } from './useUserKey';
 export { default as useNewConvo } from './useNewConvo';
-export { default as useDebounce } from './useDebounce';
-export { default as useTextarea } from './useTextarea';
 export { default as useLocalize } from './useLocalize';
 export { default as useMediaQuery } from './useMediaQuery';
 export { default as useSetOptions } from './useSetOptions';
