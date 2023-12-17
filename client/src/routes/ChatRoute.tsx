eac889bb07b986f92c2f55920d8ef102e85d5eb0
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2023-12-17 18:56:01 -0500

    🚩 fix: Initialize Conversation Only when Necessary Data is Fetched (#1379)
    * fix(ChatRoute): only initialize conversation after all data is fetched (models, endpoints, initialConversationQuery if not `new`)
    
    * chore: remove unnecessary packages for rolling up api
    
    * chore: bump data-provider package.json

--- client/src/routes/ChatRoute.tsx
+++ client/src/routes/ChatRoute.tsx
@@ -1,7 +1,11 @@
 import { useRecoilValue } from 'recoil';
 import { useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
-import { useGetConvoIdQuery, useGetModelsQuery } from 'librechat-data-provider/react-query';
+import {
+  useGetConvoIdQuery,
+  useGetModelsQuery,
+  useGetEndpointsQuery,
+} from 'librechat-data-provider/react-query';
 import { useNewConvo, useConfigOverride } from '~/hooks';
 import ChatView from '~/components/Chat/ChatView';
 import useAuthRedirect from './useAuthRedirect';
@@ -21,12 +25,23 @@ export default function ChatRoute() {
   const initialConvoQuery = useGetConvoIdQuery(conversationId ?? '', {
     enabled: isAuthenticated && conversationId !== 'new',
   });
+  const endpointsQuery = useGetEndpointsQuery({ enabled: isAuthenticated && modelsQueryEnabled });
 
   useEffect(() => {
-    if (conversationId === 'new' && modelsQuery.data && !hasSetConversation.current) {
+    if (
+      conversationId === 'new' &&
+      endpointsQuery.data &&
+      modelsQuery.data &&
+      !hasSetConversation.current
+    ) {
       newConversation({ modelsData: modelsQuery.data });
       hasSetConversation.current = true;
-    } else if (initialConvoQuery.data && modelsQuery.data && !hasSetConversation.current) {
+    } else if (
+      initialConvoQuery.data &&
+      endpointsQuery.data &&
+      modelsQuery.data &&
+      !hasSetConversation.current
+    ) {
       newConversation({
         template: initialConvoQuery.data,
         modelsData: modelsQuery.data,
@@ -34,7 +49,7 @@ export default function ChatRoute() {
       hasSetConversation.current = true;
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [initialConvoQuery.data, modelsQuery.data]);
+  }, [initialConvoQuery.data, modelsQuery.data, endpointsQuery.data]);
 
   if (!isAuthenticated) {
     return null;
--- packages/data-provider/server-rollup.config.js
+++ packages/data-provider/server-rollup.config.js
@@ -3,7 +3,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import alias from '@rollup/plugin-alias';
 import json from '@rollup/plugin-json';
-import babel from '@rollup/plugin-babel';
 
 const rootPath = path.resolve(__dirname, '../../');
 const rootServerPath = path.resolve(__dirname, '../../api');
@@ -30,19 +29,6 @@ export default {
     }),
     commonjs(),
     json(),
-    babel({
-      exclude: 'node_modules/**',
-      babelHelpers: 'bundled',
-      presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        '@babel/preset-typescript',
-      ],
-      plugins: [
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-        '@babel/plugin-proposal-optional-chaining',
-      ],
-    }),
   ],
   external: (id) => {
     // More selective external function
