ac9543a6737b7cc1fa4c67438e81cb528496380c
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2024-01-03 10:07:36 -0500

    🎨 style: Add Dynamic Height to Endpoint/Model Menus (#1480)
    * style(EndpointsMenu): add scrolling and dynamic height
    
    * style(SelectDropDownPop): add dynamic height

--- client/src/components/Chat/Menus/EndpointsMenu.tsx
+++ client/src/components/Chat/Menus/EndpointsMenu.tsx
@@ -36,7 +36,7 @@ const EndpointsMenu: FC = () => {
           <Content
             side="bottom"
             align="start"
-            className="mt-2 min-w-[340px] overflow-hidden rounded-lg border border-gray-100 bg-white shadow-lg dark:border-gray-700 dark:bg-gray-900 dark:text-white"
+            className="mt-2 max-h-[65vh] min-w-[340px] overflow-y-auto rounded-lg border border-gray-100 bg-white shadow-lg dark:border-gray-700 dark:bg-gray-900 dark:text-white lg:max-h-[75vh]"
           >
             <EndpointItems endpoints={endpoints} selected={selected} />
           </Content>
