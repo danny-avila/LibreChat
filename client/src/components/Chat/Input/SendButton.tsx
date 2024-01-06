c93a5ef42741c0fe8688bc5186cf3498ebbb5abb
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2024-01-06 15:57:47 -0500

    ⬆️ fix(SendButton): correct dark theme bg when `enabled` (#1503)

--- client/src/components/Chat/Input/SendButton.tsx
+++ client/src/components/Chat/Input/SendButton.tsx
@@ -6,7 +6,7 @@ export default function SendButton({ text, disabled }) {
     <button
       disabled={!text || disabled}
       className={cn(
-        'enabled:bg-brand-purple absolute rounded-lg rounded-md border border-black p-0.5 p-1 text-white transition-colors enabled:bg-black disabled:bg-black disabled:text-gray-400 disabled:opacity-10 dark:border-white dark:bg-white dark:disabled:bg-white ',
+        'absolute rounded-lg rounded-md border border-black p-0.5 p-1 text-white transition-colors enabled:bg-black enabled:dark:bg-white disabled:bg-black disabled:text-gray-400 disabled:opacity-10 dark:border-white dark:bg-white dark:disabled:bg-white ',
         'bottom-1.5 right-1.5 md:bottom-2.5 md:right-3 md:p-[2px]',
       )}
       data-testid="send-button"
