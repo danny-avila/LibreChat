1aca921d96a072ed7fa64f3a5b2f2906320e69fb
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2024-01-07 14:43:27 -0500

    🔄 refactor(config): Move `connectWithTimeout` Outside of Helpers Module (#1513)

--- config/add-balance.js
+++ config/add-balance.js
@@ -1,11 +1,12 @@
 const path = require('path');
 require('module-alias')({ base: path.resolve(__dirname, '..', 'api') });
-const { askQuestion, silentExit, connectWithTimeout } = require('./helpers');
+const { askQuestion, silentExit } = require('./helpers');
 const Transaction = require('~/models/Transaction');
 const User = require('~/models/User');
+const connect = require('./connect');
 
 (async () => {
-  await connectWithTimeout();
+  await connect();
 
   /**
    * Show the welcome / help menu
