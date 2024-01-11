7df615abe9fd158afccb6f67567e19c2bd9a2a4e
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2024-01-11 13:42:00 -0500

    🐛 fix: Remove `resendImages`, `imageDetail` from `modelOptions` for Custom Endpoints (#1537)

--- api/server/services/Endpoints/custom/buildOptions.js
+++ api/server/services/Endpoints/custom/buildOptions.js
@@ -1,10 +1,12 @@
 const buildOptions = (endpoint, parsedBody, endpointType) => {
-  const { chatGptLabel, promptPrefix, ...rest } = parsedBody;
+  const { chatGptLabel, promptPrefix, resendImages, imageDetail, ...rest } = parsedBody;
   const endpointOption = {
     endpoint,
     endpointType,
     chatGptLabel,
     promptPrefix,
+    resendImages,
+    imageDetail,
     modelOptions: {
       ...rest,
     },
