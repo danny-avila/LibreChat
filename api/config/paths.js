7dbc7d980369b835ba2ba71bcef26be682e2a33d
Danny Avila <messagedaniel@protonmail.com>
2024-01-12 17:25:16 -0500

    chore(paths): add `uploads` path

--- api/config/paths.js
+++ api/config/paths.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  uploads: path.resolve(__dirname, '..', '..', 'uploads'),
   dist: path.resolve(__dirname, '..', '..', 'client', 'dist'),
   publicPath: path.resolve(__dirname, '..', '..', 'client', 'public'),
   imageOutput: path.resolve(__dirname, '..', '..', 'client', 'public', 'images'),
