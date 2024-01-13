73f4b979c34a2466f4bb624f489b640c035e1e97
Danny Avila <messagedaniel@protonmail.com>
2024-01-13 13:02:50 -0500

    fix(config): fix helper scripts to resolve paths correctly on both linx-based and windows OS

--- config/connect.js
+++ config/connect.js
@@ -1,5 +1,11 @@
 const path = require('path');
-require('module-alias')({ base: path.resolve(__dirname, '..', 'api') });
+
+require('module-alias/register');
+const moduleAlias = require('module-alias');
+
+const basePath = path.resolve(__dirname, '..', 'api');
+moduleAlias.addAlias('~', basePath);
+
 const connectDb = require('~/lib/db/connectDb');
 
 async function connect() {
