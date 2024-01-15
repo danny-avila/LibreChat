016c3b598c2263fac6f513bf967ae985229c3bc8
Danny Avila <messagedaniel@protonmail.com>
2024-01-15 03:08:11 -0500

    refactor(assistants/chat): - throw error if no thread_id for existing convo - check message gaps (synchronize db to api) - send new SSE message type to sync any recovered messages to UI - omit model from req.body until user can explicitly set it - modify messages to add metadata, to correspond them to db messageId's

--- api/server/routes/assistants/chat.js
+++ api/server/routes/assistants/chat.js
@@ -5,8 +5,9 @@ const { EModelEndpoint, Constants } = require('librechat-data-provider');
 const {
   initThread,
   saveUserMessage,
+  checkMessageGaps,
+  addThreadMetadata,
   saveAssistantMessage,
-  modifyMessages,
 } = require('~/server/services/Threads');
 const { runAssistant, createOnTextProgress } = require('~/server/services/AssistantService');
 const { createRun } = require('~/server/services/Runs');
@@ -24,7 +25,7 @@ const {
   // createAbortController,
 } = require('~/server/middleware');
 
-const defaultModel = 'gpt-3.5-turbo-1106';
+// const defaultModel = 'gpt-3.5-turbo-1106';
 /**
  * @route POST /
  * @desc Chat with an assistant
@@ -37,26 +38,66 @@ router.post('/', setHeaders, async (req, res) => {
   try {
     logger.debug('[/assistants/chat/] req.body', req.body);
     const {
-      messages: _messages,
       text,
-      messageId,
+      messageId: _messageId,
       files = [],
       promptPrefix,
       assistant_id,
       instructions,
       thread_id: _thread_id,
       conversationId: convoId,
-      parentMessageId = Constants.NO_PARENT,
+      parentMessageId: _parentId = Constants.NO_PARENT,
       // TODO: model is not currently sent from the frontend
       // maybe it should only be sent when changed from the assistant's model?
-      model: _model = defaultModel,
+      // model: _model = defaultModel,
     } = req.body;
 
+    if (convoId && !thread_id) {
+      throw new Error('Missing thread_id for existing conversation');
+    }
+
     // Temporary: Can't use 0613 models
-    const model = _model.replace(/gpt-4.*$/, 'gpt-4-1106-preview');
+    // const model = _model.replace(/gpt-4.*$/, 'gpt-4-1106-preview');
 
     /** @type {string|undefined} - the current thread id */
     let thread_id = _thread_id;
+
+    let parentMessageId = _parentId;
+
+    if (!assistant_id) {
+      throw new Error('Missing assistant_id');
+    }
+
+    /** @type {string} - The conversation UUID - created if undefined */
+    const conversationId = convoId ?? v4();
+    const responseMessageId = v4();
+    const userMessageId = v4();
+
+    // TODO: needs to be initialized with `initializeClient`
+    /** @type {OpenAIClient} */
+    const openai = new OpenAI(process.env.OPENAI_API_KEY);
+    openai.req = req;
+    openai.res = res;
+    createOnTextProgress({ openai, conversationId, userMessageId, messageId: responseMessageId });
+
+    /** @type {TMessage[]} */
+    let previousMessages = [];
+
+    if (thread_id) {
+      previousMessages = await checkMessageGaps({ openai, thread_id, conversationId });
+    }
+
+    if (previousMessages.length) {
+      parentMessageId = previousMessages[previousMessages.length - 1].messageId;
+    }
+
+    const userMessage = {
+      role: 'user',
+      content: text,
+      metadata: {
+        messageId: userMessageId,
+      },
+    };
 
     let thread_file_ids = [];
     if (convoId) {
@@ -66,33 +107,43 @@ router.post('/', setHeaders, async (req, res) => {
       }
     }
 
-    /** @type {string} - The conversation UUID - created if undefined */
-    const conversationId = convoId ?? v4();
-
-    if (!assistant_id) {
-      throw new Error('Missing assistant_id');
-    }
-
-    // TODO: needs to be initialized with `initializeClient`
-    /** @type {OpenAIClient} */
-    const openai = new OpenAI(process.env.OPENAI_API_KEY);
-    openai.req = req;
-    openai.res = res;
-    createOnTextProgress(openai);
-
-    const userMessage = {
-      role: 'user',
-      content: text,
-      metadata: {
-        messageId,
-      },
-    };
-
     const file_ids = files.map(({ file_id }) => file_id);
     if (file_ids.length || thread_file_ids.length) {
       userMessage.file_ids = file_ids;
       openai.attachedFileIds = new Set([...file_ids, ...thread_file_ids]);
     }
+
+    const requestMessage = {
+      user: req.user.id,
+      text,
+      messageId: userMessageId,
+      parentMessageId,
+      // TODO: make sure client sends correct format for `files`, use zod
+      files,
+      file_ids,
+      conversationId,
+      isCreatedByUser: true,
+      assistant_id,
+      thread_id,
+      model: assistant_id,
+    };
+
+    previousMessages.push(requestMessage);
+
+    sendMessage(res, {
+      sync: true,
+      conversationId,
+      messages: previousMessages,
+      responseMessage: {
+        user: req.user.id,
+        messageId: openai.responseMessage.messageId,
+        parentMessageId: userMessageId,
+        conversationId,
+        assistant_id,
+        thread_id,
+        model: assistant_id,
+      },
+    });
 
     // TODO: may allow multiple messages to be created beforehand in a future update
     const initThreadBody = {
@@ -114,26 +165,14 @@ router.post('/', setHeaders, async (req, res) => {
       promptPrefix: promptPrefix,
       instructions: instructions,
       assistant_id,
-      model,
+      // model,
     };
 
     if (file_ids.length) {
       conversation.file_ids = file_ids;
     }
 
-    await saveUserMessage({
-      user: req.user.id,
-      text,
-      messageId,
-      parentMessageId,
-      // TODO: make sure client sends correct format for `files`, use zod
-      files,
-      file_ids,
-      conversationId,
-      assistant_id,
-      thread_id,
-      model,
-    });
+    await saveUserMessage(requestMessage);
 
     /* NOTE:
      * By default, a Run will use the model and tools configuration specified in Assistant object,
@@ -142,7 +181,7 @@ router.post('/', setHeaders, async (req, res) => {
     const run = await createRun({
       openai,
       thread_id,
-      body: { assistant_id, model: req.body.model ?? defaultModel },
+      body: { assistant_id },
     });
 
     // todo: retry logic
@@ -152,12 +191,12 @@ router.post('/', setHeaders, async (req, res) => {
     /** @type {ResponseMessage} */
     const responseMessage = {
       ...openai.responseMessage,
-      parentMessageId: messageId,
+      parentMessageId: userMessageId,
       conversationId,
       user: req.user.id,
       assistant_id,
       thread_id,
-      model,
+      model: assistant_id,
     };
 
     // responseMessage.tokenCount = getTotalTokenCount(responseMessage.content);
@@ -176,7 +215,7 @@ router.post('/', setHeaders, async (req, res) => {
 
     await saveAssistantMessage(responseMessage);
 
-    await modifyMessages({
+    await addThreadMetadata({
       openai,
       thread_id,
       messageId: responseMessage.messageId,
