ecf4e18eb76bdafc91310936cbcb330bf722339a
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2024-01-16 13:00:53 -0500

    chore(PopoverButtons): remove comments, re-organize imports

--- client/src/components/Chat/Input/PopoverButtons.tsx
+++ client/src/components/Chat/Input/PopoverButtons.tsx
@@ -1,10 +1,10 @@
-import { ReactNode } from 'react';
+import { EModelEndpoint } from 'librechat-data-provider';
+import type { ReactNode } from 'react';
 import { MessagesSquared, GPTIcon } from '~/components/svg';
 import { useChatContext } from '~/Providers';
 import { Button } from '~/components/ui';
+import { useLocalize } from '~/hooks';
 import { cn } from '~/utils/';
-import { useLocalize } from '~/hooks'; // Import the Uselocalize function
-import { EModelEndpoint } from 'librechat-data-provider'; // Amount of the type EModelEndpoint
 
 type TPopoverButton = {
   label: string;
@@ -21,7 +21,7 @@ export default function PopoverButtons({
   iconClass?: string;
 }) {
   const { conversation, optionSettings, setOptionSettings, showAgentSettings, setShowAgentSettings } = useChatContext();
-  const localize = useLocalize(); // Use the Uselocalize function
+  const localize = useLocalize();
 
   const { model, endpoint: _endpoint, endpointType } = conversation ?? {};
   const endpoint = endpointType ?? _endpoint;
@@ -37,7 +37,7 @@ export default function PopoverButtons({
   const buttons: { [key: string]: TPopoverButton[] } = {
     [EModelEndpoint.google]: [
       {
-        label: localize(showExamples ? 'com_hide_examples' : 'com_show_examples'), // Use Locate for the label
+        label: localize(showExamples ? 'com_hide_examples' : 'com_show_examples'),
         buttonClass: isGenerativeModel || isTextModel ? 'disabled' : '',
         handler: triggerExamples,
         icon: <MessagesSquared className={cn('mr-1 w-[14px]', iconClass)} />,
@@ -45,7 +45,7 @@ export default function PopoverButtons({
     ],
     [EModelEndpoint.gptPlugins]: [
       {
-        label: localize(showAgentSettings ? 'com_show_completion_settings' : 'com_show_agent_settings'), // Use Locate for the label
+        label: localize(showAgentSettings ? 'com_show_completion_settings' : 'com_show_agent_settings'),
         buttonClass: '',
         handler: () => setShowAgentSettings((prev) => !prev),
         icon: <GPTIcon className={cn('mr-1 w-[14px]', iconClass)} size={24} />,
