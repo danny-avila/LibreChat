a8d6bfde7a737b906753ef0c475f634bf96837a7
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2024-01-18 14:44:10 -0500

    ✏️ feat: LaTeX parsing for Messages (#1585)
    * feat: Beta features tab in Settings and LaTeX Parsing toggle
    
    * feat: LaTex parsing with spec

--- client/src/components/Nav/SettingsTabs/Beta/Beta.tsx
+++ client/src/components/Nav/SettingsTabs/Beta/Beta.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import { memo } from 'react';
 import * as Tabs from '@radix-ui/react-tabs';
 import { SettingsTabValues } from 'librechat-data-provider';
-import Avatar from './Avatar';
+import LaTeXParsing from './LaTeXParsing';
+import ModularChat from './ModularChat';
 
-function Account() {
+function Beta() {
   return (
     <Tabs.Content
-      value={SettingsTabValues.ACCOUNT}
+      value={SettingsTabValues.BETA}
       role="tabpanel"
       className="w-full md:min-h-[300px]"
     >
       <div className="flex flex-col gap-3 text-sm text-gray-600 dark:text-gray-300">
         <div className="border-b pb-3 last-of-type:border-b-0 dark:border-gray-700">
-          <Avatar />
+          <ModularChat />
+        </div>
+        <div className="border-b pb-3 last-of-type:border-b-0 dark:border-gray-700">
+          <LaTeXParsing />
         </div>
       </div>
-      <div className="border-b pb-3 last-of-type:border-b-0 dark:border-gray-700"></div>
     </Tabs.Content>
   );
 }
 
-export default React.memo(Account);
+export default memo(Beta);
