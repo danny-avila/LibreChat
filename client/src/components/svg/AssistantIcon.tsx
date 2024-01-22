966d36ceceeee9cc3c1f9fc923a0ed6e1f1da15d
Danny Avila <messagedaniel@protonmail.com>
2024-01-22 13:42:52 -0500

    feat: assistant name/icon in Landing & Header

--- client/src/components/svg/AssistantIcon.tsx
+++ client/src/components/svg/AssistantIcon.tsx
@@ -1,17 +1,26 @@
 import { cn } from '~/utils';
 
-export default function AssistantIcon({ className = '' }) {
+export default function AssistantIcon({
+  className = '',
+  size = '1em',
+}: {
+  className?: string;
+  size?: string | number;
+}) {
+  const unit = 24;
+  const height = size;
+  const width = size;
   return (
     <svg
+      width={width}
+      height={height}
+      viewBox={`0 0 ${unit} ${unit}`}
       stroke="currentColor"
       fill="none"
       strokeWidth="2"
-      viewBox="0 0 24 24"
       strokeLinecap="round"
       strokeLinejoin="round"
       className={cn('text-token-secondary h-2/3 w-2/3', className)}
-      height="1em"
-      width="1em"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" />
