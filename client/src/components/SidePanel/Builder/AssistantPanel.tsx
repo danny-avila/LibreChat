1a352b8e9709cf968fd910ea162cf03d4c848d42
Danny Avila <messagedaniel@protonmail.com>
2024-01-23 18:16:27 -0500

    chore: move assistant builder components to separate directory

--- client/src/components/SidePanel/Builder/AssistantPanel.tsx
+++ client/src/components/SidePanel/Builder/AssistantPanel.tsx
@@ -258,6 +258,29 @@ export default function AssistantPanel({ index = 0 }) {
           </div>
         </div>
         <div className="flex items-center justify-end">
+          {/* Context Button */}
+          <button
+            className="btn btn-neutral border-token-border-light relative h-8 rounded-lg font-medium"
+            type="button"
+          >
+            <div className="flex w-full items-center justify-center gap-2">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon-md"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M3 12C3 10.8954 3.89543 10 5 10C6.10457 10 7 10.8954 7 12C7 13.1046 6.10457 14 5 14C3.89543 14 3 13.1046 3 12ZM10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12ZM17 12C17 10.8954 17.8954 10 19 10C20.1046 10 21 10.8954 21 12C21 13.1046 20.1046 14 19 14C17.8954 14 17 13.1046 17 12Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </div>
+          </button>
           {/* Use Button */}
           <button
             className="focus:shadow-outline mx-2 rounded bg-green-500 px-4 py-2 font-semibold text-white hover:bg-green-400 focus:border-green-500 focus:outline-none focus:ring-0"
