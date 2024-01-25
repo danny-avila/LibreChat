eeffee449a26c3051add5d71399dc1c3c81b6b25
Danny Avila <messagedaniel@protonmail.com>
2024-01-25 12:27:16 -0500

    refactor(FileContainer): make file preview reusable

--- client/src/components/Chat/Input/Files/FileContainer.tsx
+++ client/src/components/Chat/Input/Files/FileContainer.tsx
@@ -1,7 +1,6 @@
 import type { TFile } from 'librechat-data-provider';
 import type { ExtendedFile } from '~/common';
-import FileIcon from '~/components/svg/Files/FileIcon';
-import ProgressCircle from './ProgressCircle';
+import FilePreview from './FilePreview';
 import RemoveFile from './RemoveFile';
 import { getFileType } from '~/utils';
 
@@ -12,16 +11,6 @@ const FileContainer = ({
   file: ExtendedFile | TFile;
   onDelete?: () => void;
 }) => {
-  const radius = 55; // Radius of the SVG circle
-  const circumference = 2 * Math.PI * radius;
-  const progress = file['progress'] ?? 1;
-
-  // Calculate the offset based on the loading progress
-  const offset = circumference - progress * circumference;
-  const circleCSSProperties = {
-    transition: 'stroke-dashoffset 0.3s linear',
-  };
-
   const fileType = getFileType(file.type);
 
   return (
@@ -29,16 +18,7 @@ const FileContainer = ({
       <div className="relative overflow-hidden rounded-xl border border-gray-200 dark:border-gray-600">
         <div className="w-60 p-2 dark:bg-gray-600">
           <div className="flex flex-row items-center gap-2">
-            <div className="relative h-10 w-10 shrink-0 overflow-hidden rounded-md">
-              <FileIcon file={file} fileType={fileType} />
-              {progress < 1 && (
-                <ProgressCircle
-                  circumference={circumference}
-                  offset={offset}
-                  circleCSSProperties={circleCSSProperties}
-                />
-              )}
-            </div>
+            <FilePreview file={file} fileType={fileType} />
             <div className="overflow-hidden">
               <div className="truncate font-medium">{file.filename}</div>
               <div className="truncate text-gray-300">{fileType.title}</div>
