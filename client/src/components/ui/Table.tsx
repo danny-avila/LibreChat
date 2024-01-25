4bc6f8662a7b0b4db0164aeb37483b0afc18bbf9
Danny Avila <messagedaniel@protonmail.com>
2024-01-25 10:51:36 -0500

    fix(Table): make headers sticky

--- client/src/components/ui/Table.tsx
+++ client/src/components/ui/Table.tsx
@@ -4,9 +4,7 @@ import { cn } from '../../utils';
 
 const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
   ({ className = '', ...props }, ref) => (
-    <div className="w-full overflow-auto">
-      <table ref={ref} className={cn('w-full caption-bottom text-sm', className)} {...props} />
-    </div>
+    <table ref={ref} className={cn('w-full caption-bottom text-sm', className)} {...props} />
   ),
 );
 Table.displayName = 'Table';
