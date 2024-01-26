3b0b22941edea87e4602f5432654801d696a19c2
Danny Avila <messagedaniel@protonmail.com>
2024-01-25 22:51:08 -0500

    feat: gpt-4-turbo/0125 model support

--- api/models/tx.spec.js
+++ api/models/tx.spec.js
@@ -84,6 +84,12 @@ describe('getMultiplier', () => {
     expect(getMultiplier({ tokenType: 'completion', model: 'gpt-4-1106-vision-preview' })).toBe(
       tokenValues['gpt-4-1106'].completion,
     );
+    expect(getMultiplier({ tokenType: 'completion', model: 'gpt-4-0125-preview' })).toBe(
+      tokenValues['gpt-4-1106'].completion,
+    );
+    expect(getMultiplier({ tokenType: 'completion', model: 'gpt-4-turbo-vision-preview' })).toBe(
+      tokenValues['gpt-4-1106'].completion,
+    );
   });
 
   it('should return defaultRate if derived valueKey does not match any known patterns', () => {
