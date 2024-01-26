8a379562c416a18f409336dd3c2cddd3dde6e621
Raí Santos <140329135+itzraiss@users.noreply.github.com>
2024-01-26 11:51:13 -0300

    🖌️feat: Scrolltobottom Style

--- client/src/components/Messages/ScrollToBottom.tsx
+++ client/src/components/Messages/ScrollToBottom.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 type Props = {
   scrollHandler: React.MouseEventHandler<HTMLButtonElement>;
 };
@@ -6,22 +8,10 @@ export default function ScrollToBottom({ scrollHandler }: Props) {
   return (
     <button
       onClick={scrollHandler}
-      className="absolute bottom-2 right-6 z-10 cursor-pointer rounded-full border border-gray-200 bg-gray-50 text-gray-600 dark:border-white/10 dark:bg-white/10 dark:text-gray-200"
+      className="cursor-pointer absolute z-10 rounded-full bg-clip-padding border text-gray-600 dark:border-white/10 dark:bg-white/10 dark:text-gray-200 right-1/2 border-black/10 bg-token-surface-primary bottom-5"
     >
-      <svg
-        stroke="currentColor"
-        fill="none"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="m-1 h-4 w-4"
-        height="1em"
-        width="1em"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <line x1="12" y1="5" x2="12" y2="19" />
-        <polyline points="19 12 12 19 5 12" />
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" className="m-1 text-black dark:text-white">
+        <path d="M17 13L12 18L7 13M12 6L12 17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
       </svg>
     </button>
   );
