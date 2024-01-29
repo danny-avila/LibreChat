aa77b86212a978f620f84fb626f3ff64bfa67486
Danny Avila <messagedaniel@protonmail.com>
2024-01-29 08:47:54 -0500

    feat: programmatic iv encryption/decryption helpers

--- api/server/utils/crypto.js
+++ api/server/utils/crypto.js
@@ -19,4 +19,23 @@ function decrypt(encryptedValue) {
   return decrypted;
 }
 
-module.exports = { encrypt, decrypt };
+// Programatically generate iv
+function encryptV2(value) {
+  const gen_iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(algorithm, key, gen_iv);
+  let encrypted = cipher.update(value, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return gen_iv.toString('hex') + ':' + encrypted;
+}
+
+function decryptV2(encryptedValue) {
+  const parts = encryptedValue.split(':');
+  const gen_iv = Buffer.from(parts.shift(), 'hex');
+  const encrypted = parts.join(':');
+  const decipher = crypto.createDecipheriv(algorithm, key, gen_iv);
+  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+}
+
+module.exports = { encrypt, decrypt, encryptV2, decryptV2 };
