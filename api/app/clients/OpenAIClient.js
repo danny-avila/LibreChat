f12f94e5e55f6e045a13ea1fc7cc7b7e17a48831
Danny Avila <messagedaniel@protonmail.com>
2024-01-30 10:13:32 -0500

    🔝fix: Re-order System Message to Top for Mistral API Payloads (#1678)
    * fix: re-order System Message if Mistral AI API as it only allows System Message at start of Payload
    
    * fix: re-introduce singular system message change role to `user` if `system`

--- api/app/clients/OpenAIClient.js
+++ api/app/clients/OpenAIClient.js
@@ -976,9 +976,22 @@ ${convo}
         ...opts,
       });
 
-      /* hacky fix for Mistral AI API not allowing a singular system message in payload */
+      /* hacky fixes for Mistral AI API:
+      - Re-orders system message to the top of the messages payload, as not allowed anywhere else
+      - If there is only one message and it's a system message, change the role to user
+      */
       if (opts.baseURL.includes('https://api.mistral.ai/v1') && modelOptions.messages) {
         const { messages } = modelOptions;
+
+        const systemMessageIndex = messages.findIndex((msg) => msg.role === 'system');
+
+        if (systemMessageIndex > 0) {
+          const [systemMessage] = messages.splice(systemMessageIndex, 1);
+          messages.unshift(systemMessage);
+        }
+
+        modelOptions.messages = messages;
+
         if (messages.length === 1 && messages[0].role === 'system') {
           modelOptions.messages[0].role = 'user';
         }
