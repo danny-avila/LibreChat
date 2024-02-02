41e24792bc11d95c0cbe8440c229be6555209f3b
Danny Avila <messagedaniel@protonmail.com>
2024-02-02 00:59:07 -0500

    ci: fix tx test

--- api/models/tx.js
+++ api/models/tx.js
@@ -30,6 +30,8 @@ const getValueKey = (model, endpoint) => {
 
   if (modelName.includes('gpt-3.5-turbo-16k')) {
     return '16k';
+  } else if (modelName.includes('gpt-3.5-turbo-0125')) {
+    return 'gpt-3.5-turbo-0125';
   } else if (modelName.includes('gpt-3.5-turbo-1106')) {
     return 'gpt-3.5-turbo-1106';
   } else if (modelName.includes('gpt-3.5')) {
