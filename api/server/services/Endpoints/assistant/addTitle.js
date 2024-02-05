678937c5cbd06603f90af62f297a2a12fd22e124
Danny Avila <messagedaniel@protonmail.com>
2024-02-05 15:41:15 -0500

    feat(assistants): addTitle cache setting

--- api/server/services/Endpoints/assistant/addTitle.js
+++ api/server/services/Endpoints/assistant/addTitle.js
@@ -1,4 +1,6 @@
+const { CacheKeys } = require('librechat-data-provider');
 const { saveConvo } = require('~/models/Conversation');
+const getLogStores = require('~/cache/getLogStores');
 const { isEnabled } = require('~/server/utils');
 
 const addTitle = async (req, { text, responseText, conversationId, client }) => {
@@ -11,7 +13,12 @@ const addTitle = async (req, { text, responseText, conversationId, client }) => 
     return;
   }
 
+  const titleCache = getLogStores(CacheKeys.GEN_TITLE);
+  const key = `${req.user.id}-${conversationId}`;
+
   const title = await client.titleConvo({ text, responseText });
+  await titleCache.set(key, title);
+
   await saveConvo(req.user.id, {
     conversationId,
     title,
