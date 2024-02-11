cf2cebc831b9265c9e4fe7da10856dd8d5cf644b
Trevor Swanson <83826109+trevorswanson@users.noreply.github.com>
2024-02-10 20:04:20 -0500

    fix: Output month name instead of number in conversation group name

--- client/src/utils/convos.ts
+++ client/src/utils/convos.ts
@@ -1,4 +1,4 @@
-import { parseISO, isToday, isWithinInterval, subDays, getMonth, getYear, startOfDay, startOfYear } from 'date-fns';
+import { parseISO, isToday, isWithinInterval, subDays, getMonth, getYear, startOfDay, startOfYear, format } from 'date-fns';
 import type {
   TConversation,
   ConversationData,
@@ -21,7 +21,7 @@ const getGroupName = (date: Date) => {
     return 'Previous 30 days';
   }
   if (isWithinInterval(date, { start: startOfYear(now), end: now })) {
-    return ' ' + getMonth(date).toString();
+    return ' ' + format(getMonth(date), 'MMMM');
   }
   return ' ' + getYear(date).toString();
 };
