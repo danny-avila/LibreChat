6b86d5eb413eb535d72d3d0e9f970ceb75353bd8
Berry-13 <81851188+Berry-13@users.noreply.github.com>
2024-02-23 18:46:50 +0100

    revert(types.ts): showHelpAndFaq

--- packages/data-provider/src/types.ts
+++ packages/data-provider/src/types.ts
@@ -210,7 +210,6 @@ export type TStartupConfig = {
   emailEnabled: boolean;
   checkBalance: boolean;
   showBirthdayIcon: boolean;
-  showHelpAndFaq: boolean;
   helpAndFaqURL: string;
   customFooter?: string;
 };
