23d83cf77dec72081858248d8b0b24727d34f212
Shi Jin <jinzishuai@gmail.com>
2024-02-24 15:46:08 -0700

    swap order

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -308,12 +308,6 @@ export default function Nav({ navVisible, setNavVisible }) {
                     text={localize('com_ui_ask_me_anything')}
                     clickHandler={user ? openAskMeAnythingHandler : navigateToRegister}
                   />
-                  <NavLink
-                    className="flex w-full cursor-pointer items-center gap-3 rounded-none px-3 py-3 text-sm text-white transition-colors duration-200 hover:bg-gray-700"
-                    svg={() => <LeaderboardIcon />}
-                    text={localize('com_ui_referrals_leaderboard')}
-                    clickHandler={user ? openLeaderboardHandler : navigateToRegister}
-                  />
                   {window.location.hostname !== "drhu.aitok.ai" && (
                   <NavLink
                     className="flex w-full cursor-pointer items-center gap-3 rounded-none px-3 py-3 text-sm text-white transition-colors duration-200 hover:bg-gray-700"
@@ -326,6 +320,12 @@ export default function Nav({ navVisible, setNavVisible }) {
                     clickHandler={user ? copyLinkHandler : navigateToRegister}
                   />
                   )}
+                  <NavLink
+                    className="flex w-full cursor-pointer items-center gap-3 rounded-none px-3 py-3 text-sm text-white transition-colors duration-200 hover:bg-gray-700"
+                    svg={() => <LeaderboardIcon />}
+                    text={localize('com_ui_referrals_leaderboard')}
+                    clickHandler={user ? openLeaderboardHandler : navigateToRegister}
+                  />
                 </nav>
               </div>
             </div>
