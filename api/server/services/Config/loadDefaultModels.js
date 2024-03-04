27eff4f14695a992a18b9ad743b92c0e7b59d696
Danny Avila <messagedaniel@protonmail.com>
2024-03-04 08:54:21 -0500

    chore(loadDefaultModels): change naming of assistant -> assistants

--- api/server/services/Config/loadDefaultModels.js
+++ api/server/services/Config/loadDefaultModels.js
@@ -24,7 +24,7 @@ async function loadDefaultModels(req) {
     azure: useAzurePlugins,
     plugins: true,
   });
-  const assistant = await getOpenAIModels({ assistants: true });
+  const assistants = await getOpenAIModels({ assistants: true });
 
   return {
     [EModelEndpoint.openAI]: openAI,
@@ -34,7 +34,7 @@ async function loadDefaultModels(req) {
     [EModelEndpoint.azureOpenAI]: azureOpenAI,
     [EModelEndpoint.bingAI]: ['BingAI', 'Sydney'],
     [EModelEndpoint.chatGPTBrowser]: chatGPTBrowser,
-    [EModelEndpoint.assistants]: assistant,
+    [EModelEndpoint.assistants]: assistants,
   };
 }
 
