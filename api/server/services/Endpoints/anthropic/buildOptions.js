6381184247c974109c636dde1aed880561050a8f
Danny Avila <messagedaniel@protonmail.com>
2024-03-05 14:56:23 -0500

    feat(anthropic): resendImages feature

--- api/server/services/Endpoints/anthropic/buildOptions.js
+++ api/server/services/Endpoints/anthropic/buildOptions.js
@@ -1,9 +1,10 @@
 const buildOptions = (endpoint, parsedBody) => {
-  const { modelLabel, promptPrefix, ...rest } = parsedBody;
+  const { modelLabel, promptPrefix, resendImages, ...rest } = parsedBody;
   const endpointOption = {
     endpoint,
     modelLabel,
     promptPrefix,
+    resendImages,
     modelOptions: {
       ...rest,
     },
