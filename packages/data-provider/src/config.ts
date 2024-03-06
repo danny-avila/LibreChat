388dc7733c0809e821d255c9f5614fb32e8bc333
Danny Avila <messagedaniel@protonmail.com>
2024-03-05 23:12:04 -0500

    refactor(validateVisionModel): use object as arg, pass in additional/available models

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -311,11 +311,20 @@ export const supportsBalanceCheck = {
 
 export const visionModels = ['gpt-4-vision', 'llava-13b', 'gemini-pro-vision', 'claude-3'];
 
-export function validateVisionModel(
-  model: string | undefined,
-  additionalModels: string[] | undefined = [],
-) {
+export function validateVisionModel({
+  model,
+  additionalModels = [],
+  availableModels,
+}: {
+  model: string;
+  additionalModels?: string[];
+  availableModels?: string[];
+}) {
   if (!model) {
+    return false;
+  }
+
+  if (availableModels && !availableModels.includes(model)) {
     return false;
   }
 
