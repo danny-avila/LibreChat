418d8f9e02d25ed0d138b94571b8281f01ab007d
Danny Avila <messagedaniel@protonmail.com>
2024-03-10 12:52:29 -0400

    chore: remove forms provider

--- client/src/components/Chat/Input/ChatForm.tsx
+++ client/src/components/Chat/Input/ChatForm.tsx
@@ -1,7 +1,7 @@
 import { useRecoilState } from 'recoil';
 import { useCallback, useRef } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
-import { useForm, FormProvider, Controller } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import {
   supportsFiles,
   mergeFileConfig,
@@ -63,84 +63,82 @@ export default function ChatForm({ index = 0 }) {
   const text = methods.watch('text');
 
   return (
-    <FormProvider {...methods}>
-      <form
-        onSubmit={methods.handleSubmit((data) => submitMessage(data))}
-        className="stretch mx-2 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl"
-      >
-        <div className="relative flex h-full flex-1 items-stretch md:flex-col">
-          <div className="flex w-full items-center">
-            <div className="[&:has(textarea:focus)]:border-token-border-xheavy dark:border-token-border-medium border-token-border-medium bg-token-main-surface-primary relative flex w-full flex-grow flex-col overflow-hidden rounded-2xl border dark:text-white [&:has(textarea:focus)]:shadow-[0_2px_6px_rgba(0,0,0,.05)]">
-              <FileRow
-                files={files}
-                setFiles={setFiles}
-                setFilesLoading={setFilesLoading}
-                Wrapper={({ children }) => (
-                  <div className="mx-2 mt-2 flex flex-wrap gap-2 px-2.5 md:pl-0 md:pr-4">
-                    {children}
-                  </div>
-                )}
+    <form
+      onSubmit={methods.handleSubmit((data) => submitMessage(data))}
+      className="stretch mx-2 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl"
+    >
+      <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+        <div className="flex w-full items-center">
+          <div className="[&:has(textarea:focus)]:border-token-border-xheavy dark:border-token-border-medium border-token-border-medium bg-token-main-surface-primary relative flex w-full flex-grow flex-col overflow-hidden rounded-2xl border dark:text-white [&:has(textarea:focus)]:shadow-[0_2px_6px_rgba(0,0,0,.05)]">
+            <FileRow
+              files={files}
+              setFiles={setFiles}
+              setFilesLoading={setFilesLoading}
+              Wrapper={({ children }) => (
+                <div className="mx-2 mt-2 flex flex-wrap gap-2 px-2.5 md:pl-0 md:pr-4">
+                  {children}
+                </div>
+              )}
+            />
+            {endpoint && (
+              <Controller
+                name="text"
+                control={methods.control}
+                render={({ field: { onChange, onBlur, value, name, ref } }) => {
+                  return (
+                    <TextareaAutosize
+                      name={name}
+                      autoFocus
+                      ref={(e) => {
+                        ref(e);
+                        textAreaRef.current = e;
+                      }}
+                      value={value}
+                      onBlur={onBlur}
+                      onChange={onChange}
+                      disabled={!!requiresKey}
+                      onPaste={handlePaste}
+                      onKeyUp={handleKeyUp}
+                      onKeyDown={handleKeyDown}
+                      onCompositionStart={handleCompositionStart}
+                      onCompositionEnd={handleCompositionEnd}
+                      id="prompt-textarea"
+                      tabIndex={0}
+                      data-testid="text-input"
+                      style={{ height: 44, overflowY: 'auto' }}
+                      rows={1}
+                      className={cn(
+                        supportsFiles[endpointType ?? endpoint ?? ''] &&
+                          !endpointFileConfig?.disabled
+                          ? ' pl-10 md:pl-[55px]'
+                          : 'pl-3 md:pl-4',
+                        'm-0 w-full resize-none border-0 bg-transparent py-[10px] pr-10 placeholder-black/50 focus:ring-0 focus-visible:ring-0 dark:bg-transparent dark:placeholder-white/50 md:py-3.5 md:pr-12 ',
+                        removeFocusOutlines,
+                        'max-h-[65vh] md:max-h-[85vh]',
+                      )}
+                    />
+                  );
+                }}
               />
-              {endpoint && (
-                <Controller
-                  name="text"
-                  control={methods.control}
-                  render={({ field: { onChange, onBlur, value, name, ref } }) => {
-                    return (
-                      <TextareaAutosize
-                        name={name}
-                        autoFocus
-                        ref={(e) => {
-                          ref(e);
-                          textAreaRef.current = e;
-                        }}
-                        value={value}
-                        onChange={onChange}
-                        onBlur={onBlur}
-                        disabled={!!requiresKey}
-                        onPaste={handlePaste}
-                        onKeyUp={handleKeyUp}
-                        onKeyDown={handleKeyDown}
-                        onCompositionStart={handleCompositionStart}
-                        onCompositionEnd={handleCompositionEnd}
-                        id="prompt-textarea"
-                        tabIndex={0}
-                        data-testid="text-input"
-                        style={{ height: 44, overflowY: 'auto' }}
-                        rows={1}
-                        className={cn(
-                          supportsFiles[endpointType ?? endpoint ?? ''] &&
-                            !endpointFileConfig?.disabled
-                            ? ' pl-10 md:pl-[55px]'
-                            : 'pl-3 md:pl-4',
-                          'm-0 w-full resize-none border-0 bg-transparent py-[10px] pr-10 placeholder-black/50 focus:ring-0 focus-visible:ring-0 dark:bg-transparent dark:placeholder-white/50 md:py-3.5 md:pr-12 ',
-                          removeFocusOutlines,
-                          'max-h-[65vh] md:max-h-[85vh]',
-                        )}
-                      />
-                    );
-                  }}
+            )}
+            <AttachFile
+              endpoint={_endpoint ?? ''}
+              endpointType={endpointType}
+              disabled={requiresKey}
+            />
+            {isSubmitting && showStopButton ? (
+              <StopButton stop={handleStopGenerating} setShowStopButton={setShowStopButton} />
+            ) : (
+              endpoint && (
+                <SendButton
+                  ref={submitButtonRef}
+                  disabled={!text || !!(filesLoading || isSubmitting || requiresKey)}
                 />
-              )}
-              <AttachFile
-                endpoint={_endpoint ?? ''}
-                endpointType={endpointType}
-                disabled={requiresKey}
-              />
-              {isSubmitting && showStopButton ? (
-                <StopButton stop={handleStopGenerating} setShowStopButton={setShowStopButton} />
-              ) : (
-                endpoint && (
-                  <SendButton
-                    ref={submitButtonRef}
-                    disabled={!text || !!(filesLoading || isSubmitting || requiresKey)}
-                  />
-                )
-              )}
-            </div>
+              )
+            )}
           </div>
         </div>
-      </form>
-    </FormProvider>
+      </div>
+    </form>
   );
 }
