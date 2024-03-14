b968186730772bf8592005539075d5b77acd07d9
MACHINSOFT <110278369+machinsoft@users.noreply.github.com>
2024-03-14 16:07:55 +0300

    ⛔ style: Auth Error and Preset Items Styling (#2069)
    * Change the style of the error message.
    
    * ui preset items
    
    * fix style
    
    * Change the color of the border and adjust the background of the selected input

--- client/src/components/Auth/Login.tsx
+++ client/src/components/Auth/Login.tsx
@@ -105,7 +105,7 @@ function Login() {
         </h1>
         {error && (
           <div
-            className="relative mt-4 rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700 dark:bg-gray-900 dark:text-red-500"
+            className="rounded-md border border-red-500 bg-red-500/10 px-3 py-2 text-sm text-gray-600 dark:text-gray-200"
             role="alert"
           >
             {localize(getLoginError(error))}
