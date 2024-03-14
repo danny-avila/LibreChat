5c0c84cab8f838ba4402d02ed19d4c82c61536d6
Alexei Smirnov <alexei@virtuman.com>
2024-03-14 18:26:44 +0300

    🌍 : Update Russian Translation (#2061)
    * feat(chore): add missing translations in Ru.tsx
    
    * feat(chore): add missing translation for My Files menu and headers
    
    * change com_ui_my_files to com_ui_nav_files
    
    * move useLocalize above utils
    
    * feat(chore): add missing translation for My Files menu and headers

--- client/src/components/Chat/Input/Files/FilesView.tsx
+++ client/src/components/Chat/Input/Files/FilesView.tsx
@@ -3,9 +3,12 @@ import type { TFile } from 'librechat-data-provider';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '~/components/ui';
 import { useGetFiles } from '~/data-provider';
 import { DataTable, columns } from './Table';
+import { useLocalize } from '~/hooks';
 import { cn } from '~/utils/';
 
 export default function Files({ open, onOpenChange }) {
+  const localize = useLocalize();
+
   const { data: files = [] } = useGetFiles<TFile[]>({
     select: (files) =>
       files.map((file) => {
@@ -27,7 +30,7 @@ export default function Files({ open, onOpenChange }) {
       <DialogContent className={cn('overflow-x-auto shadow-2xl dark:bg-gray-900 dark:text-white')}>
         <DialogHeader>
           <DialogTitle className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-200">
-            My Files
+            {localize('com_nav_my_files')}
           </DialogTitle>
         </DialogHeader>
         <div className="overflow-x-auto p-0 sm:p-6 sm:pt-4">
