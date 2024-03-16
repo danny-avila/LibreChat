a1e8f0b3d325ee7703d02b58315ce9b38816da27
Berry-13 <81851188+Berry-13@users.noreply.github.com>
2024-03-17 00:59:36 +0100

    style: assistant panel

--- client/src/components/SidePanel/Files/PanelFileCell.tsx
+++ client/src/components/SidePanel/Files/PanelFileCell.tsx
@@ -77,7 +77,7 @@ export default function PanelFileCell({ row }: { row: Row<TFile> }) {
     return (
       <div
         onClick={handleFileClick}
-        className="flex cursor-pointer gap-2 rounded-md dark:hover:bg-gray-900"
+        className="flex cursor-pointer gap-2 rounded-md dark:hover:bg-gray-700"
       >
         <ImagePreview
           url={file.filepath}
