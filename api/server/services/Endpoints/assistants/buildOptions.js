d8c30e37167cf825fad4ea7534577b2135772987
Danny Avila <messagedaniel@protonmail.com>
2024-03-18 19:48:42 -0400

    🪰 fix: Azure Parsing and Assistants Payload (#2133)
    * fix(azure): fix regex to prevent edge cases
    
    * fix(assistants): pass relevant endpoint options to avoid sending them to API

--- api/server/services/Endpoints/assistants/buildOptions.js
+++ api/server/services/Endpoints/assistants/buildOptions.js
@@ -1,9 +1,10 @@
 const buildOptions = (endpoint, parsedBody) => {
   // eslint-disable-next-line no-unused-vars
-  const { promptPrefix, chatGptLabel, resendImages, imageDetail, ...rest } = parsedBody;
+  const { promptPrefix, assistant_id, ...rest } = parsedBody;
   const endpointOption = {
     endpoint,
     promptPrefix,
+    assistant_id,
     modelOptions: {
       ...rest,
     },
