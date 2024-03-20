d0fb426606795f3734e627bed15738abb6af1959
Ivan Dachev <i_dachev@yahoo.co.uk>
2024-03-20 13:26:16 +0200

    Add CONSOLE_JSON

---
+++ docker-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+[ "$1" = -x ] && shift && set -x
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+TAG=$1
+
+if [[ -z "${TAG}" ]]; then
+  TAG=${LIBRE_CHAT_DOCKER_TAG}
+fi
+
+if [[ -z "${TAG}" ]]; then
+  TAG=latest
+fi
+
+LOCAL_DOCKER_IMG=librechat:${TAG}
+
+set -e
+
+docker build -t ${LOCAL_DOCKER_IMG} .
