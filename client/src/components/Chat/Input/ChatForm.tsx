91e47c79e694d92688ab10cdd6f271a76e6ba751
Danny Avila <messagedaniel@protonmail.com>
2024-03-21 08:58:22 -0400

    style: lower textarea max height slightly

--- client/src/components/Chat/Input/ChatForm.tsx
+++ client/src/components/Chat/Input/ChatForm.tsx
@@ -121,7 +121,7 @@ const ChatForm = ({ index = 0 }) => {
                     : 'pl-3 md:pl-4',
                   'm-0 w-full resize-none border-0 bg-transparent py-[10px] pr-10 placeholder-black/50 focus:ring-0 focus-visible:ring-0 dark:bg-transparent dark:placeholder-white/50 md:py-3.5 md:pr-12 ',
                   removeFocusOutlines,
-                  'max-h-[65vh] md:max-h-[85vh]',
+                  'max-h-[65vh] md:max-h-[75vh]',
                 )}
               />
             )}
