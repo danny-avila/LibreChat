d28e588edcdae8a7b14fbe4b1548d65841a58a19
Danny Avila <messagedaniel@protonmail.com>
2024-03-21 14:04:21 -0400

    fix(assistants): render streaming cursor

--- client/src/components/Chat/Messages/Content/Part.tsx
+++ client/src/components/Chat/Messages/Content/Part.tsx
@@ -17,6 +17,7 @@ const DisplayMessage = ({ text, isCreatedByUser = false, message, showCursor }: 
   return (
     <div
       className={cn(
+        showCursor && !!text?.length ? 'result-streaming' : '',
         'markdown prose dark:prose-invert light w-full break-words',
         isCreatedByUser ? 'whitespace-pre-wrap dark:text-gray-20' : 'dark:text-gray-70',
       )}
