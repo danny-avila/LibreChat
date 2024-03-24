bb713b2568688e3c0bab232b61f8918dc6ee5a6f
Berry-13 <81851188+Berry-13@users.noreply.github.com>
2024-03-24 21:38:31 +0100

    style: update settings dark theme

--- client/src/components/Nav/Settings.tsx
+++ client/src/components/Nav/Settings.tsx
@@ -98,11 +98,11 @@ export default function Settings({ open, onOpenChange }: TDialogProps) {
               </Tabs.Trigger>
               <Tabs.Trigger
                 className={cn(
-                  'group m-1 flex items-center justify-start gap-2 rounded-md px-2 py-1.5 text-sm text-black radix-state-active:bg-white radix-state-active:text-black dark:text-white dark:radix-state-active:bg-gray-750',
+                  'group m-1 flex items-center justify-start gap-2 rounded-md px-2 py-1.5 text-sm text-black radix-state-active:bg-white radix-state-active:text-black dark:text-white dark:radix-state-active:bg-gray-600',
                   isSmallScreen
                     ? 'flex-1 flex-col items-center justify-center text-sm dark:text-gray-500 dark:radix-state-active:text-white'
                     : 'bg-white radix-state-active:bg-gray-200',
-                  isSmallScreen ? '' : 'dark:bg-gray-800',
+                  isSmallScreen ? '' : 'dark:bg-gray-700',
                 )}
                 value={SettingsTabValues.SPEECH}
                 style={{ userSelect: 'none' }}
