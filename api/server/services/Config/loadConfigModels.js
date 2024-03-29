45cb52421b57b12bb2ef1ac03a5b02666a939c27
Danny Avila <messagedaniel@protonmail.com>
2024-03-29 11:49:38 -0400

    ⚓ refactor(loadConfigModels): Stricter Default Model Fallback (#2239)
    * chore: add TEndpoint type/typedef
    
    * refactor(loadConfigModels.spec): stricter default model matching (fails with current impl.)
    
    * refactor(loadConfigModels): return default models on endpoint basis and not fetch basis
    
    * refactor: rename `uniqueKeyToNameMap` to `uniqueKeyToEndpointsMap` for clarity

--- api/server/services/Config/loadConfigModels.js
+++ api/server/services/Config/loadConfigModels.js
@@ -46,12 +46,23 @@ async function loadConfigModels(req) {
       (endpoint.models.fetch || endpoint.models.default),
   );
 
-  const fetchPromisesMap = {}; // Map for promises keyed by unique combination of baseURL and apiKey
-  const uniqueKeyToNameMap = {}; // Map to associate unique keys with endpoint names
+  /**
+   * @type {Record<string, string[]>}
+   * Map for promises keyed by unique combination of baseURL and apiKey */
+  const fetchPromisesMap = {};
+  /**
+   * @type {Record<string, string[]>}
+   * Map to associate unique keys with endpoint names; note: one key may can correspond to multiple endpoints */
+  const uniqueKeyToEndpointsMap = {};
+  /**
+   * @type {Record<string, Partial<TEndpoint>>}
+   * Map to associate endpoint names to their configurations */
+  const endpointsMap = {};
 
   for (let i = 0; i < customEndpoints.length; i++) {
     const endpoint = customEndpoints[i];
     const { models, name, baseURL, apiKey } = endpoint;
+    endpointsMap[name] = endpoint;
 
     const API_KEY = extractEnvVariable(apiKey);
     const BASE_URL = extractEnvVariable(baseURL);
@@ -69,11 +80,9 @@ async function loadConfigModels(req) {
           apiKey: API_KEY,
           name,
           userIdQuery: models.userIdQuery,
-        }).then((result) => {
-          return !result?.length ? models.default ?? [] : result;
         });
-      uniqueKeyToNameMap[uniqueKey] = uniqueKeyToNameMap[uniqueKey] || [];
-      uniqueKeyToNameMap[uniqueKey].push(name);
+      uniqueKeyToEndpointsMap[uniqueKey] = uniqueKeyToEndpointsMap[uniqueKey] || [];
+      uniqueKeyToEndpointsMap[uniqueKey].push(name);
       continue;
     }
 
@@ -88,10 +97,11 @@ async function loadConfigModels(req) {
   for (let i = 0; i < fetchedData.length; i++) {
     const currentKey = uniqueKeys[i];
     const modelData = fetchedData[i];
-    const associatedNames = uniqueKeyToNameMap[currentKey];
+    const associatedNames = uniqueKeyToEndpointsMap[currentKey];
 
     for (const name of associatedNames) {
-      modelsConfig[name] = modelData;
+      const endpoint = endpointsMap[name];
+      modelsConfig[name] = !modelData?.length ? endpoint.models.default ?? [] : modelData;
     }
   }
 
