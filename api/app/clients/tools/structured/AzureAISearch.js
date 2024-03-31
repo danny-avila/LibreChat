61d88c56b90f8606f643702253933c1d8ec33d89
yinglj <yinglj@gmail.com>
2024-03-31 10:53:29 +0800

    fix ESLint found 6 errors of AzureAiSearch.js

--- api/app/clients/tools/structured/AzureAISearch.js
+++ api/app/clients/tools/structured/AzureAISearch.js
@@ -18,22 +18,51 @@ class AzureAISearch extends StructuredTool {
     const getValue = (fieldNames, defaultValue) => {
       for (const name of fieldNames) {
         const value = fields[name] || process.env[name];
-        if (value !== undefined && value !== null) return value;
+        if (value !== undefined && value !== null) {
+          return value;
+        }
       }
       return defaultValue;
     };
 
-    this.serviceEndpoint = getValue(['AZURE_AI_SEARCH_SERVICE_ENDPOINT', 'AZURE_COGNITIVE_SEARCH_SERVICE_ENDPOINT'], this.getServiceEndpoint());
-    this.indexName = getValue(['AZURE_AI_SEARCH_INDEX_NAME', 'AZURE_COGNITIVE_SEARCH_INDEX_NAME'], this.getIndexName());
-    this.apiKey = getValue(['AZURE_AI_SEARCH_API_KEY', 'AZURE_COGNITIVE_SEARCH_API_KEY'], this.getApiKey());
-    this.apiVersion = getValue(['AZURE_AI_SEARCH_API_VERSION', 'AZURE_COGNITIVE_SEARCH_API_VERSION'], AzureAISearch.DEFAULT_API_VERSION);
-    this.queryType = getValue(['AZURE_AI_SEARCH_SEARCH_OPTION_QUERY_TYPE', 'AZURE_COGNITIVE_SEARCH_SEARCH_OPTION_QUERY_TYPE'], AzureAISearch.DEFAULT_QUERY_TYPE);
-    this.top = getValue(['AZURE_AI_SEARCH_SEARCH_OPTION_TOP', 'AZURE_COGNITIVE_SEARCH_SEARCH_OPTION_TOP'], AzureAISearch.DEFAULT_TOP);
+    this.serviceEndpoint = getValue(
+      ['AZURE_AI_SEARCH_SERVICE_ENDPOINT', 'AZURE_COGNITIVE_SEARCH_SERVICE_ENDPOINT'],
+      this.getServiceEndpoint(),
+    );
+    this.indexName = getValue(
+      ['AZURE_AI_SEARCH_INDEX_NAME', 'AZURE_COGNITIVE_SEARCH_INDEX_NAME'],
+      this.getIndexName(),
+    );
+    this.apiKey = getValue(
+      ['AZURE_AI_SEARCH_API_KEY', 'AZURE_COGNITIVE_SEARCH_API_KEY'],
+      this.getApiKey(),
+    );
+    this.apiVersion = getValue(
+      ['AZURE_AI_SEARCH_API_VERSION', 'AZURE_COGNITIVE_SEARCH_API_VERSION'],
+      AzureAISearch.DEFAULT_API_VERSION,
+    );
+    this.queryType = getValue(
+      [
+        'AZURE_AI_SEARCH_SEARCH_OPTION_QUERY_TYPE',
+        'AZURE_COGNITIVE_SEARCH_SEARCH_OPTION_QUERY_TYPE',
+      ],
+      AzureAISearch.DEFAULT_QUERY_TYPE,
+    );
+    this.top = getValue(
+      ['AZURE_AI_SEARCH_SEARCH_OPTION_TOP', 'AZURE_COGNITIVE_SEARCH_SEARCH_OPTION_TOP'],
+      AzureAISearch.DEFAULT_TOP,
+    );
     this.select = this.getSelect();
   }
 
   initializeClient() {
-    this.client = new SearchClient(this.serviceEndpoint, this.indexName, new AzureKeyCredential(this.apiKey), { apiVersion: this.apiVersion });
+    // eslint-disable-next-line max-len
+    this.client = new SearchClient(
+      this.serviceEndpoint,
+      this.indexName,
+      new AzureKeyCredential(this.apiKey),
+      { apiVersion: this.apiVersion },
+    );
   }
 
   initializeSchema() {
@@ -44,8 +73,7 @@ class AzureAISearch extends StructuredTool {
 
   name = 'azure-ai-search';
 
-  description =
-    'Use the \'azure-ai-search\' tool to retrieve search results relevant to your input';
+  description = 'Use the \'azure-ai-search\' tool to retrieve search results relevant to your input';
 
   async _call(data) {
     const { query } = data;
@@ -53,11 +81,11 @@ class AzureAISearch extends StructuredTool {
       const searchOptions = {
         queryType: this.queryType,
         top: this.top,
-        select: this.select
+        select: this.select,
       };
 
       const searchResults = await this.client.search(query, searchOptions);
-      return JSON.stringify(searchResults.results.map(result => result.document));
+      return JSON.stringify(searchResults.results.map((result) => result.document));
     } catch (error) {
       console.error(`Azure AI Search request failed: ${error}`);
       return 'There was an error with Azure AI Search.';
