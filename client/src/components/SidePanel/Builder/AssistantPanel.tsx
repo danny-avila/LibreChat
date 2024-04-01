d07396d308c9c4a2a092c46340f1c47e1b1d3951
Danny Avila <messagedaniel@protonmail.com>
2024-04-01 09:20:11 -0400

    🐞 fix: Handle Empty Model Error in Assistants Form (#2265)

--- client/src/components/SidePanel/Builder/AssistantPanel.tsx
+++ client/src/components/SidePanel/Builder/AssistantPanel.tsx
@@ -317,19 +317,28 @@ export default function AssistantPanel({
             <Controller
               name="model"
               control={control}
-              render={({ field }) => (
-                <SelectDropDown
-                  emptyTitle={true}
-                  value={field.value}
-                  setValue={field.onChange}
-                  availableValues={modelsQuery.data?.[EModelEndpoint.assistants] ?? []}
-                  showAbove={false}
-                  showLabel={false}
-                  className={cn(
-                    cardStyle,
-                    'flex h-[40px] w-full flex-none items-center justify-center px-4 hover:cursor-pointer',
+              rules={{ required: true, minLength: 1 }}
+              render={({ field, fieldState: { error } }) => (
+                <>
+                  <SelectDropDown
+                    emptyTitle={true}
+                    value={field.value}
+                    setValue={field.onChange}
+                    availableValues={modelsQuery.data?.[EModelEndpoint.assistants] ?? []}
+                    showAbove={false}
+                    showLabel={false}
+                    className={cn(
+                      cardStyle,
+                      'flex h-[40px] w-full flex-none items-center justify-center px-4 hover:cursor-pointer',
+                    )}
+                    containerClassName={cn('rounded-md', error ? 'border-red-500 border-2' : '')}
+                  />
+                  {error && (
+                    <span className="text-sm text-red-500 transition duration-300 ease-in-out">
+                      {localize('com_ui_field_required')}
+                    </span>
                   )}
-                />
+                </>
               )}
             />
           </div>
