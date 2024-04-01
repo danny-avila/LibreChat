fbf316ee0ba697591d8b24b2aace0e75bb0f9739
Danny Avila <messagedaniel@protonmail.com>
2024-04-01 13:40:21 -0400

    📋 fix: Ensure Textarea Resizes in Clipboard Edge Case (#2268)
    * chore: ts-ignore fake conversation data used for testing
    
    * chore(useTextarea): import helper functions to declutter hook
    
    * fix(Textarea): reset textarea value explicitly by resetting `textAreaRef.current.value`

--- client/src/utils/convos.fakeData.ts
+++ client/src/utils/convos.fakeData.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import { EModelEndpoint, ImageDetail } from 'librechat-data-provider';
 import type { ConversationData } from 'librechat-data-provider';
 
