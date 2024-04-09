f3e26fdf291e12f2705d50198793eb269e6e7679
yinglj <yinglj@gmail.com>
2024-04-09 10:36:44 +0800

    bueatify the tabs color style

--- client/src/components/Profile/index.tsx
+++ client/src/components/Profile/index.tsx
@@ -593,21 +593,21 @@ function ProfileContent() {
             onValueChange={(value: string) => setTabValue(value)}
             className={defaultClasses}
           >
-            <TabsList className="bg-white">
+            <TabsList className="rounded-lg bg-blue-500 dark:bg-blue-500">
               {userId === user?.id && (
-                <TabsTrigger value="likes" className="text-gray-500 dark:text-gray-200">
+                <TabsTrigger value="likes" className="px-4 py-2 text-white dark:text-white">
                   {localize('com_ui_my_likes')}
                 </TabsTrigger>
               )}
               {userId != user?.id && (
-                <TabsTrigger value="conversations" className="text-gray-500 dark:text-gray-200">
+                <TabsTrigger value="conversations" className="px-4 py-2 text-white dark:text-white">
                   {localize('com_ui_conversations')}
                 </TabsTrigger>
               )}
-              <TabsTrigger value="followers" className="text-gray-500 dark:text-gray-200">
+              <TabsTrigger value="followers" className="px-4 py-2 text-white dark:text-white">
                 {localize('com_ui_followers')}
               </TabsTrigger>
-              <TabsTrigger value="following" className="text-gray-500 dark:text-gray-200">
+              <TabsTrigger value="following" className="px-4 py-2 text-white dark:text-white">
                 {localize('com_ui_following')}
               </TabsTrigger>
             </TabsList>
