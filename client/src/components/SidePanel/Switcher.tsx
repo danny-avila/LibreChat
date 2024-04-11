3f5da97ce861e733ce6036f670f28eccf808854d
Danny Avila <danny@librechat.ai>
2024-04-10 23:43:37 -0400

    fix(SidePanel): intended behavior for non-user provided keys

--- client/src/components/SidePanel/Switcher.tsx
+++ client/src/components/SidePanel/Switcher.tsx
@@ -1,19 +1,13 @@
 import { EModelEndpoint } from 'librechat-data-provider';
 import type { SwitcherProps } from '~/common';
 import AssistantSwitcher from './AssistantSwitcher';
-import { useChatContext } from '~/Providers';
 import ModelSwitcher from './ModelSwitcher';
 
 export default function Switcher(props: SwitcherProps) {
-  const { conversation } = useChatContext();
-  const { endpoint } = conversation ?? {};
-
-  if (!props.endpointKeyProvided) {
-    return null;
-  }
-
-  if (endpoint === EModelEndpoint.assistants) {
+  if (props.endpoint === EModelEndpoint.assistants && props.endpointKeyProvided) {
     return <AssistantSwitcher {...props} />;
+  } else if (props.endpoint === EModelEndpoint.assistants) {
+    return null;
   }
 
   return <ModelSwitcher {...props} />;
