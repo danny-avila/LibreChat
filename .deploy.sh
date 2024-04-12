c44191682baae222e67ad1cbcfbf66cc18e9a536
noobskie <t.noobskie@gmail.com>
2024-04-12 15:22:43 -0400

    deploy

---
+++ .deploy.sh
@@ -0,0 +1,13 @@
+  GNU nano 6.2                                                                                                                        chinagpt/.deploy.sh                                                                                                                                 
+#!/bin/bash
+# Pull the latest changes from your repository
+git pull
+
+# Stop and remove all containers
+sudo docker-compose down
+
+# Rebuild the services
+sudo docker-compose build --no-cache --force-recreate
+
+# Start up the services
+sudo docker-compose up --force-recreate -d
