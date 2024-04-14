b83583b68a7b30e20541aa40ef57f480633d7528
noobskie <t.noobskie@gmail.com>
2024-04-14 14:19:12 -0400

    deploy

--- .deploy.sh
+++ .deploy.sh
@@ -3,10 +3,10 @@
 git pull
 
 # Stop and remove all containers
-sudo docker-compose down
+sudo docker-compose -f docker-compose.yml -f docker-compose.override.yml down --remove-orphans
 
 # Rebuild the services
-sudo docker-compose build --no-cache --force-recreate
+sudo docker-compose -f docker-compose.yml -f docker-compose.override.yml build --no-cache
 
 # Start up the services
-sudo docker-compose up --force-recreate -d
+sudo docker-compose -f docker-compose.yml -f docker-compose.override.yml up --force-recreate -d
