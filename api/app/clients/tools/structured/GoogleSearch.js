e41d627c22674446e0cd31ba4875e9536cfdc1e1
Charl P. Botha <cpbotha@vxlabs.com>
2024-04-15 16:02:13 +0200

    Extract and return contents of search result pages

--- api/app/clients/tools/structured/GoogleSearch.js
+++ api/app/clients/tools/structured/GoogleSearch.js
@@ -2,6 +2,9 @@ const { z } = require('zod');
 const { Tool } = require('@langchain/core/tools');
 const { getEnvironmentVariable } = require('@langchain/core/utils/env');
 
+const { JSDOM, VirtualConsole } = require('jsdom');
+const { Readability } = require('@mozilla/readability');
+
 class GoogleSearchResults extends Tool {
   static lc_name() {
     return 'GoogleSearchResults';
@@ -33,6 +36,33 @@ class GoogleSearchResults extends Tool {
     });
   }
 
+  async handleItem(item, idx) {
+    // fetch page via URL, then parse into DOM for readability
+    // but first setup to ignore jsdom errors, because it often can't parse style sheets
+    // see https://stackoverflow.com/a/69958999/532513
+    const virtualConsole = new VirtualConsole();
+    virtualConsole.on('error', () => {
+      // No-op to skip console errors.
+    });
+    const dom = await fetch(item.link)
+      .then((res) => res.text())
+      .then((html) => new JSDOM(html, { virtualConsole }))
+      .then((dom) => dom.window.document);
+
+    // parse DOM using Readability
+    // title, content, textContent (this is what you want), length (characters), except,
+    const article = new Readability(dom).parse();
+
+    if (!article) {
+      return '';
+    }
+
+    // collapse empty lines in textContent, using start of line and end of line anchors
+    return `## Web page ${idx + 1}\nTitle: ${item.title}\n\nLink: ${
+      item.link
+    }\n\n${article.textContent.replace(/^[\s]*$/gm, '')}\n`;
+  }
+
   async _call(input) {
     const validationResult = this.schema.safeParse(input);
     if (!validationResult.success) {
@@ -58,7 +88,27 @@ class GoogleSearchResults extends Tool {
       throw new Error(`Request failed with status ${response.status}: ${json.error.message}`);
     }
 
-    return JSON.stringify(json);
+    // now go through all of the search results, and retrieve the text contents of all search result pages in parallel
+    const webPages = await Promise.all(json.items.map((item, idx) => this.handleItem(item, idx)));
+
+    // truncate each webPage to truncWords words
+    const truncWords = 1300;
+    const truncWebPages = webPages.map((webPage) => {
+      const words = webPage.split(' ');
+      if (words.length <= truncWords) {
+        return webPage;
+      } else {
+        return words.slice(0, truncWords).join(' ') + ' ...';
+      }
+    });
+
+    const res =
+      'When writing your response, please cite the web pages inline using `[^N]` (markdown footnote syntax) ' +
+      'with the number N from the `## Web page N` headings. List all pages at the end of your answer ' +
+      'in a markdown bullet list even if you don\'t cite them in the text.\n\n' +
+      `${truncWebPages.join('\n\n')}`;
+    //console.log(res);
+    return res;
   }
 }
 
