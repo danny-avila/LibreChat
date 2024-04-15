c18123f2a13f598a2b15486d7ba63d2db1bb0c03
noobskie <t.noobskie@gmail.com>
2024-04-15 14:56:07 -0400

    env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,64 +4,84 @@ import react from '@vitejs/plugin-react';
 import path, { resolve } from 'path';
 import type { Plugin } from 'vite';
 
+function htmlPlugin(env: ReturnType<typeof loadEnv>) {
+  return {
+    name: 'html-transform',
+    transformIndexHtml: {
+      enforce: 'pre' as const,
+      transform: (html: string): string =>
+        html.replace(/%(.*?)%/g, (match, p1) => env[p1] ?? match),
+    },
+  };
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    fs: {
-      cachedChecks: false,
-    },
-    host: 'localhost',
-    port: 3090,
-    strictPort: false,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3080',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      fs: {
+        cachedChecks: false,
       },
-      '/oauth': {
-        target: 'http://localhost:3080',
-        changeOrigin: true,
-      },
-    },
-  },
-  // All other env variables are filtered out
-  envDir: '../',
-  envPrefix: ['VITE_', 'SCRIPT_', 'DOMAIN_', 'ALLOW_'],
-  plugins: [react(), nodePolyfills(), sourcemapExclude({ excludeNodeModules: true })],
-  publicDir: './public',
-  build: {
-    sourcemap: process.env.NODE_ENV === 'development',
-    outDir: './dist',
-    rollupOptions: {
-      // external: ['uuid'],
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
+      host: 'localhost',
+      port: 3090,
+      strictPort: false,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3080',
+          changeOrigin: true,
+        },
+        '/oauth': {
+          target: 'http://localhost:3080',
+          changeOrigin: true,
         },
       },
-      /**
-       * Ignore "use client" waning since we are not using SSR
-       * @see {@link https://github.com/TanStack/query/pull/5161#issuecomment-1477389761 Preserve 'use client' directives TanStack/query#5161}
-       */
-      onwarn(warning, warn) {
-        if (
-          // warning.code === 'MODULE_LEVEL_DIRECTIVE' &&
-          warning.message.includes('Error when using sourcemap')
-        ) {
-          return;
-        }
-        warn(warning);
+    },
+    // All other env variables are filtered out
+    envDir: '../',
+    envPrefix: ['VITE_', 'SCRIPT_', 'DOMAIN_', 'ALLOW_'],
+    plugins: [
+      react(),
+      nodePolyfills(),
+      sourcemapExclude({ excludeNodeModules: true }),
+      htmlPlugin(env),
+    ],
+    publicDir: './public',
+    build: {
+      sourcemap: process.env.NODE_ENV === 'development',
+      outDir: './dist',
+      rollupOptions: {
+        // external: ['uuid'],
+        output: {
+          manualChunks: (id) => {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+        },
+        /**
+         * Ignore "use client" waning since we are not using SSR
+         * @see {@link https://github.com/TanStack/query/pull/5161#issuecomment-1477389761 Preserve 'use client' directives TanStack/query#5161}
+         */
+        onwarn(warning, warn) {
+          if (
+            // warning.code === 'MODULE_LEVEL_DIRECTIVE' &&
+            warning.message.includes('Error when using sourcemap')
+          ) {
+            return;
+          }
+          warn(warning);
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '~': path.join(__dirname, 'src/'),
-      $fonts: resolve('public/fonts'),
+    resolve: {
+      alias: {
+        '~': path.join(__dirname, 'src/'),
+        $fonts: resolve('public/fonts'),
+      },
     },
-  },
+  };
 });
 
 interface SourcemapExclude {
@@ -78,17 +98,6 @@ export function sourcemapExclude(opts?: SourcemapExclude): Plugin {
           map: { mappings: '' },
         };
       }
-    },
-  };
-}
-
-function htmlPlugin(env: ReturnType<typeof loadEnv>) {
-  return {
-    name: 'html-transform',
-    transformIndexHtml: {
-      enforce: 'pre' as const,
-      transform: (html: string): string =>
-        html.replace(/%(.*?)%/g, (match, p1) => env[p1] ?? match),
     },
   };
 }
