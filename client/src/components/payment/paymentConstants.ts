79b4796cca21783f4495487dbdcc28676d537219
noobskie <t.noobskie@gmail.com>
2024-04-18 13:04:48 -0400

    amount value

--- client/src/components/payment/paymentConstants.ts
+++ client/src/components/payment/paymentConstants.ts
@@ -3,7 +3,7 @@ export const tokenOptions = [
     tokens: 100000,
     label: 'com_token_package_label_100k',
     price: 'com_token_package_price_100k_global',
-    amount: 10,
+    amount: 1.5,
     currency: 'USD',
     priceId: 'price_1P6dqBHKD0byXXClWuA2RGY2', // 1.50 USD - only for global users
   },
@@ -11,7 +11,7 @@ export const tokenOptions = [
     tokens: 500000,
     label: 'com_token_package_label_500k',
     price: 'com_token_package_price_500k_global',
-    amount: 35,
+    amount: 5.0,
     currency: 'USD',
     priceId: 'price_1P6dqdHKD0byXXClcboa06Tu', // 5.00 USD - only for global users
   },
@@ -19,14 +19,14 @@ export const tokenOptions = [
     tokens: 1000000,
     label: 'com_token_package_label_1m',
     price: 'com_token_package_price_1m_global',
-    amount: 50,
+    amount: 7.5,
     priceId: 'price_1P6drEHKD0byXXClOjmSkPKm', // 7.50 USD - only for global users
   },
   {
     tokens: 10000000,
     label: 'com_token_package_label_10m',
     price: 'com_token_package_price_10m_global',
-    amount: 250,
+    amount: 40.0,
     currency: 'USD',
     priceId: 'price_1P6drxHKD0byXXClVVLokkLh', // 40.00 USD - only for global users
   },
