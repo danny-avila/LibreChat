09afaf4584e1d3b789935de44b62c0de09123adf
Danny Avila <danny@librechat.ai>
2024-04-22 12:52:30 -0400

    🔧 fix: Resolve Proper Dependencies to fix Application Error (#2488)
    * chore: bump data-provider
    
    * feat: script to check recent dependency updates
    
    * fix: override vite/rollup version for vite build fix
    - also remove unused vite-plugin-html
    - add vite build to file output command
    
    * chore: bump rollup override to last known working version (v4.16.0 is breaking)
    
    * chore(vite): increase file size cache for workbox
    
    * fix: resolve openai to last known version using assistants v1 latest features and default header
    
    * chore: update openrouter examples

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -39,6 +39,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['assets/**/*.{png,jpg,svg,ico}', '**/*.{js,css,html,ico,woff2}'],
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
       },
       manifest: {
         name: 'LibreChat',
