5e49b90363a29b27b16a87326c6f28dce34444f2
Danny Avila <danny@librechat.ai>
2024-04-24 18:10:52 -0400

    feat: `stop` conversation parameter

--- packages/data-provider/src/schemas.ts
+++ packages/data-provider/src/schemas.ts
@@ -283,6 +283,7 @@ export const tConversationSchema = z.object({
   instructions: z.string().optional(),
   /** Used to overwrite active conversation settings when saving a Preset */
   presetOverride: z.record(z.unknown()).optional(),
+  stop: z.array(z.string()).optional(),
 });
 
 export const tPresetSchema = tConversationSchema
@@ -319,7 +320,7 @@ export type TPreset = z.infer<typeof tPresetSchema>;
 
 export type TSetOption = (
   param: number | string,
-) => (newValue: number | string | boolean | Partial<TPreset>) => void;
+) => (newValue: number | string | boolean | string[] | Partial<TPreset>) => void;
 
 export type TConversation = z.infer<typeof tConversationSchema> & {
   presetOverride?: Partial<TPreset>;
