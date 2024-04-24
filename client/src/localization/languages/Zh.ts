1bd5f185d467e103b94e9c29128fd84d1b83769d
noobskie <t.noobskie@gmail.com>
2024-04-24 16:10:24 -0400

    formatting

--- client/src/localization/languages/Zh.ts
+++ client/src/localization/languages/Zh.ts
@@ -500,7 +500,7 @@ export default {
   com_auth_registration_success: '注册成功。请检查您的电子邮件以验证您的电子邮件地址。',
   com_token_usage_overview: '算力使用概览',
   com_token_usage_description:
-    '购买算力后，可以在我们平台上的各种语言模型中使用。每个模型都有一个相关的"燃烧率"（BR），它可以比较不同模型之间的算力消耗率。这个比率为您提供了一个乘数，反映了输入和输出的平均算力消耗。',
+    '算力可以在我们平台上的各种语言模型中使用。每个模型都有一个相关的"燃烧率"（BR），它可以比较不同模型之间的算力消耗率。这个比率为您提供了一个乘数，反映了输入和输出的平均算力消耗。',
   com_model_consumption_rates: '模型消耗率',
   com_model_rates_table: '下表概述了不同模型的燃烧率：',
   com_model: '模型',
@@ -515,12 +515,11 @@ export default {
   com_token_consumption_depends:
     '与模型交互时的算力消耗取决于输入和输出算力的总和。输入包括上下文算力和提示算力，其中上下文算力提供背景，提示算力指导模型的任务。',
   com_how_to_calculate_token_consumption: '如何计算算力消耗',
-  com_total_token_consumption_formula:
-    'For estimating total token consumption, utilize the following formula:',
+  com_total_token_consumption_formula: '要估算总算力消耗，请使用以下公式：',
   com_example_of_token_consumption: '算力消耗示例',
   com_important_considerations: '重要考虑因素',
   com_important_considerations_text:
-    'The provided burn rate is a tool to compare the token usage efficiency among different models, it is not a direct measure of total token cost. Be aware that token consumption can vary based on specific interactions, and the burn rate should be used for a rough estimation. The extent of your input will have a significant impact on the total token consumption.',
+    '提供的燃烧率是一个比较不同模型算力使用效率的工具，它不是总算力成本的直接衡量标准。请注意，算力消耗可能会根据具体的交互而有所不同，燃烧率应该用于粗略估计。您的输入程度将对总算力消耗产生重大影响。',
   com_comparative_analysis: '跨不同基准的AI模型比较分析',
   com_undergraduate_biology: '本科生水平生物学 MMLU',
   com_graduate_reasoning: '研究生水平推理 GFVR，Diamond',
@@ -532,9 +531,9 @@ export default {
   com_mixed_evaluations: '混合评估 Big-bench Hard',
   com_knowledge_qa: '知识问答 ARC Challenge',
   com_common_knowledge: '常识 Demknow',
-  com_ai_model_use_cases: 'AI Models and Their Use Cases',
+  com_ai_model_use_cases: 'AI模型及其用例',
   com_gpt_3_5_use_case:
-    '一个多功能模型，在各种任务中表现良好，尽管不如更高级的模型强大。用例：适用于一般语言任务和不需要最高性能的应用程序。',
+    '一个多功能模型，在各种任务中表现良好，尽管不如更高级的模型强大。用例：适用于一般语言任务和不需要最高性能的应用。',
   com_gpt_4_use_case:
     '一个强大的模型，在性能上紧跟 Claude 3 Opus，并在基于知识的问答方面表现出色。用例：非常适合需要深入理解、复杂推理和广泛知识检索的任务。',
   com_claude_3_haiku_use_case:
