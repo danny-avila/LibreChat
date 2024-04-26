e62b16bbb6e9c7df6672adb431b3e2301b7920a8
Martin Dahlö <m.dahlo@gmail.com>
2024-04-26 02:26:33 +0200

    📋 refactor: allow paste in confirm field when resetting passwords (#2542)
    * Disabled paste prevention in the confirm password field when resetting passwords.
    
    * chore(ResetPassword): remove comments
    
    ---------
    
    Co-authored-by: Danny Avila <danacordially@gmail.com>

--- client/src/components/Auth/ResetPassword.tsx
+++ client/src/components/Auth/ResetPassword.tsx
@@ -139,11 +139,6 @@ function ResetPassword() {
                   type="password"
                   id="confirm_password"
                   aria-label={localize('com_auth_password_confirm')}
-                  // uncomment to prevent pasting in confirm field
-                  onPaste={(e) => {
-                    e.preventDefault();
-                    return false;
-                  }}
                   {...register('confirm_password', {
                     validate: (value) =>
                       value === password || localize('com_auth_password_not_match'),
