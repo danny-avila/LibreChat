559c6d439a016744dcad648943f484f442fac8c9
Danny Avila <danny@librechat.ai>
2024-04-29 09:57:05 -0400

    feat: enforce modelSpecs if configured

--- api/server/middleware/buildEndpointOption.js
+++ api/server/middleware/buildEndpointOption.js
@@ -7,6 +7,7 @@ const anthropic = require('~/server/services/Endpoints/anthropic');
 const openAI = require('~/server/services/Endpoints/openAI');
 const custom = require('~/server/services/Endpoints/custom');
 const google = require('~/server/services/Endpoints/google');
+const { handleError } = require('~/server/utils');
 
 const buildFunction = {
   [EModelEndpoint.openAI]: openAI.buildOptions,
@@ -21,6 +22,35 @@ const buildFunction = {
 async function buildEndpointOption(req, res, next) {
   const { endpoint, endpointType } = req.body;
   const parsedBody = parseConvo({ endpoint, endpointType, conversation: req.body });
+
+  if (req.app.locals.modelSpecs?.list && req.app.locals.modelSpecs?.enforce) {
+    /** @type {{ list: TModelSpec[] }}*/
+    const { list } = req.app.locals.modelSpecs;
+    const { spec } = parsedBody;
+
+    if (!spec) {
+      return handleError(res, { text: 'No model spec selected' });
+    }
+
+    const currentModelSpec = list.find((s) => s.name === spec);
+    if (!currentModelSpec) {
+      return handleError(res, { text: 'Invalid model spec' });
+    }
+
+    if (endpoint !== currentModelSpec.preset.endpoint) {
+      return handleError(res, { text: 'Model spec mismatch' });
+    }
+
+    for (const [key, value] of Object.entries(currentModelSpec.preset)) {
+      if (key === 'endpoint') {
+        continue;
+      }
+      if (parsedBody[key] !== value) {
+        return handleError(res, { text: 'Model spec mismatch' });
+      }
+    }
+  }
+
   req.body.endpointOption = buildFunction[endpointType ?? endpoint](
     endpoint,
     parsedBody,
