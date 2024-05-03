c8baceac76d35c60c7aad617830add86f342aa5f
Danny Avila <danny@librechat.ai>
2024-05-03 12:49:26 -0400

    🐛 fix: Prevent Empty File Uploads & Assistants Fixes (#2611)
    * chore: update default models for openai/assistants
    
    * fix: allows assistants models fetching
    
    * change default models order, ensure assistant_id is defined if intended
    
    * fix: prevent empty files from being uploaded

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -345,14 +345,16 @@ export const alternateName = {
 
 export const defaultModels = {
   [EModelEndpoint.assistants]: [
+    'gpt-3.5-turbo',
     'gpt-3.5-turbo-0125',
+    'gpt-4-turbo',
+    'gpt-4-turbo-2024-04-09',
     'gpt-4-0125-preview',
     'gpt-4-turbo-preview',
     'gpt-4-1106-preview',
     'gpt-3.5-turbo-1106',
     'gpt-3.5-turbo-16k-0613',
     'gpt-3.5-turbo-16k',
-    'gpt-3.5-turbo',
     'gpt-4',
     'gpt-4-0314',
     'gpt-4-32k-0314',
@@ -387,6 +389,8 @@ export const defaultModels = {
   ],
   [EModelEndpoint.openAI]: [
     'gpt-3.5-turbo-0125',
+    'gpt-4-turbo',
+    'gpt-4-turbo-2024-04-09',
     'gpt-3.5-turbo-16k-0613',
     'gpt-3.5-turbo-16k',
     'gpt-4-turbo-preview',
