fb562620ddc6615ace55023a44ca5496e1519153
techwithanirudh <techwithanirudh@gmail.com>
2024-05-05 12:13:40 +0000

    feat: fix scrolling for dropdown, and make border width lighter

--- client/src/components/ui/Dropdown.tsx
+++ client/src/components/ui/Dropdown.tsx
@@ -67,7 +67,7 @@ const Dropdown: FC<DropdownProps> = ({
           </Listbox.Button>
           <Listbox.Options
             className={cn(
-              'absolute z-50 mt-1 flex max-h-[40vh] flex-col items-center gap-1 overflow-hidden rounded-lg border border-gray-300 bg-white p-1.5 text-gray-700 shadow-lg transition-opacity focus:outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-white',
+              'absolute z-50 mt-1 flex max-h-[40vh] flex-col items-start gap-1 overflow-auto rounded-lg border border-gray-300 bg-white p-1.5 text-gray-700 shadow-lg transition-opacity focus:outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-white',
               className,
             )}
             style={{ width: width ? `${width}px` : 'auto' }}
@@ -79,7 +79,7 @@ const Dropdown: FC<DropdownProps> = ({
                 className={cn(
                   'relative cursor-pointer select-none rounded border-gray-300 bg-white py-2.5 pl-3 pr-6 text-gray-700 hover:bg-gray-100 dark:border-gray-300 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600',
                 )}
-                style={{ width: width ? `${width - 10}px` : 'auto' }}
+                style={{ width: '100%' }}
                 data-theme={typeof item === 'string' ? item : (item as OptionType).value}
               >
                 <span className="block truncate">
