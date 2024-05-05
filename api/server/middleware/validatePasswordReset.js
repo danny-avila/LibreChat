244601d9649c5507e70f9ccf04294fbece6987dc
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2024-05-05 20:27:40 +0200

    refactor: use a helper function

--- api/server/middleware/validatePasswordReset.js
+++ api/server/middleware/validatePasswordReset.js
@@ -1,6 +1,7 @@
+const { isEnabled } = require('~/server/utils');
+
 function validatePasswordReset(req, res, next) {
-  const setting = process.env.ALLOW_PASSWORD_RESET?.toLowerCase();
-  if (setting === 'true') {
+  if (isEnabled(process.env.ALLOW_PASSWORD_RESET)) {
     next();
   } else {
     res.status(403).send('Password reset is not allowed.');
