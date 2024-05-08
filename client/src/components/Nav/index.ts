7e7c973505f2a66ec925c959dd2c98c0be1fde2b
Danny Avila <danny@librechat.ai>
2024-05-08 16:40:20 -0400

    📶 fix: Mobile Stylings (#2639)
    * chore: remove unused mobile nav
    
    * fix: mobile nav fix for 'more' and 'archive' buttons div
    
    * refactor(useTextarea): rewrite handleKeyUp for backwards compatibility
    
    refactor(useTextarea): rewrite handleKeyUp for backwards compatibility
    
    * experimental: add processing delay to azure streams for better performance/UX
    
    * experiemental: adjust gpt-3 azureDelay
    
    * fix: perplexity titles

--- client/src/components/Nav/index.ts
+++ client/src/components/Nav/index.ts
@@ -2,7 +2,6 @@ export * from './ExportConversation';
 export * from './SettingsTabs/';
 export { default as ClearConvos } from './ClearConvos';
 export { default as Logout } from './Logout';
-export { default as MobileNav } from './MobileNav';
 export { default as Nav } from './Nav';
 export { default as NavLink } from './NavLink';
 export { default as NavLinks } from './NavLinks';
