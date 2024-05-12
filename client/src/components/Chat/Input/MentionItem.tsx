bcdddaed7274ca64048b4758d44660291169c89f
Danny Avila <danny@librechat.ai>
2024-05-12 09:20:59 -0400

    🎨 style(MentionItem): update active bg-color (#2675)

--- client/src/components/Chat/Input/MentionItem.tsx
+++ client/src/components/Chat/Input/MentionItem.tsx
@@ -22,7 +22,7 @@ export default function MentionItem({
       <div
         className={cn(
           'text-token-text-primary bg-token-main-surface-secondary group flex h-10 items-center gap-2 rounded-lg px-2 text-sm font-medium hover:bg-gray-100 dark:hover:bg-gray-600',
-          isActive ? 'dark:bg-gray-600' : '',
+          isActive ? 'bg-gray-100 dark:bg-gray-600' : '',
         )}
       >
         {icon ? icon : null}
