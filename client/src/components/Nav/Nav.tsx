1ef66009628368dd0afc1f8b4921c68cce494360
Danny Avila <danny@librechat.ai>
2024-05-15 09:53:00 -0400

    ❇️ style(ModelSpecs): optimize for Long/Chinese name and mobile styling  (#2731)
    * style: hide nav toggle for mobile
    
    * ❇️ style: optimize for Long/Chinese `modelSpec` name and mobile styling

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -160,7 +160,7 @@ const Nav = ({ navVisible, setNavVisible }) => {
           setIsHovering={setIsToggleHovering}
           onToggle={toggleNavVisible}
           navVisible={navVisible}
-          className="fixed left-0 top-1/2 z-40"
+          className="fixed left-0 top-1/2 z-40 hidden md:flex"
         />
         <div className={`nav-mask${navVisible ? ' active' : ''}`} onClick={toggleNavVisible} />
       </Tooltip>
