ef291c934e1c5f08378f9a5294601fa3f48ef171
Danny Avila <danny@librechat.ai>
2024-05-20 10:47:08 -0400

    WIP tts routes rewrite

--- api/server/routes/files/tts.js
+++ api/server/routes/files/tts.js
@@ -1,25 +1,40 @@
-const express = require('express');
-const router = express.Router();
 const multer = require('multer');
-const { requireJwtAuth } = require('~/server/middleware/');
+const express = require('express');
 const {
-  textToSpeech,
   getVoices,
-  streamAudioFromWebSocket,
+  streamAudio,
+  // textToSpeech,
+  // streamAudioFromWebSocket,
 } = require('~/server/services/Files/Audio');
+const { requireJwtAuth } = require('~/server/middleware/');
+const { Message } = require('~/models/Message');
+const router = express.Router();
 
 const upload = multer();
 
-router.post('/', requireJwtAuth, upload.none(), async (req, res) => {
-  const { websocket } = req.body;
-  if (websocket) {
-    streamAudioFromWebSocket(req, res);
-  } else {
-    await textToSpeech(req, res);
+// router.use(requireJwtAuth);
+
+// router.post('/', upload.none(), async (req, res) => {
+//   const { websocket } = req.body;
+//   if (websocket) {
+//     streamAudioFromWebSocket(req, res);
+//   } else {
+//     await textToSpeech(req, res);
+//   }
+// });
+
+router.post('/', async (req, res) => {
+  try {
+    console.log('start stream audio');
+    await streamAudio(req, res);
+    console.log('end stream audio');
+  } catch (error) {
+    console.error('Failed to stream audio:', error);
+    res.status(500).json({ error: 'Failed to stream audio' });
   }
 });
 
-router.get('/voices', requireJwtAuth, async (req, res) => {
+router.get('/voices', async (req, res) => {
   await getVoices(req, res);
 });
 
