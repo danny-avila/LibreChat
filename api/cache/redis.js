3539eec7e82de223c78d879ad126aa19957216d2
Arthur Barrett <abarrett@fas.harvard.edu>
2024-05-24 09:54:33 -0400

    🔧 fix(redis): Resolve Redis Standalone vs Cluster mode discrepancy for social logins (#2848)

--- api/cache/redis.js
+++ api/cache/redis.js
@@ -1,4 +1,9 @@
 const Redis = require('ioredis');
+const { logger } = require('~/config');
 const { REDIS_URI } = process.env ?? {};
-const redis = new Redis.Cluster(REDIS_URI);
+const redis = new Redis(REDIS_URI);
+redis
+  .on('error', (err) => logger.error('ioredis error:', err))
+  .on('ready', () => logger.info('ioredis successfully initialized.'))
+  .on('reconnecting', () => logger.info('ioredis reconnecting...'));
 module.exports = redis;
