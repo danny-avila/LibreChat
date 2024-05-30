62ff5cbdd0b8afc4e2c389fd02c83855b6ebc6b5
Danny Avila <danny@librechat.ai>
2024-05-30 18:39:21 -0400

    🔉 feat: TTS/STT rate limiters  (#2925)
    * fix: remove double initialization of speech routes
    
    * refactor(useMessageHelpers): more consistent latestMessage updates based on unique textKey and early returns when setting
    
    * feat: TTS/STT rate limiters
    
    * chore: remove console log
    
    * fix: make modular chat true by default

--- api/cache/getLogStores.js
+++ api/cache/getLogStores.js
@@ -60,6 +60,8 @@ const namespaces = {
   message_limit: createViolationInstance('message_limit'),
   token_balance: createViolationInstance(ViolationTypes.TOKEN_BALANCE),
   registrations: createViolationInstance('registrations'),
+  [ViolationTypes.TTS_LIMIT]: createViolationInstance(ViolationTypes.TTS_LIMIT),
+  [ViolationTypes.STT_LIMIT]: createViolationInstance(ViolationTypes.STT_LIMIT),
   [ViolationTypes.FILE_UPLOAD_LIMIT]: createViolationInstance(ViolationTypes.FILE_UPLOAD_LIMIT),
   [ViolationTypes.ILLEGAL_MODEL_REQUEST]: createViolationInstance(
     ViolationTypes.ILLEGAL_MODEL_REQUEST,
---
+++ api/server/middleware/speech/index.js
@@ -0,0 +1,7 @@
+const createTTSLimiters = require('./ttsLimiters');
+const createSTTLimiters = require('./sttLimiters');
+
+module.exports = {
+  createTTSLimiters,
+  createSTTLimiters,
+};
