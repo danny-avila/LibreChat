b76233bd668d8b189aec941ab352948fe37b7d9f
Danny Avila <danny@librechat.ai>
2024-05-30 20:05:01 -0400

    feat: add `bottleneck` for tts routes
    refactor: slightly increase request threshold

--- api/server/routes/files/tts.js
+++ api/server/routes/files/tts.js
@@ -1,5 +1,6 @@
 const multer = require('multer');
 const express = require('express');
+const Bottleneck = require('bottleneck');
 const { CacheKeys } = require('librechat-data-provider');
 const { getVoices, streamAudio, textToSpeech } = require('~/server/services/Files/Audio');
 const { getLogStores } = require('~/cache');
@@ -8,8 +9,25 @@ const { logger } = require('~/config');
 const router = express.Router();
 const upload = multer();
 
+// todo: can add Redis support for limiter
+const limiter = new Bottleneck({
+  minTime: 240, // Minimum time between requests (240ms per request = 250 requests per minute)
+  maxConcurrent: 100, // Maximum number of concurrent requests
+  reservoir: 250, // Initial number of available requests
+  reservoirRefreshAmount: 250, // Number of requests replenished in each interval
+  reservoirRefreshInterval: 60 * 1000, // Reservoir refresh interval (60 seconds)
+});
+
+const limitedStreamAudio = limiter.wrap(streamAudio);
+const limitedTextToSpeech = limiter.wrap(textToSpeech);
+
 router.post('/manual', upload.none(), async (req, res) => {
-  await textToSpeech(req, res);
+  try {
+    await limitedTextToSpeech(req, res);
+  } catch (error) {
+    logger.error(`[textToSpeech] user: ${req.user.id} | Failed to process textToSpeech: ${error}`);
+    res.status(500).json({ error: 'Failed to process textToSpeech' });
+  }
 });
 
 const logDebugMessage = (req, message) =>
@@ -26,7 +44,7 @@ router.post('/', async (req, res) => {
       return res.status(401).json({ error: 'Audio stream already running' });
     }
     audioRunsCache.set(req.body.runId, true);
-    await streamAudio(req, res);
+    await limitedStreamAudio(req, res);
     logDebugMessage(req, 'end stream audio');
     res.status(200).end();
   } catch (error) {
@@ -35,6 +53,7 @@ router.post('/', async (req, res) => {
   }
 });
 
+// todo: cache voices
 router.get('/voices', async (req, res) => {
   await getVoices(req, res);
 });
