c8b6155e4c53e7965247a63b3410c913778ca265
Walber Cardoso <walbercardoso@gmail.com>
2024-05-31 11:45:23 -0300

    refactor: utils files to handle new svg

--- client/src/utils/files.ts
+++ client/src/utils/files.ts
@@ -5,6 +5,7 @@ import SheetPaths from '~/components/svg/Files/SheetPaths';
 import TextPaths from '~/components/svg/Files/TextPaths';
 import FilePaths from '~/components/svg/Files/FilePaths';
 import CodePaths from '~/components/svg/Files/CodePaths';
+import UrlPaths from '~/components/svg/Files/UrlPaths'; // Importe o caminho SVG para URLs
 
 export const partialTypes = ['text/x-'];
 
@@ -23,52 +24,27 @@ const spreadsheet = {
 const codeFile = {
   paths: CodePaths,
   fill: '#FF6E3C',
-  // TODO: make this dynamic to the language
   title: 'Code',
 };
 
+const urlFile = {
+  paths: UrlPaths,
+  fill: '#FF6E3C',
+  title: 'URL',
+};
+
 export const fileTypes = {
-  /* Category matches */
   file: {
     paths: FilePaths,
     fill: '#0000FF',
     title: 'File',
   },
   text: textDocument,
-  // application:,
-
-  /* Partial matches */
   csv: spreadsheet,
   pdf: textDocument,
   'text/x-': codeFile,
-
-  /* Exact matches */
-  // 'application/json':,
-  // 'text/html':,
-  // 'text/css':,
-  // image,
+  url: urlFile, // Adicione o novo tipo de arquivo URL aqui
 };
-
-// export const getFileType = (type = '') => {
-//   let fileType = fileTypes.file;
-//   const exactMatch = fileTypes[type];
-//   const partialMatch = !exactMatch && partialTypes.find((type) => type.includes(type));
-//   const category = (!partialMatch && (type.split('/')[0] ?? 'text') || 'text');
-
-//   if (exactMatch) {
-//     fileType = exactMatch;
-//   } else if (partialMatch) {
-//     fileType = fileTypes[partialMatch];
-//   } else if (fileTypes[category]) {
-//     fileType = fileTypes[category];
-//   }
-
-//   if (!fileType) {
-//     fileType = fileTypes.file;
-//   }
-
-//   return fileType;
-// };
 
 export const getFileType = (
   type = '',
@@ -77,7 +53,6 @@ export const getFileType = (
   fill: string;
   title: string;
 } => {
-  // Direct match check
   if (fileTypes[type]) {
     return fileTypes[type];
   }
@@ -86,27 +61,19 @@ export const getFileType = (
     return spreadsheet;
   }
 
-  // Partial match check
   const partialMatch = partialTypes.find((partial) => type.includes(partial));
   if (partialMatch && fileTypes[partialMatch]) {
     return fileTypes[partialMatch];
   }
 
-  // Category check
   const category = type.split('/')[0] || 'text';
   if (fileTypes[category]) {
     return fileTypes[category];
   }
 
-  // Default file type
   return fileTypes.file;
 };
 
-/**
- * Format a date string to a human readable format
- * @example
- * formatDate('2020-01-01T00:00:00.000Z') // '1 Jan 2020'
- */
 export function formatDate(dateString) {
   const months = [
     'Jan',
@@ -123,7 +90,6 @@ export function formatDate(dateString) {
     'Dec',
   ];
   const date = new Date(dateString);
-
   const day = date.getDate();
   const month = months[date.getMonth()];
   const year = date.getFullYear();
@@ -131,9 +97,6 @@ export function formatDate(dateString) {
   return `${day} ${month} ${year}`;
 }
 
-/**
- * Adds a file to the query cache
- */
 export function addFileToCache(queryClient: QueryClient, newfile: TFile) {
   const currentFiles = queryClient.getQueryData<TFile[]>([QueryKeys.files]);
 
