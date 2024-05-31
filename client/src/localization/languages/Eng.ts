5c487ca6a1ceb9cc58bb211fcbeea3af57bf428c
Walber Cardoso <walbercardoso@gmail.com>
2024-05-31 18:23:51 -0300

    translator: update lang

--- client/src/localization/languages/Eng.ts
+++ client/src/localization/languages/Eng.ts
@@ -59,7 +59,7 @@ export default {
   com_assistants_update_error: 'There was an error updating your assistant.',
   com_assistants_create_success: 'Successfully created',
   com_assistants_create_error: 'There was an error creating your assistant.',
-  com_ui_upload_file: 'Upload File',
+  com_ui_upload_file: 'Upload a File',
   com_ui_enter_url: 'Enter URL',
   com_ui_submit_url: 'Submit URL',
   com_ui_date_today: 'Today',
