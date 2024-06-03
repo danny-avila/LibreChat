Gabbarella/Work2

Add exporting all conversations feature

api/server/utils/import/jobDefinition.js
@@ -3,8 +3,13 @@ const jobScheduler = require('~/server/utils/jobScheduler');
 const { getImporter } = require('./importers');
 const { indexSync } = require('~/lib/db');
 const { logger } = require('~/config');
+const { getAllConvos } = require('~/models/Conversation');
+const { getMessages } = require('~/models');
+const os = require('os');
+const path = require('path');
 
 const IMPORT_CONVERSATION_JOB_NAME = 'import conversation';
+const EXPORT_CONVERSATION_JOB_NAME = 'export conversation';
 
 /**
  * Job definition for importing a conversation.
@@ -35,7 +40,56 @@ const importConversationJob = async (job, done) => {
   }
 };
 
+async function createAndDeleteTempFile(content, delay, jobId) {
+  const tempDir = os.tmpdir();
+  const tempFilePath = path.join(tempDir, `export-${jobId}`);
+
+  try {
+    // Write content to the temporary file using fs.promises API
+    await fs.writeFile(tempFilePath, JSON.stringify(content));
+    console.log(`Temporary file created at: ${tempFilePath}`);
+
+    // Schedule the deletion of the temporary file
+    setTimeout(async () => {
+      try {
+        // Delete the file using fs.promises API
+        await fs.unlink(tempFilePath);
+        console.log(`Temporary file deleted: ${tempFilePath}`);
+      } catch (error) {
+        console.error('Error deleting the temporary file:', error);
+      }
+    }, delay);
+
+    return tempFilePath;
+  } catch (error) {
+    console.error('Error handling the temporary file:', error);
+  }
+}
+
+// Define the export job function
+const exportConversationJob = async (job, done) => {
+  const { requestUserId } = job.attrs.data;
+  try {
+    const convos = await getAllConvos(requestUserId);
+    //const content = req.file.buffer.toString();
+    //const job = await jobScheduler.now(EXPORT_CONVERSATION_JOB_NAME, content, req.user.id);
+
+    logger.info('Convos: ' + JSON.stringify(convos));
+
+    for (let i = 0; i < convos.conversations.length; i++) {
+      const conversationId = convos.conversations[i].conversationId;
+      convos.conversations[i].messages = await getMessages({ conversationId });
+    }
+    createAndDeleteTempFile(convos, 5 * 60 * 1000, job.attrs._id);
+    done();
+  } catch (error) {
+    logger.error('Failed to export conversation: ', error);
+    done(error);
+  }
+};
+
 // Call the jobScheduler.define function at startup
 jobScheduler.define(IMPORT_CONVERSATION_JOB_NAME, importConversationJob);
+jobScheduler.define(EXPORT_CONVERSATION_JOB_NAME, exportConversationJob);
 
-module.exports = { IMPORT_CONVERSATION_JOB_NAME };
+module.exports = { IMPORT_CONVERSATION_JOB_NAME, EXPORT_CONVERSATION_JOB_NAME };

danny-avila/LibreChat

api/server/utils/import/jobDefinition.js
@@ -3,8 +3,13 @@ const jobScheduler = require('~/server/utils/jobScheduler');
 const { getImporter } = require('./importers');
 const { indexSync } = require('~/lib/db');
 const { logger } = require('~/config');
+const { getAllConvos } = require('~/models/Conversation');
+const { getMessages } = require('~/models');
+const os = require('os');
+const path = require('path');
 
 const IMPORT_CONVERSATION_JOB_NAME = 'import conversation';
+const EXPORT_CONVERSATION_JOB_NAME = 'export conversation';
 
 /**
  * Job definition for importing a conversation.
@@ -35,7 +40,67 @@ const importConversationJob = async (job, done) => {
   }
 };
 
-// Call the jobScheduler.define function at startup
-jobScheduler.define(IMPORT_CONVERSATION_JOB_NAME, importConversationJob);
+/**
+ * Create a temporary file and delete it after a delay.
+ * @param {object} content - The content to write to the file.
+ * @param {number} delay - The delay in milliseconds to delete the file.
+ * @param {string} job - The job object.
+ * @returns {Promise<string>} The temporary file path.
+ */
+async function createAndDeleteTempFile(content, delay, job) {
+  const { requestUserId } = job.attrs.data;
+  const tempDir = os.tmpdir();
+  const tempFilePath = path.join(tempDir, `export-${job.attrs._id}`);
+  try {
+    await fs.writeFile(tempFilePath, JSON.stringify(content));
+    logger.debug(`user: ${requestUserId} | Created temporary file at: ${tempFilePath}`);
+    setTimeout(async () => {
+      try {
+        await fs.unlink(tempFilePath);
+        logger.debug(
+          `user: ${requestUserId} | Automatically deleted temporary file at: ${tempFilePath}`,
+        );
+      } catch (error) {
+        logger.error(
+          `user: ${requestUserId} | Failed to automatically delete temporary file at: ${tempFilePath}`,
+          error,
+        );
+      }
+    }, delay);
+    return tempFilePath;
+  } catch (error) {
+    logger.error(
+      `user: ${requestUserId} | Error handling the temporary file: ${tempFilePath}`,
+      error,
+    );
+  }
+}
 
-module.exports = { IMPORT_CONVERSATION_JOB_NAME };
+/**
+ * Job definition for exporting all conversations.
+ * @param {import('agenda').Job} job - The job object.
+ * @param {Function} done - The done function.
+ */
+const exportConversationJob = async (job, done) => {
+  const { requestUserId } = job.attrs.data;
+  try {
+    const convos = await getAllConvos(requestUserId);
+
+    for (let i = 0; i < convos.conversations.length; i++) {
+      const conversationId = convos.conversations[i].conversationId;
+      convos.conversations[i].messages = await getMessages({ conversationId });
+    }
+    // Temporary file will be deleted from server after 5 minutes
+    createAndDeleteTempFile(convos, 5 * 60 * 1000, job);
+    done();
+  } catch (error) {
+    logger.error('Failed to export conversation: ', error);
+    done(error);
+  }
+};
+
+// Call the jobScheduler.define functions at startup
+jobScheduler.define(IMPORT_CONVERSATION_JOB_NAME, importConversationJob);
+jobScheduler.define(EXPORT_CONVERSATION_JOB_NAME, exportConversationJob);
+
+module.exports = { IMPORT_CONVERSATION_JOB_NAME, EXPORT_CONVERSATION_JOB_NAME };