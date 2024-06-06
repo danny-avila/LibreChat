21d644fbaa99d5799774b9d337e887481a5a6e58
Danny Avila <danny@librechat.ai>
2024-06-06 11:17:24 -0400

    chore: Add logging to password reset validation middleware

--- api/server/middleware/validatePasswordReset.js
+++ api/server/middleware/validatePasswordReset.js
@@ -1,9 +1,11 @@
 const { isEnabled } = require('~/server/utils');
+const { logger } = require('~/config');
 
 function validatePasswordReset(req, res, next) {
   if (isEnabled(process.env.ALLOW_PASSWORD_RESET)) {
     next();
   } else {
+    logger.warn(`Password reset attempt while not allowed. IP: ${req.ip}`);
     res.status(403).send('Password reset is not allowed.');
   }
 }
