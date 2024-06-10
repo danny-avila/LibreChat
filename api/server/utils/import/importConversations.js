438ee1ceb970580edbd7e3863f8302b150307432
Danny Avila <danny@librechat.ai>
2024-06-10 12:52:35 -0400

    fix: import conversations

--- api/server/utils/import/importConversations.js
+++ api/server/utils/import/importConversations.js
@@ -6,10 +6,9 @@ const { logger } = require('~/config');
 /**
  * Job definition for importing a conversation.
  * @param {{ filepath, requestUserId }} job - The job object.
- * @param {Function} callback - The callback function.
  */
-const importConversations = async (job, callback) => {
-  const { filepath, requestUserId } = job.attrs.data;
+const importConversations = async (job) => {
+  const { filepath, requestUserId } = job;
   try {
     logger.debug(`user: ${requestUserId} | Importing conversation(s) from file...`);
     const fileData = await fs.readFile(filepath, 'utf8');
@@ -19,10 +18,8 @@ const importConversations = async (job, callback) => {
     // Sync Meilisearch index
     await indexSync();
     logger.debug(`user: ${requestUserId} | Finished importing conversations`);
-    callback();
   } catch (error) {
     logger.error(`user: ${requestUserId} | Failed to import conversation: `, error);
-    callback(error);
   } finally {
     try {
       await fs.unlink(filepath);
