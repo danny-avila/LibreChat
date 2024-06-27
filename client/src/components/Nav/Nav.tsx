aadc73774bdade10eadf35e6735ae3df63efcae4
Arthur Barrett <abarrett@fas.harvard.edu>
2024-06-27 10:56:32 -0400

    📱fix: set initial nav visibility for small screens (#3208)
    * fix: hide nav on small screens by default
    
    * test: add spec for Nav component

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -42,6 +42,10 @@ const Nav = ({ navVisible, setNavVisible }) => {
 
   useEffect(() => {
     if (isSmallScreen) {
+      const savedNavVisible = localStorage.getItem('navVisible');
+      if (savedNavVisible === null) {
+        toggleNavVisible();
+      }
       setNavWidth('320px');
     } else {
       setNavWidth('260px');
@@ -102,6 +106,7 @@ const Nav = ({ navVisible, setNavVisible }) => {
     <TooltipProvider delayDuration={250}>
       <Tooltip>
         <div
+          data-testid="nav"
           className={
             'nav active max-w-[320px] flex-shrink-0 overflow-x-hidden bg-gray-50 dark:bg-gray-850 md:max-w-[260px]'
           }
