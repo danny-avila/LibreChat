30c2d7216299395de3a9c5096dc51f1c225a5154
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2024-07-10 00:40:45 +0200

    fix: `autoSendText` timeout

--- client/src/hooks/Input/useSpeechToTextExternal.ts
+++ client/src/hooks/Input/useSpeechToTextExternal.ts
@@ -27,10 +27,11 @@ const useSpeechToTextExternal = (onTranscriptionComplete: (text: string) => void
       const extractedText = data.text;
       setText(extractedText);
       setIsRequestBeingMade(false);
-      if (autoSendText < -1 && speechToText && extractedText.length > 0) {
+
+      if (autoSendText > -1 && speechToText && extractedText.length > 0) {
         setTimeout(() => {
           onTranscriptionComplete(extractedText);
-        }, autoSendText);
+        }, autoSendText * 1000);
       }
     },
     onError: () => {
