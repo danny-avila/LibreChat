394a0784a3d22978cdae72aba56190e5f59892ec
Yuichi Ohneda <ohneda@gmail.com>
2024-07-10 22:26:40 -0700

    fix: failed test

--- client/src/utils/conversationTags.spec.ts
+++ client/src/utils/conversationTags.spec.ts
@@ -7,7 +7,7 @@ describe('ConversationTag Utilities', () => {
   beforeEach(() => {
     conversations = [
       {
-        title: 'saved',
+        tag: 'saved',
         count: 1,
         position: 0,
         description: 'description1',
@@ -16,7 +16,7 @@ describe('ConversationTag Utilities', () => {
         user: 'user1',
       },
       {
-        title: 'title1',
+        tag: 'tag1',
         count: 1,
         position: 1,
         description: 'description1',
@@ -25,7 +25,7 @@ describe('ConversationTag Utilities', () => {
         user: 'user1',
       },
       {
-        title: 'title2',
+        tag: 'tag2',
         count: 20,
         position: 2,
         description: 'description2',
@@ -34,7 +34,7 @@ describe('ConversationTag Utilities', () => {
         user: 'user1',
       },
       {
-        title: 'title3',
+        tag: 'tag3',
         count: 30,
         position: 3,
         description: 'description3',
@@ -43,7 +43,7 @@ describe('ConversationTag Utilities', () => {
         user: 'user1',
       },
       {
-        title: 'title4',
+        tag: 'tag4',
         count: 40,
         position: 4,
         description: 'description4',
@@ -52,7 +52,7 @@ describe('ConversationTag Utilities', () => {
         user: 'user1',
       },
       {
-        title: 'title5',
+        tag: 'tag5',
         count: 50,
         position: 5,
         description: 'description5',
@@ -67,115 +67,115 @@ describe('ConversationTag Utilities', () => {
     it('updates the first tag correctly', () => {
       const updated = updateConversationTag(
         conversations,
-        { title: 'title1-new', description: 'description1-new' },
+        { tag: 'tag1-new', description: 'description1-new' },
         {
           ...conversations[1],
-          title: 'title1-new',
+          tag: 'tag1-new',
           description: 'description1-new',
         },
-        'title1',
+        'tag1',
       );
 
-      expect(updated[0].title).toBe('saved');
+      expect(updated[0].tag).toBe('saved');
       expect(updated[0].position).toBe(0);
-      expect(updated[1].title).toBe('title1-new');
+      expect(updated[1].tag).toBe('tag1-new');
       expect(updated[1].description).toBe('description1-new');
       expect(updated[1].position).toBe(1);
-      expect(updated[2].title).toBe('title2');
+      expect(updated[2].tag).toBe('tag2');
       expect(updated[2].position).toBe(2);
-      expect(updated[3].title).toBe('title3');
+      expect(updated[3].tag).toBe('tag3');
       expect(updated[3].position).toBe(3);
-      expect(updated[4].title).toBe('title4');
+      expect(updated[4].tag).toBe('tag4');
       expect(updated[4].position).toBe(4);
-      expect(updated[5].title).toBe('title5');
+      expect(updated[5].tag).toBe('tag5');
       expect(updated[5].position).toBe(5);
     });
   });
   it('updates the third tag correctly', () => {
     const updated = updateConversationTag(
       conversations,
-      { title: 'title3-new', description: 'description3-new' },
+      { tag: 'tag3-new', description: 'description3-new' },
       {
         ...conversations[3],
-        title: 'title3-new',
+        tag: 'tag3-new',
         description: 'description3-new',
       },
-      'title3',
+      'tag3',
     );
 
-    expect(updated[0].title).toBe('saved');
+    expect(updated[0].tag).toBe('saved');
     expect(updated[0].position).toBe(0);
-    expect(updated[1].title).toBe('title1');
+    expect(updated[1].tag).toBe('tag1');
     expect(updated[1].position).toBe(1);
-    expect(updated[2].title).toBe('title2');
+    expect(updated[2].tag).toBe('tag2');
     expect(updated[2].position).toBe(2);
-    expect(updated[3].title).toBe('title3-new');
+    expect(updated[3].tag).toBe('tag3-new');
     expect(updated[3].description).toBe('description3-new');
     expect(updated[3].position).toBe(3);
-    expect(updated[4].title).toBe('title4');
+    expect(updated[4].tag).toBe('tag4');
     expect(updated[4].position).toBe(4);
-    expect(updated[5].title).toBe('title5');
+    expect(updated[5].tag).toBe('tag5');
     expect(updated[5].position).toBe(5);
   });
 
   it('updates the order of other tags if the order of the tags is moving up', () => {
     const updated = updateConversationTag(
       conversations,
-      // move title3 to the second position
+      // move tag3 to the second position
       { position: 2 },
       {
         ...conversations[3],
         position: 2,
       },
-      'title3',
+      'tag3',
     );
 
-    expect(updated[0].title).toBe('saved');
+    expect(updated[0].tag).toBe('saved');
     expect(updated[0].position).toBe(0);
-    expect(updated[1].title).toBe('title1');
+    expect(updated[1].tag).toBe('tag1');
     expect(updated[1].position).toBe(1);
-    expect(updated[2].title).toBe('title3');
+    expect(updated[2].tag).toBe('tag3');
     expect(updated[2].position).toBe(2);
-    expect(updated[3].title).toBe('title2');
+    expect(updated[3].tag).toBe('tag2');
     expect(updated[3].position).toBe(3);
-    expect(updated[4].title).toBe('title4');
+    expect(updated[4].tag).toBe('tag4');
     expect(updated[4].position).toBe(4);
-    expect(updated[5].title).toBe('title5');
+    expect(updated[5].tag).toBe('tag5');
     expect(updated[5].position).toBe(5);
   });
 
   it('updates the order of other tags if the order of the tags is moving down', () => {
     const updated = updateConversationTag(
       conversations,
-      // move title3 to the last position
+      // move tag3 to the last position
       { position: 5 },
       {
         ...conversations[3],
         position: 5,
       },
-      'title3',
+      'tag3',
     );
 
-    expect(updated[0].title).toBe('saved');
+    expect(updated[0].tag).toBe('saved');
     expect(updated[0].position).toBe(0);
-    expect(updated[1].title).toBe('title1');
+    expect(updated[1].tag).toBe('tag1');
     expect(updated[1].position).toBe(1);
-    expect(updated[2].title).toBe('title2');
+    expect(updated[2].tag).toBe('tag2');
     expect(updated[2].position).toBe(2);
-    expect(updated[3].title).toBe('title4');
+    expect(updated[3].tag).toBe('tag4');
     expect(updated[3].position).toBe(3);
-    expect(updated[4].title).toBe('title5');
+    expect(updated[4].tag).toBe('tag5');
     expect(updated[4].position).toBe(4);
-    expect(updated[5].title).toBe('title3');
+    expect(updated[5].tag).toBe('tag3');
     expect(updated[5].position).toBe(5);
   });
 
   it('updates the order of other tags if new tag is added', () => {
     const updated = updateConversationTag(
       conversations,
-      { title: 'newTitle', description: 'newDescription' },
+      { tag: 'newtag', description: 'newDescription' },
       {
-        title: 'newTitle',
+        tag: 'newtag',
         description: 'newDescription',
         position: 1,
         updatedAt: new Date().toISOString(),
@@ -183,43 +183,43 @@ describe('ConversationTag Utilities', () => {
         user: 'user1',
         count: 30,
       },
-      // no tag title specified
+      // no tag tag specified
     );
 
-    expect(updated[0].title).toBe('saved');
+    expect(updated[0].tag).toBe('saved');
     expect(updated[0].position).toBe(0);
-    expect(updated[1].title).toBe('newTitle');
+    expect(updated[1].tag).toBe('newtag');
     expect(updated[1].description).toBe('newDescription');
     expect(updated[1].position).toBe(1);
-    expect(updated[2].title).toBe('title1');
+    expect(updated[2].tag).toBe('tag1');
     expect(updated[2].position).toBe(2);
-    expect(updated[3].title).toBe('title2');
+    expect(updated[3].tag).toBe('tag2');
     expect(updated[3].position).toBe(3);
-    expect(updated[4].title).toBe('title3');
+    expect(updated[4].tag).toBe('tag3');
     expect(updated[4].position).toBe(4);
-    expect(updated[5].title).toBe('title4');
+    expect(updated[5].tag).toBe('tag4');
     expect(updated[5].position).toBe(5);
-    expect(updated[6].title).toBe('title5');
+    expect(updated[6].tag).toBe('tag5');
     expect(updated[6].position).toBe(6);
   });
 
   it('returns a new array for new tag if no tags exist', () => {
     const updated = updateConversationTag(
       [],
-      { title: 'newTitle', description: 'newDescription' },
+      { tag: 'newtag', description: 'newDescription' },
       {
-        title: 'saved',
+        tag: 'saved',
         description: 'newDescription',
-        position: 1,
+        position: 0,
         updatedAt: new Date().toISOString(),
         createdAt: new Date().toISOString(),
         user: 'user1',
         count: 30,
       },
-      // no tag title specified
+      // no tag tag specified
     );
-
-    expect(updated[0].title).toBe('saved');
+    expect(updated.length).toBe(1);
+    expect(updated[0].tag).toBe('saved');
     expect(updated[0].position).toBe(0);
   });
 });
