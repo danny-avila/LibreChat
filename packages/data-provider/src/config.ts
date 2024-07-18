fe667d42840e1a572864ead379a64bb3c458b80d
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2024-07-19 00:43:40 +0200

    feat: retrival

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -12,6 +12,8 @@ export const defaultSocialLogins = ['google', 'facebook', 'openid', 'github', 'd
 export const defaultRetrievalModels = [
   'gpt-4o',
   'gpt-4o-2024-05-13',
+  'gpt-4o-mini',
+  'gpt-4o-mini-2024-07-18',
   'gpt-4-turbo-preview',
   'gpt-3.5-turbo-0125',
   'gpt-4-0125-preview',
