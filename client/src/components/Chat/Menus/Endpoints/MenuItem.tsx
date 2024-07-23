2052cd221cc40b9d15a49eb8eb6158b51793d4e5
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2024-07-23 18:17:44 +0200

    style: endpoint selection update

--- client/src/components/Chat/Menus/Endpoints/MenuItem.tsx
+++ client/src/components/Chat/Menus/Endpoints/MenuItem.tsx
@@ -97,8 +97,7 @@ const MenuItem: FC<MenuItemProps> = ({
       <div
         role="menuitem"
         className={cn(
-          'group m-1.5 flex max-h-[40px] cursor-pointer gap-2 rounded px-5 py-2.5 !pr-3 text-sm !opacity-100 hover:bg-black/5 radix-disabled:pointer-events-none radix-disabled:opacity-50 dark:hover:bg-gray-600',
-          'focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900',
+          'focus-visible-button group m-1.5 flex max-h-[40px] cursor-pointer gap-2 rounded px-5 py-2.5 !pr-3 text-sm !opacity-100 hover:bg-black/5 radix-disabled:pointer-events-none radix-disabled:opacity-50 dark:hover:bg-gray-600',
         )}
         tabIndex={1}
         {...rest}
@@ -133,12 +132,11 @@ const MenuItem: FC<MenuItemProps> = ({
               <div className="text-token-text-primary" key={`set-key-${endpoint}`}>
                 <button
                   className={cn(
-                    'invisible flex gap-x-1 group-hover:visible',
+                    'focus-visible-button invisible flex gap-x-1 group-hover:visible',
                     selected ? 'visible' : '',
                     expiryTime
-                      ? 'w-full rounded-lg p-2 hover:text-gray-400 dark:hover:text-gray-400'
+                      ? 'w-full rounded-lg p-2 text-gray-400 hover:text-black dark:text-gray-400 dark:hover:text-white'
                       : '',
-                    'focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900',
                   )}
                   onClick={(e) => {
                     e.preventDefault();
--- client/src/components/Chat/Menus/UI/TitleButton.tsx
+++ client/src/components/Chat/Menus/UI/TitleButton.tsx
@@ -7,7 +7,7 @@ export default function TitleButton({ primaryText = '', secondaryText = '' }) {
   return (
     <Trigger asChild>
       <button
-        className="focus-visible-button group relative flex cursor-pointer items-center gap-1 rounded-xl px-3 py-2 text-lg font-medium hover:bg-gray-50 radix-state-open:bg-gray-50 dark:hover:bg-gray-700 dark:radix-state-open:bg-gray-700"
+        className="focus-visible-button group relative flex cursor-pointer items-center gap-1 rounded-xl px-3 py-2 text-lg font-medium hover:bg-gray-100 radix-state-open:bg-gray-100 dark:hover:bg-gray-700 dark:radix-state-open:bg-gray-700"
         aria-haspopup="dialog"
         aria-expanded={isExpanded}
         aria-controls="radix-:r6:"
