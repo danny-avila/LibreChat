a2d31c0f4d953640768b8c6761e5df1702f4c170
Danny Avila <danny@librechat.ai>
2024-08-15 11:34:25 -0400

    🔀 refactor: Modularize TTS Logic for Improved Browser support (#3657)
    * WIP: message audio refactor
    
    * WIP: use MessageAudio by provider
    
    * fix: Update MessageAudio component to use TTSEndpoints enum
    
    * feat: Update useTextToSpeechBrowser hook to handle errors and improve error logging
    
    * feat: Add voice dropdown components for different TTS engines
    
    * docs: update incorrect `voices` example
    
    changed `voice: ''` to `voices: ['alloy']`
    
    * feat: Add brwoser support check for Edge TTS engine component with error toast if not supported
    
    ---------
    
    Co-authored-by: Marco Beretta <81851188+berry-13@users.noreply.github.com>

--- client/src/localization/languages/Eng.ts
+++ client/src/localization/languages/Eng.ts
@@ -664,6 +664,8 @@ export default {
   com_nav_audio_process_error: 'Error processing audio: {0}',
   com_nav_long_audio_warning: 'Longer texts will take longer to process.',
   com_nav_tts_init_error: 'Failed to initialize text-to-speech: {0}',
+  com_nav_tts_unsupported_error:
+    'Text-to-speech for the selected engine is not supported in this browser.',
   com_nav_source_buffer_error: 'Error setting up audio playback. Please refresh the page.',
   com_nav_media_source_init_error:
     'Unable to prepare audio player. Please check your browser settings.',
