5cecaef1d0f2836b8cdbb64cdd170b2c3f4b8270
Danny Avila <danny@librechat.ai>
2024-08-22 18:51:57 -0400

    refactor: first pass rewrite

--- client/src/components/Artifacts/Artifact.tsx
+++ client/src/components/Artifacts/Artifact.tsx
@@ -1,34 +1,72 @@
+import { useEffect, useCallback } from 'react';
 import { visit } from 'unist-util-visit';
-import type { Plugin } from 'unified';
-import type { Root } from 'mdast';
+import { useSetRecoilState } from 'recoil';
+import { artifactsState, artifactIdsState } from '~/store/artifacts';
+import type { Pluggable } from 'unified';
 
-export function artifactPlugin() {
+export const artifactPlugin: Pluggable = () => {
   return (tree) => {
-    visit(
-      tree,
-      ['textDirective', 'leafDirective', 'containerDirective'],
-      (node) => {
-        node.data = {
-          hName: node.name,
-          hProperties: node.attributes,
-          ...node.data,
-        };
-        return node;
-      },
-    );
+    visit(tree, ['textDirective', 'leafDirective', 'containerDirective'], (node) => {
+      node.data = {
+        hName: node.name,
+        hProperties: node.attributes,
+        ...node.data,
+      };
+      return node;
+    });
   };
-}
+};
 
-export function artifact({ node, ...props }) {
-  // if (props.className === 'artifact') {
-  console.dir(props, { depth: null });
+export function Artifact({ node, ...props }) {
+  const setArtifacts = useSetRecoilState(artifactsState);
+  const setArtifactIds = useSetRecoilState(artifactIdsState);
+
+  const updateArtifact = useCallback(() => {
+    const content =
+      props.children && typeof props.children === 'string'
+        ? props.children
+        : props.children?.props?.children || '';
+
+    const title = props.title || 'Untitled Artifact';
+    const type = props.type || 'unknown';
+    const identifier = props.identifier || 'no-identifier';
+    const artifactKey = `${identifier}_${type}_${title}`.replace(/\s+/g, '_').toLowerCase();
+
+    setArtifacts((prevArtifacts) => {
+      if (prevArtifacts[artifactKey] && prevArtifacts[artifactKey].content === content) {
+        return prevArtifacts;
+      }
+
+      return {
+        ...prevArtifacts,
+        [artifactKey]: {
+          id: artifactKey,
+          identifier,
+          title,
+          type,
+          content,
+        },
+      };
+    });
+
+    setArtifactIds((prevIds) => {
+      if (!prevIds.includes(artifactKey)) {
+        return [...prevIds, artifactKey];
+      }
+      return prevIds;
+    });
+  }, [props, setArtifacts, setArtifactIds]);
+
+  useEffect(() => {
+    updateArtifact();
+  }, [updateArtifact]);
+
   return (
     <div className="artifact">
-      <h3>{props.dataIdentifier}</h3>
-      <p>Type: {props.dataType}</p>
+      <b>{props.title || 'Untitled Artifact'}</b>
+      <p>Type: {props.type || 'unknown'}</p>
+      <p>Identifier: {props.identifier || 'No identifier'}</p>
       {props.children}
     </div>
   );
-  // }
-  // return <div {...props} />;
 }
--- client/src/components/Chat/Messages/Content/Markdown.tsx
+++ client/src/components/Chat/Messages/Content/Markdown.tsx
@@ -10,7 +10,7 @@ import remarkDirective from 'remark-directive';
 import type { PluggableList, Pluggable } from 'unified';
 import { langSubset, preprocessLaTeX, handleDoubleClick } from '~/utils';
 import { CodeBlockArtifact, CodeMarkdown } from '~/components/Artifacts/Code';
-import { artifact, artifactPlugin } from '~/components/Artifacts/Artifact';
+import { Artifact as artifact, artifactPlugin } from '~/components/Artifacts/Artifact';
 import CodeBlock from '~/components/Messages/Content/CodeBlock';
 import { useFileDownload } from '~/data-provider';
 import useLocalize from '~/hooks/useLocalize';
@@ -156,11 +156,13 @@ const Markdown = memo(({ content = '', showCursor, isLatestMessage }: TContentPr
   return (
     <ReactMarkdown
       remarkPlugins={[
-        /* @ts-ignore */
         remarkDirective,
+        /* @ts-ignore */
         supersub,
         remarkGfm,
+        /* @ts-ignore */
         [remarkMath, { singleDollarTextMath: true }],
+        /* @ts-ignore */
         artifactPlugin,
       ]}
       /* @ts-ignore */
