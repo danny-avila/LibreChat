163aa024cfd07aeaa3df34af2eb51da67878f94f
Danny Avila <danny@librechat.ai>
2024-08-24 23:16:31 -0400

    chore: preprocess artifacts for now

--- client/src/components/Chat/Messages/Content/Markdown.tsx
+++ client/src/components/Chat/Messages/Content/Markdown.tsx
@@ -9,10 +9,11 @@ import rehypeHighlight from 'rehype-highlight';
 import remarkDirective from 'remark-directive';
 import type { PluggableList, Pluggable } from 'unified';
 import type { Pluggable } from 'unified';
-import { langSubset, preprocessLaTeX, handleDoubleClick } from '~/utils';
 import { CodeBlockArtifact, CodeMarkdown } from '~/components/Artifacts/Code';
 import { Artifact, artifactPlugin } from '~/components/Artifacts/Artifact';
+import { langSubset, preprocessLaTeX, handleDoubleClick } from '~/utils';
 import CodeBlock from '~/components/Messages/Content/CodeBlock';
+import { preprocessCodeArtifacts } from '~/utils/artifacts';
 import { useFileDownload } from '~/data-provider';
 import useLocalize from '~/hooks/useLocalize';
 import { useToastContext } from '~/Providers';
@@ -133,6 +134,7 @@ const Markdown = memo(({ content = '', showCursor, isLatestMessage }: TContentPr
   if (!isInitializing) {
     currentContent = currentContent.replace('z-index: 1;', '') || '';
     currentContent = LaTeXParsing ? preprocessLaTeX(currentContent) : currentContent;
+    currentContent = codeArtifacts ? preprocessCodeArtifacts(currentContent) : currentContent;
   }
 
   const rehypePlugins = [
