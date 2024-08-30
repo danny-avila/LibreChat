f9443692012269f7500526fa984ea9e78ead790b
Danny Avila <danny@librechat.ai>
2024-08-30 16:57:09 -0400

    refactor: Update ConvoStarter component styles and class names for a11y and theme

--- client/src/components/Chat/ConvoStarter.tsx
+++ client/src/components/Chat/ConvoStarter.tsx
@@ -7,11 +7,9 @@ export default function ConvoStarter({ text, onClick }: ConvoStarterProps) {
   return (
     <button
       onClick={onClick}
-      className="hover:bg-token-main-surface-secondary relative flex w-40 cursor-pointer flex-col gap-2 rounded-2xl border px-3 pb-4 pt-3 text-start align-top text-[15px] shadow-[0_0_2px_0_rgba(0,0,0,0.05),0_4px_6px_0_rgba(0,0,0,0.02)] transition-colors duration-300 ease-in-out fade-in hover:bg-slate-100 dark:border-gray-600 dark:hover:bg-gray-700"
+      className="relative flex w-40 cursor-pointer flex-col gap-2 rounded-2xl border border-border-medium px-3 pb-4 pt-3 text-start align-top text-[15px] shadow-[0_0_2px_0_rgba(0,0,0,0.05),0_4px_6px_0_rgba(0,0,0,0.02)] transition-colors duration-300 ease-in-out fade-in hover:bg-surface-tertiary"
     >
-      <p className="break-word line-clamp-3 text-balance text-gray-600 dark:text-gray-400">
-        {text}
-      </p>
+      <p className="break-word line-clamp-3 text-balance text-text-secondary">{text}</p>
     </button>
   );
 }
