3a0b0ab5ae637b682a1a7dd19d721d9d7fc649b5
Danny Avila <danny@librechat.ai>
2024-09-09 16:11:03 -0400

    feat: render Multi-Convo based on UI permissions

--- client/src/components/Nav/SettingsTabs/Commands/Commands.tsx
+++ client/src/components/Nav/SettingsTabs/Commands/Commands.tsx
@@ -1,26 +1,52 @@
 import { memo } from 'react';
 import * as Tabs from '@radix-ui/react-tabs';
-import { SettingsTabValues } from 'librechat-data-provider';
+import { SettingsTabValues, PermissionTypes, Permissions } from 'librechat-data-provider';
+import HoverCardSettings from '~/components/Nav/SettingsTabs/HoverCardSettings';
+import { useLocalize, useHasAccess } from '~/hooks';
 import SlashCommandSwitch from './SlashCommandSwitch';
 import PlusCommandSwitch from './PlusCommandSwitch';
 import AtCommandSwitch from './AtCommandSwitch';
 
 function Commands() {
+  const localize = useLocalize();
+
+  const hasAccessToPrompts = useHasAccess({
+    permissionType: PermissionTypes.PROMPTS,
+    permission: Permissions.USE,
+  });
+
+  const hasAccessToMultiConvo = useHasAccess({
+    permissionType: PermissionTypes.MULTI_CONVO,
+    permission: Permissions.USE,
+  });
+
   return (
     <Tabs.Content
       value={SettingsTabValues.COMMANDS}
       role="tabpanel"
       className="w-full md:min-h-[271px]"
     >
-      <div className="flex flex-col gap-3 text-sm text-text-primary">
-        <div className="border-b border-border-medium pb-3 last-of-type:border-b-0">
-          <AtCommandSwitch />
+      <div className="space-y-4">
+        <div className="flex items-center gap-2">
+          <h3 className="text-lg font-medium text-text-primary">
+            {localize('com_nav_chat_commands')}
+          </h3>
+          <HoverCardSettings side="bottom" text="com_nav_chat_commands_info" />
         </div>
-        <div className="border-b border-border-medium pb-3 last-of-type:border-b-0">
-          <PlusCommandSwitch />
-        </div>
-        <div className="border-b border-border-medium pb-3 last-of-type:border-b-0">
-          <SlashCommandSwitch />
+        <div className="flex flex-col gap-3 text-sm text-text-primary">
+          <div className="border-b border-border-medium pb-3 last-of-type:border-b-0">
+            <AtCommandSwitch />
+          </div>
+          {hasAccessToMultiConvo === true && (
+            <div className="border-b border-border-medium pb-3 last-of-type:border-b-0">
+              <PlusCommandSwitch />
+            </div>
+          )}
+          {hasAccessToPrompts === true && (
+            <div className="border-b border-border-medium pb-3 last-of-type:border-b-0">
+              <SlashCommandSwitch />
+            </div>
+          )}
         </div>
       </div>
     </Tabs.Content>
