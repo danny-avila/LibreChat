fc8225a8e2eb7a6f0b443630237ed140336a4358
Albert.Huang <albert.huang@qima.com>
2024-09-11 19:03:38 +0800

    librechat bak update,follow wiki.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -64,8 +64,8 @@ export default defineConfig({
         maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
       },
       manifest: {
-        name: 'LibreChat',
-        short_name: 'LibreChat',
+        name: 'QIMA GPT',
+        short_name: 'QIMA GPT',
         start_url: '/',
         display: 'standalone',
         background_color: '#000000',
