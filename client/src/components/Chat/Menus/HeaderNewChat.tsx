e86b2ba5412434975c6484a1452ca2061e77fda3
Vesna Tan <tanvezi@gmail.com>
2024-09-16 14:34:23 -0400

    add label and focus for new chat button, made labels use localize names

--- client/src/components/Chat/Menus/HeaderNewChat.tsx
+++ client/src/components/Chat/Menus/HeaderNewChat.tsx
@@ -1,18 +1,20 @@
 import { NewChatIcon } from '~/components/svg';
 import { useChatContext } from '~/Providers';
-import { useMediaQuery } from '~/hooks';
+import { useMediaQuery, useLocalize } from '~/hooks';
 
 export default function HeaderNewChat() {
   const { newConversation } = useChatContext();
   const isSmallScreen = useMediaQuery('(max-width: 768px)');
+  const localize = useLocalize();
   if (isSmallScreen) {
     return null;
   }
   return (
     <button
       data-testid="wide-header-new-chat-button"
+      aria-label={localize("com_ui_new_chat")}
       type="button"
-      className="btn btn-neutral btn-small border-token-border-medium relative ml-2 flex hidden h-9 w-9 items-center justify-center whitespace-nowrap rounded-lg rounded-lg border focus:ring-0 focus:ring-offset-0 md:flex"
+      className="btn btn-neutral btn-small border-token-border-medium relative ml-2 flex hidden h-9 w-9 items-center justify-center whitespace-nowrap rounded-lg rounded-lg border focus:border-black-500 dark:focus:border-white-500 md:flex"
       onClick={() => newConversation()}
     >
       <div className="flex w-full items-center justify-center gap-2">
