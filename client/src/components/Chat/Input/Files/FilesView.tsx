cfdd5fa56558139a47f62b319b42ad1bfd1a16a3
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2024-09-21 02:51:14 +0200

    style: update my files dialog

--- client/src/components/Chat/Input/Files/FilesView.tsx
+++ client/src/components/Chat/Input/Files/FilesView.tsx
@@ -1,10 +1,10 @@
 import { FileSources, FileContext } from 'librechat-data-provider';
 import type { TFile } from 'librechat-data-provider';
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '~/components/ui';
+import OGDialogTemplate from '~/components/ui/DialogTemplate';
 import { useGetFiles } from '~/data-provider';
 import { DataTable, columns } from './Table';
+import { Dialog } from '~/components/ui';
 import { useLocalize } from '~/hooks';
-import { cn } from '~/utils/';
 
 export default function Files({ open, onOpenChange }) {
   const localize = useLocalize();
@@ -20,19 +20,12 @@ export default function Files({ open, onOpenChange }) {
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent
-        className={cn('w-11/12 overflow-x-auto shadow-2xl dark:bg-gray-700 dark:text-white')}
-      >
-        <DialogHeader>
-          <DialogTitle className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-200">
-            {localize('com_nav_my_files')}
-          </DialogTitle>
-        </DialogHeader>
-        <div className="overflow-x-auto p-0 sm:p-6 sm:pt-4">
-          <DataTable columns={columns} data={files} />
-          <div className="mt-5 sm:mt-4" />
-        </div>
-      </DialogContent>
+      <OGDialogTemplate
+        showCancelButton={false}
+        title={localize('com_nav_my_files')}
+        main={<DataTable columns={columns} data={files} />}
+        className="w-11/12 overflow-x-auto bg-background text-text-primary shadow-2xl"
+      />
     </Dialog>
   );
 }
