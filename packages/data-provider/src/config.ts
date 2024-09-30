3d71acdbd731df159783a83ba2861c95b71e475a
Eric Thong <thongerick@gmail.com>
2024-09-29 19:38:13 -0700

    feat: Add Deep Infra to known endpoints

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -532,6 +532,7 @@ export enum KnownEndpoints {
   apipie = 'apipie',
   cohere = 'cohere',
   fireworks = 'fireworks',
+  deepinfra = 'deepinfra',
   deepseek = 'deepseek',
   groq = 'groq',
   huggingface = 'huggingface',
