f270cf3ce290d4dd6872e15a5bf70584e6c2418a
Danny Avila <danny@librechat.ai>
2024-10-15 19:37:41 -0400

    🕒 feat: Add 5-second timeout for Fetching Model Lists (#4423)
    * refactor: add 5 second timeout for fetching AI provider model lists
    
    * ci: fix test due to recent changes

--- api/utils/index.js
+++ api/utils/index.js
@@ -1,19 +1,19 @@
 const loadYaml = require('./loadYaml');
+const axiosHelpers = require('./axios');
 const tokenHelpers = require('./tokens');
 const azureUtils = require('./azureUtils');
 const deriveBaseURL = require('./deriveBaseURL');
-const logAxiosError = require('./logAxiosError');
 const extractBaseURL = require('./extractBaseURL');
 const findMessageContent = require('./findMessageContent');
 const replaceSpecialVars = require('./replaceSpecialVars');
 
 module.exports = {
   loadYaml,
-  ...tokenHelpers,
-  ...azureUtils,
   deriveBaseURL,
-  logAxiosError,
   extractBaseURL,
+  ...azureUtils,
+  ...axiosHelpers,
+  ...tokenHelpers,
   findMessageContent,
   replaceSpecialVars
 };
