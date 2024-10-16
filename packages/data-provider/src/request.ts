2296836a7694ad354836766b4ea6618e7217f732
Danny Avila <danny@librechat.ai>
2024-10-16 11:24:40 -0400

    ⬆️ feat: Cancel chat file uploads; fix: Assistant uploads (#4433)
    * refactor: move file mutations to dedicated file, improve typing
    
    * refactor(ChatForm): utilize FileFormWrapper to consolidate file upload logic/rendering to single parent
    
    * refactor: better TSX heirarchies between AttachFile and FileFormWrapper
    
    * refactor: `abortUpload` WIP
    
    * fix: file debugging and file upload issues
    
    * refactor: reject promise outright if axios intercepted error does not include response property
    
    * chore: bump data-provider version to 0.7.428
    
    * refactor: Add return type to localize function in Translation.ts
    
    * refactor: allow message file attachment upload request cancellations, and add localizations for file upload errors
    
    * refactor: include Azure OpenAI in paramEndpoints set
    
    * fix: assistant form uploads and better typing
    
    * refactor: consolidate logic

--- packages/data-provider/src/request.ts
+++ packages/data-provider/src/request.ts
@@ -80,6 +80,9 @@ axios.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config;
+    if (!error.response) {
+      return Promise.reject(error);
+    }
 
     if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
