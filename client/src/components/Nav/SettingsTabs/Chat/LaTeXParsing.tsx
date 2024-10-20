bd249ec7aad979190f28fdc298a821b236bd143e
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2024-10-20 17:29:47 +0200

    🎨 style: UI Style Enhancements and Refactor for Improved Consistency and Layout (#4471)
    * 🎨 style: adjust padding and class names in UI components
    
    * 🎨 style: update ExportModal export button, update Export button hover style, refactor ChatForm style and fixed isRTL styles, update AttachFile position
    
    * 🎨 style: remove redundant border classes in SettingsTabs components for cleaner UI
    
    * 🎨 style: refactor Account component, extract DisplayUsernameMessages, and remove redundant border classes for cleaner layout
    
    * 🎨 style: conditionally render Dropdown in ForkSettings component for improved UI responsiveness
    
    * 🎨 style: replace DropdownNoState with Dropdown in voice selection components for consistency
    
    * 🎨 style: update Settings component layout for better responsivenes on large screens
    
    * 🎨 style: remove redundant margin-top classes for cleaner layout in various components

--- client/src/components/Nav/SettingsTabs/Chat/LaTeXParsing.tsx
+++ client/src/components/Nav/SettingsTabs/Chat/LaTeXParsing.tsx
@@ -29,7 +29,7 @@ export default function LaTeXParsingSwitch({
         id="LaTeXParsing"
         checked={LaTeXParsing}
         onCheckedChange={handleCheckedChange}
-        className="ml-4 mt-2"
+        className="ml-4"
         data-testid="LaTeXParsing"
       />
     </div>
