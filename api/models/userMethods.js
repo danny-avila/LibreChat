6d3e2cff27e90e6ac68fa0bc893674a16eac3561
MSITE.TOP <admin@msite.top>
2024-10-21 18:16:11 +0400

    automatically add  start balance
    https://github.com/danny-avila/LibreChat/issues/2687

--- api/models/userMethods.js
+++ api/models/userMethods.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs');
 const signPayload = require('~/server/services/signPayload');
+const { isEnabled } = require('~/server/utils/handleText');
 const User = require('./User');
+const Balance = require('./Balance');
 
 /**
  * Retrieve a user by ID and convert the found user document to a plain object.
@@ -71,6 +73,16 @@ const createUser = async (data, disableTTL = true, returnUser = false) => {
   }
 
   const user = await User.create(userData);
+
+  if (isEnabled(process.env.CHECK_BALANCE) && process.env.START_BALANCE) {
+    let incrementValue = parseInt(process.env.START_BALANCE);
+    await Balance.findOneAndUpdate(
+      { user: user._id },
+      { $inc: { tokenCredits: incrementValue } },
+      { upsert: true, new: true },
+    ).lean();
+  }
+
   if (returnUser) {
     return user.toObject();
   }
