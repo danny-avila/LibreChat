969fb6f6ce8081d41332a9bcac544242b1227d76
paridhi7 <theparidhi0@gmail.com>
2024-10-29 01:10:50 +0530

    Create a transaction record when tokens are allocated to a user

--- api/server/services/SubscriptionService.js
+++ api/server/services/SubscriptionService.js
@@ -1,5 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const { updateUser, getUserById } = require('~/models/userMethods');
+const { Transaction } = require('~/models/Transaction');
 const { logger } = require('~/config');
 
 const upgradeSubscription = async (userId, planId) => {
@@ -69,23 +70,45 @@ const handleSubscriptionUpdated = async (subscription) => {
       subscriptionExpiresAt: new Date(subscription.current_period_end * 1000),
     };
 
-    // Update token balance based on the plan
-    switch (updatedData.subscription) {
+    // Define token amounts based on plan
+    let tokenAmount = 0;
+    switch (planName) {
       case 'plus':
-        updatedData.tokenBalance = 5000;
+        tokenAmount = 5000;
         break;
       case 'pro':
-        updatedData.tokenBalance = 10000;
+        tokenAmount = 10000;
         break;
       case 'enterprise':
-        updatedData.tokenBalance = 50000;
+        tokenAmount = 50000;
         break;
       default:
-        updatedData.tokenBalance = 0; // Free plan
+        tokenAmount = 0; // Free plan
     }
 
+    // Create a transaction to add tokens
+    if (tokenAmount > 0) {
+      await Transaction.create({
+        user: user._id,
+        tokenType: 'credits',
+        context: 'subscription',
+        rawAmount: tokenAmount,
+        metadata: {
+          source: 'subscription',
+          planType: planName,
+        },
+      });
+    }
+
+    // Update user data including token balance
+    updatedData.tokenBalance = tokenAmount;
     await updateUser(user._id, updatedData);
-    logger.info(`[handleSubscriptionUpdated] Subscription updated for user ${user._id}`);
+
+    logger.info(`[handleSubscriptionUpdated] Subscription updated for user ${user._id}`, {
+      subscription: planName,
+      tokenAmount,
+      status: subscription.status,
+    });
   } catch (error) {
     logger.error('[handleSubscriptionUpdated] Error:', error);
     throw error;
