9b1f074e53babacea49eabc1180a5eba042d9479
Arian Ebrahimi <root@bachetesla.ir>
2024-11-05 22:10:19 +0330

    Update

--- packages/data-provider/src/types/mutations.ts
+++ packages/data-provider/src/types/mutations.ts
@@ -249,20 +249,9 @@ export type AcceptTermsMutationOptions = MutationOptions<
   void
 >;
 
-// New types for buying a subscription
-export type TBuySubscriptionRequest = {
-  userId: string;
-  planId: string;
-};
-
-export type TBuySubscriptionResponse = {
-  success: boolean;
-  paymentUrl?: string; // URL to redirect the user to the payment gateway
-  message?: string;
-};
 
 // Mutation options for the buy subscription process
 export type BuySubscriptionOptions = MutationOptions<
-    TBuySubscriptionResponse,
-    TBuySubscriptionRequest
+    types.TBuySubscriptionResponse,
+    types.TBuySubscriptionRequest
 >;
