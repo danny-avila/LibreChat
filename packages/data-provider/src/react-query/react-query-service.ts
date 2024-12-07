66500e77ee38ecf85509851c3cde54ff9df8fc03
Danny Avila <danny@librechat.ai>
2024-12-07 16:55:17 -0500

    🔧 fix: URL params, package mismatch, typing, shared link redirect, and o1 (azure) (#4899)
    * fix: double decoding of URL params
    
    * fix: prevent streaming options for O1 model (azure)
    
    * fix: update @langchain/openai to version 0.3.14 in package-lock.json
    
    * chore(AuthContext): typing
    
    * chore(useTimeout): typing
    
    * fix: shared link redirecting to login when code block includes "Run Code" button
    
    * fix: typing

--- packages/data-provider/src/react-query/react-query-service.ts
+++ packages/data-provider/src/react-query/react-query-service.ts
@@ -338,7 +338,7 @@ export const useRegisterUserMutation = (
 };
 
 export const useRefreshTokenMutation = (): UseMutationResult<
-  t.TRefreshTokenResponse,
+  t.TRefreshTokenResponse | undefined,
   unknown,
   unknown,
   unknown
