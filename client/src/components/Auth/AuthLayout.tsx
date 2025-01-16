5e15b53954338f5f24444a03d06f994d42871889
IlitA <ilit.amar@parserdigital.com>
2025-01-16 17:14:06 +0100

    .env-title

--- client/src/components/Auth/AuthLayout.tsx
+++ client/src/components/Auth/AuthLayout.tsx
@@ -65,7 +65,7 @@ function AuthLayout({
           <img
             src="/assets/logo.svg"
             className="h-full w-full object-contain"
-            alt={localize('com_ui_logo', startupConfig?.appTitle ?? 'LibreChat')}
+            alt={localize('com_ui_logo', startupConfig?.appTitle ?? 'Parser AI')}
           />
         </div>
       </BlinkAnimation>
