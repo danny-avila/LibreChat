32e2971ff55eb7f1c58d522f50cd33a7f2859b7f
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2025-01-19 13:08:09 +0530

    📜 refactor: Keeping other props that uses alg.

--- api/strategies/openidStrategy.js
+++ api/strategies/openidStrategy.js
@@ -114,11 +114,18 @@ async function setupOpenId() {
       logger.info(`[openidStrategy] proxy agent added: ${process.env.PROXY}`);
     }
     const issuer = await Issuer.discover(process.env.OPENID_ISSUER);
+    const supported_alg = {
+      id_token_signed_response_alg: issuer.id_token_signing_alg_values_supported[0] || 'RS256',
+      // request_object_signing_alg: jar ? alg : undefined, // Both in v5 and v6
+      // userinfo_signed_response_alg: jwtUserinfo ? alg : undefined, // Only in v6
+      // introspection_signed_response_alg: alg, // Only in v6
+      // authorization_signed_response_alg: alg, // Only in v6
+    };
     const client = new issuer.Client({
       client_id: process.env.OPENID_CLIENT_ID,
       client_secret: process.env.OPENID_CLIENT_SECRET,
       redirect_uris: [process.env.DOMAIN_SERVER + process.env.OPENID_CALLBACK_URL],
-      id_token_signed_response_alg: issuer.id_token_signing_alg_values_supported[0] || 'RS256',
+      ...supported_alg,
     });
     const requiredRole = process.env.OPENID_REQUIRED_ROLE;
     const requiredRoleParameterPath = process.env.OPENID_REQUIRED_ROLE_PARAMETER_PATH;
