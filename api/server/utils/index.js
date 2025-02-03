974f19e21b2a6c0d2feb48aec259d3f42309c66e
peturgq <petur@petur.io>
2025-02-03 10:42:42 +0000

    Add better logging to utils module

--- api/server/utils/index.js
+++ api/server/utils/index.js
@@ -1,3 +1,4 @@
+console.log({ "Starting utils"})
 const streamResponse = require('./streamResponse');
 const removePorts = require('./removePorts');
 const countTokens = require('./countTokens');
@@ -8,7 +9,19 @@ const cryptoUtils = require('./crypto');
 const queue = require('./queue');
 const files = require('./files');
 const math = require('./math');
-
+console.log("Logging Modules you should not see undefined:")
+console.log({
+  streamResponse,
+  removePorts,
+  countTokens,
+  handleText,
+  citations,
+  sendEmail,
+  cryptoUtils,
+  queue,
+  files,
+  math,
+});
 /**
  * Check if email configuration is set
  * @returns {Boolean}
@@ -21,7 +34,19 @@ function checkEmailConfig() {
     !!process.env.EMAIL_FROM
   );
 }
-
+console.log("Exporting utils:", {
+  ...streamResponse,
+  checkEmailConfig,
+  ...cryptoUtils,
+  ...handleText,
+  ...citations,
+  countTokens,
+  removePorts,
+  sendEmail,
+  ...files,
+  ...queue,
+  math,
+});
 module.exports = {
   ...streamResponse,
   checkEmailConfig,
