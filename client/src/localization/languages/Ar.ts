478f606e9cee705a2fcdf1d40d882d864c5e6007
Igor <31377238+TonyMahoney@users.noreply.github.com>
2025-02-03 18:56:44 +0300

    🌍 i18n: Add Missing "Balance" Localization For All Languages (#5594)
    * Update AccountSettings.tsx
    
    ---------
    
    Co-authored-by: Danny Avila <danacordially@gmail.com>

--- client/src/localization/languages/Ar.ts
+++ client/src/localization/languages/Ar.ts
@@ -914,4 +914,5 @@ export default {
   com_ui_collapse_chat: 'طي الدردشة',
   com_endpoint_message_new: 'رسالة {0}',
   com_ui_speech_while_submitting: 'لا يمكن إرسال الكلام أثناء إنشاء الرد',
+  com_nav_balance: 'توازن',
 };
