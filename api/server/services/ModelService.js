0312d4f4f479f6ffde65c6d617d380ed55d8fec5
Danny Avila <danny@librechat.ai>
2025-02-03 16:08:34 -0500

    🔧 refactor: Revamp Model and Tool Filtering Logic (#5637)
    * 🔧 fix: Update regex to correctly match OpenAI model identifiers
    
    * 🔧 fix: Enhance tool filtering logic in ToolService to handle inclusion and exclusion criteria for basic tools and toolkits
    
    * feat: support o3-mini Azure streaming
    
    * chore: Update model filtering logic to exclude audio and realtime models
    
    * ci: linting error

--- api/server/services/ModelService.js
+++ api/server/services/ModelService.js
@@ -159,8 +159,9 @@ const fetchOpenAIModels = async (opts, _models = []) => {
   }
 
   if (baseURL === openaiBaseURL) {
-    const regex = /(text-davinci-003|gpt-|o1-)/;
-    models = models.filter((model) => regex.test(model));
+    const regex = /(text-davinci-003|gpt-|o\d+-)/;
+    const excludeRegex = /audio|realtime/;
+    models = models.filter((model) => regex.test(model) && !excludeRegex.test(model));
     const instructModels = models.filter((model) => model.includes('instruct'));
     const otherModels = models.filter((model) => !model.includes('instruct'));
     models = otherModels.concat(instructModels);
