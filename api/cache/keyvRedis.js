e8244f56b72075170282d99eb85a65945b817e5f
João Trigo Soares <joao.soares@parceiros.nos.pt>
2025-02-05 13:44:30 +0000

    feat: Enhance Redis support with cluster configuration and TLS options

--- api/cache/keyvRedis.js
+++ api/cache/keyvRedis.js
@@ -1,13 +1,31 @@
 const KeyvRedis = require('@keyv/redis');
 const { isEnabled } = require('~/server/utils');
+<<<<<<< HEAD
 const logger = require('~/config/winston');
+=======
+const fs = require('fs');
+const ioredis = require('ioredis');
+>>>>>>> 82717228 (feat: Enhance Redis support with cluster configuration and TLS options)
 
-const { REDIS_URI, USE_REDIS } = process.env;
+const { REDIS_URI, USE_REDIS, USE_REDIS_CLUSTER, REDIS_CA } = process.env;
 
 let keyvRedis;
 
 if (REDIS_URI && isEnabled(USE_REDIS)) {
-  keyvRedis = new KeyvRedis(REDIS_URI, { useRedisSets: false });
+  let redisOptions = null;
+  let keyvOpts = { useRedisSets: false };
+  let keyvRedis;
+  if (REDIS_CA) {
+    const ca = fs.readFileSync(REDIS_CA);
+    redisOptions = { tls: { ca } };
+  }
+  if (USE_REDIS_CLUSTER) {
+    const hosts = REDIS_URI.split(',').map((host) => {return { url: host }});
+    const cluster = new ioredis.Cluster(hosts, { redisOptions });
+    keyvRedis = new KeyvRedis(cluster, keyvOpts);
+  } else {
+    keyvRedis = new KeyvRedis(REDIS_URI, keyvOpts);
+  }
   keyvRedis.on('error', (err) => logger.error('KeyvRedis connection error:', err));
   keyvRedis.setMaxListeners(20);
   logger.info(
