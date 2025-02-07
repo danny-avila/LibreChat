47f3a78beb6d656c5f43a3ed70906bf8b1c19a6e
Gil Assunção <gil.assuncao@parceiros.nos.pt>
2025-02-07 16:46:08 +0000

    Add URL mapping

--- api/cache/keyvRedis.js
+++ api/cache/keyvRedis.js
@@ -11,6 +11,43 @@ const { REDIS_URI, USE_REDIS, USE_REDIS_CLUSTER, REDIS_CA } = process.env;
 
 let keyvRedis;
 
+function mapURI(uri) {
+  const regex = /^(?<scheme>[a-zA-Z]+):\/\/(?<user>[^:]*):?(?<password>[^@]*)?@?(?<host>[^:\/]+):?(?<port>\d+)?/;
+  const match = uri.match(regex);
+
+  if (match) {
+      const { scheme, user, password, host, port } = match.groups;
+
+      return {
+          scheme: scheme || 'none',
+          user: user || null,
+          password: password || null,
+          host: host || null,
+          port: port || null,
+      };
+  } else {
+      // Handle cases without a scheme
+      const parts = uri.split(':');
+      if (parts.length === 2) {
+          return {
+              scheme: 'none',
+              user: null,
+              password: null,
+              host: parts[0],
+              port: parts[1],
+          };
+      }
+
+      return {
+          scheme: 'none',
+          user: null,
+          password: null,
+          host: uri,
+          port: null,
+      };
+  }
+}
+
 if (REDIS_URI && isEnabled(USE_REDIS)) {
   let redisOptions = null;
   let keyvOpts = { useRedisSets: false };
@@ -19,18 +56,21 @@ if (REDIS_URI && isEnabled(USE_REDIS)) {
     redisOptions = { tls: { ca } };
   }
   if (isEnabled(USE_REDIS_CLUSTER)) {
-    const hosts = REDIS_URI.split(',')
-      .map((host) => {
-        // TODO parse `host` input to handle connection strings
-        return { host: host };
-      });
+    const hosts = REDIS_URI.split(',').map((item) => {
+      var value = mapURI(item);
+
+      return {
+        host: value.host,
+        port: value.port
+      };
+    });
     const cluster = new ioredis.Cluster(hosts, { redisOptions });
     keyvRedis = new KeyvRedis(cluster, keyvOpts);
   } else {
     keyvRedis = new KeyvRedis(REDIS_URI, keyvOpts);
   }
   keyvRedis.on('error', (err) => logger.error('KeyvRedis connection error:', err));
-  keyvRedis.setMaxListeners(20);
+  keyvRedis.setMaxListeners(0);
   logger.info(
     '[Optional] Redis initialized. Note: Redis support is experimental. If you have issues, disable it. Cache needs to be flushed for values to refresh.',
   );
