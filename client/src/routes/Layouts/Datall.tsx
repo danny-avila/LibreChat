a113e99e155f803ca06359ce2335557a00a50791
Saeed Kabootari <saeedkabootari@gmail.com>
2025-02-08 16:35:21 +0330

    add RegisterWithRole component

--- client/src/routes/Layouts/Datall.tsx
+++ client/src/routes/Layouts/Datall.tsx
@@ -1,11 +1,14 @@
 import Register from "~/components/Datall/Register";
+import RegisterWithRole from "~/components/Datall/RegisterWithRole";
 import { Outlet, useLocation } from "react-router-dom";
 
 
 const Datall:React.FC =() =>{
     const location = useLocation()
 
-    return <>{location.pathname === '/datall/welcome' ? <Outlet/> : <Register /> }</>
+    // return <>{location.pathname === '/datall/welcome' ? <Outlet/> : <Register /> }</>
+    return <>{location.pathname === '/datall/welcome' ? <Outlet/> : <RegisterWithRole /> }</>
+
 }
 
 export default Datall
