154d80963bd97356040b1d0017cd58cdf3685c9d
Danny Avila <danny@librechat.ai>
2025-02-09 11:00:01 -0500

    fix: handle undefined values in SortFilterHeader component

--- client/src/components/Chat/Input/Files/Table/SortFilterHeader.tsx
+++ client/src/components/Chat/Input/Files/Table/SortFilterHeader.tsx
@@ -77,30 +77,30 @@ export function SortFilterHeader<TData, TValue>({
           </DropdownMenuItem>
           <DropdownMenuSeparator className="dark:bg-gray-500" />
           {filters &&
-                Object.entries(filters).map(([key, values]) =>
-                  values.map((value: string | number) => {
-                    const translationKey = valueMap?.[value];
-                    const filterValue =
-                          translationKey && translationKey.length
-                            ? localize(translationKey as TranslationKeys)
-                            : String(value);
-                    if (!filterValue) {
-                      return null;
-                    }
-                    return (
-                      <DropdownMenuItem
-                        className="cursor-pointer text-text-primary"
-                        key={`${key}-${value}`}
-                        onClick={() => {
-                          column.setFilterValue(value);
-                        }}
-                      >
-                        <ListFilter className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
-                        {filterValue}
-                      </DropdownMenuItem>
-                    );
-                  }),
-                )}
+            Object.entries(filters).map(([key, values]) =>
+              values.map((value?: string | number) => {
+                const translationKey = valueMap?.[value ?? ''];
+                const filterValue =
+                  translationKey != null && translationKey.length
+                    ? localize(translationKey as TranslationKeys)
+                    : String(value);
+                if (!filterValue) {
+                  return null;
+                }
+                return (
+                  <DropdownMenuItem
+                    className="cursor-pointer text-text-primary"
+                    key={`${key}-${value}`}
+                    onClick={() => {
+                      column.setFilterValue(value);
+                    }}
+                  >
+                    <ListFilter className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+                    {filterValue}
+                  </DropdownMenuItem>
+                );
+              }),
+            )}
           {filters && (
             <DropdownMenuItem
               className={
