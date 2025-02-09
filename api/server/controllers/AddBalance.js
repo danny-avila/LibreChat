e895ec6fd6dc54be5101ce63fd03109ee98355b9
Saeed Kabootari <saeedkabootari@gmail.com>
2025-02-09 11:36:48 +0330

    modified addBalanceController and now we can add balance to that user is logged in

--- api/server/controllers/AddBalance.js
+++ api/server/controllers/AddBalance.js
@@ -1,12 +1,31 @@
 const Balance = require('~/models/Balance');
 
 async function addBalanceController(req, res) {
-  const newBalnce = req.body.balance;
-  const { tokenCredits: balance = '' } =
-    (await Balance.findOne({ user: req.user.id }, 'tokenCredits').lean()) ?? {};
-  console.log(req.user);
+  const newBalance = req.body.balance;
 
-  res.status(200).send(`your real balance is: ${balance} and you send this for balance: ${newBalnce} `);
+  if (typeof newBalance !== 'number') {
+    return res.status(400).json({ message: 'Balance must be a number.' });
+}
+
+  try {
+    const currentRecord = await Balance.findOne({ user: req.user.id }).lean();
+    
+console.log('currentRecord:' ,currentRecord)
+    if (!currentRecord) {
+      return res.status(404).json({ message: 'Balance record not found for the user.' });
+    }
+    const updatedBalance = currentRecord.tokenCredits + newBalance;
+    console.log('updatedBalance:',updatedBalance)
+
+    await Balance.updateOne({ user: req.user.id }, { tokenCredits: updatedBalance });
+
+    return res
+      .status(200)
+      .json({ message: 'Balance updated successfully.', balance: updatedBalance });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error.' });
+  }
 }
 
 module.exports = addBalanceController;
