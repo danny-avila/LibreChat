c42b42897d3d83ba10172a26a9ba8a54778fc93d
Danny Avila <danny@librechat.ai>
2025-02-09 11:37:05 -0500

    fix: improve handling of optional properties in cleanupPreset function

--- client/src/utils/cleanupPreset.ts
+++ client/src/utils/cleanupPreset.ts
@@ -1,33 +1,34 @@
 import { parseConvo } from 'librechat-data-provider';
 import type { TPreset } from 'librechat-data-provider';
 
+type UIPreset = Partial<TPreset> & { presetOverride?: Partial<TPreset> };
 type TCleanupPreset = {
-  preset: Partial<TPreset>;
+  preset?: UIPreset;
 };
 
 const cleanupPreset = ({ preset: _preset }: TCleanupPreset): TPreset => {
-  const { endpoint, endpointType } = _preset;
-  if (!endpoint) {
+  const { endpoint, endpointType } = _preset ?? ({} as UIPreset);
+  if (endpoint == null || endpoint === '') {
     console.error(`Unknown endpoint ${endpoint}`, _preset);
     return {
       endpoint: null,
-      presetId: _preset.presetId ?? null,
-      title: _preset.title ?? 'New Preset',
+      presetId: _preset?.presetId ?? null,
+      title: _preset?.title ?? 'New Preset',
     };
   }
 
-  const { presetOverride = {}, ...rest } = _preset;
+  const { presetOverride = {}, ...rest } = _preset ?? {};
   const preset = { ...rest, ...presetOverride };
 
   /* @ts-ignore: endpoint can be a custom defined name */
   const parsedPreset = parseConvo({ endpoint, endpointType, conversation: preset });
 
   return {
-    presetId: _preset.presetId ?? null,
+    presetId: _preset?.presetId ?? null,
     ...parsedPreset,
     endpoint,
     endpointType,
-    title: _preset.title ?? 'New Preset',
+    title: _preset?.title ?? 'New Preset',
   } as TPreset;
 };
 
