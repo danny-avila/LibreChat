22405e71c8d12befbb2e265c497b9de57984fea4
Ruben Talstra <RubenTalstra1211@outlook.com>
2025-02-09 18:05:31 +0100

    🌎 i18n: React-i18next & i18next Integration (#5720)
    * better i18n support an internationalization-framework.
    
    * removed unused package
    
    * auto sort for translation.json
    
    * fixed tests with the new locales function
    
    * added new CI actions from locize
    
    * to use locize a mention in the README.md
    
    * to use locize a mention in the README.md
    
    * updated README.md and added TRANSLATION.md to the repo
    
    * updated TRANSLATION.md badges
    
    * updated README.md to go to the TRANSLATION.md when clicking on the Translation Progress badge
    
    * updated TRANSLATION.md and added a new issue template.
    
    * updated TRANSLATION.md and added a new issue template.
    
    * updated issue template to add the iso code link.
    
    * updated the new GitHub actions for `locize`
    
    * updated label for new issue template --> i18n
    
    * fixed type issue
    
    * Fix eslint
    
    * Fix eslint with key-spacing spacing
    
    * fix: error type
    
    * fix: handle undefined values in SortFilterHeader component
    
    * fix: typing in Image component
    
    * fix: handle optional promptGroup in PromptCard component
    
    * fix: update localize function to accept string type and remove unnecessary JSX element
    
    * fix: update localize function to enforce TranslationKeys type for better type safety
    
    * fix: improve type safety and handle null values in Assistants component
    
    * fix: enhance null checks for fileId in FilesListView component
    
    * fix: localize 'Go back' button text in FilesListView component
    
    * fix: update aria-label for menu buttons and add translation for 'Close Menu'
    
    * docs: add Reasoning UI section for Chain-of-Thought AI models in README
    
    * fix: enhance type safety by adding type for message in MultiMessage component
    
    * fix: improve null checks and optional chaining in useAutoSave hook
    
    * fix: improve handling of optional properties in cleanupPreset function
    
    * fix: ensure isFetchingNextPage defaults to false and improve null checks for messages in Search component
    
    * fix: enhance type safety and null checks in useBuildMessageTree hook
    
    ---------
    
    Co-authored-by: Danny Avila <danny@librechat.ai>

--- client/src/components/SidePanel/Agents/Sequential/HideSequential.tsx
+++ client/src/components/SidePanel/Agents/Sequential/HideSequential.tsx
@@ -38,7 +38,7 @@ export default function HideSequential() {
             type="button"
             className="flex items-center space-x-2"
             onClick={() =>
-              // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+
               setValue(
                 AgentCapabilities.hide_sequential_outputs,
                 !getValues(AgentCapabilities.hide_sequential_outputs),
