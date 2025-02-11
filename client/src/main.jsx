02707f67f2aceb75e8a9455b5a5b71fbb6a99878
Saeed Kabootari <saeedkabootari@gmail.com>
2025-02-11 14:38:45 +0330

    add mui to project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,17 @@ import App from './App';
 import './style.css';
 import './mobile.css';
 import { ApiErrorBoundaryProvider } from './hooks/ApiErrorBoundaryContext';
+import { ThemeProvider } from '@mui/material';
+
+// const theme = cerateTheme();
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <ApiErrorBoundaryProvider>
+    {/* <ThemeProvider> */}
     <App />
+    {/* </ThemeProvider> */}
   </ApiErrorBoundaryProvider>,
 );
--- client/src/routes/Layouts/Datall.tsx
+++ client/src/routes/Layouts/Datall.tsx
@@ -1,17 +1,22 @@
-import Register from "~/components/Datall/Register";
-import RegisterWithRole from "~/components/Datall/RegisterWithRole";
-import { Outlet, useLocation } from "react-router-dom";
+import Register from '~/components/Datall/Register';
+import RegisterWithRole from '~/components/Datall/RegisterWithRole';
+import { Outlet, useLocation } from 'react-router-dom';
+import { Box, Container, Typography } from '@mui/material';
 
+const Datall: React.FC = () => {
+  const location = useLocation();
 
-const Datall:React.FC =() =>{
-    const location = useLocation()
+  // return <>{location.pathname === '/datall/welcome' ? <Outlet/> : <Register /> }</>
+  // return <>{location.pathname === '/datall/welcome' ? <Outlet/> : <RegisterWithRole /> }</>
+  // return <div className="flex justify-center"><Outlet/> <div className=" bg-red-400 "></div></div>
+  return (
+    <Container sx={{ bgcolor: 'tomato', height: '100vh' }}>
+      <Outlet />
+      <Typography variant='h1'>hello world!</Typography>
+      <Box sx={{width:{xs:100, md:200} ,height:200 ,bgcolor:'blueviolet'}}></Box>
+      
+    </Container>
+  );
+};
 
-    // return <>{location.pathname === '/datall/welcome' ? <Outlet/> : <Register /> }</>
-    // return <>{location.pathname === '/datall/welcome' ? <Outlet/> : <RegisterWithRole /> }</>
-    return <div className="flex justify-center"><Outlet/> <div className=" bg-red-400 "></div></div>
-
-}
-
-export default Datall
-   
-    
+export default Datall;
