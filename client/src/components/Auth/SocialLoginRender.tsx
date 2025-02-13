47a5b0a4d6a29e98d231bcde1c0f2f4cfb5dbc5b
Ruben Talstra <RubenTalstra1211@outlook.com>
2025-02-13 08:53:47 +0100

    Test commit with GPG signing

--- client/src/components/Auth/SocialLoginRender.tsx
+++ client/src/components/Auth/SocialLoginRender.tsx
@@ -113,7 +113,7 @@ function SocialLoginRender({ startupConfig, mode, onPasskeyClick }: SocialLoginR
           <>
             <div className="relative mt-6 flex w-full items-center justify-center border border-t border-gray-300 uppercase dark:border-gray-600">
               <div className="absolute bg-white px-3 text-xs text-black dark:bg-gray-900 dark:text-white">
-                      Or
+                Or
               </div>
             </div>
             <div className="mt-8" />
