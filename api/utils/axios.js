b63c007b39b8e9d2cda63d629141786cd3773627
Danny Avila <danny@librechat.ai>
2025-02-16 11:10:40 -0500

    refactor: Improve error handling in logAxiosError function

--- api/utils/axios.js
+++ api/utils/axios.js
@@ -5,28 +5,32 @@ const { logger } = require('~/config');
  *
  * @param {Object} options - The options object.
  * @param {string} options.message - The custom message to be logged.
- * @param {Error} options.error - The Axios error object.
+ * @param {import('axios').AxiosError} options.error - The Axios error object.
  */
 const logAxiosError = ({ message, error }) => {
-  if (error.response) {
-    const { status, headers, data } = error.response;
-    logger.error(`${message} The server responded with status ${status}: ${error.message}`, {
-      status,
-      headers,
-      data,
-    });
-  } else if (error.request) {
-    const { method, url } = error.config || {};
-    logger.error(
-      `${message} No response received for ${method ? method.toUpperCase() : ''} ${url || ''}: ${error.message}`,
-      { requestInfo: { method, url } },
-    );
-  } else if (error?.message?.includes('Cannot read properties of undefined (reading \'status\')')) {
-    logger.error(
-      `${message} It appears the request timed out or was unsuccessful: ${error.message}`,
-    );
-  } else {
-    logger.error(`${message} An error occurred while setting up the request: ${error.message}`);
+  try {
+    if (error.response?.status) {
+      const { status, headers, data } = error.response;
+      logger.error(`${message} The server responded with status ${status}: ${error.message}`, {
+        status,
+        headers,
+        data,
+      });
+    } else if (error.request) {
+      const { method, url } = error.config || {};
+      logger.error(
+        `${message} No response received for ${method ? method.toUpperCase() : ''} ${url || ''}: ${error.message}`,
+        { requestInfo: { method, url } },
+      );
+    } else if (error?.message?.includes('Cannot read properties of undefined (reading \'status\')')) {
+      logger.error(
+        `${message} It appears the request timed out or was unsuccessful: ${error.message}`,
+      );
+    } else {
+      logger.error(`${message} An error occurred while setting up the request: ${error.message}`);
+    }
+  } catch (err) {
+    logger.error(`Error in logAxiosError: ${err.message}`);
   }
 };
 
