08ef903227e9795557f68b1bd88315b806d3b4db
Danny Avila <danny@librechat.ai>
2025-02-21 15:02:07 -0500

    👐 refactor: Agents Accessibility and Gemini Error Handling (#5972)
    * style: Enhance ControlCombobox with Carat Display, ClassName, and Disabled State
    
    * refactor(ModelPanel): replace SelectDropdown with ControlCombobox for improved accessibility
    
    * style: Adjust padding and positioning in ModelPanel for improved layout
    
    * style(ControlCombobox): add containerClassName and iconSide props for enhanced customization
    
    * style(ControlCombobox): add iconClassName prop for customizable icon styling
    
    * refactor(AgentPanel): enhance layout with new button for creating agents and adjust structure for better alignment
    
    * refactor(AgentSelect): replace SelectDropDown with ControlCombobox for improved accessibility and layout
    
    * feat(translation): add new translation key for improved UI clarity
    
    * style(AgentSwitcher, AssistantSwitcher): add iconClassName prop for customizable icon styling
    
    * refactor(AgentPanelSkeleton): improve layout of skeleton components to match new visual structure
    
    * style(AgentPanel, AgentPanelSkeleton): add margin to flex container for improved layout consistency
    
    * a11y(AgentSelect, ControlCombobox): add selectId prop for preventing focus going to start to page after agent selection
    
    * fix(AgentSelect): update SELECT_ID constant for improved clarity in component identification
    
    * fix(GoogleClient): update type annotation, add abort handling for content generation requests, catch "uncaught" abort errors and GoogleGenerativeAI errors from `@google/generative-ai`

--- api/app/clients/GoogleClient.js
+++ api/app/clients/GoogleClient.js
@@ -641,7 +641,7 @@ class GoogleClient extends BaseClient {
     let error;
     try {
       if (!EXCLUDED_GENAI_MODELS.test(modelName) && !this.project_id) {
-        /** @type {GenAI} */
+        /** @type {GenerativeModel} */
         const client = this.client;
         /** @type {GenerateContentRequest} */
         const requestOptions = {
@@ -665,7 +665,17 @@ class GoogleClient extends BaseClient {
         /** @type {GenAIUsageMetadata} */
         let usageMetadata;
 
-        const result = await client.generateContentStream(requestOptions);
+        abortController.signal.addEventListener(
+          'abort',
+          () => {
+            logger.warn('[GoogleClient] Request was aborted', abortController.signal.reason);
+          },
+          { once: true },
+        );
+
+        const result = await client.generateContentStream(requestOptions, {
+          signal: abortController.signal,
+        });
         for await (const chunk of result.stream) {
           usageMetadata = !usageMetadata
             ? chunk?.usageMetadata
