4e4d67b6c780417043847f43295aed025eed3b9b
Danny Avila <danny@librechat.ai>
2025-02-24 22:52:54 -0500

    🔗 fix: Shared Link with Markdown Code Error (#6016)
    * refactor: Export AuthContext from AuthContextProvider
    
    * refactor: Update useHasAccess to utilize useContext for AuthContext
    
    * refactor: Enhance type definitions in useHasAccess for better type safety

--- client/src/hooks/Roles/useHasAccess.ts
+++ client/src/hooks/Roles/useHasAccess.ts
@@ -1,6 +1,6 @@
-import { useMemo, useCallback } from 'react';
-import { PermissionTypes, Permissions } from 'librechat-data-provider';
-import { useAuthContext } from '~/hooks/AuthContext';
+import { useMemo, useCallback, useContext } from 'react';
+import type { TUser, PermissionTypes, Permissions } from 'librechat-data-provider';
+import { AuthContext } from '~/hooks/AuthContext';
 
 const useHasAccess = ({
   permissionType,
@@ -9,16 +9,31 @@ const useHasAccess = ({
   permissionType: PermissionTypes;
   permission: Permissions;
 }) => {
-  const { isAuthenticated, user, roles } = useAuthContext();
+  const authContext = useContext(AuthContext);
+  const user = authContext?.user;
+  const roles = authContext?.roles;
+  const isAuthenticated = authContext?.isAuthenticated || false;
 
   const checkAccess = useCallback(
-    ({ user, permissionType, permission }) => {
+    ({
+      user,
+      permissionType,
+      permission,
+    }: {
+      user?: TUser | null;
+      permissionType: PermissionTypes;
+      permission: Permissions;
+    }) => {
+      if (!authContext) {
+        return false;
+      }
+
       if (isAuthenticated && user?.role != null && roles && roles[user.role]) {
         return roles[user.role]?.[permissionType]?.[permission] === true;
       }
       return false;
     },
-    [isAuthenticated, roles],
+    [authContext, isAuthenticated, roles],
   );
 
   const hasAccess = useMemo(
