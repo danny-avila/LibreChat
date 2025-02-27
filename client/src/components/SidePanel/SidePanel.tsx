31f6bfd4af31e8e88a438cafce6c78b346f9babd
xConnorTV <connorbreuer@gmail.com>
2025-02-27 11:31:25 +0100

    custom link

--- client/src/components/SidePanel/SidePanel.tsx
+++ client/src/components/SidePanel/SidePanel.tsx
@@ -11,6 +11,9 @@ import { cn, getEndpointField } from '~/utils';
 import { useChatContext } from '~/Providers';
 import Switcher from './Switcher';
 import Nav from './Nav';
+import { useAuthContext } from '~/hooks/AuthContext';
+import showdown from 'showdown';
+import { SaveAllIcon } from 'lucide-react';
 
 const defaultMinSize = 20;
 
@@ -48,8 +51,10 @@ const SidePanel = ({
   const [newUser, setNewUser] = useLocalStorage('newUser', true);
   const { data: endpointsConfig = {} as TEndpointsConfig } = useGetEndpointsQuery();
 
+  const { user } = useAuthContext();
+
   const isSmallScreen = useMediaQuery('(max-width: 767px)');
-  const { conversation } = useChatContext();
+  const { conversation, getMessages } = useChatContext();
   const { endpoint } = conversation ?? {};
   const { data: keyExpiry = { expiresAt: undefined } } = useUserKeyQuery(endpoint ?? '');
 
@@ -91,7 +96,59 @@ const SidePanel = ({
     keyProvided,
     endpointType,
     interfaceConfig,
-    customLinks: interfaceConfig.customLinks,
+    customLinks: [
+      {
+        id: 'custom-settings',
+        title: 'com_ui_custom_settings', // localizeKey
+        icon: SaveAllIcon,
+        onClick: async () => {
+          const messages = getMessages();
+
+          const converter = new showdown.Converter();
+
+          const html = messages?.map((message) => message.isCreatedByUser ?
+            `
+            <div style="margin-bottom: 10px;">
+                <div style="background-color: #e0e0e0; padding: 10px; border-radius: 8px; max-width: 80%;">
+                    <strong>${user?.username}</strong>
+                    <p>${converter.makeHtml(message.text)}</p>
+                </div>
+            </div>
+            ` : `
+            <div style="margin-bottom: 10px;">
+                <div style="background-color: #007bff; color: #fff; padding: 10px; border-radius: 8px; max-width: 80%; margin-left: auto;">
+                     <div style="text-align: right;">
+                        <strong>InsuraiBot_${message.sender}</strong>              
+                    </div>
+                    <p>${converter.makeHtml(message.text)}</p>
+                </div>
+            </div>
+            `).join('');
+
+          const final = `
+              <div><div style="flex: 1; padding: 10px;">${html}</div></div>
+            `;
+          const response = await fetch('https://aizpun.autarc-ai.de/api/Insurai/chat/save', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'key': '5ff586e432a1efdfab77b0e19ad7d211834ec61402ef93c9856945e4d02780d3',
+              'license': '05688',
+            },
+            body: JSON.stringify({
+              conversationId: conversation?.conversationId,
+              content: final,
+            }),
+          });
+
+          if(response.ok) {
+            alert('Erfolgreich gespeichert');
+          }else {
+            alert('Fehler beim Speichern');
+          }
+        },
+      },
+    ]
   });
 
   const toggleNavVisible = useCallback(() => {
