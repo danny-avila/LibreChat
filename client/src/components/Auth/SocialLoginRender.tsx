316b3eaf6ab759c0a6420846e5309af5cad19935
tsutsu3 <tsutsu3prog@gmail.com>
2025-02-27 22:37:06 +0900

    feat: add SAML authentication

--- client/src/components/Auth/SocialLoginRender.tsx
+++ client/src/components/Auth/SocialLoginRender.tsx
@@ -1,4 +1,11 @@
-import { GoogleIcon, FacebookIcon, OpenIDIcon, GithubIcon, DiscordIcon, AppleIcon } from '~/components';
+import {
+  GoogleIcon,
+  FacebookIcon,
+  OpenIDIcon,
+  GithubIcon,
+  DiscordIcon,
+  AppleIcon,
+} from '~/components';
 
 import SocialButton from './SocialButton';
 
@@ -88,6 +95,23 @@ function SocialLoginRender({
         }
         label={startupConfig.openidLabel}
         id="openid"
+      />
+    ),
+    saml: startupConfig.samlLoginEnabled && (
+      <SocialButton
+        key="saml"
+        enabled={startupConfig.samlLoginEnabled}
+        serverDomain={startupConfig.serverDomain}
+        oauthPath="saml"
+        Icon={() =>
+          startupConfig.samlImageUrl ? (
+            <img src={startupConfig.samlImageUrl} alt="SAML Logo" className="h-5 w-5" />
+          ) : (
+            <OpenIDIcon /> // TODO: Replace with SAML icon
+          )
+        }
+        label={startupConfig.samlLabel}
+        id="saml"
       />
     ),
   };
