f5f206377b1cb6f88fc399800fec7aee163f366e
tsutsu3 <tsutsu3prog@gmail.com>
2025-03-04 20:05:29 +0900

    feat: update samlStrategy

--- api/server/socialLogins.js
+++ api/server/socialLogins.js
@@ -67,7 +67,12 @@ const configureSocialLogins = (app) => {
 
     logger.info('OpenID Connect configured.');
   }
-  if (process.env.SAML_METADATA && process.env.SAML_SESSION_SECRET) {
+  if (
+    process.env.SAML_ENTRY_POINT &&
+    process.env.SAML_ISSUER &&
+    process.env.SAML_CERT &&
+    process.env.SAML_SESSION_SECRET
+  ) {
     const sessionOptions = {
       secret: process.env.SAML_SESSION_SECRET,
       resave: false,
