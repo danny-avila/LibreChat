24a5f7e2a08e1f0d7b9260239fe8a0d6f5a45841
Desmond Cox <desmond@desmondgc.com>
2025-03-05 07:58:57 +0000

    refactor: use global logger instead of utils/debug in requireLocalAuth middleware

--- api/server/middleware/requireLocalAuth.js
+++ api/server/middleware/requireLocalAuth.js
@@ -1,32 +1,19 @@
 const passport = require('passport');
-const DebugControl = require('../../utils/debug.js');
-
-function log({ title, parameters }) {
-  DebugControl.log.functionName(title);
-  if (parameters) {
-    DebugControl.log.parameters(parameters);
-  }
-}
+const { logger } = require('~/config');
 
 const requireLocalAuth = (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     if (err) {
-      log({
-        title: '(requireLocalAuth) Error at passport.authenticate',
-        parameters: [{ name: 'error', value: err }],
-      });
+      logger.debug('EXECUTING: (requireLocalAuth) Error at passport.authenticate');
+      logger.debug('error:', err);
       return next(err);
     }
     if (!user) {
-      log({
-        title: '(requireLocalAuth) Error: No user',
-      });
+      logger.debug('EXECUTING: (requireLocalAuth) Error: No user');
       return res.status(404).send(info);
     }
     if (info && info.message) {
-      log({
-        title: '(requireLocalAuth) Error: ' + info.message,
-      });
+      logger.debug('EXECUTING: (requireLocalAuth) Error: ' + info.message);
       return res.status(422).send({ message: info.message });
     }
     req.user = user;
