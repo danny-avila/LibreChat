5eec90d629dd03bfcb9f28e06f29e90db2eb3904
Desmond Cox <desmond@desmondgc.com>
2025-03-05 09:18:14 +0000

    refactor: reformat log message

--- api/server/middleware/requireLocalAuth.js
+++ api/server/middleware/requireLocalAuth.js
@@ -4,16 +4,15 @@ const { logger } = require('~/config');
 const requireLocalAuth = (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     if (err) {
-      logger.debug('EXECUTING: (requireLocalAuth) Error at passport.authenticate');
-      logger.debug('error:', err);
+      logger.debug('[requireLocalAuth] Error at passport.authenticate:', err);
       return next(err);
     }
     if (!user) {
-      logger.debug('EXECUTING: (requireLocalAuth) Error: No user');
+      logger.debug('[requireLocalAuth] Error: No user');
       return res.status(404).send(info);
     }
     if (info && info.message) {
-      logger.debug('EXECUTING: (requireLocalAuth) Error: ' + info.message);
+      logger.debug('[requireLocalAuth] Error: ' + info.message);
       return res.status(422).send({ message: info.message });
     }
     req.user = user;
