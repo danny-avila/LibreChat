fdd6977934d6f2a9e2cca1acb612197be6df3143
Ruben Talstra <RubenTalstra1211@outlook.com>
2025-03-08 21:27:20 +0100

    ✨ feat: Add rating and ratingContent fields to message schema

--- api/models/schema/messageSchema.js
+++ api/models/schema/messageSchema.js
@@ -2,36 +2,6 @@ const mongoose = require('mongoose');
 const mongoMeili = require('~/models/plugins/mongoMeili');
 const { messageSchema } = require('@librechat/data-schemas');
 
-// Needs to be moved to the new package
-//    rating: {
-//       type: String,
-//       enum: ['thumbsUp', 'thumbsDown'],
-//       default: null,
-//     },
-//     ratingContent: {
-//       tags: {
-//         type: [String],
-//         default: [],
-//       },
-//       tagChoices: {
-//         type: [String],
-//         default: [
-//           'Shouldn\'t have used Memory',
-//           'Don\'t like the style',
-//           'Not factually correct',
-//           'Didn\'t fully follow instructions',
-//           'Refused when it shouldn\'t have',
-//           'Being lazy',
-//           'Unsafe or problematic',
-//           'Biased',
-//           'Other',
-//         ],
-//       },
-//       text: {
-//         type: String,
-//         default: null,
-//       },
-
 if (process.env.MEILI_HOST && process.env.MEILI_MASTER_KEY) {
   messageSchema.plugin(mongoMeili, {
     host: process.env.MEILI_HOST,
