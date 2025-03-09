e8e514b123f30c9504b53e35fde778cdf34bb2b2
caitzpp <caitlyn.zuppinger@outlook.com>
2025-03-09 21:16:40 +0100

    icon adapted

--- client/src/components/Auth/Footer.tsx
+++ client/src/components/Auth/Footer.tsx
@@ -34,10 +34,13 @@ function Footer({ startupConfig }: { startupConfig: TStartupConfig | null | unde
   return (
     <div className="align-end m-4 flex justify-center gap-2" role="contentinfo">
       {privacyPolicyRender}
-      {privacyPolicyRender && termsOfServiceRender && (
+      {privacyPolicyRender && (
         <div className="border-r-[1px] border-gray-300 dark:border-gray-600" />
       )}
-      {termsOfServiceRender}
+      {/* {privacyPolicyRender && termsOfServiceRender && (
+        <div className="border-r-[1px] border-gray-300 dark:border-gray-600" />
+      )}
+      {termsOfServiceRender} */}
     </div>
   );
 }
--- client/src/components/svg/CustomMinimalIcon.tsx
+++ client/src/components/svg/CustomMinimalIcon.tsx
@@ -1,30 +1,39 @@
 import { cn } from '~/utils';
 export default function CustomMinimalIcon({
-  size = 25,
+  size = 50,
   className = '',
 }: {
   size?: number;
   className?: string;
 }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
+    <img
+      src="/assets/llama_icon_kapo.PNG"
       width={size}
       height={size}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={cn('lucide lucide-bot', className)}
-    >
-      <path d="M12 8V4H8" />
-      <rect width="16" height="12" x="4" y="8" rx="2" />
-      <path d="M2 14h2" />
-      <path d="M20 14h2" />
-      <path d="M15 13v2" />
-      <path d="M9 13v2" />
-    </svg>
+      className={cn(className)}
+      alt="Custom Icon"
+    />
   );
+  // return (
+  //   <svg
+  //     xmlns="http://www.w3.org/2000/svg"
+  //     width={size}
+  //     height={size}
+  //     viewBox="0 0 24 24"
+  //     fill="none"
+  //     stroke="currentColor"
+  //     strokeWidth="2"
+  //     strokeLinecap="round"
+  //     strokeLinejoin="round"
+  //     className={cn('lucide lucide-bot', className)}
+  //   >
+  //     <path d="M12 8V4H8" />
+  //     <rect width="16" height="12" x="4" y="8" rx="2" />
+  //     <path d="M2 14h2" />
+  //     <path d="M20 14h2" />
+  //     <path d="M15 13v2" />
+  //     <path d="M9 13v2" />
+  //   </svg>
+  // );
 }
