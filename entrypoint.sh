1bfa5c3e420ce58519a50560a9e7ae924a9cc601
Snorri Hjörvar Jóhannsson <snorri@andes.is>
2025-03-14 13:00:57 +0000

    feat(ci): adding capability to endpoint for branding (#38)
    * feat(ci): adding capability to endpoint for branding
    
    * Adding the version variable
    
    * Fixing Workflows to allow entrypoints building and removing dist upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,19 @@ echo "This run.sh will run container command: $CONTAINER_RUN_COMMAND"
 S3_BUCKET=$SERVICE_BUCKET_NAME
 CONTAINER_DIR=/app
 PREFIX=$S3_prefix
+VERSION=$VERSION
 
 # Sync files from an S3 bucket to a docker volume
 echo "Syncing files from S3 bucket $S3_BUCKET to $CONTAINER_DIR"
 echo "S3 bucket: $S3_BUCKET"
 echo "Prefix: $PREFIX"
 echo "Container directory: $CONTAINER_DIR"
-aws s3 cp s3://"$S3_BUCKET"/"$PREFIX"/librechat.yaml "$CONTAINER_DIR/api/"
+echo "Version: $VERSION"
 
-#TODO: Pull a zipped /dist from S3 and unzip it.
+#Librechat Config file
+aws s3 cp s3://"$S3_BUCKET"/"$PREFIX"/version/"$VERSION"/librechat.yaml "$CONTAINER_DIR/api/"
+#Assets
+aws s3 sync s3://"$S3_BUCKET"/"$PREFIX"/version/"$VERSION"/assets "$CONTAINER_DIR/client/dist/assets"
 
 # Run container command
 echo "Running container command: $CONTAINER_RUN_COMMAND"
