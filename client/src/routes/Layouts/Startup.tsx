a1a64478c23898c6d1cc0c5a376bb8c96c9e1bfd
Michelle Ward <--global>
2025-03-28 12:26:19 -0400

    branded welcome message

--- client/src/routes/Layouts/Startup.tsx
+++ client/src/routes/Layouts/Startup.tsx
@@ -5,14 +5,6 @@ import { useGetStartupConfig } from '~/data-provider';
 import AuthLayout from '~/components/Auth/AuthLayout';
 import { TranslationKeys, useLocalize } from '~/hooks';
 
-const headerMap: Record<string, TranslationKeys> = {
-  '/login': 'com_auth_welcome_back',
-  '/register': 'com_auth_create_account',
-  '/forgot-password': 'com_auth_reset_password',
-  '/reset-password': 'com_auth_reset_password',
-  '/login/2fa': 'com_auth_verify_your_identity',
-};
-
 export default function StartupLayout({ isAuthenticated }: { isAuthenticated?: boolean }) {
   const [error, setError] = useState<TranslationKeys | null>(null);
   const [headerText, setHeaderText] = useState<TranslationKeys | null>(null);
@@ -27,13 +19,24 @@ export default function StartupLayout({ isAuthenticated }: { isAuthenticated?: b
   const localize = useLocalize();
   const navigate = useNavigate();
   const location = useLocation();
+  const welcomeBackKey = startupConfig?.customWelcomeMessage || 'com_auth_welcome_back';
+
+  const headerMap: Record<string, TranslationKeys> = {
+    '/login': welcomeBackKey as TranslationKeys,
+    '/register': 'com_auth_create_account',
+    '/forgot-password': 'com_auth_reset_password',
+    '/reset-password': 'com_auth_reset_password',
+    '/login/2fa': 'com_auth_verify_your_identity',
+  };
 
   useEffect(() => {
     if (isAuthenticated) {
       navigate('/c/new', { replace: true });
     }
     if (data) {
+      console.log('data', data);
       setStartupConfig(data);
+      console.log('startupConfig', data.appTitle);
     }
   }, [isAuthenticated, navigate, data]);
 
@@ -58,11 +61,7 @@ export default function StartupLayout({ isAuthenticated }: { isAuthenticated?: b
 
   return (
     <AuthLayout
-      header={
-        headerText
-          ? `${localize(headerText)}${startupConfig?.appTitle ? ` ${startupConfig.appTitle}` : ''}`
-          : localize(headerMap[location.pathname])
-      }
+      header={headerText ? localize(headerText) : localize(headerMap[location.pathname])}
       isFetching={isFetching}
       startupConfig={startupConfig}
       startupConfigError={startupConfigError}
