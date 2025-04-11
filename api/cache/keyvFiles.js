2a4e112de8bd7f6f59c66d4af92c239432015db2
Danny Avila <danny@librechat.ai>
2025-04-11 01:48:44 -0400

    fix: Set max listeners to expected amount

--- api/cache/keyvFiles.js
+++ api/cache/keyvFiles.js
@@ -1,7 +1,7 @@
 const { KeyvFile } = require('keyv-file');
 
-const logFile = new KeyvFile({ filename: './data/logs.json' });
-const violationFile = new KeyvFile({ filename: './data/violations.json' });
+const logFile = new KeyvFile({ filename: './data/logs.json' }).setMaxListeners(20);
+const violationFile = new KeyvFile({ filename: './data/violations.json' }).setMaxListeners(20);
 
 module.exports = {
   logFile,
