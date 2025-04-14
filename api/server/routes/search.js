2a25c2801d15ec19d13ae7d0bbe5eea25618aef3
Denis Palnitsky <denis.palnitsky@zendesk.com>
2025-04-14 09:42:36 +0200

    simplify logic in search/enable endpoint

--- api/server/routes/search.js
+++ api/server/routes/search.js
@@ -46,8 +46,7 @@ router.get('/enable', async function (req, res) {
     });
 
     const { status } = await client.health();
-    result = status === 'available' && !!process.env.SEARCH;
-    return res.send(result);
+    return res.send(status === 'available');
   } catch (error) {
     return res.send(false);
   }
