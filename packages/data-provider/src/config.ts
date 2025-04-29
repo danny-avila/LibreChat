b99c2b5692331d1c630a8db42b6df80e7984fc74
Danny Avila <danny@librechat.ai>
2025-04-29 17:38:17 -0400

    ✨ v0.7.8-rc1

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -1227,7 +1227,7 @@ export enum TTSProviders {
 /** Enum for app-wide constants */
 export enum Constants {
   /** Key for the app's version. */
-  VERSION = 'v0.7.7',
+  VERSION = 'v0.7.8-rc1',
   /** Key for the Custom Config's version (librechat.yaml). */
   CONFIG_VERSION = '1.2.4',
   /** Standard value for the first message's `parentMessageId` value, to indicate no parent exists. */
