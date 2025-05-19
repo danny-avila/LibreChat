e25010706b7c025325c99d07136e5ab5d7353d16
Theo N. Truong <644650+nhtruong@users.noreply.github.com>
2025-05-19 17:33:25 -0600

    🎚️ feat: Custom Parameters (#7342)
    * #
    
    * - refactor: simplified getCustomConfig func
    
    * #
    
    * - feature: persist values for parameters with optionType of custom
    
    * #
    
    * - refactor: moved `Parameters/settings.ts` into `data-provider` so that both frontend and backend code can use it.
    
    * - feature: loadCustomConfig can now parse and validate customParams property for `endpoints.custom` in `librechat.yaml`
    
    * # fixed linter
    
    * # removed .strict() in config.ts
    
    * change: added packages/data-provider/src to SOURCE_DIRS for i18n check
    
    * # removed unnecessary lodash imports
    
    * # addressed PR comments
    # fixed lint for updated files
    
    * # better import for lodash (w/o relying on tree-shaking)

--- api/server/services/Config/loadConfigEndpoints.js
+++ api/server/services/Config/loadConfigEndpoints.js
@@ -29,7 +29,14 @@ async function loadConfigEndpoints(req) {
 
     for (let i = 0; i < customEndpoints.length; i++) {
       const endpoint = customEndpoints[i];
-      const { baseURL, apiKey, name: configName, iconURL, modelDisplayLabel } = endpoint;
+      const {
+        baseURL,
+        apiKey,
+        name: configName,
+        iconURL,
+        modelDisplayLabel,
+        customParams,
+      } = endpoint;
       const name = normalizeEndpointName(configName);
 
       const resolvedApiKey = extractEnvVariable(apiKey);
@@ -41,6 +48,7 @@ async function loadConfigEndpoints(req) {
         userProvideURL: isUserProvided(resolvedBaseURL),
         modelDisplayLabel,
         iconURL,
+        customParams,
       };
     }
   }
