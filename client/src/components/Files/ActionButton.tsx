10ea449f6a8f09b47d58f2cab7f2e2c070328cd1
Danny Avila <danny@librechat.ai>
2025-05-20 12:25:24 -0400

    refactor: update ActionButton to use localized text

--- client/src/components/Files/ActionButton.tsx
+++ client/src/components/Files/ActionButton.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { CrossIcon } from '~/components/svg';
 import { Button } from '~/components/ui';
+import { useLocalize } from '~/hooks';
 
 type ActionButtonProps = {
   onClick: () => void;
 };
 
 export default function ActionButton({ onClick }: ActionButtonProps) {
+  const localize = useLocalize();
   return (
     <div className="w-32">
       <Button
         className="w-full rounded-md border border-black bg-white p-0 text-black hover:bg-black hover:text-white"
         onClick={onClick}
       >
-        Action Button
+        {/* Action Button */}
+        {localize('com_ui_action_button')}
       </Button>
     </div>
   );
