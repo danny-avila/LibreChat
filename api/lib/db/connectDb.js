50e36e67f02de513b51efc233db8237a6e218569
Jason Koh <bk7749@gmail.com>
2025-05-27 14:51:31 -0700

    clean up

--- api/lib/db/connectDb.js
+++ api/lib/db/connectDb.js
@@ -6,9 +6,6 @@ const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
 if (!MONGO_URI) {
   throw new Error('Please define the MONGO_URI environment variable');
 }
-if (!MONGO_DB_NAME) {
-    MONGO_DB_NAME = null
-}
 
 /**
  * Global is used here to maintain a cached connection across hot reloads
