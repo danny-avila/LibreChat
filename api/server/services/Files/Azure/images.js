f2bad14f53d38fbcee17b602e84ce0bd40c03da4
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2025-05-31 21:27:21 +0200

    🔧 feat: Enhance avatar processing by dynamically setting file extension based on image format. GIF upload is now supported

--- api/server/services/Files/Azure/images.js
+++ api/server/services/Files/Azure/images.js
@@ -98,10 +98,14 @@ async function prepareAzureImageURL(req, file) {
  */
 async function processAzureAvatar({ buffer, userId, manual, basePath = 'images', containerName }) {
   try {
+    const metadata = await sharp(buffer).metadata();
+    const extension = metadata.format === 'gif' ? 'gif' : 'png';
+    const fileName = `avatar.${extension}`;
+
     const downloadURL = await saveBufferToAzure({
       userId,
       buffer,
-      fileName: 'avatar.png',
+      fileName,
       basePath,
       containerName,
     });
