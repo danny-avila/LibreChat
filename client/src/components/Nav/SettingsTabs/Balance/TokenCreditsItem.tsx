5ccdb83e5ed321d0b6c0c88c42061942a699b7fb
Märt <martvaha@users.noreply.github.com>
2025-06-03 21:05:41 +0300

    🔧 fix: Use Correct Description for Balance Info (#7712)

--- client/src/components/Nav/SettingsTabs/Balance/TokenCreditsItem.tsx
+++ client/src/components/Nav/SettingsTabs/Balance/TokenCreditsItem.tsx
@@ -15,7 +15,7 @@ const TokenCreditsItem: React.FC<TokenCreditsItemProps> = ({ tokenCredits }) => 
       {/* Left Section: Label */}
       <div className="flex items-center space-x-2">
         <Label className="font-light">{localize('com_nav_balance')}</Label>
-        <HoverCardSettings side="bottom" text="com_nav_info_user_name_display" />
+        <HoverCardSettings side="bottom" text="com_nav_info_balance" />
       </div>
 
       {/* Right Section: tokenCredits Value */}
