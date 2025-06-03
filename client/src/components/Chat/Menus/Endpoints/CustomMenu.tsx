7f11f4c11e40822855decec93e55bfa53c1466fb
Danny Avila <danny@librechat.ai>
2025-06-03 14:42:59 -0400

    👓 a11y: Add Solid Marker to Improve Visibility in LLM Menu (#7714)
    * feat[a11y]: add solid left border to improve visibility in LLM's submenu items.
    
    * 🎨 style: Update CustomMenuItem class for improved border visibility
    
    ---------
    
    Co-authored-by: Derek Jackson <derek_jackson@harvard.edu>

--- client/src/components/Chat/Menus/Endpoints/CustomMenu.tsx
+++ client/src/components/Chat/Menus/Endpoints/CustomMenu.tsx
@@ -159,7 +159,7 @@ export const CustomMenuItem = React.forwardRef<HTMLDivElement, CustomMenuItemPro
       blurOnHoverEnd: false,
       ...props,
       className: cn(
-        'flex cursor-default items-center gap-2 rounded-lg p-2 outline-none! scroll-m-1 scroll-mt-[calc(var(--combobox-height,0px)+var(--label-height,4px))] aria-disabled:opacity-25 data-[active-item]:bg-black/[0.075] data-[active-item]:text-black dark:data-[active-item]:bg-white/10 dark:data-[active-item]:text-white sm:py-1 sm:text-sm min-w-0 w-full',
+        'flex cursor-default items-center gap-2 rounded-lg p-2 outline-none! scroll-m-1 scroll-mt-[calc(var(--combobox-height,0px)+var(--label-height,4px))] aria-disabled:opacity-25 border-l-2 border-transparent data-[active-item]:border-black data-[active-item]:rounded-l-none data-[active-item]:bg-black/[0.075] data-[active-item]:text-black dark:data-[active-item]:bg-white/10 dark:data-[active-item]:text-white dark:data-[active-item]:border-white sm:py-1 sm:text-sm min-w-0 w-full',
         props.className,
       ),
     };
