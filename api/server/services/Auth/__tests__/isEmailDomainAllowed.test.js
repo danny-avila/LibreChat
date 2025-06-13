a35282a8efcd9fc9a19933d134955c64334b4a01
Sakhil Chawla <your.email@example.com>
2025-06-13 01:33:51 -0400

    changes

--- api/server/services/Auth/__tests__/isEmailDomainAllowed.test.js
+++ api/server/services/Auth/__tests__/isEmailDomainAllowed.test.js
@@ -1,3 +1,5 @@
+const { isEmailDomainAllowed } = require('../isEmailDomainAllowed');
+
 describe('isEmailDomainAllowed', () => {
   it('should return false if email is falsy', async () => {
     expect(await isEmailDomainAllowed(null)).toBe(false);
