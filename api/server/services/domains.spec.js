1581a2f7769f2e5f89f16e3ecf56eef0a1eb6ed4
Sakhil Chawla <your.email@example.com>
2025-06-13 00:38:10 -0400

    lint

--- api/server/services/domains.spec.js
+++ api/server/services/domains.spec.js
@@ -6,8 +6,6 @@ jest.mock('~/server/services/Config', () => ({
 }));
 
 describe('isEmailDomainAllowed', () => {
-  
-
   it('should return false if email is falsy', async () => {
     expect(await isEmailDomainAllowed(null)).toBe(false);
     expect(await isEmailDomainAllowed(undefined)).toBe(false);
