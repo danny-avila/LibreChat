b34bf7aac9b6a36e195c90f49f74e1a572274a48
Tiago Aleff <tiago.aleff@hoamtechnology.com>
2025-06-13 16:44:21 -0300

    feat: add addUserToken property in config.ts

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -285,6 +285,7 @@ export const endpointSchema = baseEndpointSchema.merge(
     summaryModel: z.string().optional(),
     forcePrompt: z.boolean().optional(),
     modelDisplayLabel: z.string().optional(),
+    addUserToken: z.string().optional(),
     headers: z.record(z.any()).optional(),
     addParams: z.record(z.any()).optional(),
     dropParams: z.array(z.string()).optional(),
