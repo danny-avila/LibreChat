691f8852c4890fb16c7d8a89370fb124f6a01105
Ruben Talstra <RubenTalstra1211@outlook.com>
2025-06-14 14:20:01 +0200

    🔧 fix: Enhance user deletion script to allow deep deletion of related data

--- config/delete-user.js
+++ config/delete-user.js
@@ -1,6 +1,26 @@
 const path = require('path');
 const mongoose = require(path.resolve(__dirname, '..', 'api', 'node_modules', 'mongoose'));
-const { User } = require('@librechat/data-schemas').createModels(mongoose);
+const {
+  User,
+  Agent,
+  Assistant,
+  Balance,
+  Transaction,
+  ConversationTag,
+  Conversation,
+  Message,
+  File,
+  Key,
+  MemoryEntry,
+  PluginAuth,
+  Prompt,
+  PromptGroup,
+  Preset,
+  Session,
+  SharedLink,
+  ToolCall,
+  Token,
+} = require('@librechat/data-schemas').createModels(mongoose);
 require('module-alias')({ base: path.resolve(__dirname, '..', 'api') });
 const { askQuestion, silentExit } = require('./helpers');
 const connect = require('./connect');
@@ -12,29 +32,60 @@ const connect = require('./connect');
    * Show the welcome / help menu
    */
   console.purple('---------------');
-  console.purple('Deleting a user');
+  console.purple('Deleting a user and all related data');
   console.purple('---------------');
 
-  let email = '';
-  if (process.argv.length >= 3) {
-    email = process.argv[2];
-  } else {
+  // 1) Get email
+  let email = process.argv[2];
+  if (!email) {
     email = await askQuestion('Email:');
   }
-  let user = await User.findOne({ email: email });
-  if (user !== null) {
-    if ((await askQuestion(`Delete user ${user}?`)) === 'y') {
-      user = await User.findOneAndDelete({ _id: user._id });
-      if (user !== null) {
-        console.yellow(`Deleted user ${user}`);
-      } else {
-        console.yellow(`Couldn't delete user with email ${email}`);
-      }
-    }
-  } else {
-    console.yellow(`Didn't find user with email ${email}`);
+
+  // 2) Find user
+  const user = await User.findOne({ email: email.toLowerCase() });
+  if (!user) {
+    console.yellow(`No user found with email "${email}"`);
+    return silentExit(0);
   }
 
+  // 3) Confirm
+  const answer = await askQuestion(
+    `Really delete user ${user.email} (${user._id}) and ALL their data? (y/N)`,
+  );
+  if (answer.toLowerCase() !== 'y') {
+    console.yellow('Aborted.');
+    return silentExit(0);
+  }
+
+  // 4) Delete related documents
+  const uid = user._id;
+  const uidStr = uid.toString();
+
+  await Promise.all([
+    Agent.deleteMany({ author: uid }),
+    Assistant.deleteMany({ user: uid }),
+    Balance.deleteMany({ user: uid }),
+    Transaction.deleteMany({ user: uid }),
+    ConversationTag.deleteMany({ user: uidStr }),
+    Conversation.deleteMany({ user: uidStr }),
+    Message.deleteMany({ user: uidStr }),
+    File.deleteMany({ user: uid }),
+    Key.deleteMany({ userId: uid }),
+    MemoryEntry.deleteMany({ userId: uid }),
+    PluginAuth.deleteMany({ userId: uidStr }),
+    Prompt.deleteMany({ author: uid }),
+    PromptGroup.deleteMany({ author: uid }),
+    Preset.deleteMany({ user: uidStr }),
+    Session.deleteMany({ user: uid }),
+    SharedLink.deleteMany({ user: uidStr }),
+    ToolCall.deleteMany({ user: uid }),
+    Token.deleteMany({ userId: uid }),
+  ]);
+
+  // 5) Finally delete the user document itself
+  await User.deleteOne({ _id: uid });
+
+  console.green(`✔ Successfully deleted user ${email} and all associated data.`);
   silentExit(0);
 })();
 
@@ -42,9 +93,6 @@ process.on('uncaughtException', (err) => {
   if (!err.message.includes('fetch failed')) {
     console.error('There was an uncaught error:');
     console.error(err);
-  }
-
-  if (!err.message.includes('fetch failed')) {
     process.exit(1);
   }
 });
