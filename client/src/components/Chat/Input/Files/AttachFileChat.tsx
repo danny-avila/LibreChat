529dff44960599b2c4d385e04778472c55c7e753
Danny Avila <danny@librechat.ai>
2025-06-23 08:35:37 -0400

    refactor: reinstate agent check in AttachFileChat component, as individual providers will ahve different file configurations

--- client/src/components/Chat/Input/Files/AttachFileChat.tsx
+++ client/src/components/Chat/Input/Files/AttachFileChat.tsx
@@ -1,19 +1,21 @@
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 import {
   Constants,
   supportsFiles,
   mergeFileConfig,
+  isAgentsEndpoint,
   EndpointFileConfig,
   fileConfig as defaultFileConfig,
 } from 'librechat-data-provider';
-import { useChatContext } from '~/Providers';
 import { useGetFileConfig } from '~/data-provider';
 import AttachFileMenu from './AttachFileMenu';
+import { useChatContext } from '~/Providers';
 
 function AttachFileChat({ disableInputs }: { disableInputs: boolean }) {
   const { conversation } = useChatContext();
   const conversationId = conversation?.conversationId ?? Constants.NEW_CONVO;
   const { endpoint: _endpoint, endpointType } = conversation ?? { endpoint: null };
+  const isAgents = useMemo(() => isAgentsEndpoint(_endpoint), [_endpoint]);
 
   const { data: fileConfig = defaultFileConfig } = useGetFileConfig({
     select: (data) => mergeFileConfig(data),
@@ -26,7 +28,7 @@ function AttachFileChat({ disableInputs }: { disableInputs: boolean }) {
   const endpointSupportsFiles: boolean = supportsFiles[endpointType ?? _endpoint ?? ''] ?? false;
   const isUploadDisabled = (disableInputs || endpointFileConfig?.disabled) ?? false;
 
-  if (endpointSupportsFiles && !isUploadDisabled) {
+  if (isAgents || (endpointSupportsFiles && !isUploadDisabled)) {
     return <AttachFileMenu disabled={disableInputs} conversationId={conversationId} />;
   }
 
