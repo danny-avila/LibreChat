da609dab0633fd50dcac8c81c2eafcd14e96594d
chiru <chiranthan1205@gmail.com>
2025-06-27 19:26:06 +0530

    cbi/feat/updated-brand-to-cbi

--- client/src/components/Chat/Footer.tsx
+++ client/src/components/Chat/Footer.tsx
@@ -37,9 +37,7 @@ export default function Footer({ className }: { className?: string }) {
   const mainContentParts = (
     typeof config?.customFooter === 'string'
       ? config.customFooter
-      : '[LibreChat ' +
-        Constants.VERSION +
-        '](https://librechat.ai) - ' +
+      : 'Constellation Brands - ' +
         localize('com_ui_latest_footer')
   ).split('|');
 
