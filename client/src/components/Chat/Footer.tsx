47725e40475bad852de7b08ea11045a3a931cc5a
nick-fullpath <nick.kaplan@fullpath.com>
2025-06-29 15:35:03 +0300

    wip

--- client/src/components/Chat/Footer.tsx
+++ client/src/components/Chat/Footer.tsx
@@ -37,10 +37,7 @@ export default function Footer({ className }: { className?: string }) {
   const mainContentParts = (
     typeof config?.customFooter === 'string'
       ? config.customFooter
-      : '[LibreChat ' +
-        Constants.VERSION +
-        '](https://librechat.ai) - ' +
-        localize('com_ui_latest_footer')
+      : 'Powered By Fullpath'
   ).split('|');
 
   useEffect(() => {
