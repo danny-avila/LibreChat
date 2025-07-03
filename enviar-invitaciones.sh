98dcf4be9ed2f9e561abce6d84f1f600c1c7a902
Edo_Andres <edo.andres.ec@gmail.com>
2025-07-03 04:07:19 -0400

    upgrade para enviar-invitaciones

--- enviar-invitaciones.sh
+++ enviar-invitaciones.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 for email in $(cat emails.txt); do
     echo "Enviando invitación a: $email"
-    docker exec -it LibreChat-API /bin/sh -c "cd .. && npm run invite-user $email"    
+    npm run invite-user "$email"
 done
-
 echo "¡Listo! Todas las invitaciones enviadas."
