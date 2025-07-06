ce8e40444ad0bc245057b19d4694e81ba4903cfa
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2025-07-06 12:26:03 +0200

    feat: Add common types and interfaces for accessibility, agents, artifacts, assistants, and tools

---
+++ client/src/common/assistants-types.ts
@@ -0,0 +1,31 @@
+import { Capabilities, EModelEndpoint } from 'librechat-data-provider';
+import type { Assistant, AssistantsEndpoint } from 'librechat-data-provider';
+import type { Option, ExtendedFile } from './types';
+
+export type ActionsEndpoint = AssistantsEndpoint | EModelEndpoint.agents;
+
+export type TAssistantOption =
+  | string
+  | (Option &
+      Assistant & {
+        files?: Array<[string, ExtendedFile]>;
+        code_files?: Array<[string, ExtendedFile]>;
+      });
+
+export type Actions = {
+  [Capabilities.code_interpreter]: boolean;
+  [Capabilities.image_vision]: boolean;
+  [Capabilities.retrieval]: boolean;
+};
+
+export type AssistantForm = {
+  assistant: TAssistantOption;
+  id: string;
+  name: string | null;
+  description: string | null;
+  instructions: string | null;
+  conversation_starters: string[];
+  model: string;
+  functions: string[];
+  append_current_datetime: boolean;
+} & Actions;
