396d96332e53eebb99a3329f93b068577769cd40
Sebastien Bruel <sebastien.bruel@shopify.com>
2025-07-08 16:44:16 +0900

    Fix scanning of the uploaded images folder on startup

--- api/server/routes/static.js
+++ api/server/routes/static.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const staticCache = require('../utils/staticCache');
 const paths = require('~/config/paths');
+const { isEnabled } = require('~/server/utils');
+
+const skipGzipScan = isEnabled(process.env.DISABLE_IMAGES_OUTPUT_STATIC_CACHE);
 
 const router = express.Router();
-router.use(staticCache(paths.imageOutput));
+router.use(staticCache(paths.imageOutput, { skipGzipScan }));
 
 module.exports = router;
