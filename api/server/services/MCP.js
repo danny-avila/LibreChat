74c393900ebca162594dc2f0db6eefe0649464cb
Olivier Schiavo <olivier.schiavo@wengo.com>
2025-07-24 19:06:36 +0200

    feat: eliciting user input

--- api/server/services/MCP.js
+++ api/server/services/MCP.js
@@ -288,11 +288,12 @@ async function createMCPTool({
     });
     toolDefinition = result?.availableTools?.[toolKey]?.function;
   }
-
   if (!toolDefinition) {
     logger.warn(`[MCP][${serverName}][${toolName}] Tool definition not found, cannot create tool.`);
     return;
   }
+
+  logger.debug(`createMCPTool(${toolKey})`);
 
   return createToolInstance({
     res,
@@ -366,6 +367,7 @@ function createToolInstance({ res, toolName, serverName, toolDefinition, provide
         toolArguments,
         options: {
           signal: derivedSignal,
+          tool_call_id: toolCall?.id,
         },
         user: config?.configurable?.user,
         requestBody: config?.configurable?.requestBody,
