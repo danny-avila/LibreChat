3c6198723b04ef63a34feae5177d08d25b1415a8
Segye Park <iworldpark@gmail.com>
2025-07-27 21:19:53 +0900

    Hide ModelSelect

--- client/src/components/Chat/Header.tsx
+++ client/src/components/Chat/Header.tsx
@@ -55,7 +55,7 @@ export default function Header() {
               !isSmallScreen ? 'transition-all duration-200 ease-in-out' : ''
             } ${!navVisible ? 'translate-x-0' : 'translate-x-[-100px]'}`}
           >
-            <ModelSelector startupConfig={startupConfig} />
+            {interfaceConfig.modelSelect && <ModelSelector startupConfig={startupConfig} />}
             {interfaceConfig.presets === true && interfaceConfig.modelSelect && <PresetsMenu />}
             {hasAccessToBookmarks === true && <BookmarkMenu />}
             {hasAccessToMultiConvo === true && <AddMultiConvo />}
