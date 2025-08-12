db0235289a2b46048ee73fa3e92b8caa074460c5
Matthew Mueller <mattmueller@stripe.com>
2025-08-12 14:48:17 -0500

    pass srcs from request to fetch calls

---
+++ api/server/stripe/secure-request-context.js
@@ -0,0 +1,60 @@
+const { logger } = require('@librechat/data-schemas');
+const { AsyncLocalStorage } = require('async_hooks');
+const asyncLocalStorage = new AsyncLocalStorage();
+
+const SECURE_REQUEST_CONTEXT_HEADER = 'X-Secure-Request-Context-bin';
+
+/**
+ * Middleware to attach a secure request context to the request.
+ * @param {import('express').Request} req - The request object.
+ * @param {import('express').Response} _ - The response object (not used).
+ * @param {function} next - The next middleware function.
+ * @returns {void}
+ */
+function middleware(req, _, next) {
+  // Get the header by name
+  const srcHeader = req.header(SECURE_REQUEST_CONTEXT_HEADER);
+  if (srcHeader) {
+    logger.info(
+      `[Stripe] Added 'X-Secure-Request-Context-bin: ${maskHeader(srcHeader)}' to the request context`,
+    );
+  } else {
+    logger.warn(`[Stripe] No '${SECURE_REQUEST_CONTEXT_HEADER}' found in the request headers`);
+  }
+  asyncLocalStorage.run(srcHeader, () => {
+    next();
+  });
+}
+
+/**
+ * Attaches the secure request context to the request options.
+ * @param {import("node-fetch").RequestInit} [options]
+ */
+function attach(options = {}) {
+  // Retrieve the secure request context from request context
+  const src = asyncLocalStorage.getStore();
+  if (!src) {
+    logger.warn(`[Stripe] No '${SECURE_REQUEST_CONTEXT_HEADER}' found in the request context`);
+    return options;
+  }
+
+  // Set the header in the options
+  const headers = options.headers || new Headers();
+  headers.set(SECURE_REQUEST_CONTEXT_HEADER, src);
+  logger.info(`[Stripe] Attached '${SECURE_REQUEST_CONTEXT_HEADER}' to fetch headers`);
+
+  // Return a modified options object with the src header
+  return {
+    ...options,
+    headers,
+  };
+}
+
+function maskHeader(header) {
+  return header.replace(/./g, '*');
+}
+
+module.exports = {
+  middleware,
+  attach,
+};
