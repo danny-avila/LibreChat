62f24719f10d159912c589f494731a9dfb7610f3
Colin Lin <colinlin@stripe.com>
2025-08-12 18:40:47 -0400

    feat: use proxy agent when HTTP PROXY is set

--- api/server/utils/patchFetch.js
+++ api/server/utils/patchFetch.js
@@ -1,4 +1,7 @@
 const { logger } = require('@librechat/data-schemas');
+const nodeFetch = require('node-fetch');
+const  {ProxyAgent} = require('proxy-agent');
+
 
 /**
  * Patches the global fetch to use node-fetch instead of undici
@@ -9,13 +12,27 @@ function patchFetchPorts() {
     const enableNodeFetch = process.env.ENABLE_NODE_FETCH === 'true';
 
     if (enableNodeFetch) {
-      global.fetch = require('node-fetch');
-      logger.info('[patchFetch] Successfully set global.fetch to node-fetch');
+      global.fetch = fetchLike;
+      logger.info(
+        `[patchFetch] Successfully set global.fetch to fetchLike`,
+      );
+    }
+    else {
+      logger.info('[patchFetch] Not patching fetch');
     }
   } catch (error) {
     logger.error(`[patchFetch] Failed to patch fetch: ${error.message}`);
     return;
   }
+}
+
+function fetchLike(url, options = {}) {
+  if (process.env.HTTP_PROXY) {
+    logger.info(`[patchFetch] Using HTTP_PROXY: ${process.env.HTTP_PROXY}`);
+    return nodeFetch(url, {...options, agent: new ProxyAgent() });
+  }
+  return nodeFetch(url, options);
+  
 }
 
 module.exports = {
