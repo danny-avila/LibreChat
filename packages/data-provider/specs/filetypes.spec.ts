18866e3430d9cda118dd54a80c86434b48853cd1
Dustin Healy <dustinhealy1@gmail.com>
2025-08-18 06:06:52 -0700

    chore: update unittest since mp3 filetypes are now partially supported

--- packages/data-provider/specs/filetypes.spec.ts
+++ packages/data-provider/specs/filetypes.spec.ts
@@ -14,7 +14,7 @@ import {
 } from '../src/file-config';
 
 describe('MIME Type Regex Patterns', () => {
-  const unsupportedMimeTypes = ['text/x-unknown', 'application/unknown', 'image/bmp', 'audio/mp3'];
+  const unsupportedMimeTypes = ['text/x-unknown', 'application/unknown', 'image/bmp'];
 
   // Testing general supported MIME types
   fullMimeTypesList.forEach((mimeType) => {
