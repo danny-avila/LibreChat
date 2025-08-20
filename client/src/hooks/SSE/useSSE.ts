a6354d04b9a0dee1fa1dd6310601ae6dba85873d
Colin Lin <colinlin@stripe.com>
2025-08-20 11:24:38 -0400

    fix:[stripe] delete instead of comment out

--- client/src/hooks/SSE/useSSE.ts
+++ client/src/hooks/SSE/useSSE.ts
@@ -199,30 +199,8 @@ export default function useSSE(
 
     sse.addEventListener('error', async (e: MessageEvent) => {
 
-      // <stripe
-      // Removing refresh token logic
-      // /* @ts-ignore */
-      // if (e.responseCode === 401) {
-      //   /* token expired, refresh and retry */
-      //   try {
-      //     const refreshResponse = await request.refreshToken();
-      //     const token = refreshResponse?.token ?? '';
-      //     if (!token) {
-      //       throw new Error('Token refresh failed.');
-      //     }
-      //     sse.headers = {
-      //       'Content-Type': 'application/json',
-      //       Authorization: `Bearer ${token}`,
-      //     };
-
-      //     request.dispatchTokenUpdatedEvent(token);
-      //     sse.stream();
-      //     return;
-      //   } catch (error) {
-      //     /* token refresh failed, continue handling the original 401 */
-      //     console.log(error);
-      //   }
-      // }
+      // <stripe>
+      // Removing refresh token logic for 401 error
       // </stripe>
 
       console.log('error in server stream.');
