88070c48883563dc20acc77c12e17d03b4f57e97
Colin Lin <colinlin@stripe.com>
2025-08-22 13:19:47 -0400

    fix: [stripe] disable image upload in UI

--- client/src/components/SidePanel/Agents/AgentAvatar.tsx
+++ client/src/components/SidePanel/Agents/AgentAvatar.tsx
@@ -191,7 +191,7 @@ function Avatar({
           </button>
         </Popover.Trigger>
       </div>
-      {<AvatarMenu handleFileChange={handleFileChange} />}
+      {/* <stripe> {<AvatarMenu handleFileChange={handleFileChange} />} </stripe> */}
     </Popover.Root>
   );
 }
--- client/src/components/SidePanel/Builder/AssistantAvatar.tsx
+++ client/src/components/SidePanel/Builder/AssistantAvatar.tsx
@@ -218,7 +218,7 @@ function Avatar({
           </button>
         </Popover.Trigger>
       </div>
-      {<AvatarMenu handleFileChange={handleFileChange} />}
+      {/* <stripe> {<AvatarMenu handleFileChange={handleFileChange} />} </stripe> */}
     </Popover.Root>
   );
 }
