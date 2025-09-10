c0f95f971a9f4af5312518dbfe6d976f775f2e2b
Theo N. Truong <644650+nhtruong@users.noreply.github.com>
2025-09-10 16:46:54 -0600

    🗄️ refactor: Make `APP_CONFIG` a Dedicated Cache Store (#9558)
    - This allows use APP_CONFIG in FORCED_IN_MEMORY_CACHE_NAMESPACES
    - Remove the complexity of nested namespace (e.g. we no longer have to worry about the prefix of every role key)

--- api/cache/getLogStores.js
+++ api/cache/getLogStores.js
@@ -31,6 +31,7 @@ const namespaces = {
   [CacheKeys.SAML_SESSION]: sessionCache(CacheKeys.SAML_SESSION),
 
   [CacheKeys.ROLES]: standardCache(CacheKeys.ROLES),
+  [CacheKeys.APP_CONFIG]: standardCache(CacheKeys.APP_CONFIG),
   [CacheKeys.CONFIG_STORE]: standardCache(CacheKeys.CONFIG_STORE),
   [CacheKeys.STATIC_CONFIG]: standardCache(CacheKeys.STATIC_CONFIG),
   [CacheKeys.PENDING_REQ]: standardCache(CacheKeys.PENDING_REQ),
