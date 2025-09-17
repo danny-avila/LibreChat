3ecf9011812602005e4c13715b490318d0252318
Danny Avila <danny@librechat.ai>
2025-09-17 16:37:15 -0400

    chore: reorder import statements in index.js

--- api/config/index.js
+++ api/config/index.js
@@ -1,6 +1,6 @@
-const { MCPManager, FlowStateManager, OAuthReconnectionManager } = require('@librechat/api');
 const { EventSource } = require('eventsource');
 const { Time } = require('librechat-data-provider');
+const { MCPManager, FlowStateManager, OAuthReconnectionManager } = require('@librechat/api');
 const logger = require('./winston');
 
 global.EventSource = EventSource;
