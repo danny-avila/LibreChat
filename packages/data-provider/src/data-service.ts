95c8b1cc7c018196a2582914084b13d46cff63a2
Megan Greenberg <mgreenberg@networkninja.com>
2025-10-01 09:12:47 -0400

    feat: fixing bug with data-provider found

--- packages/data-provider/src/data-service.ts
+++ packages/data-provider/src/data-service.ts
@@ -160,15 +160,15 @@ export const getMCPAuthValues = (serverName: string): Promise<q.MCPAuthValuesRes
   return request.get(endpoints.mcpAuthValues(serverName));
 };
 
-export function getMCPPromptGroup(serverName: string): Promise<q.TMCPPromptArgument> {
+export function getMCPPromptGroup(serverName: string): Promise<t.TMCPPromptArgument> {
   return request.get(endpoints.mcpPromptGroup(serverName));
 }
 
-export function getMCPPrompt(serverName: string): Promise<q.MCPPromptResponse> {
+export function getMCPPrompt(serverName: string): Promise<t.MCPPromptResponse> {
   return request.get(endpoints.mcpPrompt(serverName));
 }
 
-export function allMCPPrompts(): Promise<q.MCPPromptResponseArray> {
+export function allMCPPrompts(): Promise<t.MCPPromptResponseArray> {
   return request.get(endpoints.getAllMCPPrompts());
 }
 
