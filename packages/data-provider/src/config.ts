33bc532e0c144e18ff22ca33fae8ee56f0d69a3c
Danny Avila <danny@librechat.ai>
2025-10-01 17:32:13 -0400

    🔧 chore: Update config version to 1.3.0

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -1539,7 +1539,7 @@ export enum Constants {
   /** Key for the app's version. */
   VERSION = 'v0.8.0',
   /** Key for the Custom Config's version (librechat.yaml). */
-  CONFIG_VERSION = '1.2.9',
+  CONFIG_VERSION = '1.3.0',
   /** Standard value for the first message's `parentMessageId` value, to indicate no parent exists. */
   NO_PARENT = '00000000-0000-0000-0000-000000000000',
   /** Standard value to use whatever the submission prelim. `responseMessageId` is */
