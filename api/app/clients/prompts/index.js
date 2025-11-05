df8ce1b4898bb7bbbe7af60c8bb2346dec41f78f
Danny Avila <danny@librechat.ai>
2025-11-05 17:26:09 -0500

    chore: remove legacy code with unnecessary dependencies

--- api/app/clients/prompts/index.js
+++ api/app/clients/prompts/index.js
@@ -2,7 +2,6 @@ const formatMessages = require('./formatMessages');
 const summaryPrompts = require('./summaryPrompts');
 const handleInputs = require('./handleInputs');
 const instructions = require('./instructions');
-const titlePrompts = require('./titlePrompts');
 const truncate = require('./truncate');
 const createVisionPrompt = require('./createVisionPrompt');
 const createContextHandlers = require('./createContextHandlers');
@@ -12,7 +11,6 @@ module.exports = {
   ...summaryPrompts,
   ...handleInputs,
   ...instructions,
-  ...titlePrompts,
   ...truncate,
   createVisionPrompt,
   createContextHandlers,
