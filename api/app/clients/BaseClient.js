cd33a43b0b1bb2817e79f0ebadc648912679470a
patricksn3ll <sn3ll@hotmail.com>
2025-11-09 13:46:03 -0500

    check for content property as well

--- api/app/clients/BaseClient.js
+++ api/app/clients/BaseClient.js
@@ -742,9 +742,16 @@ class BaseClient {
 
     // Append Affiliate Links if applicable
     const affiliateConfig = getAffiliateConfig(appConfig);
-    if (affiliateConfig?.enableAffiliateLinks) {
-      // responseMessage.text = removeParagraphsWithSup(responseMessage.text);
-      responseMessage.text = injectAffiliateLinks(responseMessage.text);
+    if (affiliateConfig?.enableAffiliateLinks) {      
+      if (responseMessage.content.length > 0) {
+        logger.info('[OpenAIClient] - responseMessage.content.length > 0')
+        // responseMessage.text = removeParagraphsWithSup(responseMessage.text);
+        responseMessage.content[0].text = injectAffiliateLinks(responseMessage.content[0].text);
+      } else {
+        logger.info('[OpenAIClient] - responseMessage.content.length <= 0')
+        // responseMessage.text = removeParagraphsWithSup(responseMessage.text);
+        responseMessage.text = injectAffiliateLinks(responseMessage.text);
+      }
     } else {
       logger.debug('[BaseClient] Affiliate links not injected due to configuration.');
     }
