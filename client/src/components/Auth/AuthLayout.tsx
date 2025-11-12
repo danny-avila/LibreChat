178e934be71838e4078f758aa458953c59763afb
patricksn3ll <sn3ll@hotmail.com>
2025-11-12 08:34:53 -0500

    Update theme

--- client/src/components/Auth/AuthLayout.tsx
+++ client/src/components/Auth/AuthLayout.tsx
@@ -73,7 +73,16 @@ function AuthLayout({
         <ThemeSelector />
       </div>
 
-      <div className="flex flex-grow items-center justify-center">
+      <div className="flex flex-grow items-center justify-center">        
+          {!hasStartupConfigError && !isFetching && (
+            <h1
+              className="mb-4 text-center text-3xl font-semibold text-black dark:text-white"
+              style={{ userSelect: 'none' }}
+            >
+              {startupConfig.appTitle}
+            </h1>
+          )}
+
         <div className="w-authPageWidth overflow-hidden bg-white px-6 py-4 dark:bg-gray-900 sm:max-w-md sm:rounded-lg">
           {!hasStartupConfigError && !isFetching && (
             <h1
