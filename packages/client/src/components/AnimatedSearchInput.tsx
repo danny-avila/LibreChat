9e7d92a4941af05d6ab8ea855e5e9e9671b9c582
Dustin Healy <54083382+dustinhealy@users.noreply.github.com>
2025-11-13 21:50:46 -0800

    feat: add mobile view sidebar for prompts page

--- packages/client/src/components/AnimatedSearchInput.tsx
+++ packages/client/src/components/AnimatedSearchInput.tsx
@@ -36,7 +36,7 @@ const AnimatedSearchInput = ({
             value={value}
             onChange={onChange}
             placeholder={placeholder}
-            className={`peer relative z-20 w-full rounded-lg bg-surface-secondary px-10 py-2 outline-none backdrop-blur-sm transition-all duration-500 ease-in-out placeholder:text-gray-400 focus:ring-ring`}
+            className={`peer relative z-20 w-full rounded-lg bg-surface-secondary py-2 pl-10 outline-none backdrop-blur-sm transition-all duration-500 ease-in-out placeholder:text-gray-400 focus:ring-ring`}
           />
 
           {/* Gradient overlay */}
