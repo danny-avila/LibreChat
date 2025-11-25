7e585b11405a7a910f415f1f95a0cf4353d82deb
Danny Avila <danny@librechat.ai>
2025-11-25 15:59:50 -0500

    chore: Enable writable req.query for mongoSanitize compatibility in Express 5

--- api/server/experimental.js
+++ api/server/experimental.js
@@ -245,6 +245,20 @@ if (cluster.isMaster) {
     app.use(noIndex);
     app.use(express.json({ limit: '3mb' }));
     app.use(express.urlencoded({ extended: true, limit: '3mb' }));
+
+    /**
+     * Express 5 Compatibility: Make req.query writable for mongoSanitize
+     * In Express 5, req.query is read-only by default, but express-mongo-sanitize needs to modify it
+     */
+    app.use((req, _res, next) => {
+      Object.defineProperty(req, 'query', {
+        ...Object.getOwnPropertyDescriptor(req, 'query'),
+        value: req.query,
+        writable: true,
+      });
+      next();
+    });
+
     app.use(mongoSanitize());
     app.use(cors());
     app.use(cookieParser());
