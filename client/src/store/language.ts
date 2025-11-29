0b356904da796d033e64668955cfc58f0e5cb564
maksm <maks.mrzek@gmail.com>
2025-11-29 14:08:20 +0100

    set default language

--- client/src/store/language.ts
+++ client/src/store/language.ts
@@ -3,7 +3,8 @@ import { atomWithLocalStorage } from './utils';
 
 const defaultLang = () => {
   const userLang = navigator.language || navigator.languages[0];
-  return Cookies.get('lang') || localStorage.getItem('lang') || userLang;
+  const envDefaultLang = import.meta.env.VITE_DEFAULT_LANGUAGE;
+  return Cookies.get('lang') || localStorage.getItem('lang') || envDefaultLang || userLang;
 };
 
 const lang = atomWithLocalStorage('lang', defaultLang());
