65cb5d18daabe600f6aacf956259d962fa804686
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2025-12-01 20:12:15 +0100

    ⌛ feat: Add Default Temporary Chat User Setting (#10731)

--- client/src/store/temporary.ts
+++ client/src/store/temporary.ts
@@ -1,7 +1,9 @@
 import { atomWithLocalStorage } from '~/store/utils';
 
 const isTemporary = atomWithLocalStorage('isTemporary', false);
+const defaultTemporaryChat = atomWithLocalStorage('defaultTemporaryChat', false);
 
 export default {
   isTemporary,
+  defaultTemporaryChat,
 };
