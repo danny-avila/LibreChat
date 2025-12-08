47f1477a8b3968aff92430b76fdb3e88879a1c58
Dustin Healy <54083382+dustinhealy@users.noreply.github.com>
2025-12-08 00:43:10 -0800

    chore: add missing subItems prop to MenuItemProps interface

--- packages/client/src/common/menus.ts
+++ packages/client/src/common/menus.ts
@@ -33,4 +33,5 @@ export interface MenuItemProps {
     | RenderProp<React.HTMLAttributes<any> & { ref?: React.Ref<any> | undefined }>
     | React.ReactElement<any, string | React.JSXElementConstructor<any>>
     | undefined;
+  subItems?: MenuItemProps[];
 }
