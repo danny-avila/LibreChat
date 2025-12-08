fa3ce1473d0da09a23a5ed65504006fb0abbc579
Bajwa, Inderdeep Singh <ibajwa@paychex.com>
2025-12-08 18:46:56 -0500

    fix(tavily): fix ci tests

--- api/server/utils/patchZodToJsonSchema.js
+++ api/server/utils/patchZodToJsonSchema.js
@@ -8,35 +8,33 @@ const modulePath = (() => {
   }
 })();
 
-if (!modulePath) {
+if (modulePath) {
+  const cachedModule = require(modulePath);
+  const originalFn =
+    (typeof cachedModule === 'function' && cachedModule) ||
+    cachedModule?.default ||
+    cachedModule?.zodToJsonSchema;
+
+  if (typeof originalFn !== 'function' || originalFn.__lcSanitized) {
+    module.exports = originalFn;
+  } else {
+    const patchedFn = (...args) => {
+      const schema = originalFn(...args);
+      return sanitizeSchemaMetadata(schema);
+    };
+
+    patchedFn.__lcSanitized = true;
+
+    if (typeof cachedModule === 'function') {
+      require.cache[modulePath].exports = patchedFn;
+    } else if (cachedModule && typeof cachedModule === 'object') {
+      cachedModule.default = patchedFn;
+      cachedModule.zodToJsonSchema = patchedFn;
+      require.cache[modulePath].exports = cachedModule;
+    }
+
+    module.exports = patchedFn;
+  }
+} else {
   module.exports = null;
-  return;
 }
-
-const cachedModule = require(modulePath);
-const originalFn =
-  (typeof cachedModule === 'function' && cachedModule) ||
-  cachedModule?.default ||
-  cachedModule?.zodToJsonSchema;
-
-if (typeof originalFn !== 'function' || originalFn.__lcSanitized) {
-  module.exports = originalFn;
-  return;
-}
-
-const patchedFn = (...args) => {
-  const schema = originalFn(...args);
-  return sanitizeSchemaMetadata(schema);
-};
-
-patchedFn.__lcSanitized = true;
-
-if (typeof cachedModule === 'function') {
-  require.cache[modulePath].exports = patchedFn;
-} else if (cachedModule && typeof cachedModule === 'object') {
-  cachedModule.default = patchedFn;
-  cachedModule.zodToJsonSchema = patchedFn;
-  require.cache[modulePath].exports = cachedModule;
-}
-
-module.exports = patchedFn;
