3ef54f2331c5354641f33beb3d7ab6f86874abba
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2026-01-28 16:36:37 +0100

    refactor(Prompts): move sidebar components to sidebar/ subdirectory

---
+++ client/src/components/Prompts/sidebar/index.ts
@@ -0,0 +1,4 @@
+export { default as GroupSidePanel } from './GroupSidePanel';
+export { default as PromptsAccordion } from './PromptsAccordion';
+export { default as FilterPrompts } from './FilterPrompts';
+export { default as PanelNavigation } from './PanelNavigation';
