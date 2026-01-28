54f891684d12fc9a9511aee21233a1729d5805b4
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2026-01-28 16:35:07 +0100

    refactor(Prompts): move editor components to editor/ subdirectory

---
+++ client/src/components/Prompts/editor/index.ts
@@ -0,0 +1,3 @@
+export { default as PromptEditor } from './PromptEditor';
+export { default as VariablesDropdown } from './VariablesDropdown';
+export { CodeVariableGfm, PromptVariableGfm } from './Markdown';
