e426ed3e89a817ee86570c3c8811dfcfec407b42
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2026-01-28 16:34:50 +0100

    refactor(Prompts): move display components to display/ subdirectory

---
+++ client/src/components/Prompts/display/index.ts
@@ -0,0 +1,4 @@
+export { default as PromptDetails } from './PromptDetails';
+export { default as PromptVariables } from './PromptVariables';
+export { default as PromptVersions } from './PromptVersions';
+export { default as EmptyPromptPreview } from './EmptyPromptPreview';
