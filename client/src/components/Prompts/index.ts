011857c15ebe7a60c8be4202ecc60fed037974e1
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2026-02-02 18:07:45 +0100

    refactor(Prompts): reorder exports for better organization and clarity

--- client/src/components/Prompts/index.ts
+++ client/src/components/Prompts/index.ts
@@ -1,9 +1,23 @@
 export { PromptsView } from './layouts';
-export { PromptForm, CreatePromptForm, VariableForm, PromptLabelsForm } from './forms';
-export { PreviewPrompt, DeleteVersion, VariableDialog, SharePrompt } from './dialogs';
-export { PromptEditor, VariablesDropdown, CodeVariableGfm, PromptVariableGfm } from './editor';
+export { CategoryIcon, SkeletonForm } from './utils';
 export { PromptName, Command, Description, CategorySelector } from './fields';
+export { PreviewPrompt, DeleteVersion, VariableDialog, SharePrompt } from './dialogs';
+export { PromptForm, CreatePromptForm, VariableForm, PromptLabelsForm } from './forms';
+export { PromptEditor, VariablesDropdown, CodeVariableGfm, PromptVariableGfm } from './editor';
 export { PromptDetails, PromptVariables, PromptVersions, EmptyPromptPreview } from './display';
+export {
+  GroupSidePanel as PromptSidePanel,
+  PromptsAccordion,
+  FilterPrompts,
+  PanelNavigation,
+} from './sidebar';
+export {
+  List as PromptGroupsList,
+  DashGroupItem,
+  ChatGroupItem,
+  ListCard,
+  NoPromptGroup,
+} from './lists';
 export {
   CreatePromptButton,
   AdminSettings,
@@ -13,17 +27,3 @@ export {
   BackToChat,
   ManagePrompts,
 } from './buttons';
-export {
-  List as PromptGroupsList,
-  DashGroupItem,
-  ChatGroupItem,
-  ListCard,
-  NoPromptGroup,
-} from './lists';
-export {
-  GroupSidePanel as PromptSidePanel,
-  PromptsAccordion,
-  FilterPrompts,
-  PanelNavigation,
-} from './sidebar';
-export { CategoryIcon, SkeletonForm } from './utils';
