5fe1db5f942aadc0d2965e7dee927487862aaaa6
David Newman <david.newman@automattic.com>
2026-02-18 18:32:30 +1000

    Fix typo

--- api/server/services/Files/S3/crud.js
+++ api/server/services/Files/S3/crud.js
@@ -15,7 +15,7 @@ const endpoint = process.env.AWS_ENDPOINT_URL;
 const bucketName = process.env.AWS_BUCKET_NAME;
 const defaultBasePath = 'images';
 const forcePathStyle = ['1', 'true', 'yes'].includes(
-  (process.env.AWS_FORCE_PATH_STYLE ?? '').toLowerCase().
+  (process.env.AWS_FORCE_PATH_STYLE ?? '').toLowerCase(),
 );
 
 let s3UrlExpirySeconds = 2 * 60; // 2 minutes
