ea82d25d79b3d2929ca7b3fae67b5f4a8db05ac6
Igor Churmeev <ingvarch@gmail.com>
2026-03-04 12:23:28 +0000

    🔄 feat: Add POST /api/models/refresh endpoint to clear model cache

--- api/server/routes/models.js
+++ api/server/routes/models.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { modelController } = require('~/server/controllers/ModelController');
-const { requireJwtAuth } = require('~/server/middleware/');
+const { modelController, refreshModelsController } = require('~/server/controllers/ModelController');
+const { requireJwtAuth, checkAdmin } = require('~/server/middleware/');
 
 const router = express.Router();
 router.get('/', requireJwtAuth, modelController);
+router.post('/refresh', requireJwtAuth, checkAdmin, refreshModelsController);
 
 module.exports = router;
