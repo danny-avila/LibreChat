a37870091199d579e398983186e3246d8519762a
Josh <jfinkfast5@gmail.com>
2026-03-12 12:22:26 -0500

    Josh/log-levels (#190)
    * log levels
    
    * console_json
    
    ---------
    
    Co-authored-by: Josh Fink <josh.fink@innovation.nj.gov>

--- api/utils/logger.js
+++ api/utils/logger.js
@@ -1,7 +1,7 @@
 const winston = require('winston');
 
 const logger = winston.createLogger({
-  level: 'info',
+  level: process.env.LOG_LEVEL || 'info',
   format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
   transports: [
     new winston.transports.Console(),
