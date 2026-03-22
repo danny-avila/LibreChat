48d3bd4ad53bc9addc746dfc7bd655ed3d59b599
Paulose Kurian <paulose610@gmail.com>
2026-03-22 10:12:58 +0530

    notice modal button color change

--- client/src/components/ui/ImportantNoticeModal.tsx
+++ client/src/components/ui/ImportantNoticeModal.tsx
@@ -66,14 +66,14 @@ const ImportantNoticeModal = ({
           <>
             <button
               onClick={onDecline}
-              className="inline-flex items-center justify-center rounded-lg border border-border-heavy bg-surface-secondary px-4 py-2 text-sm text-text-primary hover:bg-surface-active"
+              className="inline-flex items-center justify-center rounded-lg px-4 py-2 text-sm font-medium text-white bg-red-600 hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-800"
             >
               {/* <span>DECLINE</span> */}
               {'ਅਸਵੀਕਾਰ ਕਰੋ'}
             </button>
             <button
               onClick={onAccept}
-              className="inline-flex items-center justify-center rounded-lg border border-border-heavy bg-surface-secondary px-4 py-2 text-sm text-text-primary hover:bg-green-500 hover:text-white focus:bg-green-500 focus:text-white dark:hover:bg-green-600 dark:focus:bg-green-600"
+              className="inline-flex items-center justify-center rounded-lg px-4 py-2 text-sm font-medium text-white bg-green-600 hover:bg-green-700 dark:bg-green-700 dark:hover:bg-green-800"
             >
               {/* <span>I AGREE &amp; PROCEED</span> */}
               {'ਮੈਂ ਸਹਿਮਤ ਹਾਂ & ਅੱਗੇ ਵਧੋ'}
