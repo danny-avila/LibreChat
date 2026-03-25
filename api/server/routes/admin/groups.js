ff7a68c376edfbc836bd955d658486c3c1a7c7ad
Dustin Healy <54083382+dustinhealy@users.noreply.github.com>
2026-03-24 21:43:12 -0700

    feat: add admin groups handler factory and Express routes

---
+++ api/server/routes/admin/groups.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { createAdminGroupsHandlers } = require('@librechat/api');
+const { SystemCapabilities } = require('@librechat/data-schemas');
+const { requireCapability } = require('~/server/middleware/roles/capabilities');
+const { requireJwtAuth } = require('~/server/middleware');
+const db = require('~/models');
+
+const router = express.Router();
+
+const requireAdminAccess = requireCapability(SystemCapabilities.ACCESS_ADMIN);
+const requireReadGroups = requireCapability(SystemCapabilities.READ_GROUPS);
+const requireManageGroups = requireCapability(SystemCapabilities.MANAGE_GROUPS);
+
+const handlers = createAdminGroupsHandlers({
+  listGroups: db.listGroups,
+  findGroupById: db.findGroupById,
+  createGroup: db.createGroup,
+  updateGroupById: db.updateGroupById,
+  deleteGroup: db.deleteGroup,
+  addUserToGroup: db.addUserToGroup,
+  removeUserFromGroup: db.removeUserFromGroup,
+  findGroupsByMemberId: db.findGroupsByMemberId,
+  findUser: db.findUser,
+});
+
+router.use(requireJwtAuth, requireAdminAccess);
+
+router.get('/', requireReadGroups, handlers.listGroups);
+router.post('/', requireManageGroups, handlers.createGroup);
+router.get('/:id', requireReadGroups, handlers.getGroup);
+router.patch('/:id', requireManageGroups, handlers.updateGroup);
+router.delete('/:id', requireManageGroups, handlers.deleteGroup);
+router.get('/:id/members', requireReadGroups, handlers.getGroupMembers);
+router.post('/:id/members', requireManageGroups, handlers.addGroupMember);
+router.delete('/:id/members/:userId', requireManageGroups, handlers.removeGroupMember);
+
+module.exports = router;
--- packages/api/src/admin/index.ts
+++ packages/api/src/admin/index.ts
@@ -1,2 +1,4 @@
 export { createAdminConfigHandlers } from './config';
 export type { AdminConfigDeps } from './config';
+export { createAdminGroupsHandlers } from './groups';
+export type { AdminGroupsDeps } from './groups';
