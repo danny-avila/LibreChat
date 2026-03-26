9576c4b4fd77e04fb65e9467bbcfd3eb1471b7ea
Dan Lew <daniel@mightyacorn.com>
2026-03-26 15:56:47 -0500

    Disable export & share button (again)

--- client/src/components/Chat/Header.tsx
+++ client/src/components/Chat/Header.tsx
@@ -59,7 +59,8 @@ function Header() {
               {interfaceConfig.presets === true && interfaceConfig.modelSelect && <PresetsMenu />}
               {hasAccessToBookmarks === true && <BookmarkMenu />}
               {hasAccessToMultiConvo === true && <AddMultiConvo />}
-              {isSmallScreen && (
+              {/* NJ: We disable exporting/sharing */}
+              {isSmallScreen && false && (
                 <>
                   <ExportAndShareMenu
                     isSharedButtonEnabled={startupConfig?.sharedLinksEnabled ?? false}
@@ -71,7 +72,8 @@ function Header() {
           )}
         </div>
 
-        {!isSmallScreen && (
+        {/* NJ: We disable exporting/sharing */}
+        {!isSmallScreen && false && (
           <div className="flex items-center gap-2">
             <ExportAndShareMenu
               isSharedButtonEnabled={startupConfig?.sharedLinksEnabled ?? false}
