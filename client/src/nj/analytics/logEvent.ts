b3654000072aa29b16d3d8183e925afa18e253f1
Dan Lew <daniel@mightyacorn.com>
2026-04-08 10:41:13 -0500

    Added link to docs on how to set up GTM events

--- client/src/nj/analytics/logEvent.ts
+++ client/src/nj/analytics/logEvent.ts
@@ -5,7 +5,13 @@ declare global {
 }
 
 /**
- * Logs an event with Google Tag Manager (GTM) if it's been initialized.
+ * Logs an event with Google Tag Manager (GTM).
+ *
+ * !! IMPORTANT: Each event must be configured in GTM in order to work; simply calling this function
+ * does not mean a new event will start showing up in our analytics!
+ *
+ * For detailed instructions on setting up your new event, see this page:
+ * https://newjersey.github.io/innovation-engineering/guides/monitoring/setting-up-google-analytics/
  */
 export function logEvent(eventName: string, extraParameters: object = {}) {
   if (window.dataLayer) {
