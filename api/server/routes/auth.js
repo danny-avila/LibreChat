be78dc8864f95c0c3787b5b94b075d61966adcd5
pajgrtondrej <pajgrt.ondrej@gmail.com>
2026-04-13 08:29:49 +0200

    feat: add GET /api/auth/magic-link login route

--- api/server/routes/auth.js
+++ api/server/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { createSetBalanceConfig } = require('@librechat/api');
+const { hashToken } = require('@librechat/data-schemas');
 const {
   resetPasswordRequestController,
   resetPasswordController,
@@ -17,9 +18,12 @@ const {
 const { verify2FAWithTempToken } = require('~/server/controllers/auth/TwoFactorAuthController');
 const { logoutController } = require('~/server/controllers/auth/LogoutController');
 const { loginController } = require('~/server/controllers/auth/LoginController');
+const { setAuthTokens } = require('~/server/services/AuthService');
 const { findBalanceByUser, upsertBalanceFields } = require('~/models');
+const { logger } = require('~/config/winston');
 const { getAppConfig } = require('~/server/services/Config');
 const middleware = require('~/server/middleware');
+const db = require('~/models');
 
 const setBalanceConfig = createSetBalanceConfig({
   getAppConfig,
@@ -72,5 +76,56 @@ router.post('/2fa/disable', middleware.requireJwtAuth, disable2FA);
 router.post('/2fa/backup/regenerate', middleware.requireJwtAuth, regenerateBackupCodes);
 
 router.get('/graph-token', middleware.requireJwtAuth, graphTokenController);
+
+router.get('/magic-link', async (req, res) => {
+  const { token } = req.query;
+  if (!token || typeof token !== 'string') {
+    return res.redirect('/login?error=invalid_magic_link');
+  }
+  try {
+    const hash = await hashToken(token);
+    const link = await db.findMagicLink({ token: hash, active: true });
+    if (!link) {
+      return res.redirect('/login?error=invalid_magic_link');
+    }
+
+    let user;
+    if (link.userId) {
+      user = await db.findUser({ _id: link.userId });
+    } else {
+      user = await db.findUser({ email: link.email });
+      if (!user) {
+        user = await db.createUser(
+          {
+            email: link.email,
+            provider: 'magic_link',
+            role: 'USER',
+            emailVerified: true,
+            name: link.email.split('@')[0],
+          },
+          undefined,
+          true,
+          true,
+        );
+      }
+      await db.updateMagicLink(link._id.toString(), { userId: user._id });
+    }
+
+    if (!user) {
+      return res.redirect('/login?error=invalid_magic_link');
+    }
+
+    await db.updateMagicLink(link._id.toString(), {
+      useCount: (link.useCount ?? 0) + 1,
+      lastUsedAt: new Date(),
+    });
+
+    await setAuthTokens(user._id.toString(), res);
+    return res.redirect('/');
+  } catch (err) {
+    logger.error('[magic-link login]', err);
+    return res.redirect('/login?error=invalid_magic_link');
+  }
+});
 
 module.exports = router;
