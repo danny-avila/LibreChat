15582b9a25f77057750fd8534ab6b1fb34c1efbc
mfish911 <33205066+mfish911@users.noreply.github.com>
2026-04-24 23:33:49 -0400

    fix spelling error

--- api/server/index.js
+++ api/server/index.js
@@ -199,7 +199,7 @@ const startServer = async () => {
     });
 
     const rawLang = req.cookies.lang || req.headers['accept-language']?.split(',')[0] || 'en-US';
-    const sanitizedLang = typoeof rawLang === 'string' ? escapeHtml(rawLang) : 'en-US';
+    const sanitizedLang = typeof rawLang === 'string' ? escapeHtml(rawLang) : 'en-US';
     let updatedIndexHtml = indexHTML.replace(/lang="en-US"/g, `lang="${sanitizedLang}"`);
 
     res.type('html');
