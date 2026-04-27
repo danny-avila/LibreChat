9ce814bc218b4100616c6e50a108e31ea6be3a3f
Himanshu Choudhary <himanshukakran8@gmail.com>
2026-04-27 17:16:59 +0530

    feat(schema-contact-get||post endpoint):(route/index)

--- api/models/Contact.js
+++ api/models/Contact.js
@@ -1,11 +1,41 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const ContactSchema = new mongoose.Schema({
-  name: String,
-  company: String,
-  role: String,
-  email: String,
-  fields: Object, // flexible extra fields
+const ContactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+      index: true, // important for search
+    },
+    role: {
+      type: String,
+    },
+    email: {
+      type: String,
+       index: true,
+    },
+    notes: {
+      type: String,
+    },
+    metadata: {
+      type: mongoose.Schema.Types.Mixed, 
+      default: {},
+    },
+  },
+  {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+  }
+);
+
+
+ContactSchema.index({
+  name: 'text',
+  company: 'text',
+  role: 'text',
+  notes: 'text',
 });
 
-module.exports = mongoose.model("Contact", ContactSchema);
+module.exports = mongoose.model('Contact', ContactSchema);
