bb25728111b39a56b4f6635965cf67f9cc05afcb
vishwas-mehta <vishwas.mehta.work@gmail.com>
2026-05-12 10:38:51 +0530

    feat: made notifications modal and added the bell icon in the sidebar

---
+++ api/db/notification.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      index: true,
+    },
+    originalQuestion: {
+      type: String,
+      required: true,
+    },
+    isVisited: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model('Notification', notificationSchema);
