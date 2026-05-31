a10c21962fc7160d13a7833383b5a9b401441771
Danny Avila <danny@librechat.ai>
2026-05-31 12:01:02 -0700

    chore: bump LibreChat to v0.8.6

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -2191,7 +2191,7 @@ export enum Constants {
    */
   VERSION = '__LIBRECHAT_VERSION__',
   /** Key for the Custom Config's version (librechat.yaml). */
-  CONFIG_VERSION = '1.3.11',
+  CONFIG_VERSION = '1.3.12',
   /** Standard value for the first message's `parentMessageId` value, to indicate no parent exists. */
   NO_PARENT = '00000000-0000-0000-0000-000000000000',
   /** Standard value to use whatever the submission prelim. `responseMessageId` is */
