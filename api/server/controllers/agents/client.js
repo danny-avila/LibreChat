4dbc277b910095d3a6ea271a9f92ee0034ad2846
Danny Avila <danny@librechat.ai>
2026-06-02 22:28:02 -0400

    fix: persist project id in agent chat saves

--- api/server/controllers/agents/client.js
+++ api/server/controllers/agents/client.js
@@ -209,6 +209,7 @@ class AgentClient extends BaseClient {
         {
           spec: this.options.spec,
           iconURL: this.options.iconURL,
+          chatProjectId: this.options.chatProjectId,
           endpoint: this.options.endpoint,
           agent_id: this.options.agent.id,
           modelLabel: this.options.modelLabel,
