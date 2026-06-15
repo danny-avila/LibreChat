416bf621856483fdd25dc13050469a31ba2b2d6f
Ravi Kumar L <ravi.lazar@clickhouse.com>
2026-06-15 17:14:37 +0200

    test(metrics): add local supertest types

--- packages/api/types/index.d.ts
+++ packages/api/types/index.d.ts
@@ -2,3 +2,26 @@
 interface String {
   isWellFormed(): boolean;
 }
+
+declare module 'supertest' {
+  import type { Express } from 'express';
+
+  interface SuperTestResponse {
+    text: string;
+  }
+
+  interface SuperTestRequest extends Promise<SuperTestResponse> {
+    attach(field: string, file: Uint8Array, filename?: string): this;
+    expect(status: number): this;
+    send(body: Uint8Array | object | string): this;
+    set(field: string, value: string): this;
+  }
+
+  interface SuperTest {
+    delete(url: string): SuperTestRequest;
+    get(url: string): SuperTestRequest;
+    post(url: string): SuperTestRequest;
+  }
+
+  export default function request(app: Express): SuperTest;
+}
